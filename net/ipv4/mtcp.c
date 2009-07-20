multipath-tcp/mptcp

commit 6040bfcd0afb1b1c1b8e6056bbf057dcb3e1ccd0

Added support for fallback to normal tcp
Fallback to normal tcp is enabled when the other end host is found not
to support MTCP

net/ipv4/mtcp.c
@@ -335,7 +335,10 @@ int mtcp_sendmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg,
 	struct iovec *iov;
 	struct multipath_pcb *mpcb;
 	int iovlen,copied,msg_size;
-
+	
+	if (!tp->mpc)
+		return tcp_sendmsg(iocb,sock, msg, size);
+	
 	mpcb=mpcb_from_tcpsock(tcp_sk(master_sk));
 	if (mpcb==NULL){
 		printk(KERN_ERR "MPCB null in %s\n",__FUNCTION__);

include/linux/tcp.h
@@ -250,6 +250,7 @@ struct tcp_sock {
 	u32     peek_seq;       /* Peek seq, for use by MTCP            */
 	u32     *seq;
 	u32     copied;
+	u8      mpc:1;          /* Multipath capable                    */
 #endif
 	u32	rcv_wup;	/* rcv_nxt on last window update sent	*/
  	u32	snd_nxt;	/* Next sequence we send		*/

net/ipv4/tcp_input.c
@@ -3527,6 +3527,7 @@ static int tcp_fast_parse_options(struct sk_buff *skb, struct tcphdr *th,
 		     mpcb->received_options.saw_dsn)) {
 		/*This is the beginning of the multipath session, init
 		  the dsn value*/
+		tp->mpc=1;
 		mpcb->copied_seq=mpcb->received_options.data_seq+1;
 	}
 	return 1;
@@ -5383,6 +5384,7 @@ static int tcp_rcv_synsent_state_process(struct sock *sk, struct sk_buff *skb,
 		     mpcb->received_options.saw_dsn)) {
 		/*This is the beginning of the multipath session, init
 		  the dsn value*/
+		tp->mpc=1;
 		mpcb->copied_seq=mpcb->received_options.data_seq+1;
 	}
 

net/ipv4/tcp_output.c
@@ -439,7 +439,7 @@ static void tcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 		*ptr++ = htonl((TCPOPT_MPC << 24) |
 			       (TCPOLEN_MPC << 16));
 	}
-	if (likely(OPTION_DSN & opts->options)) {
+	if (OPTION_DSN & opts->options) {
 		*ptr++ = htonl((TCPOPT_NOP << 24) |
 			       (TCPOPT_NOP << 16) |
 			       (TCPOPT_DSN << 8) |
@@ -619,7 +619,7 @@ static unsigned tcp_established_options(struct sock *sk, struct sk_buff *skb,
 			opts->num_sack_blocks * TCPOLEN_SACK_PERBLOCK;
 	}
 #ifdef CONFIG_MTCP
-	if (!skb || skb->len!=0) {
+	if (tp->mpc && (!skb || skb->len!=0)) {
 		opts->options |= OPTION_DSN;
 		opts->data_seq=tcb?tcb->data_seq:0;
 		size += TCPOLEN_DSN_ALIGNED;

commit 16fd38eea4621804cc29f1cec51a585d515c4b73

Fixed segfault in mtcp_sendmsg introduced by patch 6040bfcd

net/ipv4/mtcp.c
@@ -336,7 +336,7 @@ int mtcp_sendmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg,
 	struct multipath_pcb *mpcb;
 	int iovlen,copied,msg_size;
 	
-	if (!tp->mpc)
+	if (!tcp_sk(master_sk)->mpc)
 		return tcp_sendmsg(iocb,sock, msg, size);
 	
 	mpcb=mpcb_from_tcpsock(tcp_sk(master_sk));