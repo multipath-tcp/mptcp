fe1edbaf1375d24e58b9ae24cf4554638ef069ac
Sébastien Barré <sebastien.barre@uclouvain.be>
2010-07-14 11:07:51 +0200

    other small fix
    only insert the sack base if sack blocks are present.
    Otherwise we can both experience option space overflows, and
    have the sack base without any sack block.

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -718,8 +718,9 @@ static unsigned tcp_established_options(struct sock *sk, struct sk_buff *skb,
 			min_t(unsigned, tp->rx_opt.eff_sacks,
 			      (remaining - TCPOLEN_SACK_BASE_ALIGNED) /
 			      TCPOLEN_SACK_PERBLOCK);
-		size += TCPOLEN_SACK_BASE_ALIGNED +
-			opts->num_sack_blocks * TCPOLEN_SACK_PERBLOCK;
+		if (opts->num_sack_blocks)
+			size += TCPOLEN_SACK_BASE_ALIGNED +
+				opts->num_sack_blocks * TCPOLEN_SACK_PERBLOCK;
 	}
 
 	if (size>MAX_TCP_OPTION_SPACE) {
