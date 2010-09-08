fe87d1a33cbcbfdd72b225051033ae29781b4206
Sébastien Barré <sebastien.barre@uclouvain.be>
2010-09-08 18:49:43 +0200

    changed debug info

--- net/ipv4/tcp_probe.c
+++ net/ipv4/tcp_probe.c
@@ -263,7 +263,8 @@ static int jtcp_transmit_skb(struct sock *sk, struct sk_buff *skb, int clone_it,
 			p->snd_nxt = tp->snd_nxt;
 			p->snd_una = tp->snd_una;
 			p->snd_cwnd = tp->snd_cwnd;
-			p->snd_wnd = tp->snd_wnd;
+			p->snd_wnd = (tp->mpcb)?tp->mpcb->tp.snd_wnd:
+				tp->snd_wnd;
 			p->ssthresh = tcp_current_ssthresh(sk);
 			p->srtt = tp->srtt >> 3;
 			p->rcv_nxt=tp->rcv_nxt;
