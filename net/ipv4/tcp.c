b494d6e79877d85476979fefd2441759530b40cb
Andreas Seelinger <Andreas.Seelinger@rwth-aachen.de>
2010-09-16 13:54:27 +0200

    bug fix: check for mpc before entering mtcp_check_new_subflow
    Signed-off-by: Andreas Seelinger <Andreas.Seelinger@rwth-aachen.de>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -357,7 +357,8 @@ unsigned int tcp_poll(struct file *file, struct socket *sock, poll_table *wait)
 	poll_wait(file, master_sk->sk_sleep, wait);
 
 #ifdef CONFIG_MTCP_PM
-	mtcp_check_new_subflow(mpcb);
+	if (master_tp->mpc)
+		mtcp_check_new_subflow(mpcb);
 #endif
 
 	if (master_sk->sk_state == TCP_LISTEN) {
