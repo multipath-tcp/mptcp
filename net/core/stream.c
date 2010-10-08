c5f741e37d642adfbbbf1c4a8d00aa4648f0e9bf
Christoph Paasch <christoph.paasch@uclouvain.be>
2010-10-08 15:31:26 +0200

    FIX: do not crash when iperf is not launched on the server-side
    A bug happened, when iperf is not launched on the server side.
    Just remove the BUG(); as in the official mainstream kernel.

--- net/core/stream.c
+++ net/core/stream.c
@@ -61,12 +61,9 @@ int sk_stream_wait_connect(struct sock *sk, long *timeo_p)
 			printk(KERN_ERR "line %d, err %d\n",__LINE__,err);
 			return err;
 		}
-		if ((1 << sk->sk_state) & ~(TCPF_SYN_SENT | TCPF_SYN_RECV)) {
-			printk(KERN_ERR "line %d, state %#x\n",__LINE__,
-			       sk->sk_state);
-			BUG();
+		if ((1 << sk->sk_state) & ~(TCPF_SYN_SENT | TCPF_SYN_RECV))
 			return -EPIPE;
-		}
+
 		if (!*timeo_p) {
 			printk(KERN_ERR "line %d\n",__LINE__);
 			return -EAGAIN;
