06dd42d2303588090fbe139f0b377e0e4a011dee
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-01-18 10:13:39 +0100

    Fix: Wrong DSS-len
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -411,7 +411,7 @@ struct mp_rst {
 
 static inline int mptcp_sub_len_dss(struct mp_dss *m, int csum)
 {
-	return 4 + m->A * (4 + m->a * 4) + m->M * (8 + csum * 2 + m->m * 4);
+	return 4 + m->A * (4 + m->a * 4) + m->M * (10  +m->m * 4 + csum * 2);
 }
 
 /* Two separate cases must be handled:
