multipath-tcp/mptcp

commit 68dae05f5eead7fda0e1a6fb057af215752f11d1

Fix: use skb_pull we need it for later pskb_copy
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_output.c
@@ -2200,7 +2200,8 @@ int tcp_retransmit_skb(struct sock *sk, struct sk_buff *skb)
 	    && TCP_SKB_CB(skb)->seq != tp->snd_una)
 		return -EAGAIN;
 
-	if (skb->len > cur_mss) {
+	/* We cannot use skb->len here, MPTCP modified it in mptcp_skb_entail. */
+	if (TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq - tcp_hdr(skb)->fin > cur_mss) {
 		if (tcp_fragment(sk, skb, cur_mss, cur_mss))
 			return -ENOMEM; /* We'll try again later. */
 	} else {
@@ -2956,9 +2957,12 @@ int tcp_write_wakeup(struct sock *sk)
 		/* We are probing the opening of a window
 		 * but the window size is != 0
 		 * must have been a result SWS avoidance ( sender )
+		 *
+		 * For the second condition, we cannot use skb-len, because
+		 * MPTCP modified it in mptcp_skb_entail.
 		 */
 		if (seg_size < TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq ||
-		    skb->len > mss) {
+		    TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq - tcp_hdr(skb)->fin > mss) {
 			seg_size = min(seg_size, mss);
 			TCP_SKB_CB(skb)->tcp_flags |= TCPHDR_PSH;
 			if (tcp_fragment(sk, skb, seg_size, mss))

net/mptcp/mptcp_output.c
@@ -138,9 +138,7 @@ static struct mp_dss *mptcp_skb_find_dss(const struct sk_buff *skb)
 	if (!(TCP_SKB_CB(skb)->mptcp_flags & MPTCPHDR_SEQ))
 		return NULL;
 
-	return (struct mp_dss *)(skb->data - (MPTCP_SUB_LEN_DSS_ALIGN +
-			      	      	      MPTCP_SUB_LEN_ACK_ALIGN +
-			      	      	      MPTCP_SUB_LEN_SEQ_ALIGN));
+	return (struct mp_dss *)skb->data;
 }
 
 /* Reinject data from one TCP subflow to the meta_sk. If sk == NULL, we are
@@ -200,6 +198,10 @@ static int __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk,
 		p32++;
 		p16 = (u16 *)p32;
 		TCP_SKB_CB(skb)->end_seq = ntohs(*p16) + TCP_SKB_CB(skb)->seq;
+
+		skb_pull(skb, MPTCP_SUB_LEN_DSS_ALIGN +
+			      MPTCP_SUB_LEN_ACK_ALIGN +
+			      MPTCP_SUB_LEN_SEQ_ALIGN);
 	}
 
 	skb->sk = meta_sk;
@@ -480,12 +482,12 @@ static void mptcp_skb_entail(struct sock *sk, struct sk_buff *skb)
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct tcp_skb_cb *tcb = TCP_SKB_CB(skb);
 	int fin = (tcb->tcp_flags & TCPHDR_FIN) ? 1 : 0;
+	unsigned int old_len = skb->len;
 
 	/**** Write MPTCP DSS-option to the packet. ****/
-
-	ptr = (__be32 *)(skb->data - (MPTCP_SUB_LEN_DSS_ALIGN +
-				      MPTCP_SUB_LEN_ACK_ALIGN +
-				      MPTCP_SUB_LEN_SEQ_ALIGN));
+	ptr = (__be32 *) skb_push(skb, MPTCP_SUB_LEN_DSS_ALIGN +
+				       MPTCP_SUB_LEN_ACK_ALIGN +
+				       MPTCP_SUB_LEN_SEQ_ALIGN);
 
 	/* Then we start writing it from the start */
 	mdss = (struct mp_dss *) ptr;
@@ -544,7 +546,7 @@ static void mptcp_skb_entail(struct sock *sk, struct sk_buff *skb)
 			  * of this subflow, we are sending a brand new
 			  * segment */
 	/* Take into account seg len */
-	tp->write_seq += skb->len + fin;
+	tp->write_seq += old_len + fin;
 	tcb->end_seq = tp->write_seq;
 
 	/* If it's a non-payload DATA_FIN (also no subflow-fin), the
@@ -1089,6 +1091,10 @@ unsigned mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 			*size += MPTCP_SUB_LEN_ACK_ALIGN;
 		} else {
 			opts->data_ack = mpcb_meta_tp(mpcb)->rcv_nxt;
+
+			skb_pull(skb, MPTCP_SUB_LEN_DSS_ALIGN +
+			      MPTCP_SUB_LEN_ACK_ALIGN +
+			      MPTCP_SUB_LEN_SEQ_ALIGN);
 
 			/* Doesn't matter, if csum included or not. It will be
 			 * either 10 or 12, and thus aligned = 12 */

commit 6771d850110a42ced8eb32afd650fe5a5994b6d7

Fix: Handle pskb_copy differently
68dae05f5eead7fda0e1a6fb057af215752f11d1 introduced a bug. Actually,
skb->len does not anymore reflect tcb->end_seq - tcb->seq and thus we
had all kind of strange behaviors under certain conditions.

This patch reverts it back again and handles pskb_copy in
tcp_transmit_skb as needed.

Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_output.c
@@ -835,10 +835,27 @@ int tcp_transmit_skb(struct sock *sk, struct sk_buff *skb, int clone_it,
 	tp = tcp_sk(sk);
 
 	if (likely(clone_it)) {
-		if (unlikely((!tp->mpc && skb_cloned(skb)) || mptcp_skb_cloned(skb, tp)))
-			skb = pskb_copy(skb, gfp_mask);
-		else
+		if (unlikely((!tp->mpc && skb_cloned(skb)) || mptcp_skb_cloned(skb, tp))) {
+			struct sk_buff *newskb;
+			if (TCP_SKB_CB(skb)->mptcp_flags & MPTCPHDR_SEQ)
+				skb_push(skb, MPTCP_SUB_LEN_DSS_ALIGN +
+					      MPTCP_SUB_LEN_ACK_ALIGN +
+					      MPTCP_SUB_LEN_SEQ_ALIGN);
+
+			newskb = pskb_copy(skb, gfp_mask);
+
+			if (TCP_SKB_CB(skb)->mptcp_flags & MPTCPHDR_SEQ) {
+				skb_pull(skb, MPTCP_SUB_LEN_DSS_ALIGN +
+					      MPTCP_SUB_LEN_ACK_ALIGN +
+					      MPTCP_SUB_LEN_SEQ_ALIGN);
+				skb_pull(newskb, MPTCP_SUB_LEN_DSS_ALIGN +
+					      MPTCP_SUB_LEN_ACK_ALIGN +
+					      MPTCP_SUB_LEN_SEQ_ALIGN);
+			}
+			skb = newskb;
+		} else {
 			skb = skb_clone(skb, gfp_mask);
+		}
 		if (unlikely(!skb))
 			return -ENOBUFS;
 	}
@@ -2189,8 +2206,7 @@ int tcp_retransmit_skb(struct sock *sk, struct sk_buff *skb)
 	    && TCP_SKB_CB(skb)->seq != tp->snd_una)
 		return -EAGAIN;
 
-	/* We cannot use skb->len here, MPTCP modified it in mptcp_skb_entail. */
-	if (TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq - (TCP_SKB_CB(skb)->tcp_flags & TCPHDR_FIN ? 1 : 0) > cur_mss) {
+	if (skb->len > cur_mss) {
 		if (tcp_fragment(sk, skb, cur_mss, cur_mss))
 			return -ENOMEM; /* We'll try again later. */
 	} else {
@@ -2941,12 +2957,9 @@ int tcp_write_wakeup(struct sock *sk)
 		/* We are probing the opening of a window
 		 * but the window size is != 0
 		 * must have been a result SWS avoidance ( sender )
-		 *
-		 * For the second condition, we cannot use skb-len, because
-		 * MPTCP modified it in mptcp_skb_entail.
 		 */
 		if (seg_size < TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq ||
-		    TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq - (TCP_SKB_CB(skb)->tcp_flags & TCPHDR_FIN ? 1 : 0) > mss) {
+		    skb->len > mss) {
 			seg_size = min(seg_size, mss);
 			TCP_SKB_CB(skb)->tcp_flags |= TCPHDR_PSH;
 			if (tcp_fragment(sk, skb, seg_size, mss))

net/mptcp/mptcp_output.c
@@ -138,7 +138,9 @@ static struct mp_dss *mptcp_skb_find_dss(const struct sk_buff *skb)
 	if (!(TCP_SKB_CB(skb)->mptcp_flags & MPTCPHDR_SEQ))
 		return NULL;
 
-	return (struct mp_dss *)skb->data;
+	return (struct mp_dss *)(skb->data - (MPTCP_SUB_LEN_DSS_ALIGN +
+			      	      	      MPTCP_SUB_LEN_ACK_ALIGN +
+			      	      	      MPTCP_SUB_LEN_SEQ_ALIGN));
 }
 
 /* Reinject data from one TCP subflow to the meta_sk. If sk == NULL, we are
@@ -198,10 +200,6 @@ static int __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk,
 		p32++;
 		p16 = (u16 *)p32;
 		TCP_SKB_CB(skb)->end_seq = ntohs(*p16) + TCP_SKB_CB(skb)->seq;
-
-		skb_pull(skb, MPTCP_SUB_LEN_DSS_ALIGN +
-			      MPTCP_SUB_LEN_ACK_ALIGN +
-			      MPTCP_SUB_LEN_SEQ_ALIGN);
 	}
 
 	skb->sk = meta_sk;
@@ -489,12 +487,12 @@ static void mptcp_skb_entail(struct sock *sk, struct sk_buff *skb)
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct tcp_skb_cb *tcb = TCP_SKB_CB(skb);
 	int fin = (tcb->tcp_flags & TCPHDR_FIN) ? 1 : 0;
-	unsigned int old_len = skb->len;
 
 	/**** Write MPTCP DSS-option to the packet. ****/
-	ptr = (__be32 *) skb_push(skb, MPTCP_SUB_LEN_DSS_ALIGN +
-				       MPTCP_SUB_LEN_ACK_ALIGN +
-				       MPTCP_SUB_LEN_SEQ_ALIGN);
+
+	ptr = (__be32 *)(skb->data - (MPTCP_SUB_LEN_DSS_ALIGN +
+				      MPTCP_SUB_LEN_ACK_ALIGN +
+				      MPTCP_SUB_LEN_SEQ_ALIGN));
 
 	/* Then we start writing it from the start */
 	mdss = (struct mp_dss *) ptr;
@@ -553,7 +551,7 @@ static void mptcp_skb_entail(struct sock *sk, struct sk_buff *skb)
 			  * of this subflow, we are sending a brand new
 			  * segment */
 	/* Take into account seg len */
-	tp->write_seq += old_len + fin;
+	tp->write_seq += skb->len + fin;
 	tcb->end_seq = tp->write_seq;
 
 	/* If it's a non-payload DATA_FIN (also no subflow-fin), the
@@ -1103,10 +1101,6 @@ unsigned mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 			*size += MPTCP_SUB_LEN_ACK_ALIGN;
 		} else {
 			opts->data_ack = mpcb_meta_tp(mpcb)->rcv_nxt;
-
-			skb_pull(skb, MPTCP_SUB_LEN_DSS_ALIGN +
-			      MPTCP_SUB_LEN_ACK_ALIGN +
-			      MPTCP_SUB_LEN_SEQ_ALIGN);
 
 			/* Doesn't matter, if csum included or not. It will be
 			 * either 10 or 12, and thus aligned = 12 */