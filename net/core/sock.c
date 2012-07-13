a535d333f0bf84d5dbedea61e993b9e67025d601
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-07-14 00:24:46 +0200

    sock_alloc_send_pskb doesn't need to handle mptcp
    When it is called, we may come from tcp_v4_send_ack or
    tcp_v4_send_reset. In that case, the socket is always the one from
    init_net and thus non-mptcp.
    
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    Reviewed-by: Octavian Purdila <octavian.purdila@intel.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1503,9 +1503,6 @@ struct sk_buff *sock_alloc_send_pskb(struct sock *sk, unsigned long header_len,
 				     unsigned long data_len, int noblock,
 				     int *errcode)
 {
-	struct sock *meta_sk = (sk->sk_protocol == IPPROTO_TCP &&
-				tcp_sk(sk)->mpcb) ?
-				(struct sock *)tcp_sk(sk)->mpcb : sk;
 	struct sk_buff *skb;
 	gfp_t gfp_mask;
 	long timeo;
@@ -1563,8 +1560,8 @@ struct sk_buff *sock_alloc_send_pskb(struct sock *sk, unsigned long header_len,
 			err = -ENOBUFS;
 			goto failure;
 		}
-		set_bit(SOCK_ASYNC_NOSPACE, &meta_sk->sk_socket->flags);
-		set_bit(SOCK_NOSPACE, &meta_sk->sk_socket->flags);
+		set_bit(SOCK_ASYNC_NOSPACE, &sk->sk_socket->flags);
+		set_bit(SOCK_NOSPACE, &sk->sk_socket->flags);
 		err = -EAGAIN;
 		if (!timeo)
 			goto failure;
