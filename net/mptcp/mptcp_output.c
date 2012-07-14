c6ff500b363fb6249181c8b9b8e05318e238ed3d
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-07-14 19:00:01 +0200

    Trivial - use mptcp_is_data_fin
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -307,8 +307,7 @@ void mptcp_reinject_data(struct sock *sk, int clone_it)
 
 	skb_it = tcp_write_queue_tail(meta_sk);
 	/* If sk has sent the empty data-fin, we have to reinject it too. */
-	if (skb_it && TCP_SKB_CB(skb_it)->mptcp_flags & MPTCPHDR_FIN &&
-	    skb_it->len == 0 &&
+	if (skb_it && mptcp_is_data_fin(skb_it) && skb_it->len == 0 &&
 	    TCP_SKB_CB(skb_it)->path_mask & mptcp_pi_to_flag(tp->mptcp->path_index)) {
 		__mptcp_reinject_data(skb_it, meta_sk, NULL, 1);
 	}
