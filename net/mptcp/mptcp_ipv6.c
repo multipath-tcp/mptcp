0ff04474d018f6d7a3ab787ea0a80ac5ff7bcf99
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-08-06 15:00:25 +0200

    Build-Fix
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/mptcp/mptcp_ipv6.c
+++ net/mptcp/mptcp_ipv6.c
@@ -518,7 +518,7 @@ static void mptcp_dad_callback(unsigned long arg)
 	} else {
 		mptcp_pm_inet6_addr_event(NULL, NETDEV_UP, data->ifa);
 		kfree(data);
-		int6_ifa_put(data->ifa);
+		in6_ifa_put(data->ifa);
 	}
 }
 
