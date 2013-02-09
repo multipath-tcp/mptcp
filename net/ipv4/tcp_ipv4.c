multipath-tcp/mptcp

commit 9c2c649cc5747ab35c54379207548e3432c45ea2

Fix: Race condition while creating subsocket on the server-side
There may happen a race-condition, because tp->mpc is set to 0 in
mptcp_inherit_sk.

What happens:
tp->mpc is set to 0 while creating a new subflow, and added to the
socket-hash-table by tcp_v4_syn_recv_sock. Then, a new packet comes in,
and waits for the lock on the subflow (not on the meta, because tp->mpc
is still 0).

In mptcp_add_sock, tp->mpc will finally be correctly set to 1. But hell,
this is too late. The above packet already came in. We have now a race,
as we are inside a subsocket, without holding the meta-socket lock.

Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ctrl.c
@@ -516,7 +516,6 @@ static int mptcp_inherit_sk(const struct sock *sk, struct sock *newsk,
 	newsk->sk_send_head	= NULL;
 	newsk->sk_userlocks	= sk->sk_userlocks & ~SOCK_BINDPORT_LOCK;
 
-	tcp_sk(newsk)->mpc = 0;
 	tcp_sk(newsk)->mptcp = NULL;
 
 	sock_reset_flag(newsk, SOCK_DONE);
@@ -1525,7 +1524,10 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk, struct sock *child,
 	sk_set_socket(child, meta_sk->sk_socket);
 	child->sk_wq = meta_sk->sk_wq;
 
-	if (mptcp_add_sock(meta_sk, child, mtreq->rem_id, GFP_ATOMIC))
+	if (mptcp_add_sock(meta_sk, child, mtreq->rem_id, GFP_ATOMIC)) {
+		child_tp->mpc = 0; /* Has been inherited, but now
+				    * child_tp->mptcp is NULL
+				    */
 		/* TODO when we support acking the third ack for new subflows,
 		 * we should silently discard this third ack, by returning NULL.
 		 *
@@ -1533,6 +1535,7 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk, struct sock *child,
 		 * fully add the socket to the meta-sk.
 		 */
 		goto teardown;
+	}
 
 	/* The child is a clone of the meta socket, we must now reset
 	 * some of the fields

commit 7f203dd2a80ba95e65c09d46c8792e3a12c793b1

Fix: NULL-pointer exception in mptcp_del_sock
9c2c649cc57 (Fix: Race condition while creating subsocket on the
server-side) introduced a regression when mptcp_check_req_child fails.
tp->mpc is set, but tp->mptcp not yet. Then, in mptcp_del_sock we will
have a NULL Pointer Exception.

Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_ipv4.c
@@ -2051,7 +2051,8 @@ void tcp_v4_destroy_sock(struct sock *sk)
 		/* mptcp_del_sock MUST be before tcp_write_queue_purge because
 		 * we try to reinject
 		 */
-		mptcp_del_sock(sk);
+		if (tp->mpc)
+			mptcp_del_sock(sk);
 
 		/* Cleanup up the write buffer. */
 		tcp_write_queue_purge(sk);

net/mptcp/mptcp_ctrl.c
@@ -875,7 +875,7 @@ void mptcp_del_sock(struct sock *sk)
 	struct tcp_sock *tp = tcp_sk(sk), *tp_prev;
 	struct mptcp_cb *mpcb;
 
-	if (!tp->mpc || !tp->mptcp->attached)
+	if (!tp->mptcp || !tp->mptcp->attached)
 		return;
 
 	if (tp->mptcp->pre_established) {