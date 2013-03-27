74aa2204b6ac9ea09e931b0819194abf8d9cc474
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-03-27 16:32:19 +0100

    Cleanup: Remove unused variables from tcp_skb_cb
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -791,8 +791,6 @@ struct tcp_skb_cb {
 	__u32		end_seq;	/* SEQ + FIN + SYN + datalen	*/
 	__u32		when;		/* used to compute rtt's	*/
 #ifdef CONFIG_MPTCP
-#define sub_seq		ack_seq
-#define	mp_data_len	when
 	__u8		mptcp_flags;	/* flags for the MPTCP layer    */
 	__u8		dss_off;	/* Number of 4-byte words until
 					 * seq-number */
