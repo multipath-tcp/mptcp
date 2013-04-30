45a200d1e86b7d8d1de448065fb7d17533243888
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-04-30 14:34:49 +0200

    Fix: Handle meta-socket in inet_csk_listen_stop
    We have to hold the mutex, to prevent any work-queue from accessing the
    meta-sk while closing it.
    
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -765,6 +765,8 @@ void inet_csk_listen_stop(struct sock *sk)
 
 		acc_req = req->dl_next;
 
+		if (is_meta_sk(child))
+			mutex_lock(&tcp_sk(child)->mpcb->mutex);
 		local_bh_disable();
 		bh_lock_sock(child);
 		WARN_ON(sock_owned_by_user(child));
@@ -780,6 +782,8 @@ void inet_csk_listen_stop(struct sock *sk)
 
 		bh_unlock_sock(child);
 		local_bh_enable();
+		if (is_meta_sk(child))
+			mutex_unlock(&tcp_sk(child)->mpcb->mutex);
 		sock_put(child);
 
 		sk_acceptq_removed(sk);
