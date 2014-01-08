3a8a2a54da0556ff486a418f392a830359a7e147
Christoph Paasch <christoph.paasch@uclouvain.be>
2014-01-08 11:18:28 +0100

    mptcp: Make packet-sizes equal to the maximum among all subflows
    Since dbeb18b2219 (tcp: TSO packets automatic sizing) the TSO
    packet-size depends on the sending-rate. As MPTCP took the packet-size
    as being the minimum among all subflows, if you had a very slow subflow
    the packet-size would be quite low.
    
    This patch makes the packet-size become rather the maximum among all
    subflows. We will be making more fragmentations in mptcp_write_xmit in
    case of a slight MSS-difference, but at least we are no more hurting the
    subflow with the fastest sending-rate with too-small packets.
    
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -2154,7 +2154,7 @@ unsigned int mptcp_current_mss(struct sock *meta_sk)
 			continue;
 
 		this_mss = tcp_current_mss(sk);
-		if (!mss || this_mss < mss)
+		if (this_mss > mss)
 			mss = this_mss;
 	}
 
@@ -2176,7 +2176,7 @@ int mptcp_select_size(const struct sock *meta_sk, bool sg)
 			continue;
 
 		this_mss = tcp_sk(sk)->mss_cache;
-		if (!mss || this_mss < mss)
+		if (this_mss > mss)
 			mss = this_mss;
 	}
 
@@ -2234,7 +2234,7 @@ unsigned int mptcp_xmit_size_goal(struct sock *meta_sk, u32 mss_now,
 				continue;
 
 			this_size_goal = tcp_xmit_size_goal(sk, mss_now, 1);
-			if (!xmit_size_goal || this_size_goal < xmit_size_goal)
+			if (this_size_goal > xmit_size_goal)
 				xmit_size_goal = this_size_goal;
 		}
 	}
