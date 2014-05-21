multipath-tcp/mptcp

commit be54ab36580a97dbc9062a28122a03517e59559d

Fix tsq-deferred
When the write-timer fires, we do not know if it was for the meta-sk or
rather for the subflow. If it fired for the meta-sk and we are owned by
the user, this would make us get stuck, as the write-timer won't ever
fire again.

We fix this by changing the way we handle tsq-deferring in MPTCP.

A new bit is set, if the deferring is for the subflow. Then, we iterate
over the subflows to handle the deferring.

That way, we can disambiguate between meta-deferring and
subflow-deferring. We also change the way TCP small-queue handling is
done. It will now be handled with a call to mptcp_write_xmit and
tcp_write_xmit, to be able to process both kinds of limitations.

Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_output.c
@@ -737,12 +737,21 @@ static void tcp_tasklet_func(unsigned long data)
 		bh_lock_sock(meta_sk);
 
 		if (!sock_owned_by_user(meta_sk)) {
-			tcp_tsq_handler(meta_sk);
+			tcp_tsq_handler(sk);
+			if (tp->mpc)
+				tcp_tsq_handler(meta_sk);
 		} else {
 			/* defer the work to tcp_release_cb() */
 			set_bit(TCP_TSQ_DEFERRED, &tp->tsq_flags);
-			if (tp->mpc)
+
+			/* For MPTCP, we set the tsq-bit on the meta, and the
+			 * subflow as we don't know if the limitation happened
+			 * while inside mptcp_write_xmit or during tcp_write_xmit.
+			 */
+			if (tp->mpc) {
 				set_bit(TCP_TSQ_DEFERRED, &tcp_sk(meta_sk)->tsq_flags);
+				mptcp_tsq_flags(sk);
+			}
 		}
 		bh_unlock_sock(meta_sk);
 
@@ -754,38 +763,8 @@ static void tcp_tasklet_func(unsigned long data)
 #define TCP_DEFERRED_ALL ((1UL << TCP_TSQ_DEFERRED) |		\
 			  (1UL << TCP_WRITE_TIMER_DEFERRED) |	\
 			  (1UL << TCP_DELACK_TIMER_DEFERRED) |	\
-			  (1UL << TCP_MTU_REDUCED_DEFERRED))
-
-static void mptcp_release_cb(struct sock *meta_sk)
-{
-	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
-	struct sock *sk;
-	unsigned long flags, nflags;
-
-	/* perform an atomic operation only if at least one flag is set */
-	do {
-		flags = meta_tp->tsq_flags;
-		if (!(flags & TCP_DEFERRED_ALL))
-			return;
-		nflags = flags & ~TCP_DEFERRED_ALL;
-	} while (cmpxchg(&meta_tp->tsq_flags, flags, nflags) != flags);
-
-	if (flags & (1UL << TCP_TSQ_DEFERRED))
-		tcp_tsq_handler(meta_sk);
-
-	if (flags & (1UL << TCP_WRITE_TIMER_DEFERRED))
-		__sock_put(meta_sk);
-	if (flags & (1UL << TCP_DELACK_TIMER_DEFERRED))
-		__sock_put(meta_sk);
-	if (flags & (1UL << TCP_MTU_REDUCED_DEFERRED))
-		__sock_put(meta_sk);
-
-	while ((sk = meta_tp->mpcb->callback_list) != NULL) {
-		meta_tp->mpcb->callback_list = tcp_sk(sk)->mptcp->next_cb;
-		tcp_sk(sk)->mptcp->next_cb = NULL;
-		sk->sk_prot->release_cb(sk);
-	}
-}
+			  (1UL << TCP_MTU_REDUCED_DEFERRED) |	\
+			  (1UL << MPTCP_SUB_DEFERRED))
 
 /**
  * tcp_release_cb - tcp release_sock() callback
@@ -798,11 +777,6 @@ void tcp_release_cb(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	unsigned long flags, nflags;
-
-	if (is_meta_sk(sk)) {
-		mptcp_release_cb(sk);
-		return;
-	}
 
 	/* perform an atomic operation only if at least one flag is set */
 	do {
@@ -827,6 +801,8 @@ void tcp_release_cb(struct sock *sk)
 		sk->sk_prot->mtu_reduced(sk);
 		__sock_put(sk);
 	}
+	if (flags & (1UL << MPTCP_SUB_DEFERRED))
+		mptcp_tsq_sub_deferred(sk);
 }
 EXPORT_SYMBOL(tcp_release_cb);
 

include/linux/tcp.h
@@ -402,6 +402,7 @@ enum tsq_flags {
 	TCP_MTU_REDUCED_DEFERRED,  /* tcp_v{4|6}_err() could not call
 				    * tcp_v{4|6}_mtu_reduced()
 				    */
+	MPTCP_SUB_DEFERRED, /* A subflow got deferred - process them */
 };
 
 static inline struct tcp_sock *tcp_sk(const struct sock *sk)

include/net/mptcp.h
@@ -714,7 +714,8 @@ void mptcp_update_tw_socks(const struct tcp_sock *tp, int state);
 void mptcp_disconnect(struct sock *sk);
 bool mptcp_should_expand_sndbuf(struct sock *meta_sk);
 int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb);
-void mptcp_tsq_flags(struct sock *sk, int bit);
+void mptcp_tsq_flags(struct sock *sk);
+void mptcp_tsq_sub_deferred(struct sock *meta_sk);
 
 static inline
 struct mptcp_request_sock *mptcp_rsk(const struct request_sock *req)
@@ -1376,7 +1377,8 @@ static inline bool mptcp_should_expand_sndbuf(struct sock *meta_sk)
 {
 	return false;
 }
-static inline void mptcp_tsq_flags(struct sock *sk, int bit) {}
+static inline void mptcp_tsq_flags(struct sock *sk) {}
+static inline void mptcp_tsq_sub_deferred(struct sock *meta_sk) {}
 #endif /* CONFIG_MPTCP */
 
 #endif /* _MPTCP_H */

net/ipv4/tcp_ipv4.c
@@ -422,7 +422,7 @@ void tcp_v4_err(struct sk_buff *icmp_skb, u32 info)
 				if (!test_and_set_bit(TCP_MTU_REDUCED_DEFERRED, &tp->tsq_flags))
 					sock_hold(sk);
 				if (tp->mpc)
-					mptcp_tsq_flags(sk, TCP_MTU_REDUCED_DEFERRED);
+					mptcp_tsq_flags(sk);
 			}
 			goto out;
 		}

net/ipv4/tcp_timer.c
@@ -260,7 +260,7 @@ static void tcp_delack_timer(unsigned long data)
 		if (!test_and_set_bit(TCP_DELACK_TIMER_DEFERRED, &tcp_sk(sk)->tsq_flags))
 			sock_hold(sk);
 		if (tp->mpc)
-			mptcp_tsq_flags(sk, TCP_DELACK_TIMER_DEFERRED);
+			mptcp_tsq_flags(sk);
 	}
 	bh_unlock_sock(meta_sk);
 	sock_put(sk);
@@ -539,7 +539,7 @@ static void tcp_write_timer(unsigned long data)
 		if (!test_and_set_bit(TCP_WRITE_TIMER_DEFERRED, &tcp_sk(sk)->tsq_flags))
 			sock_hold(sk);
 		if (tcp_sk(sk)->mpc)
-			mptcp_tsq_flags(sk, TCP_WRITE_TIMER_DEFERRED);
+			mptcp_tsq_flags(sk);
 	}
 	bh_unlock_sock(meta_sk);
 	sock_put(sk);

net/ipv6/tcp_ipv6.c
@@ -404,7 +404,7 @@ static void tcp_v6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 					   &tp->tsq_flags))
 				sock_hold(sk);
 			if (tp->mpc)
-				mptcp_tsq_flags(sk, TCP_MTU_REDUCED_DEFERRED);
+				mptcp_tsq_flags(sk);
 		}
 		goto out;
 	}

net/mptcp/mptcp_ctrl.c
@@ -1820,18 +1820,46 @@ void mptcp_update_tw_socks(const struct tcp_sock *tp, int state)
 	rcu_read_unlock_bh();
 }
 
-void mptcp_tsq_flags(struct sock *sk, int bit)
+void mptcp_tsq_flags(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct sock *meta_sk = mptcp_meta_sk(sk);
 
+	/* It will be handled as a regular deferred-call */
+	if (is_meta_sk(sk))
+		return;
+
 	if (!tp->mptcp->next_cb) {
 		tp->mptcp->next_cb = tp->mpcb->callback_list;
 		tp->mpcb->callback_list = sk;
+		/* We need to hold it here, as the sock_hold is not assured
+		 * by the release_sock as it is done in regular TCP.
+		 *
+		 * The subsocket may get inet_csk_destroy'd while it is inside
+		 * the callback_list.
+		 */
+		sock_hold(sk);
 	}
 
-	if (!test_and_set_bit(bit, &tcp_sk(meta_sk)->tsq_flags))
+	if (!test_and_set_bit(MPTCP_SUB_DEFERRED, &tcp_sk(meta_sk)->tsq_flags))
 		sock_hold(meta_sk);
+}
+
+void mptcp_tsq_sub_deferred(struct sock *meta_sk)
+{
+	struct sock *sk;
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+
+	BUG_ON(!is_meta_sk(meta_sk) && !meta_tp->was_meta_sk);
+
+	__sock_put(meta_sk);
+	while ((sk = meta_tp->mpcb->callback_list) != NULL) {
+		meta_tp->mpcb->callback_list = tcp_sk(sk)->mptcp->next_cb;
+		tcp_sk(sk)->mptcp->next_cb = NULL;
+		sk->sk_prot->release_cb(sk);
+		/* Final sock_put (cfr. mptcp_tsq_flags */
+		sock_put(sk);
+	}
 }
 
 struct workqueue_struct *mptcp_wq;

commit 91552fc29d09ef5ce65f2f867ab109fdb9222410

mptcp: Fix Bug #42: Do not reschedule tsq on closed socket
This socket anyway cannot send data anymore. Otherwise, if the sk_free
will indeed free the socket, we will bug in mptcp_sock_destruct as the
cb_list is not empty.

Reported-by: dykelin (https://github.com/dykelin)
Fixes: Bug #42 (https://github.com/multipath-tcp/mptcp/issues/42)
Fixes: be54ab36580a9 (Fix tsq-deferred)
Tested-by: dykelin (https://github.com/dykelin)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_output.c
@@ -745,6 +745,9 @@ static void tcp_tasklet_func(unsigned long data)
 			if (tp->mpc)
 				tcp_tsq_handler(meta_sk);
 		} else {
+			if (tp->mpc && sk->sk_state == TCP_CLOSE)
+				goto exit;
+
 			/* defer the work to tcp_release_cb() */
 			set_bit(TCP_TSQ_DEFERRED, &tp->tsq_flags);
 
@@ -757,6 +760,7 @@ static void tcp_tasklet_func(unsigned long data)
 				mptcp_tsq_flags(sk);
 			}
 		}
+exit:
 		bh_unlock_sock(meta_sk);
 
 		clear_bit(TSQ_QUEUED, &tp->tsq_flags);