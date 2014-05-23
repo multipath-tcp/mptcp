multipath-tcp/mptcp

commit ba1582e1314d15176a184611103aa7832d3f7e42

mptcp: Correct support of TCP-Keepalive
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_minisocks.c
@@ -477,7 +477,11 @@ struct sock *tcp_create_openreq_child(struct sock *sk, struct request_sock *req,
 
 		newtp->urg_data = 0;
 
-		if (sock_flag(newsk, SOCK_KEEPOPEN))
+		/* MPTCP: If we are creating a subflow, KEEPOPEN might have been
+		 * set on the meta. But, keepalive is entirely handled at the
+		 * meta-socket, so let's keep it there.
+		 */
+		if (sock_flag(newsk, SOCK_KEEPOPEN) && is_meta_sk(sk))
 			inet_csk_reset_keepalive_timer(newsk,
 						       keepalive_time_when(newtp));
 

net/mptcp/mptcp_ctrl.c
@@ -560,16 +560,6 @@ static void mptcp_set_state(struct sock *sk)
 	}
 }
 
-void mptcp_set_keepalive(struct sock *sk, int val)
-{
-	struct sock *sk_it;
-
-	mptcp_for_each_sk(tcp_sk(sk)->mpcb, sk_it) {
-		tcp_set_keepalive(sk_it, val);
-		sock_valbool_flag(sk, SOCK_KEEPOPEN, val);
-	}
-}
-
 u32 mptcp_secret[MD5_MESSAGE_BYTES / 4] ____cacheline_aligned;
 u32 mptcp_key_seed = 0;
 
@@ -671,8 +661,6 @@ static void mptcp_mpcb_inherit_sockopts(struct sock *meta_sk, struct sock *maste
 	 *
 	 * Socket-options handled in this function here
 	 * ======
-	 * SO_KEEPALIVE
-	 * TCP_KEEP*
 	 * TCP_DEFER_ACCEPT
 	 *
 	 * Socket-options on the todo-list
@@ -704,18 +692,8 @@ static void mptcp_mpcb_inherit_sockopts(struct sock *meta_sk, struct sock *maste
 	 * TCP_CONGESTION
 	 * TCP_SYNCNT
 	 * TCP_QUICKACK
+	 * SO_KEEPALIVE
 	 */
-	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
-
-	/****** KEEPALIVE-handler ******/
-
-	/* Keepalive-timer has been started already, but it is handled at the
-	 * subflow level.
-	 */
-	if (sock_flag(meta_sk, SOCK_KEEPOPEN)) {
-		inet_csk_delete_keepalive_timer(meta_sk);
-		inet_csk_reset_keepalive_timer(master_sk, keepalive_time_when(meta_tp));
-	}
 
 	/****** DEFER_ACCEPT-handler ******/
 
@@ -727,13 +705,6 @@ static void mptcp_mpcb_inherit_sockopts(struct sock *meta_sk, struct sock *maste
 
 static void mptcp_sub_inherit_sockopts(struct sock *meta_sk, struct sock *sub_sk)
 {
-	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
-	/* Keepalive is handled at the subflow-level */
-	if (sock_flag(meta_sk, SOCK_KEEPOPEN)) {
-		inet_csk_reset_keepalive_timer(sub_sk, keepalive_time_when(meta_tp));
-		sock_valbool_flag(sub_sk, SOCK_KEEPOPEN, keepalive_time_when(meta_tp));
-	}
-
 	/* IP_TOS also goes to the subflow. */
 	if (inet_sk(sub_sk)->tos != inet_sk(meta_sk)->tos) {
 		inet_sk(sub_sk)->tos = inet_sk(meta_sk)->tos;

include/net/mptcp.h
@@ -752,7 +752,6 @@ void mptcp_fallback_meta_sk(struct sock *meta_sk);
 int mptcp_backlog_rcv(struct sock *meta_sk, struct sk_buff *skb);
 struct sock *mptcp_sk_clone(const struct sock *sk, int family, const gfp_t priority);
 void mptcp_ack_handler(unsigned long);
-void mptcp_set_keepalive(struct sock *sk, int val);
 int mptcp_check_rtt(const struct tcp_sock *tp, int time);
 int mptcp_check_snd_buf(const struct tcp_sock *tp);
 int mptcp_handle_options(struct sock *sk, const struct tcphdr *th, struct sk_buff *skb);
@@ -1375,7 +1374,6 @@ static inline struct sock *mptcp_sk_clone(const struct sock *sk, int family,
 {
 	return NULL;
 }
-static inline void mptcp_set_keepalive(struct sock *sk, int val) {}
 static inline int mptcp_handle_options(struct sock *sk,
 				       const struct tcphdr *th,
 				       struct sk_buff *skb)

net/ipv4/tcp.c
@@ -2673,13 +2673,6 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 					elapsed = tp->keepalive_time - elapsed;
 				else
 					elapsed = 0;
-				if (tp->mpc) {
-					struct sock *sk_it = sk;
-					mptcp_for_each_sk(tp->mpcb, sk_it)
-						if (!(1 << sk->sk_state & (TCPF_CLOSE | TCPF_LISTEN)))
-							inet_csk_reset_keepalive_timer(sk_it, elapsed);
-					break;
-				}
 				inet_csk_reset_keepalive_timer(sk, elapsed);
 			}
 		}

net/ipv4/tcp_timer.c
@@ -569,11 +569,6 @@ void tcp_set_keepalive(struct sock *sk, int val)
 	if ((1 << sk->sk_state) & (TCPF_CLOSE | TCPF_LISTEN))
 		return;
 
-	if (is_meta_sk(sk)) {
-		mptcp_set_keepalive(sk, val);
-		return;
-	}
-
 	if (val && !sock_flag(sk, SOCK_KEEPOPEN))
 		inet_csk_reset_keepalive_timer(sk, keepalive_time_when(tcp_sk(sk)));
 	else if (!val)
@@ -597,6 +592,11 @@ static void tcp_keepalive_timer (unsigned long data)
 		goto out;
 	}
 
+	if (sk->sk_state == TCP_LISTEN) {
+		tcp_synack_timer(sk);
+		goto out;
+	}
+
 	if (sk->sk_state == TCP_FIN_WAIT2 && sock_flag(sk, SOCK_DEAD)) {
 		if (tp->linger2 >= 0) {
 			const int tmo = tcp_fin_time(sk) - TCP_TIMEWAIT_LEN;
@@ -610,13 +610,7 @@ static void tcp_keepalive_timer (unsigned long data)
 		goto death;
 	}
 
-	if (sk->sk_state == TCP_LISTEN || is_meta_sk(sk)) {
-		tcp_synack_timer(sk);
-		goto out;
-	}
-
-	/* MPTCP: Keepalive timers are handled at the subflow level */
-	if (!sock_flag(sk, SOCK_KEEPOPEN) || sk->sk_state == TCP_CLOSE || is_meta_sk(sk))
+	if (!sock_flag(sk, SOCK_KEEPOPEN) || sk->sk_state == TCP_CLOSE)
 		goto out;
 
 	elapsed = keepalive_time_when(tp);

net/mptcp/mptcp_ipv4.c
@@ -93,14 +93,29 @@ static void mptcp_v4_reqsk_queue_hash_add(struct sock *meta_sk,
 					  struct request_sock *req,
 					  unsigned long timeout)
 {
-	const u32 h = inet_synq_hash(inet_rsk(req)->ir_rmt_addr,
+	const u32 h1 = inet_synq_hash(inet_rsk(req)->ir_rmt_addr,
 				     inet_rsk(req)->ir_rmt_port,
 				     0, MPTCP_HASH_SIZE);
+	/* We cannot call inet_csk_reqsk_queue_hash_add(), because we do not
+	 * want to reset the keepalive-timer (responsible for retransmitting
+	 * SYN/ACKs). We do not retransmit SYN/ACKs+MP_JOINs, because we cannot
+	 * overload the keepalive timer. Also, it's not a big deal, because the
+	 * third ACK of the MP_JOIN-handshake is sent in a reliable manner. So,
+	 * if the third ACK gets lost, the client will handle the retransmission
+	 * anyways. If our SYN/ACK gets lost, the client will retransmit the
+	 * SYN.
+	 */ 
+	struct inet_connection_sock *meta_icsk = inet_csk(meta_sk);
+	struct listen_sock *lopt = meta_icsk->icsk_accept_queue.listen_opt;
+	const u32 h2 = inet_synq_hash(inet_rsk(req)->ir_rmt_addr,
+				     inet_rsk(req)->ir_rmt_port,
+				     lopt->hash_rnd, lopt->nr_table_entries);
 
-	inet_csk_reqsk_queue_hash_add(meta_sk, req, timeout);
+	reqsk_queue_hash_req(&meta_icsk->icsk_accept_queue, h2, req, timeout);
+	reqsk_queue_added(&meta_icsk->icsk_accept_queue);
 
 	spin_lock(&mptcp_reqsk_hlock);
-	list_add(&mptcp_rsk(req)->collide_tuple, &mptcp_reqsk_htb[h]);
+	list_add(&mptcp_rsk(req)->collide_tuple, &mptcp_reqsk_htb[h1]);
 	spin_unlock(&mptcp_reqsk_hlock);
 }
 

net/mptcp/mptcp_ipv6.c
@@ -121,14 +121,29 @@ static void mptcp_v6_reqsk_queue_hash_add(struct sock *meta_sk,
 					  struct request_sock *req,
 					  unsigned long timeout)
 {
-	const u32 h = inet6_synq_hash(&inet_rsk(req)->ir_v6_rmt_addr,
+	const u32 h1 = inet6_synq_hash(&inet_rsk(req)->ir_v6_rmt_addr,
 				      inet_rsk(req)->ir_rmt_port,
 				      0, MPTCP_HASH_SIZE);
+	/* We cannot call inet6_csk_reqsk_queue_hash_add(), because we do not
+	 * want to reset the keepalive-timer (responsible for retransmitting
+	 * SYN/ACKs). We do not retransmit SYN/ACKs+MP_JOINs, because we cannot
+	 * overload the keepalive timer. Also, it's not a big deal, because the
+	 * third ACK of the MP_JOIN-handshake is sent in a reliable manner. So,
+	 * if the third ACK gets lost, the client will handle the retransmission
+	 * anyways. If our SYN/ACK gets lost, the client will retransmit the
+	 * SYN.
+	 */ 
+	struct inet_connection_sock *meta_icsk = inet_csk(meta_sk);
+	struct listen_sock *lopt = meta_icsk->icsk_accept_queue.listen_opt;
+	const u32 h2 = inet6_synq_hash(&inet_rsk(req)->ir_v6_rmt_addr,
+				      inet_rsk(req)->ir_rmt_port,
+				      lopt->hash_rnd, lopt->nr_table_entries);
 
-	inet6_csk_reqsk_queue_hash_add(meta_sk, req, timeout);
+	reqsk_queue_hash_req(&meta_icsk->icsk_accept_queue, h2, req, timeout);
+	reqsk_queue_added(&meta_icsk->icsk_accept_queue);
 
 	spin_lock(&mptcp_reqsk_hlock);
-	list_add(&mptcp_rsk(req)->collide_tuple, &mptcp_reqsk_htb[h]);
+	list_add(&mptcp_rsk(req)->collide_tuple, &mptcp_reqsk_htb[h1]);
 	spin_unlock(&mptcp_reqsk_hlock);
 }
 

commit f40efe40d08d81648aca803be7e951f15f6ad259

mptcp: Correct support of TCP-keepalive cont'd
This patch fixes and complements ba1582e13 (mptcp: Correct support of
TCP-Keepalive).

Fixes:
 - correctly disable using keepalive timer at the subflow level
 - avoid calling inet_csk_reqsk_queue_drop when new subflows are
   established that will stop the keepalive timer at the meta.

Fixes: ba1582e13 (mptcp: Correct support of TCP-Keepalive)
Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_minisocks.c
@@ -475,7 +475,7 @@ struct sock *tcp_create_openreq_child(struct sock *sk, struct request_sock *req,
 		 * set on the meta. But, keepalive is entirely handled at the
 		 * meta-socket, so let's keep it there.
 		 */
-		if (sock_flag(newsk, SOCK_KEEPOPEN) && is_meta_sk(sk))
+		if (sock_flag(newsk, SOCK_KEEPOPEN) && !tcp_sk(sk)->mpc)
 			inet_csk_reset_keepalive_timer(newsk,
 						       keepalive_time_when(newtp));
 
@@ -799,7 +799,16 @@ embryonic_reset:
 		tcp_reset(sk);
 	}
 	if (!fastopen) {
-		inet_csk_reqsk_queue_drop(sk, req, prev);
+		if (is_meta_sk(sk)) {
+			/* We want to avoid stoping the keepalive-timer and so
+			 * avoid ending up in inet_csk_reqsk_queue_removed ...
+			 */
+			inet_csk_reqsk_queue_unlink(sk, req, prev);
+			reqsk_queue_removed(&inet_csk(sk)->icsk_accept_queue, req);
+			reqsk_free(req);
+		} else {
+			inet_csk_reqsk_queue_drop(sk, req, prev);
+		}
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_EMBRYONICRSTS);
 	}
 	return NULL;

net/mptcp/mptcp_ctrl.c
@@ -1949,12 +1949,16 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk, struct sock *child,
 	/* Subflows do not use the accept queue, as they
 	 * are attached immediately to the mpcb.
 	 */
-	inet_csk_reqsk_queue_drop(meta_sk, req, prev);
+	inet_csk_reqsk_queue_unlink(meta_sk, req, prev);
+	reqsk_queue_removed(&inet_csk(meta_sk)->icsk_accept_queue, req);
+	reqsk_free(req);
 	return child;
 
 teardown:
 	/* Drop this request - sock creation failed. */
-	inet_csk_reqsk_queue_drop(meta_sk, req, prev);
+	inet_csk_reqsk_queue_unlink(meta_sk, req, prev);
+	reqsk_queue_removed(&inet_csk(meta_sk)->icsk_accept_queue, req);
+	reqsk_free(req);
 	inet_csk_prepare_forced_close(child);
 	tcp_done(child);
 	return meta_sk;