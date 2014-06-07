multipath-tcp/mptcp

commit 47080a0c118236d121cea154198b34449ea398ef

Simplify tcp read code from application
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_input.c
@@ -270,8 +270,13 @@ static inline void mptcp_prepare_skb(struct sk_buff *skb, struct sk_buff *next,
 	 * mapping), and data_fin is enqueued, the end_data_seq is +1.
 	 */
 	if (skb_queue_is_last(&sk->sk_receive_queue, skb) ||
-	    after(TCP_SKB_CB(next)->end_seq, tp->mptcp->map_subseq + tp->mptcp->map_data_len))
+	    after(TCP_SKB_CB(next)->end_seq, tp->mptcp->map_subseq + tp->mptcp->map_data_len)) {
 		tcb->end_seq += tp->mptcp->map_data_fin;
+		if (mptcp_is_data_fin(skb))
+			tcp_hdr(skb)->fin = 1;
+		else
+			tcp_hdr(skb)->fin = 0;
+	}
 }
 
 /**

net/ipv4/tcp.c
@@ -786,11 +786,6 @@ static ssize_t do_tcp_sendpages(struct sock *sk, struct page **pages, int poffse
 	ssize_t copied;
 	long timeo = sock_sndtimeo(sk, flags & MSG_DONTWAIT);
 
-	if (tp->mpc) {
-		printk(KERN_ERR "%s: function not yet supported\n", __func__);
-		BUG();
-	}
-
 	/* Wait for a connection to finish. */
 	if ((1 << sk->sk_state) & ~(TCPF_ESTABLISHED | TCPF_CLOSE_WAIT))
 		if ((err = sk_stream_wait_connect(sk, &timeo)) != 0)
@@ -902,7 +897,7 @@ int tcp_sendpage(struct sock *sk, struct page *page, int offset,
 	ssize_t res;
 
 	if (!(sk->sk_route_caps & NETIF_F_SG) ||
-	    !(sk->sk_route_caps & NETIF_F_ALL_CSUM) || tcp_sk(sk)->mpcb)
+	    !(sk->sk_route_caps & NETIF_F_ALL_CSUM) || tcp_sk(sk)->mpc)
 		return sock_no_sendpage(sk->sk_socket, page, offset, size,
 					flags);
 
@@ -1363,8 +1358,7 @@ static inline struct sk_buff *tcp_recv_skb(struct sock *sk, u32 seq, u32 *off)
 		offset = seq - TCP_SKB_CB(skb)->seq;
 		if (tcp_hdr(skb)->syn)
 			offset--;
-		if (offset < skb->len || (!tcp_sk(sk)->mpc && tcp_hdr(skb)->fin) ||
-		    (tcp_sk(sk)->mpc && mptcp_is_data_fin(skb))) {
+		if (offset < skb->len || tcp_hdr(skb)->fin) {
 			*off = offset;
 			return skb;
 		}
@@ -1428,8 +1422,7 @@ int tcp_read_sock(struct sock *sk, read_descriptor_t *desc,
 			if (!skb || (offset+1 != skb->len))
 				break;
 		}
-		if ((!tp->mpc && tcp_hdr(skb)->fin) ||
-		    (tp->mpc && mptcp_is_data_fin(skb))) {
+		if (tcp_hdr(skb)->fin) {
 			sk_eat_skb(sk, skb, 0);
 			++seq;
 			break;
@@ -1478,14 +1471,13 @@ int tcp_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 	int copied_early = 0;
 	struct sk_buff *skb;
 	u32 urg_hole = 0;
-	struct mptcp_cb *mpcb = tp->mpc ? tp->mpcb : NULL;
 
 	lock_sock(sk);
 
 #ifdef CONFIG_MPTCP
 	if (tp->mpc) {
 		struct sock *sk_it;
-		mptcp_for_each_sk(mpcb, sk_it) {
+		mptcp_for_each_sk(tp->mpcb, sk_it) {
 			if (!is_master_tp(tcp_sk(sk_it)))
 				sock_rps_record_flow(sk_it);
 		}
@@ -1562,8 +1554,7 @@ int tcp_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 				offset--;
 			if (offset < skb->len)
 				goto found_ok_skb;
-			if ((!mpcb && tcp_hdr(skb)->fin) ||
-			    (mpcb && mptcp_is_data_fin(skb)))
+			if (tcp_hdr(skb)->fin)
 				goto found_fin_ok;
 			WARN(!(flags & MSG_PEEK),
 			     "recvmsg bug 2: copied %X seq %X rcvnxt %X fl %X\n",
@@ -1792,8 +1783,7 @@ skip_copy:
 		if (used + offset < skb->len)
 			continue;
 
-		if ((!mpcb && tcp_hdr(skb)->fin) ||
-		    (mpcb && mptcp_is_data_fin(skb)))
+		if (tcp_hdr(skb)->fin)
 			goto found_fin_ok;
 		if (!(flags & MSG_PEEK)) {
 			sk_eat_skb(sk, skb, copied_early);
@@ -1816,6 +1806,7 @@ skip_copy:
 			int chunk;
 
 			tp->ucopy.len = copied > 0 ? len : 0;
+
 			tcp_prequeue_process(sk);
 
 			if (copied > 0 && (chunk = len - tp->ucopy.len) != 0) {

commit 5a71eff204b23168ce14a4cd4ed042a3c4fe83b8

mptcp: Allow receiving splitted data-fin without mapping on last skb
It may be, that we receive a mapping covering two segments. This mapping
may include a DATA_FIN and it may be that the last of these two segments
does not have a DSS-option. Then, mptcp_is_data_fin2() will always be
false and we do not set the fin-bit on the segment.

Fixes: 47080a0c11823 (Simplify tcp read code from application)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_input.c
@@ -382,32 +382,36 @@ static inline void mptcp_prepare_skb(struct sk_buff *skb, struct sk_buff *next,
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct tcp_skb_cb *tcb = TCP_SKB_CB(skb);
+	u32 inc = 0;
+
+	/* If skb is the end of this mapping (end is always at mapping-boundary
+	 * thanks to the splitting/trimming), then we need to increase
+	 * data-end-seq by 1 if this here is a data-fin.
+	 *
+	 * We need to do -1 because end_seq includes the subflow-FIN.
+	 */
+	if (tp->mptcp->map_data_fin &&
+	    (tcb->end_seq - (tcp_hdr(skb)->fin ? 1 : 0)) ==
+	    (tp->mptcp->map_subseq + tp->mptcp->map_data_len)) {
+		inc = 1;
+
+		/* We manually set the fin-flag if it is a data-fin. For easy
+		 * processing in tcp_recvmsg.
+		 */
+		tcp_hdr(skb)->fin = 1;
+	} else {
+		/* We may have a subflow-fin with data but without data-fin */
+		tcp_hdr(skb)->fin = 0;
+	}
+
 	/* Adapt data-seq's to the packet itself. We kinda transform the
 	 * dss-mapping to a per-packet granularity. This is necessary to
 	 * correctly handle overlapping mappings coming from different
 	 * subflows. Otherwise it would be a complete mess.
 	 */
 	tcb->seq = ((u32)tp->mptcp->map_data_seq) + tcb->seq - tp->mptcp->map_subseq;
-	tcb->end_seq = tcb->seq + skb->len;
+	tcb->end_seq = tcb->seq + skb->len + inc;
 
-	/* If cur is the last one in the rcv-queue (or the last one for this
-	 * mapping), and data_fin is enqueued, the end_data_seq is +1.
-	 */
-	if (skb_queue_is_last(&sk->sk_receive_queue, skb) ||
-	    after(TCP_SKB_CB(next)->end_seq, tp->mptcp->map_subseq + tp->mptcp->map_data_len)) {
-		tcb->end_seq += tp->mptcp->map_data_fin;
-
-		/* We manually set the fin-flag if it is a data-fin. For easy
-		 * processing in tcp_recvmsg.
-		 */
-		if (mptcp_is_data_fin2(skb, tp))
-			tcp_hdr(skb)->fin = 1;
-		else
-			tcp_hdr(skb)->fin = 0;
-	} else {
-		/* We may have a subflow-fin with data but without data-fin */
-		tcp_hdr(skb)->fin = 0;
-	}
 }
 
 /**