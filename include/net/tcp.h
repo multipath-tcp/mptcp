multipath-tcp/mptcp

commit 1ec7a15fac1f81d4bef0909eef8b76d7b6f4589d

mptcp: Add static-key check in mptcp()
Static keys allow for less overhead on systems that do not establish
MPTCP connections.

Increments are done when setting mpc to 1 and (at the same time) setting
the sk_destruct-pointer.
Thus, the decrement can be done solely in mptcp_sock_destruct, which is
absolutly necessary as we can only decrement at the very last when the
final MPTCP-socket/subsocket is being destroyed.

Depends-on: c5905afb0ee (static keys: Introduce 'struct static_key', static_key_true()/false() and static_key_slow_[inc|dec]())
Reviewed-by: Octavian Purdila <octavian.purdila@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/tcp.h
@@ -1214,9 +1214,10 @@ static inline int tcp_win_from_space(int space)
 		space - (space>>sysctl_tcp_adv_win_scale);
 }
 
+extern struct static_key mptcp_static_key;
 static inline bool mptcp(const struct tcp_sock *tp)
 {
-	return tp->mpc;
+	return static_key_false(&mptcp_static_key) && tp->mpc;
 }
 
 /* Note: caller must be prepared to deal with negative returns */ 

include/net/mptcp.h
@@ -1244,6 +1244,7 @@ bool mptcp_prune_ofo_queue(struct sock *sk);
 
 static inline void set_mpc(struct tcp_sock *tp)
 {
+	static_key_slow_inc(&mptcp_static_key);
 	tp->mpc	= 1;
 
 	tp->__select_window		= __mptcp_select_window;

net/mptcp/mptcp_ctrl.c
@@ -69,6 +69,9 @@ int sysctl_mptcp_syn_retries __read_mostly = 3;
 
 bool mptcp_init_failed __read_mostly;
 
+struct static_key mptcp_static_key = STATIC_KEY_INIT_FALSE;
+EXPORT_SYMBOL(mptcp_static_key);
+
 static int proc_mptcp_path_manager(ctl_table *ctl, int write,
 				   void __user *buffer, size_t *lenp,
 				   loff_t *ppos)
@@ -472,6 +475,13 @@ static void mptcp_sock_destruct(struct sock *sk)
 
 		mptcp_debug("%s destroying meta-sk\n", __func__);
 	}
+
+	WARN_ON(!static_key_false(&mptcp_static_key));
+	/* Must be the last call, because is_meta_sk() above still needs the
+	 * static key
+	 */
+	static_key_slow_dec(&mptcp_static_key);
+
 }
 
 void mptcp_destroy_sock(struct sock *sk)
@@ -962,7 +972,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key, u32 window)
 	mpcb->meta_sk = meta_sk;
 	mpcb->master_sk = master_sk;
 
-	set_mpc(meta_tp);
 	meta_tp->mptcp->attached = 0;
 	meta_tp->was_meta_sk = 0;
 
@@ -1001,11 +1010,11 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key, u32 window)
 		kmem_cache_free(mptcp_sock_cache, meta_tp->mptcp);
 		kmem_cache_free(mptcp_cb_cache, mpcb);
 		sk_free(master_sk);
-		reset_mpc(meta_tp);
 		return -ENOMEM;
 	}
 
 	/* Redefine function-pointers as the meta-sk is now fully ready */
+	set_mpc(meta_tp);
 	set_meta_funcs(meta_tp);
 	meta_sk->sk_backlog_rcv = mptcp_backlog_rcv;
 	meta_sk->sk_destruct = mptcp_sock_destruct;

commit 17469892b7521240c3053ffdd7f926a7a387439b

mptcp: Allow compiling with MPTCP disabled
The static key is defined in mptcp_ctrl.c. So, it would not compile if
CONFIG_ENABLED is set to n. Thus, we add redefine mptcp() in case
CONFIG_MPTCP is disabled.

Fixes: 1ec7a15fac1f81d4 (mptcp: Add static-key check in mptcp())
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/tcp.h
@@ -1214,11 +1214,18 @@ static inline int tcp_win_from_space(int space)
 		space - (space>>sysctl_tcp_adv_win_scale);
 }
 
+#ifdef CONFIG_MPTCP
 extern struct static_key mptcp_static_key;
 static inline bool mptcp(const struct tcp_sock *tp)
 {
 	return static_key_false(&mptcp_static_key) && tp->mpc;
 }
+#else
+static inline bool mptcp(const struct tcp_sock *tp)
+{
+	return 0;
+}
+#endif
 
 /* Note: caller must be prepared to deal with negative returns */ 
 static inline int tcp_space(const struct sock *sk)