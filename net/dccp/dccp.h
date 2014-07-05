multipath-tcp/mptcp

commit 4cb24a286a17b5e874474d3cef0d1b23a899f8d3

mptcp: rework mptcp_conn_request
This patch reverts the changes to the conn_request API and uses the
newly added tcp_request_sock_ops init_req method to accomplish the
same.

Note that with this approach the mptcp options are parsed twice which
adds some overhead. On the other hand this allows us to stay in sync
with upstream and once we will make it there we can easily correct
that by changing the APIs to allow passing a pointer to the mptcp
options.

Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
Tested-by: Gregory Detal <gregory.detal@uclouvain.be>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/dccp/ipv6.c
@@ -357,8 +357,7 @@ static struct sock *dccp_v6_hnd_req(struct sock *sk,struct sk_buff *skb)
 	return sk;
 }
 
-static int dccp_v6_conn_request(struct sock *sk, struct sk_buff *skb,
-				struct request_sock_ops *ops, void *init_data)
+static int dccp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 {
 	struct request_sock *req;
 	struct dccp_request_sock *dreq;

include/net/inet_connection_sock.h
@@ -41,8 +41,7 @@ struct inet_connection_sock_af_ops {
 	void	    (*send_check)(struct sock *sk, struct sk_buff *skb);
 	int	    (*rebuild_header)(struct sock *sk);
 	void	    (*sk_rx_dst_set)(struct sock *sk, const struct sk_buff *skb);
-	int	    (*conn_request)(struct sock *sk, struct sk_buff *skb,
-				    struct request_sock_ops *ops, void *init_data);
+	int	    (*conn_request)(struct sock *sk, struct sk_buff *skb);
 	struct sock *(*syn_recv_sock)(struct sock *sk, struct sk_buff *skb,
 				      struct request_sock *req,
 				      struct dst_entry *dst);

include/net/mptcp.h
@@ -808,10 +808,8 @@ int mptcp_sub_len_remove_addr_align(u16 bitfield);
 void mptcp_remove_shortcuts(const struct mptcp_cb *mpcb,
 			    const struct sk_buff *skb);
 void mptcp_init_buffer_space(struct sock *sk);
-void mptcp_reqsk_init(struct request_sock *req,
-		      struct sk_buff *skb, void *init_data);
-int mptcp_conn_request(struct sock *sk, struct sk_buff *skb,
-		       struct request_sock_ops *ops, void *init_data);
+void mptcp_reqsk_init(struct request_sock *req, struct sk_buff *skb);
+int mptcp_conn_request(struct sock *sk, struct sk_buff *skb);
 
 /* MPTCP-path-manager registration/initialization functions */
 int mptcp_register_path_manager(struct mptcp_pm_ops *pm);

include/net/mptcp_v4.h
@@ -39,6 +39,7 @@
 
 extern struct request_sock_ops mptcp_request_sock_ops;
 extern const struct inet_connection_sock_af_ops mptcp_v4_specific;
+extern struct tcp_request_sock_ops mptcp_request_sock_ipv4_ops;
 
 #ifdef CONFIG_MPTCP
 

include/net/mptcp_v6.h
@@ -38,6 +38,7 @@
 extern struct request_sock_ops mptcp6_request_sock_ops;
 extern struct proto mptcpv6_prot;
 extern const struct inet_connection_sock_af_ops mptcp_v6_specific;
+extern struct tcp_request_sock_ops mptcp_request_sock_ipv6_ops;
 
 #ifdef CONFIG_MPTCP
 

include/net/request_sock.h
@@ -41,8 +41,6 @@ struct request_sock_ops {
 	void		(*destructor)(struct request_sock *req);
 	void		(*syn_ack_timeout)(struct sock *sk,
 					   struct request_sock *req);
-	void		(*init)(struct request_sock *req, struct sk_buff *skb,
-				void *init_data);
 };
 
 int inet_rtx_syn_ack(struct sock *parent, struct request_sock *req);

include/net/tcp.h
@@ -451,8 +451,7 @@ void skb_clone_fraglist(struct sk_buff *skb);
 void copy_skb_header(struct sk_buff *new, const struct sk_buff *old);
 
 void inet_twsk_free(struct inet_timewait_sock *tw);
-int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb,
-			struct request_sock_ops *ops, void *init_data);
+int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb);
 /* These states need RST on ABORT according to RFC793 */
 static inline bool tcp_need_reset(int state)
 {
@@ -574,8 +573,7 @@ const u8 *tcp_parse_md5sig_option(const struct tcphdr *th);
  */
 
 void tcp_v4_send_check(struct sock *sk, struct sk_buff *skb);
-int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
-			struct request_sock_ops *ops, void *init_data);
+int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb);
 struct sock *tcp_create_openreq_child(struct sock *sk,
 				      struct request_sock *req,
 				      struct sk_buff *skb);
@@ -1721,7 +1719,7 @@ void tcp4_proc_exit(void);
 int tcp_rtx_synack(struct sock *sk, struct request_sock *req);
 int tcp_conn_request(struct request_sock_ops *rsk_ops,
 		     const struct tcp_request_sock_ops *af_ops,
-		     struct sock *sk, struct sk_buff *skb, void *init_data);
+		     struct sock *sk, struct sk_buff *skb);
 
 /* TCP af-specific functions */
 struct tcp_sock_af_ops {

net/dccp/ipv4.c
@@ -593,8 +593,7 @@ static struct request_sock_ops dccp_request_sock_ops __read_mostly = {
 	.syn_ack_timeout = dccp_syn_ack_timeout,
 };
 
-int dccp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
-			 struct request_sock_ops *ops, void *init_data)
+int dccp_v4_conn_request(struct sock *sk, struct sk_buff *skb)
 {
 	struct inet_request_sock *ireq;
 	struct request_sock *req;

commit 3ae75200f3d8152823c8eab5a86545adaff02202

mptcp: Build-Fix
net/dccp/input.c: In function ‘dccp_rcv_state_process’:
net/dccp/input.c:609: error: too many arguments to function ‘inet_csk((const struct sock *)sk)->icsk_af_ops->conn_request’

net/dccp/ipv4.c:596: error: conflicting types for ‘dccp_v4_conn_request’
net/dccp/dccp.h:271: note: previous declaration of ‘dccp_v4_conn_request’ was here
net/dccp/ipv4.c:674: error: conflicting types for ‘dccp_v4_conn_request’
net/dccp/dccp.h:271: note: previous declaration of ‘dccp_v4_conn_request’ was here

Fixes: 4cb24a286a1 (mptcp: rework mptcp_conn_request)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/dccp/dccp.h
@@ -268,8 +268,7 @@ void dccp_done(struct sock *sk);
 int dccp_reqsk_init(struct request_sock *rq, struct dccp_sock const *dp,
 		    struct sk_buff const *skb);
 
-int dccp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
-			 struct request_sock_ops *ops, void *init_data);
+int dccp_v4_conn_request(struct sock *sk, struct sk_buff *skb);
 
 struct sock *dccp_create_openreq_child(struct sock *sk,
 				       const struct request_sock *req,

net/dccp/input.c
@@ -604,9 +604,7 @@ int dccp_rcv_state_process(struct sock *sk, struct sk_buff *skb,
 	if (sk->sk_state == DCCP_LISTEN) {
 		if (dh->dccph_type == DCCP_PKT_REQUEST) {
 			if (inet_csk(sk)->icsk_af_ops->conn_request(sk,
-								    skb,
-								    NULL,
-								    NULL) < 0)
+								    skb) < 0)
 				return 1;
 			goto discard;
 		}

net/dccp/ipv6.c
@@ -367,7 +367,7 @@ static int dccp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 	struct dccp_skb_cb *dcb = DCCP_SKB_CB(skb);
 
 	if (skb->protocol == htons(ETH_P_IP))
-		return dccp_v4_conn_request(sk, skb, NULL, NULL);
+		return dccp_v4_conn_request(sk, skb);
 
 	if (!ipv6_unicast_destination(skb))
 		return 0;	/* discard, don't send a reset here */