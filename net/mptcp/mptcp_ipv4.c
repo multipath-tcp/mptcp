multipath-tcp/mptcp

commit 85e733cd0096f443ec7fa6b9eb8f531f80635835

tcp: add send_synack method to tcp_request_sock_ops
Create a new tcp_request_sock_ops method to unify the IPv4/IPv6
signature for tcp_v[46]_send_synack. This allows us to later unify
tcp_v4_rtx_synack with tcp_v6_rtx_synack and tcp_v4_conn_request with
tcp_v4_conn_request.

Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

Conflicts:
	net/ipv4/tcp_ipv4.c
	net/ipv6/tcp_ipv6.c

Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
[backported from net-next]
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ipv4.c
@@ -241,8 +241,8 @@ static void mptcp_v4_join_request(struct sock *meta_sk, struct sk_buff *skb)
 	mtreq->low_prio = mopt.low_prio;
 	tcp_rsk(req)->saw_mpc = 1;
 
-	if (tcp_v4_send_synack(meta_sk, dst, req, skb_get_queue_mapping(skb),
-			       NULL))
+	if (tcp_v4_send_synack(meta_sk, dst, NULL, req,
+			       skb_get_queue_mapping(skb), NULL))
 		goto drop_and_free;
 
 	/* Adding to request queue in metasocket */

net/mptcp/mptcp_ipv6.c
@@ -461,7 +461,7 @@ static void mptcp_v6_join_request(struct sock *meta_sk, struct sk_buff *skb)
 	tcp_rsk(req)->saw_mpc = 1;
 
 	if (meta_sk->sk_family == AF_INET6) {
-		if (tcp_v6_send_synack(meta_sk, dst, &fl6, req,
+		if (tcp_v6_send_synack(meta_sk, dst, (struct flowi *)&fl6, req,
 				       skb_get_queue_mapping(skb), NULL))
 			goto drop_and_free;
 	} else {

include/net/tcp.h
@@ -415,6 +415,7 @@ void tcp_v4_reqsk_send_ack(struct sock *sk, struct sk_buff *skb,
 			   struct request_sock *req);
 __u32 tcp_v4_init_sequence(const struct sk_buff *skb);
 int tcp_v4_send_synack(struct sock *sk, struct dst_entry *dst,
+		       struct flowi *fl,
 		       struct request_sock *req,
 		       u16 queue_mapping,
 		       struct tcp_fastopen_cookie *foc);
@@ -428,7 +429,7 @@ void tcp_v6_reqsk_send_ack(struct sock *sk, struct sk_buff *skb,
 			   struct request_sock *req);
 __u32 tcp_v6_init_sequence(const struct sk_buff *skb);
 int tcp_v6_send_synack(struct sock *sk, struct dst_entry *dst,
-		       struct flowi6 *fl6, struct request_sock *req,
+		       struct flowi *fl, struct request_sock *req,
 		       u16 queue_mapping, struct tcp_fastopen_cookie *foc);
 void tcp_v6_send_reset(struct sock *sk, struct sk_buff *skb);
 int tcp_v6_do_rcv(struct sock *sk, struct sk_buff *skb);
@@ -1753,6 +1754,9 @@ struct tcp_request_sock_ops {
 				       const struct request_sock *req,
 				       bool *strict);
 	__u32 (*init_seq)(const struct sk_buff *skb);
+	int (*send_synack)(struct sock *sk, struct dst_entry *dst,
+			   struct flowi *fl, struct request_sock *req,
+			   u16 queue_mapping, struct tcp_fastopen_cookie *foc);
 };
 
 #ifdef CONFIG_SYN_COOKIES

net/ipv4/tcp_ipv4.c
@@ -850,6 +850,7 @@ void tcp_v4_reqsk_send_ack(struct sock *sk, struct sk_buff *skb,
  *	socket.
  */
 int tcp_v4_send_synack(struct sock *sk, struct dst_entry *dst,
+		       struct flowi *fl,
 		       struct request_sock *req,
 		       u16 queue_mapping,
 		       struct tcp_fastopen_cookie *foc)
@@ -882,7 +883,8 @@ int tcp_v4_send_synack(struct sock *sk, struct dst_entry *dst,
 
 int tcp_v4_rtx_synack(struct sock *sk, struct request_sock *req)
 {
-	int res = tcp_v4_send_synack(sk, NULL, req, 0, NULL);
+	const struct  tcp_request_sock_ops *af_ops = tcp_rsk(req)->af_specific;
+	int res = af_ops->send_synack(sk, NULL, NULL, req, 0, NULL);
 
 	if (!res) {
 		TCP_INC_STATS_BH(sock_net(sk), TCP_MIB_RETRANSSEGS);
@@ -1323,6 +1325,7 @@ const struct tcp_request_sock_ops tcp_request_sock_ipv4_ops = {
 #endif
 	.route_req	=	tcp_v4_route_req,
 	.init_seq	=	tcp_v4_init_sequence,
+	.send_synack	=	tcp_v4_send_synack,
 };
 
 int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
@@ -1446,8 +1449,8 @@ int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
 	tcp_openreq_init_rwin(req, sk, dst);
 	fastopen = !want_cookie &&
 		   tcp_try_fastopen(sk, skb, req, &foc, dst);
-	err = tcp_v4_send_synack(sk, dst, req,
-				 skb_get_queue_mapping(skb), &foc);
+	err = af_ops->send_synack(sk, dst, NULL, req,
+				  skb_get_queue_mapping(skb), &foc);
 	if (!fastopen) {
 		if (err || want_cookie)
 			goto drop_and_free;

net/ipv6/tcp_ipv6.c
@@ -488,13 +488,14 @@ out:
 
 
 int tcp_v6_send_synack(struct sock *sk, struct dst_entry *dst,
-		       struct flowi6 *fl6,
+		       struct flowi *fl,
 		       struct request_sock *req,
 		       u16 queue_mapping,
 		       struct tcp_fastopen_cookie *foc)
 {
 	struct inet_request_sock *ireq = inet_rsk(req);
 	struct ipv6_pinfo *np = inet6_sk(sk);
+	struct flowi6 *fl6 = &fl->u.ip6;
 	struct sk_buff *skb;
 	int err = -ENOMEM;
 
@@ -525,10 +526,11 @@ done:
 
 int tcp_v6_rtx_synack(struct sock *sk, struct request_sock *req)
 {
-	struct flowi6 fl6;
+	const struct tcp_request_sock_ops *af_ops = tcp_rsk(req)->af_specific;
+	struct flowi fl;
 	int res;
 
-	res = tcp_v6_send_synack(sk, NULL, &fl6, req, 0, NULL);
+	res = af_ops->send_synack(sk, NULL, &fl, req, 0, NULL);
 	if (!res) {
 		TCP_INC_STATS_BH(sock_net(sk), TCP_MIB_RETRANSSEGS);
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_TCPSYNRETRANS);
@@ -772,7 +774,6 @@ static struct dst_entry *tcp_v6_route_req(struct sock *sk, struct flowi *fl,
 	return inet6_csk_route_req(sk, &fl->u.ip6, req);
 }
 
-
 struct request_sock_ops tcp6_request_sock_ops __read_mostly = {
 	.family		=	AF_INET6,
 	.obj_size	=	sizeof(struct tcp6_request_sock),
@@ -794,6 +795,7 @@ const struct tcp_request_sock_ops tcp_request_sock_ipv6_ops = {
 #endif
 	.route_req	=	tcp_v6_route_req,
 	.init_seq	=	tcp_v6_init_sequence,
+	.send_synack	=	tcp_v6_send_synack,
 };
 
 static void tcp_v6_send_response(struct sk_buff *skb, u32 seq, u32 ack,
@@ -1177,8 +1179,8 @@ int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb,
 	tcp_openreq_init_rwin(req, sk, dst);
 	fastopen = !want_cookie &&
 		   tcp_try_fastopen(sk, skb, req, &foc, dst);
-	err = tcp_v6_send_synack(sk, dst, &fl6, req,
-				 skb_get_queue_mapping(skb), &foc);
+	err = af_ops->send_synack(sk, dst, (struct flowi *)&fl6, req,
+				  skb_get_queue_mapping(skb), &foc);
 	if (!fastopen) {
 		if (err || want_cookie)
 			goto drop_and_free;

commit d6d116418eb04ccbeaf9140de4ade513170b40c3

mptcp: Fix request-sock initialization if TCP_MD5SIG is disabled
In mptcp_v4/6_join_request, we do not set af_specific if TCP_MD5SIG is
disabled in the kernel-config. Thus, af_specific will be garbage and a
call to any of the function-pointers will result in a crash:

[   83.153400] BUG: unable to handle kernel NULL pointer dereference at 0000000000000020
[   83.247329] IP: [<ffffffff815995ee>] tcp_rtx_synack+0x1e/0x50
[   83.316189] PGD 235dfc067 PUD 235447067 PMD 0
[   83.369652] Oops: 0000 [#1] PREEMPT SMP
[   83.416882] Modules linked in:
[   83.453497] CPU: 1 PID: 0 Comm: swapper/1 Not tainted 3.14.0-mptcp #714
[   83.532646] Hardware name: HP ProLiant DL165 G7, BIOS O37 01/26/2011
[   83.608678] task: ffff8802368dc2e0 ti: ffff8802368fe000 task.ti: ffff8802368fe000
[   83.698228] RIP: 0010:[<ffffffff815995ee>]  [<ffffffff815995ee>] tcp_rtx_synack+0x1e/0x50
[   83.796208] RSP: 0018:ffff88023fd038b8  EFLAGS: 00010246
[   83.859753] RAX: 0000000000000000 RBX: ffff88023561e8c0 RCX: ffff88023561e8c0
[   83.945143] RDX: ffff88023fd038b8 RSI: 0000000000000000 RDI: ffff880231689d00
[   84.030526] RBP: ffff88023fd038f8 R08: 0000000000000000 R09: 0000000000000000
[   84.115911] R10: ffff88023fd03938 R11: 0000000000000000 R12: ffff88023561e8c0
[   84.201301] R13: ffff880231689d00 R14: ffff880234a40d00 R15: 0000000000000000
[   84.286686] FS:  00007fb924fb5700(0000) GS:ffff88023fd00000(0000) knlGS:0000000000000000
[   84.383515] CS:  0010 DS: 0000 ES: 0000 CR0: 000000008005003b
[   84.452261] CR2: 0000000000000020 CR3: 0000000235ed8000 CR4: 00000000000007e0
[   84.537650] Stack:
[   84.561675]  0000000000000000 ffff880200000000 ffff88023fd03938 ffff8802316d5a8e
[   84.650607]  0000000000000001 ffff880234a40d28 0000000000000062 ffff8802316d5a64
[   84.739527]  ffff88023fd03918 ffffffff81588a06 ffff8802316d5a64 0000000000000200
[   84.828454] Call Trace:
[   84.857686]  <IRQ>
[   84.880675]  [<ffffffff81588a06>] inet_rtx_syn_ack+0x16/0x30
[   84.950678]  [<ffffffff815a3f92>] tcp_check_req+0x382/0x670
[   85.017348]  [<ffffffff8165acaa>] ? common_interrupt+0x6a/0x6a
[   85.087138]  [<ffffffff81588c41>] ? inet_csk_search_req+0x1/0xd0
[   85.159008]  [<ffffffff815a1f4f>] tcp_v4_hnd_req+0x6f/0x180
[   85.225680]  [<ffffffff815c9fcb>] ? ipt_do_table+0x20b/0x400
[   85.293389]  [<ffffffff81617bc5>] mptcp_v4_do_rcv+0x1e5/0x2c0
[   85.362140]  [<ffffffff815a29cd>] tcp_v4_do_rcv+0x17d/0x2e0
[   85.428811]  [<ffffffff8161d68b>] mptcp_check_req+0x18b/0x210
[   85.497560]  [<ffffffff815a305f>] tcp_v4_rcv+0x52f/0x7b0

Since 85e733cd0 (tcp: add send_synack method to tcp_request_sock_ops) uses
the callback to send a syn-ack, we must make sure that af_specific is
always initialized.

Fixes: 85e733cd0 (tcp: add send_synack method to tcp_request_sock_ops)

net/mptcp/mptcp_ipv4.c
@@ -159,9 +159,7 @@ static void mptcp_v4_join_request(struct sock *meta_sk, struct sk_buff *skb)
 	mtreq->mpcb = mpcb;
 	INIT_LIST_HEAD(&mtreq->collide_tuple);
 
-#ifdef CONFIG_TCP_MD5SIG
 	tcp_rsk(req)->af_specific = &tcp_request_sock_ipv4_ops;
-#endif
 
 	tmp_opt.tstamp_ok = tmp_opt.saw_tstamp;
 	tcp_openreq_init(req, &tmp_opt, skb);

net/mptcp/mptcp_ipv6.c
@@ -366,9 +366,7 @@ static void mptcp_v6_join_request(struct sock *meta_sk, struct sk_buff *skb)
 	mtreq->mpcb = mpcb;
 	INIT_LIST_HEAD(&mtreq->collide_tuple);
 
-#ifdef CONFIG_TCP_MD5SIG
 	tcp_rsk(req)->af_specific = &tcp_request_sock_ipv6_ops;
-#endif
 
 	tmp_opt.tstamp_ok = tmp_opt.saw_tstamp;
 	tcp_openreq_init(req, &tmp_opt, skb);