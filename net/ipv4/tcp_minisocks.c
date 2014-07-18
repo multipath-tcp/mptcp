multipath-tcp/mptcp

commit f40efe40d08d81648aca803be7e951f15f6ad259

mptcp: Correct support of TCP-keepalive cont'd
This patch fixes and complements ba1582e13 (mptcp: Correct support of
TCP-Keepalive).

Fixes:
 - correctly disable using keepalive timer at the subflow level
 - avoid calling inet_csk_reqsk_queue_drop when new subflows are
   established that will stop the keepalive timer at the meta.

Fixes: ba1582e13 (mptcp: Correct support of TCP-Keepalive)
Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_minisocks.c
@@ -475,7 +475,7 @@ struct sock *tcp_create_openreq_child(struct sock *sk, struct request_sock *req,
 		 * set on the meta. But, keepalive is entirely handled at the
 		 * meta-socket, so let's keep it there.
 		 */
-		if (sock_flag(newsk, SOCK_KEEPOPEN) && is_meta_sk(sk))
+		if (sock_flag(newsk, SOCK_KEEPOPEN) && !tcp_sk(sk)->mpc)
 			inet_csk_reset_keepalive_timer(newsk,
 						       keepalive_time_when(newtp));
 
@@ -799,7 +799,16 @@ embryonic_reset:
 		tcp_reset(sk);
 	}
 	if (!fastopen) {
-		inet_csk_reqsk_queue_drop(sk, req, prev);
+		if (is_meta_sk(sk)) {
+			/* We want to avoid stoping the keepalive-timer and so
+			 * avoid ending up in inet_csk_reqsk_queue_removed ...
+			 */
+			inet_csk_reqsk_queue_unlink(sk, req, prev);
+			reqsk_queue_removed(&inet_csk(sk)->icsk_accept_queue, req);
+			reqsk_free(req);
+		} else {
+			inet_csk_reqsk_queue_drop(sk, req, prev);
+		}
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_EMBRYONICRSTS);
 	}
 	return NULL;

net/mptcp/mptcp_ctrl.c
@@ -1949,12 +1949,16 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk, struct sock *child,
 	/* Subflows do not use the accept queue, as they
 	 * are attached immediately to the mpcb.
 	 */
-	inet_csk_reqsk_queue_drop(meta_sk, req, prev);
+	inet_csk_reqsk_queue_unlink(meta_sk, req, prev);
+	reqsk_queue_removed(&inet_csk(meta_sk)->icsk_accept_queue, req);
+	reqsk_free(req);
 	return child;
 
 teardown:
 	/* Drop this request - sock creation failed. */
-	inet_csk_reqsk_queue_drop(meta_sk, req, prev);
+	inet_csk_reqsk_queue_unlink(meta_sk, req, prev);
+	reqsk_queue_removed(&inet_csk(meta_sk)->icsk_accept_queue, req);
+	reqsk_free(req);
 	inet_csk_prepare_forced_close(child);
 	tcp_done(child);
 	return meta_sk;

commit e9e630d0b0db5dd807833fad507c56b8028808b2

mptcp: Correctly handle keepalive on active opener
f40efe40d0 (mptcp: Correct support of TCP-keepalive cont'd) enabled
correct handling of keepalive on the passive opener, by changing the
condition in tcp_create_openreq_child to prevent resetting the keepalive
timer on new subflows. However, this only fixed it on the server-side.

This patch fixes the behavior now also for the active opener.
Keepalive setting/deleting of the timer is now entirely handled in
MPTCP-functions. First, when allocating the mpcb, we set the keepalive
timer in mptcp_mpcb_inherit_sockopts. This, because on the client-side,
the keepalive timer is started in tcp_finish_connect - a function never
called on the meta-socket. At the same time, we cancel eventual started
timers on the master sk.

Further, for new subflows, we kill their keepalive-timer inside
mptcp_sub_inherit_sockopts, which is called from mptcp_add_sock. There
are two cases here:

* active opener: Actually nothing special to be done here. The
  keepalive-flag is not inherited from the meta-socket.
* passive opener: The keepalive-flag has been inherited from the
  meta-socket through mptcp_inherit_sk. And the timer has been started
  in tcp_create_openreq_child. So, we unset the flag and delete the timer.

Fixes: f40efe40d0 (mptcp: Correct support of TCP-keepalive cont'd)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_minisocks.c
@@ -503,11 +503,7 @@ struct sock *tcp_create_openreq_child(struct sock *sk, struct request_sock *req,
 
 		newtp->urg_data = 0;
 
-		/* MPTCP: If we are creating a subflow, KEEPOPEN might have been
-		 * set on the meta. But, keepalive is entirely handled at the
-		 * meta-socket, so let's keep it there.
-		 */
-		if (sock_flag(newsk, SOCK_KEEPOPEN) && !mptcp(tcp_sk(sk)))
+		if (sock_flag(newsk, SOCK_KEEPOPEN))
 			inet_csk_reset_keepalive_timer(newsk,
 						       keepalive_time_when(newtp));
 

net/mptcp/mptcp_ctrl.c
@@ -668,6 +668,7 @@ static void mptcp_mpcb_inherit_sockopts(struct sock *meta_sk, struct sock *maste
 	 * Socket-options handled in this function here
 	 * ======
 	 * TCP_DEFER_ACCEPT
+	 * SO_KEEPALIVE
 	 *
 	 * Socket-options on the todo-list
 	 * ======
@@ -698,7 +699,6 @@ static void mptcp_mpcb_inherit_sockopts(struct sock *meta_sk, struct sock *maste
 	 * TCP_CONGESTION
 	 * TCP_SYNCNT
 	 * TCP_QUICKACK
-	 * SO_KEEPALIVE
 	 */
 
 	/****** DEFER_ACCEPT-handler ******/
@@ -707,6 +707,15 @@ static void mptcp_mpcb_inherit_sockopts(struct sock *meta_sk, struct sock *maste
 	 * them
 	 */
 	inet_csk(meta_sk)->icsk_accept_queue.rskq_defer_accept = 0;
+
+	/* Keepalives are handled entirely at the MPTCP-layer */
+	if (sock_flag(meta_sk, SOCK_KEEPOPEN)) {
+		inet_csk_reset_keepalive_timer(meta_sk,
+				keepalive_time_when(tcp_sk(meta_sk)));
+		sock_reset_flag(master_sk, SOCK_KEEPOPEN);
+		inet_csk_delete_keepalive_timer(master_sk);
+	}
+
 }
 
 static void mptcp_sub_inherit_sockopts(struct sock *meta_sk, struct sock *sub_sk)
@@ -726,6 +735,12 @@ static void mptcp_sub_inherit_sockopts(struct sock *meta_sk, struct sock *sub_sk
 
 	/* Nagle/Cork is forced off on the subflows. It is handled at the meta-layer */
 	tcp_sk(sub_sk)->nonagle = TCP_NAGLE_OFF|TCP_NAGLE_PUSH;
+
+	/* Keepalives are handled entirely at the MPTCP-layer */
+	if (sock_flag(sub_sk, SOCK_KEEPOPEN)) {
+		sock_reset_flag(sub_sk, SOCK_KEEPOPEN);
+		inet_csk_delete_keepalive_timer(sub_sk);
+	}
 }
 
 int mptcp_backlog_rcv(struct sock *meta_sk, struct sk_buff *skb)