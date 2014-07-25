multipath-tcp/mptcp

commit 597173ef46a36521f1dd0d8ff1e00c728aa90130

mptcp: fullmesh: Always announce all addresses
If a connection is unidirectionally sending its data and there is no
space in the third ack (or the data segments) to add the ADD_ADDR, this
ADD_ADDR will never be sent. The problem is that we call full-mesh but
do not trigger the sending of a third ack.

We could simply call tcp_send_ack inside fullmesh_new_session, which
would work on the server-side, but on the client-side we would then send
this ADD_ADDR before the third ack + MP_CAPABLE. Then, we break hosts
who support syn-cookies on the server side.

So, the fix is to move the call to mptcp_update_metasocket (who calls
new_session) a bit later on the client-side. Additionally, we provide to
new_session the master-socket, as this is at this point the only one who
can send something.
On the server-side everything is fine, because at the point when we
receive the third ack we are ok to send an ADD_ADDR.

Issue #48: https://github.com/multipath-tcp/mptcp/issues/48

Reported-by: Simone Ferlin-Oliveira <ferlin@simula.no>
Fixes: c31d21d039d80 (Modularize the path-manager)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/mptcp.h
@@ -210,7 +210,7 @@ struct mptcp_pm_ops {
 	struct list_head list;
 
 	/* Signal the creation of a new MPTCP-session. */
-	void (*new_session)(struct sock *meta_sk);
+	void (*new_session)(struct sock *meta_sk, struct sock *sk);
 	void (*release_sock)(struct sock *meta_sk);
 	void (*fully_established)(struct sock *meta_sk);
 	void (*new_remote_address)(struct sock *meta_sk);

net/ipv4/tcp_input.c
@@ -5804,6 +5804,12 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb,
 		if (is_meta_sk(sk)) {
 			sk = tcp_sk(sk)->mpcb->master_sk;
 			tp = tcp_sk(sk);
+
+			/* Need to call it here, because it will announce new
+			 * addresses, which can only be done after the third ack
+			 * of the 3-way handshake.
+			 */
+			mptcp_update_metasocket(sk, tp->meta_sk);
 		}
 		if (queued >= 0)
 			return queued;

net/mptcp/mptcp_ctrl.c
@@ -1266,7 +1266,7 @@ void mptcp_del_sock(struct sock *sk)
 void mptcp_update_metasocket(struct sock *sk, struct sock *meta_sk)
 {
 	if (tcp_sk(sk)->mpcb->pm_ops->new_session)
-		tcp_sk(sk)->mpcb->pm_ops->new_session(meta_sk);
+		tcp_sk(sk)->mpcb->pm_ops->new_session(meta_sk, sk);
 
 	tcp_sk(sk)->mptcp->send_mp_prio = tcp_sk(sk)->mptcp->low_prio;
 }

net/mptcp/mptcp_fullmesh.c
@@ -1093,13 +1093,12 @@ static void full_mesh_add_raddr(struct mptcp_cb *mpcb,
 		mptcp_addv6_raddr(mpcb, &addr->in6, port, id);
 }
 
-static void full_mesh_new_session(struct sock *meta_sk)
+static void full_mesh_new_session(struct sock *meta_sk, struct sock *sk)
 {
 	struct mptcp_loc_addr *mptcp_local;
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct fullmesh_priv *fmp = fullmesh_get_priv(mpcb);
 	struct mptcp_fm_ns *fm_ns = fm_get_ns(sock_net(meta_sk));
-	struct sock *sk;
 	int i, index;
 	union inet_addr saddr, daddr;
 	sa_family_t family;
@@ -1132,8 +1131,6 @@ static void full_mesh_new_session(struct sock *meta_sk)
 	INIT_DELAYED_WORK(&fmp->subflow_retry_work, retry_subflow_worker);
 	fmp->mpcb = mpcb;
 
-	sk = mptcp_select_ack_sock(meta_sk);
-
 	/* Look for the address among the local addresses */
 	mptcp_for_each_bit_set(mptcp_local->loc4_bits, i) {
 		__be32 ifa_address = mptcp_local->locaddr4[i].addr.s_addr;
@@ -1164,6 +1161,8 @@ static void full_mesh_new_session(struct sock *meta_sk)
 	else
 		fmp->announced_addrs_v6 |= (1 << index);
 
+	for (i = fmp->add_addr; i && fmp->add_addr; i--)
+		tcp_send_ack(sk);
 
 	return;
 

net/mptcp/mptcp_input.c
@@ -2159,8 +2159,6 @@ int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 		sk_set_socket(sk, mptcp_meta_sk(sk)->sk_socket);
 		sk->sk_wq = mptcp_meta_sk(sk)->sk_wq;
 
-		mptcp_update_metasocket(sk, mptcp_meta_sk(sk));
-
 		 /* hold in mptcp_inherit_sk due to initialization to 2 */
 		sock_put(sk);
 	} else {

net/mptcp/mptcp_ndiffports.c
@@ -92,7 +92,7 @@ exit:
 	sock_put(meta_sk);
 }
 
-static void ndiffports_new_session(struct sock *meta_sk)
+static void ndiffports_new_session(struct sock *meta_sk, struct sock *sk)
 {
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct ndiffports_priv *fmp = (struct ndiffports_priv *)&mpcb->mptcp_pm[0];

commit 5aecbc11781b35faa636e525317ffa836e38e917

mptcp: Build-Fix
Fixes: 597173ef46a3 (mptcp: fullmesh: Always announce all addresses)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/mptcp.h
@@ -1343,6 +1343,7 @@ static inline int is_master_tp(const struct tcp_sock *tp)
 }
 static inline void mptcp_purge_ofo_queue(struct tcp_sock *meta_tp) {}
 static inline void mptcp_del_sock(const struct sock *sk) {}
+static inline void mptcp_update_metasocket(struct sock *sock, struct sock *meta_sk) {}
 static inline void mptcp_reinject_data(struct sock *orig_sk, int clone_it) {}
 static inline void mptcp_update_sndbuf(const struct mptcp_cb *mpcb) {}
 static inline void mptcp_clean_rtx_infinite(const struct sk_buff *skb,