multipath-tcp/mptcp

commit 4f42b1051e962a3df954af4f81fb4ad454d7ad7b

mptcp: use mptcp specific inet request ops
Use specific mptcp functions for inet_connection_sock_af_ops.
This will help cleanup the connection initiation code from the
TCP stack.

When the socket is created, the mptcp function pointers will be
used if mptcp is enabled (system-wide or for applications).
If mptcp is disabled, the regular tcp functions will be called,
reducing the impact of mptcp on the TCP stack in this case.

This patch introduces the new function pointers for IPv4 and IPv6
sockets. At this point, the same function pointers are used as
for IPv4/IPv6 tcp sockets.

Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/mptcp.h
@@ -819,6 +819,11 @@ void mptcp_get_default_path_manager(char *name);
 int mptcp_set_default_path_manager(const char *name);
 extern struct mptcp_pm_ops mptcp_pm_default;
 
+static inline int is_mptcp_enabled(void)
+{
+	return sysctl_mptcp_enabled && !mptcp_init_failed;
+}
+
 static inline
 struct mptcp_request_sock *mptcp_rsk(const struct request_sock *req)
 {

net/ipv4/tcp_ipv4.c
@@ -2259,7 +2259,12 @@ static int tcp_v4_init_sock(struct sock *sk)
 
 	tcp_init_sock(sk);
 
-	icsk->icsk_af_ops = &ipv4_specific;
+#ifdef CONFIG_MPTCP
+	if (is_mptcp_enabled())
+		icsk->icsk_af_ops = &mptcp_v4_specific;
+	else
+#endif
+		icsk->icsk_af_ops = &ipv4_specific;
 
 #ifdef CONFIG_TCP_MD5SIG
 	tcp_sk(sk)->af_specific = &tcp_sock_ipv4_specific;

net/ipv6/ipv6_sockglue.c
@@ -48,6 +48,8 @@
 #include <net/addrconf.h>
 #include <net/inet_common.h>
 #include <net/tcp.h>
+#include <net/mptcp.h>
+#include <net/mptcp_v4.h>
 #include <net/udp.h>
 #include <net/udplite.h>
 #include <net/xfrm.h>
@@ -196,7 +198,12 @@ static int do_ipv6_setsockopt(struct sock *sk, int level, int optname,
 				sock_prot_inuse_add(net, &tcp_prot, 1);
 				local_bh_enable();
 				sk->sk_prot = &tcp_prot;
-				icsk->icsk_af_ops = &ipv4_specific;
+#ifdef CONFIG_MPTCP
+				if (is_mptcp_enabled())
+					icsk->icsk_af_ops = &mptcp_v4_specific;
+				else
+#endif
+					icsk->icsk_af_ops = &ipv4_specific;
 				sk->sk_socket->ops = &inet_stream_ops;
 				sk->sk_family = PF_INET;
 				tcp_sync_mss(sk, icsk->icsk_pmtu_cookie);

net/ipv6/tcp_ipv6.c
@@ -75,6 +75,8 @@
 #include <linux/crypto.h>
 #include <linux/scatterlist.h>
 
+static const struct inet_connection_sock_af_ops ipv6_mapped;
+static const struct inet_connection_sock_af_ops ipv6_specific;
 #ifdef CONFIG_TCP_MD5SIG
 static const struct tcp_sock_af_ops tcp_sock_ipv6_specific;
 static const struct tcp_sock_af_ops tcp_sock_ipv6_mapped_specific;
@@ -101,7 +103,8 @@ void inet6_sk_rx_dst_set(struct sock *sk, const struct sk_buff *skb)
 void tcp_v6_hash(struct sock *sk)
 {
 	if (sk->sk_state != TCP_CLOSE) {
-		if (inet_csk(sk)->icsk_af_ops == &ipv6_mapped) {
+		if (inet_csk(sk)->icsk_af_ops == &ipv6_mapped ||
+		    inet_csk(sk)->icsk_af_ops == &mptcp_v6_mapped) {
 			tcp_prot.hash(sk);
 			return;
 		}
@@ -211,7 +214,12 @@ int tcp_v6_connect(struct sock *sk, struct sockaddr *uaddr,
 		sin.sin_port = usin->sin6_port;
 		sin.sin_addr.s_addr = usin->sin6_addr.s6_addr32[3];
 
-		icsk->icsk_af_ops = &ipv6_mapped;
+#ifdef CONFIG_MPTCP
+		if (is_mptcp_enabled())
+			icsk->icsk_af_ops = &mptcp_v6_mapped;
+		else
+#endif
+			icsk->icsk_af_ops = &ipv6_mapped;
 		sk->sk_backlog_rcv = tcp_v4_do_rcv;
 #ifdef CONFIG_TCP_MD5SIG
 		tp->af_specific = &tcp_sock_ipv6_mapped_specific;
@@ -221,7 +229,12 @@ int tcp_v6_connect(struct sock *sk, struct sockaddr *uaddr,
 
 		if (err) {
 			icsk->icsk_ext_hdr_len = exthdrlen;
-			icsk->icsk_af_ops = &ipv6_specific;
+#ifdef CONFIG_MPTCP
+			if (is_mptcp_enabled())
+				icsk->icsk_af_ops = &mptcp_v6_specific;
+			else
+#endif
+				icsk->icsk_af_ops = &ipv6_specific;
 			sk->sk_backlog_rcv = tcp_v6_do_rcv;
 #ifdef CONFIG_TCP_MD5SIG
 			tp->af_specific = &tcp_sock_ipv6_specific;
@@ -986,7 +999,7 @@ struct sock *tcp_v6_hnd_req(struct sock *sk, struct sk_buff *skb)
 /* FIXME: this is substantially similar to the ipv4 code.
  * Can some kind of merge be done? -- erics
  */
-static int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
+int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 {
 	struct tcp_options_received tmp_opt;
 	struct mptcp_options_received mopt;
@@ -1195,7 +1208,12 @@ struct sock *tcp_v6_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 
 		newsk->sk_v6_rcv_saddr = newnp->saddr;
 
-		inet_csk(newsk)->icsk_af_ops = &ipv6_mapped;
+#ifdef CONFIG_MPTCP
+		if (is_mptcp_enabled())
+			inet_csk(newsk)->icsk_af_ops = &mptcp_v6_mapped;
+		else
+#endif
+			inet_csk(newsk)->icsk_af_ops = &ipv6_mapped;
 		newsk->sk_backlog_rcv = tcp_v4_do_rcv;
 #ifdef CONFIG_TCP_MD5SIG
 		newtp->af_specific = &tcp_sock_ipv6_mapped_specific;
@@ -1762,7 +1780,7 @@ struct timewait_sock_ops tcp6_timewait_sock_ops = {
 	.twsk_destructor= tcp_twsk_destructor,
 };
 
-const struct inet_connection_sock_af_ops ipv6_specific = {
+static const struct inet_connection_sock_af_ops ipv6_specific = {
 	.queue_xmit	   = inet6_csk_xmit,
 	.send_check	   = tcp_v6_send_check,
 	.rebuild_header	   = inet6_sk_rebuild_header,
@@ -1794,7 +1812,7 @@ static const struct tcp_sock_af_ops tcp_sock_ipv6_specific = {
  *	TCP over IPv4 via INET6 API
  */
 
-const struct inet_connection_sock_af_ops ipv6_mapped = {
+static const struct inet_connection_sock_af_ops ipv6_mapped = {
 	.queue_xmit	   = ip_queue_xmit,
 	.send_check	   = tcp_v4_send_check,
 	.rebuild_header	   = inet_sk_rebuild_header,
@@ -1830,7 +1848,12 @@ static int tcp_v6_init_sock(struct sock *sk)
 
 	tcp_init_sock(sk);
 
-	icsk->icsk_af_ops = &ipv6_specific;
+#ifdef CONFIG_MPTCP
+	if (is_mptcp_enabled())
+		icsk->icsk_af_ops = &mptcp_v6_specific;
+	else
+#endif
+		icsk->icsk_af_ops = &ipv6_specific;
 
 #ifdef CONFIG_TCP_MD5SIG
 	tcp_sk(sk)->af_specific = &tcp_sock_ipv6_specific;

include/net/mptcp_v4.h
@@ -38,6 +38,7 @@
 #include <net/sock.h>
 
 extern struct request_sock_ops mptcp_request_sock_ops;
+extern const struct inet_connection_sock_af_ops mptcp_v4_specific;
 
 #ifdef CONFIG_MPTCP
 

include/net/mptcp_v6.h
@@ -37,6 +37,8 @@
 
 extern struct request_sock_ops mptcp6_request_sock_ops;
 extern struct proto mptcpv6_prot;
+extern const struct inet_connection_sock_af_ops mptcp_v6_specific;
+extern const struct inet_connection_sock_af_ops mptcp_v6_mapped;
 
 #ifdef CONFIG_MPTCP
 

include/net/tcp.h
@@ -372,9 +372,6 @@ extern struct proto tcp_prot;
 #define TCP_ADD_STATS(net, field, val)	SNMP_ADD_STATS((net)->mib.tcp_statistics, field, val)
 
 /**** START - Exports needed for MPTCP ****/
-extern const struct inet_connection_sock_af_ops ipv4_specific;
-extern const struct inet_connection_sock_af_ops ipv6_specific;
-extern const struct inet_connection_sock_af_ops ipv6_mapped;
 extern const struct tcp_request_sock_ops tcp_request_sock_ipv4_ops;
 extern const struct tcp_request_sock_ops tcp_request_sock_ipv6_ops;
 
@@ -458,6 +455,7 @@ void skb_clone_fraglist(struct sk_buff *skb);
 void copy_skb_header(struct sk_buff *new, const struct sk_buff *old);
 
 void inet_twsk_free(struct inet_timewait_sock *tw);
+int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb);
 /* These states need RST on ABORT according to RFC793 */
 static inline bool tcp_need_reset(int state)
 {

net/mptcp/mptcp_ctrl.c
@@ -891,7 +891,7 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key, u32 window)
 	}
 
 #if IS_ENABLED(CONFIG_IPV6)
-	if (meta_icsk->icsk_af_ops == &ipv6_mapped) {
+	if (meta_icsk->icsk_af_ops == &mptcp_v6_mapped) {
 		struct ipv6_pinfo *newnp, *np = inet6_sk(meta_sk);
 
 		inet_sk(master_sk)->pinet6 = &((struct tcp6_sock *)master_sk)->inet6;
@@ -1087,9 +1087,9 @@ struct sock *mptcp_sk_clone(const struct sock *sk, int family,
 		newsk->sk_prot = &tcpv6_prot;
 		newsk->sk_prot_creator = &tcpv6_prot;
 		if (family == AF_INET)
-			inet_csk(newsk)->icsk_af_ops = &ipv6_mapped;
+			inet_csk(newsk)->icsk_af_ops = &mptcp_v6_mapped;
 		else
-			inet_csk(newsk)->icsk_af_ops = &ipv6_specific;
+			inet_csk(newsk)->icsk_af_ops = &mptcp_v6_specific;
 		newsk->sk_family = AF_INET6;
 	}
 #endif

commit a75ee3adc3e6e4805c172d9b781ae6d9b62e91aa

mptcp: Correctly handle MPTCP if it is set/unset by the app
We allow to enable/disable MPTCP through the application by setting the
sysctl mptcp_enabled to 2. Thus, in is_mptcp_enabled, we have to check
for this flag as well.

Fixes: 4f42b1051e962 (mptcp: use mptcp specific inet request ops)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/mptcp.h
@@ -851,9 +851,15 @@ void mptcp_get_default_scheduler(char *name);
 int mptcp_set_default_scheduler(const char *name);
 extern struct mptcp_sched_ops mptcp_sched_default;
 
-static inline int is_mptcp_enabled(void)
+static inline bool is_mptcp_enabled(const struct sock *sk)
 {
-	return sysctl_mptcp_enabled && !mptcp_init_failed;
+	if (!sysctl_mptcp_enabled || mptcp_init_failed)
+		return false;
+
+	if (sysctl_mptcp_enabled == MPTCP_APP && !tcp_sk(sk)->mptcp_enabled)
+		return false;
+
+	return true;
 }
 
 static inline int mptcp_pi_to_flag(int pi)

net/ipv4/tcp_ipv4.c
@@ -1894,7 +1894,7 @@ static int tcp_v4_init_sock(struct sock *sk)
 	tcp_init_sock(sk);
 
 #ifdef CONFIG_MPTCP
-	if (is_mptcp_enabled())
+	if (is_mptcp_enabled(sk))
 		icsk->icsk_af_ops = &mptcp_v4_specific;
 	else
 #endif

net/ipv6/ipv6_sockglue.c
@@ -199,7 +199,7 @@ static int do_ipv6_setsockopt(struct sock *sk, int level, int optname,
 				local_bh_enable();
 				sk->sk_prot = &tcp_prot;
 #ifdef CONFIG_MPTCP
-				if (is_mptcp_enabled())
+				if (is_mptcp_enabled(sk))
 					icsk->icsk_af_ops = &mptcp_v4_specific;
 				else
 #endif

net/ipv6/tcp_ipv6.c
@@ -215,7 +215,7 @@ int tcp_v6_connect(struct sock *sk, struct sockaddr *uaddr,
 		sin.sin_addr.s_addr = usin->sin6_addr.s6_addr32[3];
 
 #ifdef CONFIG_MPTCP
-		if (is_mptcp_enabled())
+		if (is_mptcp_enabled(sk))
 			icsk->icsk_af_ops = &mptcp_v6_mapped;
 		else
 #endif
@@ -230,7 +230,7 @@ int tcp_v6_connect(struct sock *sk, struct sockaddr *uaddr,
 		if (err) {
 			icsk->icsk_ext_hdr_len = exthdrlen;
 #ifdef CONFIG_MPTCP
-			if (is_mptcp_enabled())
+			if (is_mptcp_enabled(sk))
 				icsk->icsk_af_ops = &mptcp_v6_specific;
 			else
 #endif
@@ -1125,7 +1125,7 @@ struct sock *tcp_v6_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 		newsk->sk_v6_rcv_saddr = newnp->saddr;
 
 #ifdef CONFIG_MPTCP
-		if (is_mptcp_enabled())
+		if (is_mptcp_enabled(newsk))
 			inet_csk(newsk)->icsk_af_ops = &mptcp_v6_mapped;
 		else
 #endif
@@ -1745,7 +1745,7 @@ static int tcp_v6_init_sock(struct sock *sk)
 	tcp_init_sock(sk);
 
 #ifdef CONFIG_MPTCP
-	if (is_mptcp_enabled())
+	if (is_mptcp_enabled(sk))
 		icsk->icsk_af_ops = &mptcp_v6_specific;
 	else
 #endif