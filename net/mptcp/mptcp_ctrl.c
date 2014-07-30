multipath-tcp/mptcp

commit 5a5ff876a64ed399bdc43860f791ba6a45e62d61

mptcp: add mptcp specific conn_request function
Rework tcp_v4_conn_request/tcp_v6_conn_request so that mptcp
changes are separated from the tcp stack:
- parsing options and mptcp checks is done before calling
tcp_v4_conn_request/tcp_v6_conn_request
- request_sock_ops is sent as parameter
- mptcp initialization is done through a new function pointer of
request_sock_ops

Initial flow: icsk->icsk_af_ops->conn_request -> tcp_v4/6_conn_request.
Current flow for mptcp enabled: icsk->icsk_af_ops->conn_request ->
mptcp_tcp_v4_conn_request -> tcp_v4/6_conn_request.

Also removed struct tcp_options_received as a parameter from a couple
of mptcp functions that were not using it.

Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ctrl.c
@@ -244,7 +244,6 @@ static void mptcp_set_key_reqsk(struct request_sock *req,
  * will be created in mptcp_check_req_master(), and store the received token.
  */
 void mptcp_reqsk_new_mptcp(struct request_sock *req,
-			   const struct tcp_options_received *rx_opt,
 			   const struct mptcp_options_received *mopt,
 			   const struct sk_buff *skb)
 {
@@ -1075,7 +1074,7 @@ struct sock *mptcp_sk_clone(const struct sock *sk, int family,
 		/* Set these pointers - they are needed by mptcp_inherit_sk */
 		newsk->sk_prot = &tcp_prot;
 		newsk->sk_prot_creator = &tcp_prot;
-		inet_csk(newsk)->icsk_af_ops = &ipv4_specific;
+		inet_csk(newsk)->icsk_af_ops = &mptcp_v4_specific;
 		newsk->sk_family = AF_INET;
 	}
 #if IS_ENABLED(CONFIG_IPV6)
@@ -2044,6 +2043,69 @@ void mptcp_tsq_sub_deferred(struct sock *meta_sk)
 		/* Final sock_put (cfr. mptcp_tsq_flags */
 		sock_put(sk);
 	}
+}
+
+void mptcp_reqsk_init(struct request_sock *req,
+		      struct sk_buff *skb, void *init_data)
+{
+	struct mptcp_options_received *mopt =
+	    (struct mptcp_options_received *)init_data;
+	struct mptcp_request_sock *mreq = mptcp_rsk(req);
+
+	mreq->mpcb = NULL;
+	mreq->dss_csum = mopt->dss_csum;
+	mreq->collide_tk.pprev = NULL;
+
+	mptcp_reqsk_new_mptcp(req, mopt, skb);
+}
+
+int mptcp_conn_request(struct sock *sk, struct sk_buff *skb,
+		       struct request_sock_ops *ops, void *init_data)
+{
+	struct mptcp_options_received mopt;
+	struct tcp_sock *tp = tcp_sk(sk);
+#ifdef CONFIG_SYN_COOKIES
+	__u32 isn = TCP_SKB_CB(skb)->when;
+#endif
+	bool want_cookie = false;
+
+#ifdef CONFIG_SYN_COOKIES
+	if ((sysctl_tcp_syncookies == 2 ||
+	     inet_csk_reqsk_queue_is_full(sk)) && !isn)
+		want_cookie = sysctl_tcp_syncookies;
+#endif
+
+	mptcp_init_mp_opt(&mopt);
+	tcp_parse_mptcp_options(skb, &mopt);
+
+	if (mopt.is_mp_join)
+		return mptcp_do_join_short(skb, &mopt, sock_net(sk));
+	if (mopt.drop_me)
+		goto drop;
+
+	if (sysctl_mptcp_enabled == MPTCP_APP && !tp->mptcp_enabled)
+		mopt.saw_mpc = 0;
+
+	if (skb->protocol == htons(ETH_P_IP)) {
+		if (mopt.saw_mpc && !want_cookie)
+			return tcp_v4_conn_request(sk, skb,
+						   &mptcp_request_sock_ops,
+						   &mopt);
+		return tcp_v4_conn_request(sk, skb, &tcp_request_sock_ops,
+					   NULL);
+#if IS_ENABLED(CONFIG_IPV6)
+	} else {
+		if (mopt.saw_mpc && !want_cookie)
+			return tcp_v6_conn_request(sk, skb,
+						   &mptcp6_request_sock_ops,
+						   &mopt);
+		return tcp_v6_conn_request(sk, skb, &tcp6_request_sock_ops,
+					   NULL);
+#endif
+	}
+drop:
+	NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_LISTENDROPS);
+	return 0;
 }
 
 struct workqueue_struct *mptcp_wq;

include/net/inet_connection_sock.h
@@ -30,6 +30,7 @@
 
 struct inet_bind_bucket;
 struct tcp_congestion_ops;
+struct tcp_options_received;
 
 /*
  * Pointers to address related TCP functions
@@ -40,7 +41,8 @@ struct inet_connection_sock_af_ops {
 	void	    (*send_check)(struct sock *sk, struct sk_buff *skb);
 	int	    (*rebuild_header)(struct sock *sk);
 	void	    (*sk_rx_dst_set)(struct sock *sk, const struct sk_buff *skb);
-	int	    (*conn_request)(struct sock *sk, struct sk_buff *skb);
+	int	    (*conn_request)(struct sock *sk, struct sk_buff *skb,
+				    struct request_sock_ops *ops, void *init_data);
 	struct sock *(*syn_recv_sock)(struct sock *sk, struct sk_buff *skb,
 				      struct request_sock *req,
 				      struct dst_entry *dst);

include/net/mptcp.h
@@ -724,8 +724,9 @@ void mptcp_send_fin(struct sock *meta_sk);
 void mptcp_send_active_reset(struct sock *meta_sk, gfp_t priority);
 bool mptcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 		     int push_one, gfp_t gfp);
+void tcp_parse_mptcp_options(const struct sk_buff *skb,
+			     struct mptcp_options_received *mopt);
 void mptcp_parse_options(const uint8_t *ptr, int opsize,
-			 struct tcp_options_received *opt_rx,
 			 struct mptcp_options_received *mopt,
 			 const struct sk_buff *skb);
 void mptcp_syn_options(struct sock *sk, struct tcp_out_options *opts,
@@ -795,10 +796,9 @@ void mptcp_hash_remove(struct tcp_sock *meta_tp);
 struct sock *mptcp_hash_find(struct net *net, u32 token);
 int mptcp_lookup_join(struct sk_buff *skb, struct inet_timewait_sock *tw);
 int mptcp_do_join_short(struct sk_buff *skb, struct mptcp_options_received *mopt,
-			struct tcp_options_received *tmp_opt, struct net *net);
+			struct net *net);
 void mptcp_reqsk_destructor(struct request_sock *req);
 void mptcp_reqsk_new_mptcp(struct request_sock *req,
-			   const struct tcp_options_received *rx_opt,
 			   const struct mptcp_options_received *mopt,
 			   const struct sk_buff *skb);
 int mptcp_check_req(struct sk_buff *skb, struct net *net);
@@ -808,6 +808,10 @@ int mptcp_sub_len_remove_addr_align(u16 bitfield);
 void mptcp_remove_shortcuts(const struct mptcp_cb *mpcb,
 			    const struct sk_buff *skb);
 void mptcp_init_buffer_space(struct sock *sk);
+void mptcp_reqsk_init(struct request_sock *req,
+		      struct sk_buff *skb, void *init_data);
+int mptcp_conn_request(struct sock *sk, struct sk_buff *skb,
+		       struct request_sock_ops *ops, void *init_data);
 
 /* MPTCP-path-manager registration/initialization functions */
 int mptcp_register_path_manager(struct mptcp_pm_ops *pm);
@@ -1318,7 +1322,6 @@ static inline void mptcp_sub_close(struct sock *sk, unsigned long delay) {}
 static inline void mptcp_set_rto(const struct sock *sk) {}
 static inline void mptcp_send_fin(const struct sock *meta_sk) {}
 static inline void mptcp_parse_options(const uint8_t *ptr, const int opsize,
-				       const struct tcp_options_received *opt_rx,
 				       const struct mptcp_options_received *mopt,
 				       const struct sk_buff *skb) {}
 static inline void mptcp_syn_options(struct sock *sk,

include/net/mptcp_v6.h
@@ -38,9 +38,10 @@
 extern struct request_sock_ops mptcp6_request_sock_ops;
 extern struct proto mptcpv6_prot;
 extern const struct inet_connection_sock_af_ops mptcp_v6_specific;
-extern const struct inet_connection_sock_af_ops mptcp_v6_mapped;
 
 #ifdef CONFIG_MPTCP
+
+extern const struct inet_connection_sock_af_ops mptcp_v6_mapped;
 
 int mptcp_v6_do_rcv(struct sock *meta_sk, struct sk_buff *skb);
 struct sock *mptcp_v6_search_req(const __be16 rport, const struct in6_addr *raddr,
@@ -58,6 +59,8 @@ u64 mptcp_v6_get_key(const __be32 *saddr, const __be32 *daddr,
 		     __be16 sport, __be16 dport);
 
 #else /* CONFIG_MPTCP */
+
+#define mptcp_v6_mapped ipv6_mapped
 
 static inline int mptcp_v6_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 {

include/net/request_sock.h
@@ -41,6 +41,8 @@ struct request_sock_ops {
 	void		(*destructor)(struct request_sock *req);
 	void		(*syn_ack_timeout)(struct sock *sk,
 					   struct request_sock *req);
+	void		(*init)(struct request_sock *req, struct sk_buff *skb,
+				void *init_data);
 };
 
 int inet_rtx_syn_ack(struct sock *parent, struct request_sock *req);

include/net/tcp.h
@@ -455,7 +455,8 @@ void skb_clone_fraglist(struct sk_buff *skb);
 void copy_skb_header(struct sk_buff *new, const struct sk_buff *old);
 
 void inet_twsk_free(struct inet_timewait_sock *tw);
-int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb);
+int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb,
+			struct request_sock_ops *ops, void *init_data);
 /* These states need RST on ABORT according to RFC793 */
 static inline bool tcp_need_reset(int state)
 {
@@ -577,7 +578,8 @@ const u8 *tcp_parse_md5sig_option(const struct tcphdr *th);
  */
 
 void tcp_v4_send_check(struct sock *sk, struct sk_buff *skb);
-int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb);
+int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
+			struct request_sock_ops *ops, void *init_data);
 struct sock *tcp_create_openreq_child(struct sock *sk,
 				      struct request_sock *req,
 				      struct sk_buff *skb);

net/dccp/dccp.h
@@ -268,7 +268,8 @@ void dccp_done(struct sock *sk);
 int dccp_reqsk_init(struct request_sock *rq, struct dccp_sock const *dp,
 		    struct sk_buff const *skb);
 
-int dccp_v4_conn_request(struct sock *sk, struct sk_buff *skb);
+int dccp_v4_conn_request(struct sock *sk, struct sk_buff *skb,
+			 struct request_sock_ops *ops, void *init_data);
 
 struct sock *dccp_create_openreq_child(struct sock *sk,
 				       const struct request_sock *req,

net/dccp/input.c
@@ -604,7 +604,9 @@ int dccp_rcv_state_process(struct sock *sk, struct sk_buff *skb,
 	if (sk->sk_state == DCCP_LISTEN) {
 		if (dh->dccph_type == DCCP_PKT_REQUEST) {
 			if (inet_csk(sk)->icsk_af_ops->conn_request(sk,
-								    skb) < 0)
+								    skb,
+								    NULL,
+								    NULL) < 0)
 				return 1;
 			goto discard;
 		}

commit 8ca20021db930f9e3d57178acedc3c5e1b5799b9

mptcp: Handle a full accept-queue correctly
In MPTCP, if the accept-queue is full, a call to tcp_syn_flood_action is
done. We need to do the same.

If it is a MP_JOIN, we will be redirected to mptcp_do_join_short. Later
there will be the check for the accept-queue.

Fixes: 5a5ff876a64ed (mptcp: add mptcp specific conn_request function)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ctrl.c
@@ -2148,16 +2148,16 @@ int mptcp_conn_request(struct sock *sk, struct sk_buff *skb)
 {
 	struct mptcp_options_received mopt;
 	struct tcp_sock *tp = tcp_sk(sk);
-#ifdef CONFIG_SYN_COOKIES
 	__u32 isn = TCP_SKB_CB(skb)->when;
-#endif
 	bool want_cookie = false;
 
-#ifdef CONFIG_SYN_COOKIES
 	if ((sysctl_tcp_syncookies == 2 ||
-	     inet_csk_reqsk_queue_is_full(sk)) && !isn)
-		want_cookie = sysctl_tcp_syncookies;
-#endif
+	     inet_csk_reqsk_queue_is_full(sk)) && !isn) {
+		want_cookie = tcp_syn_flood_action(sk, skb,
+						   mptcp_request_sock_ops.slab_name);
+		if (!want_cookie)
+			goto drop;
+	}
 
 	mptcp_init_mp_opt(&mopt);
 	tcp_parse_mptcp_options(skb, &mopt);