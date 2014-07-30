multipath-tcp/mptcp

commit 0629ea35ab8eded3c619260fd5ab7852d464a01e

4-way handshake during subflow establishment
Implemented the 4-way handshake in order to support a reliable
transmission of the 3rd ack containing the initiator's MAC.

Signed-off-by: Stefan Sicleru <stefan.g.sicleru@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_output.c
@@ -43,6 +43,12 @@ static int mptcp_is_available(struct sock *sk, struct sk_buff *skb)
 	if (!mptcp_sk_can_send(sk))
 		return 0;
 
+	/* We do not send data on this subflow unless it is
+	 * fully established, i.e. the 4th ack has been received.
+	 */
+	if (tp->mptcp->pre_established)
+		return 0;
+
 	if (tp->pf || (tp->mpcb->noneligible & mptcp_pi_to_flag(tp->mptcp->path_index)) ||
 	    inet_csk(sk)->icsk_ca_state == TCP_CA_Loss)
 		return 0;
@@ -1568,4 +1574,92 @@ void mptcp_send_reset(struct sock *sk, struct sk_buff *skb)
 #endif
 
 	mptcp_sub_force_close(sk);
+}
+
+void mptcp_init_ack_timer(struct sock *sk)
+{
+	struct tcp_sock *tp = tcp_sk(sk);
+
+	setup_timer(&tp->mptcp->mptcp_ack_timer, mptcp_ack_handler,
+			(unsigned long)sk);
+}
+
+static int mptcp_retransmit_skb(struct sock *sk, struct sk_buff *skb)
+{
+	struct tcp_sock *tp = tcp_sk(sk);
+
+	if (inet_csk(sk)->icsk_af_ops->rebuild_header(sk))
+		return -EHOSTUNREACH; /* Routing failure or similar */
+
+	TCP_SKB_CB(skb)->when = tcp_time_stamp;
+	mptcp_include_mpc(tp);
+
+	return tcp_transmit_skb(sk, skb, 1, GFP_ATOMIC);
+}
+
+void mptcp_ack_retransmit_timer(struct sock *sk)
+{
+	struct sk_buff *buff;
+	struct tcp_sock *tp = tcp_sk(sk);
+	struct inet_connection_sock *icsk = inet_csk(sk);
+	int err;
+
+	buff = alloc_skb(MAX_TCP_HEADER, GFP_ATOMIC);
+	if (buff == NULL) {
+		sk_reset_timer(sk, &tp->mptcp->mptcp_ack_timer,
+			       jiffies + icsk->icsk_rto);
+		return;
+	}
+
+	/* Reserve space for headers and prepare control bits */
+	skb_reserve(buff, MAX_TCP_HEADER);
+	tcp_init_nondata_skb(buff, tp->snd_nxt, TCPHDR_ACK);
+	TCP_SKB_CB(buff)->when = tcp_time_stamp;
+
+	icsk->icsk_retransmits++;
+	err = mptcp_retransmit_skb(sk, buff);
+
+	if (err > 0) {
+		/* Retransmission failed because of local congestion,
+		 * do not backoff. */
+		if (!icsk->icsk_retransmits)
+			icsk->icsk_retransmits = 1;
+		sk_reset_timer(sk, &tp->mptcp->mptcp_ack_timer,
+			       jiffies + icsk->icsk_rto);
+		return;
+	}
+
+	if (icsk->icsk_retransmits == sysctl_tcp_retries1 + 1) {
+		sk_stop_timer(sk, &tp->mptcp->mptcp_ack_timer);
+		tcp_send_active_reset(sk, GFP_ATOMIC);
+		mptcp_sub_force_close(sk);
+		kfree_skb(buff);
+		return;
+	}
+
+	icsk->icsk_rto = min(icsk->icsk_rto << 1, TCP_RTO_MAX);
+	sk_reset_timer(sk, &tp->mptcp->mptcp_ack_timer,
+		       jiffies + icsk->icsk_rto);
+}
+
+void mptcp_ack_handler(unsigned long data)
+{
+	struct sock *sk = (struct sock *)data;
+	struct sock *meta_sk = mptcp_meta_sk(sk);
+
+	bh_lock_sock(meta_sk);
+	if (sock_owned_by_user(meta_sk)) {
+		/* Try again later */
+		sk_reset_timer(sk, &tcp_sk(sk)->mptcp->mptcp_ack_timer,
+			       jiffies + (HZ / 20));
+		goto out_unlock;
+	}
+
+	mptcp_ack_retransmit_timer(sk);
+
+	sk_mem_reclaim(sk);
+
+out_unlock:
+	bh_unlock_sock(meta_sk);
+	sock_put(sk);
 }

include/net/mptcp.h
@@ -112,7 +112,10 @@ struct mptcp_tcp_sock {
 		mapping_present:1,
 		map_data_fin:1,
 		low_prio:1, /* use this socket as backup */
-		send_mp_prio:1; /* Trigger to send mp_prio on this socket */
+		send_mp_prio:1, /* Trigger to send mp_prio on this socket */
+		pre_established:1; /* State between sending 3rd ACK and receiving
+		 	 	    * the fourth ack of new subflows.
+		 	 	    */
 
 	/* isn: needed to translate abs to relative subflow seqnums */
 	u32	snt_isn;
@@ -133,6 +136,9 @@ struct mptcp_tcp_sock {
 	struct delayed_work work;
 	u32	mptcp_loc_nonce;
 	struct tcp_sock *tp; /* Where is my daddy? */
+
+	/* MP_JOIN subflow: timer for retransmitting the 3rd ack */
+	struct timer_list mptcp_ack_timer;
 };
 
 struct multipath_options {
@@ -622,6 +628,8 @@ struct sock *mptcp_select_ack_sock(const struct sock *meta_sk, int copied);
 void mptcp_destroy_meta_sk(struct sock *meta_sk);
 int mptcp_backlog_rcv(struct sock *meta_sk, struct sk_buff *skb);
 struct sock *mptcp_sk_clone(struct sock *sk, int family, const gfp_t priority);
+void mptcp_init_ack_timer(struct sock *sk);
+void mptcp_ack_handler(unsigned long);
 
 static inline void mptcp_push_pending_frames(struct sock *meta_sk)
 {

net/ipv4/tcp_input.c
@@ -5754,6 +5754,11 @@ static int tcp_rcv_synsent_state_process(struct sock *sk, struct sk_buff *skb,
 				tp->mptcp->teardown = 1;
 				goto reset_and_undo;
 			}
+
+			/* Set this flag in order to postpone data sending
+			 * until the 4th ack arrives.
+			 */
+			tp->mptcp->pre_established = 1;
 		} else if (tp->rx_opt.saw_mpc && tp->request_mptcp) {
 			if (mptcp_create_master_sk(sk, mopt.mptcp_rem_key,
 						   ntohs(th->window)))
@@ -5808,7 +5813,17 @@ static int tcp_rcv_synsent_state_process(struct sock *sk, struct sk_buff *skb,
 		 * move to established.
 		 */
 #ifdef CONFIG_MPTCP
-		tp->rx_opt.rcv_isn = TCP_SKB_CB(skb)->seq;
+		if (tp->mpc) {
+			tp->rx_opt.rcv_isn = TCP_SKB_CB(skb)->seq;
+
+			if (!is_master_tp(tp))
+				/* Timer for repeating the ACK until an answer
+				 * arrives. Used only when establishing an additional
+				 * subflow inside of an MPTCP connection.
+				 */
+				sk_reset_timer(sk, &tp->mptcp->mptcp_ack_timer,
+					       jiffies + icsk->icsk_rto);
+		}
 #endif
 		tp->rcv_nxt = TCP_SKB_CB(skb)->seq + 1;
 		tp->rcv_wup = TCP_SKB_CB(skb)->seq + 1;
@@ -6107,6 +6122,13 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb,
 				smp_mb();
 				tcp_set_state(sk, TCP_ESTABLISHED);
 				sk->sk_state_change(sk);
+
+				/* Send an ACK when establishing a new
+				 * MPTCP subflow, i.e. using an MP_JOIN
+				 * subtype.
+				 */
+				if (tp->mpc && !is_master_tp(tp))
+					tcp_send_ack(sk);
 
 				/* Note, that this wakeup is only for marginal
 				 * crossed SYN case. Passively open sockets

net/mptcp/mptcp_ctrl.c
@@ -864,6 +864,11 @@ void mptcp_del_sock(struct sock *sk)
 	if (!tp->mpc || !tp->mptcp->attached)
 		return;
 
+	if (tp->mptcp->pre_established) {
+		tp->mptcp->pre_established = 0;
+		sk_stop_timer(sk, &tp->mptcp->mptcp_ack_timer);
+	}
+
 	mpcb = tp->mpcb;
 	tp_prev = mpcb->connection_list;
 

net/mptcp/mptcp_input.c
@@ -970,6 +970,11 @@ int mptcp_data_ack(struct sock *sk, const struct sk_buff *skb)
 		 */
 		mptcp_become_fully_estab(sk);
 
+	if (tp->mptcp->pre_established) {
+		tp->mptcp->pre_established = 0;
+		sk_stop_timer(sk, &tp->mptcp->mptcp_ack_timer);
+	}
+
 	/* Get the data_seq */
 	if (mptcp_is_data_seq(skb)) {
 		mptcp_skb_set_data_seq(skb, &data_seq);

net/mptcp/mptcp_ipv4.c
@@ -454,6 +454,9 @@ void mptcp_init4_subsockets(struct sock *meta_sk, const struct mptcp_loc4 *loc,
 	tp->mptcp->slave_sk = 1;
 	tp->mptcp->low_prio = loc->low_prio;
 
+	/* Initializing the timer for an MPTCP subflow */
+	mptcp_init_ack_timer(sk);
+
 	/** Then, connect the socket to the peer */
 
 	ulid_size = sizeof(struct sockaddr_in);

net/mptcp/mptcp_ipv6.c
@@ -632,6 +632,9 @@ void mptcp_init6_subsockets(struct sock *meta_sk, const struct mptcp_loc6 *loc,
 	tp->mptcp->slave_sk = 1;
 	tp->mptcp->low_prio = loc->low_prio;
 
+	/* Initializing the timer for an MPTCP subflow */
+	mptcp_init_ack_timer(sk);
+
 	/** Then, connect the socket to the peer */
 
 	ulid_size = sizeof(struct sockaddr_in6);

commit f4d7587fc0733189f6886e53354310391ae91923

mptcp: Do not retransmit third ack if we are no more pre-established
It may be that the timer fires, and just then we receive the fourth ack.
In this case, mptcp_ack_handler will wait on the meta-lock until the
fourth ack has been processed and set pre_established to 0.

As we get the lock in mptcp_ack_handler, we will then assume that we
still need to retransmit the third ack. We should not. So, we add a
check for pre_established in the mptcp_ack_handler.

Fixes: 0629ea35ab8ed (4-way handshake during subflow establishment)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_output.c
@@ -1310,6 +1310,8 @@ void mptcp_ack_handler(unsigned long data)
 
 	if (sk->sk_state == TCP_CLOSE)
 		goto out_unlock;
+	if (!tcp_sk(sk)->mptcp->pre_established)
+		goto out_unlock;
 
 	mptcp_ack_retransmit_timer(sk);
 