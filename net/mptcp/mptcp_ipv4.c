multipath-tcp/mptcp

commit 75ec81a35c2e0f4b921590fcdc62703b6f1ca636

mptcp: Fix Bug #41: Handle missing entries in the fullmesh-path-manager
It may be that the address of the initial subflow is not yet part of the
fullmesh path-manager's list of addresses. May happen because the
path-manager waits 500ms before adding the address.

In general, we do not handle a missing address in the path-manager's
list not correctly. We always assume that it is there.

Change everything, so that get_local_id returns -1 if the address is not
part of the fullmesh list. In case -1 is returned on a MP_JOIN, we
simply ignore the SYN.

In case -1 is returned on a MP_CAPABLE, we disable the path-manager and
fall back to the 'default'-path-management, which basically does
nothing.

Fixes: Bug #41 (https://github.com/multipath-tcp/mptcp/issues/41)
Fixes: c31d21d039d80 (Modularize the path-manager)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ipv4.c
@@ -226,6 +226,8 @@ static void mptcp_v4_join_request(struct sock *meta_sk, struct sk_buff *skb)
 
 	addr.ip = ireq->ir_loc_addr;
 	mtreq->loc_id = mpcb->pm_ops->get_local_id(AF_INET, &addr, sock_net(meta_sk));
+	if (mtreq->loc_id == -1) /* Address not part of the allowed ones */
+		goto drop_and_release;
 	mtreq->rem_id = mopt.rem_id;
 	mtreq->low_prio = mopt.low_prio;
 	tcp_rsk(req)->saw_mpc = 1;

net/mptcp/mptcp_ipv6.c
@@ -444,6 +444,8 @@ static void mptcp_v6_join_request(struct sock *meta_sk, struct sk_buff *skb)
 
 	addr.in6 = treq->ir_v6_loc_addr;
 	mtreq->loc_id = mpcb->pm_ops->get_local_id(AF_INET6, &addr, sock_net(meta_sk));
+	if (mtreq->loc_id == -1) /* Address not part of the allowed ones */
+		goto drop_and_release;
 	mtreq->rem_id = mopt.rem_id;
 	mtreq->low_prio = mopt.low_prio;
 	tcp_rsk(req)->saw_mpc = 1;

include/net/mptcp.h
@@ -222,7 +222,7 @@ struct mptcp_pm_ops {
 	struct list_head list;
 
 	/* Signal the creation of a new MPTCP-session. */
-	void (*new_session)(struct sock *meta_sk, u8 id);
+	void (*new_session)(struct sock *meta_sk, int id);
 	void (*release_sock)(struct sock *meta_sk);
 	void (*fully_established)(struct sock *meta_sk);
 	void (*new_remote_address)(struct sock *meta_sk);
@@ -821,6 +821,7 @@ int mptcp_register_path_manager(struct mptcp_pm_ops *pm);
 void mptcp_unregister_path_manager(struct mptcp_pm_ops *pm);
 void mptcp_init_path_manager(struct mptcp_cb *mpcb);
 void mptcp_cleanup_path_manager(struct mptcp_cb *mpcb);
+void mptcp_fallback_default(struct mptcp_cb *mpcb);
 void mptcp_get_default_path_manager(char *name);
 int mptcp_set_default_path_manager(const char *name);
 extern struct mptcp_pm_ops mptcp_pm_default;

net/mptcp/mptcp_ctrl.c
@@ -1261,7 +1261,7 @@ void mptcp_update_metasocket(struct sock *sk, struct sock *meta_sk)
 {
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	union inet_addr addr;
-	u8 id;
+	int id;
 
 	/* Get the local address-id */
 	if (sk->sk_family == AF_INET || mptcp_v6_is_v4_mapped(sk)) {
@@ -1276,11 +1276,13 @@ void mptcp_update_metasocket(struct sock *sk, struct sock *meta_sk)
 		mptcp_v4_add_raddress(mpcb,
 				      (struct in_addr *)&inet_sk(sk)->inet_daddr,
 				      0, 0);
-		mptcp_v4_set_init_addr_bit(mpcb, inet_sk(sk)->inet_daddr, id);
+		if (id >= 0)
+			mptcp_v4_set_init_addr_bit(mpcb, inet_sk(sk)->inet_daddr, id);
 	} else {
 #if IS_ENABLED(CONFIG_IPV6)
 		mptcp_v6_add_raddress(mpcb, &sk->sk_v6_daddr, 0, 0);
-		mptcp_v6_set_init_addr_bit(mpcb, &sk->sk_v6_daddr, id);
+		if (id >= 0)
+			mptcp_v6_set_init_addr_bit(mpcb, &sk->sk_v6_daddr, id);
 #endif
 	}
 

net/mptcp/mptcp_fullmesh.c
@@ -59,6 +59,8 @@ struct mptcp_fm_ns {
 	struct net *net;
 };
 
+static struct mptcp_pm_ops full_mesh __read_mostly;
+
 static struct mptcp_fm_ns *fm_get_ns(struct net *net)
 {
 	return (struct mptcp_fm_ns *)net->mptcp.path_managers[MPTCP_PM_FULLMESH];
@@ -450,6 +452,10 @@ duno:
 			    mpcb->send_infinite_mapping)
 				goto next;
 
+			/* May be that the pm has changed in-between */
+			if (mpcb->pm_ops != &full_mesh)
+				goto next;
+
 			if (sock_owned_by_user(meta_sk)) {
 				if (!test_and_set_bit(MPTCP_PATH_MANAGER,
 						      &meta_tp->tsq_flags))
@@ -827,7 +833,7 @@ static struct notifier_block mptcp_pm_netdev_notifier = {
 		.notifier_call = netdev_event,
 };
 
-static void full_mesh_new_session(struct sock *meta_sk, u8 id)
+static void full_mesh_new_session(struct sock *meta_sk, int id)
 {
 	struct mptcp_loc_addr *mptcp_local;
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
@@ -836,6 +842,11 @@ static void full_mesh_new_session(struct sock *meta_sk, u8 id)
 	struct mptcp_fm_ns *fm_ns = fm_get_ns(net);
 	struct sock *sk;
 	int i;
+
+	if (id == -1) {
+		mptcp_fallback_default(mpcb);
+		return;
+	}
 
 	/* Initialize workqueue-struct */
 	INIT_WORK(&fmp->subflow_work, create_subflow_worker);
@@ -1041,7 +1052,7 @@ static int full_mesh_get_local_id(sa_family_t family, union inet_addr *addr,
 {
 	struct mptcp_loc_addr *mptcp_local;
 	struct mptcp_fm_ns *fm_ns = fm_get_ns(net);
-	int id = 0, i;
+	int id = -1, i;
 
 	/* Handle the backup-flows */
 	rcu_read_lock();

net/mptcp/mptcp_ndiffports.c
@@ -82,7 +82,7 @@ exit:
 	sock_put(meta_sk);
 }
 
-static void ndiffports_new_session(struct sock *meta_sk, u8 id)
+static void ndiffports_new_session(struct sock *meta_sk, int id)
 {
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct ndiffports_priv *fmp = (struct ndiffports_priv *)&mpcb->mptcp_pm[0];

net/mptcp/mptcp_pm.c
@@ -147,6 +147,19 @@ void mptcp_cleanup_path_manager(struct mptcp_cb *mpcb)
 	module_put(mpcb->pm_ops->owner);
 }
 
+/* Fallback to the default path-manager. */
+void mptcp_fallback_default(struct mptcp_cb *mpcb)
+{
+	struct mptcp_pm_ops *pm;
+
+	mptcp_cleanup_path_manager(mpcb);
+	pm = mptcp_pm_find("default");
+
+	/* Cannot fail - it's the default module */
+	try_module_get(pm->owner);
+	mpcb->pm_ops = pm;
+}
+
 /* Set default value from kernel configuration at bootup */
 static int __init mptcp_path_manager_default(void)
 {

commit dc19e4bceb80456e220d6cc1455bf1394226538a

mptcp: Handle get_local_id returning -1 correctly
mtreq->loc_id is a u8, so it can never be equal to -1. We must store the
result of get_local_id in a temporary local signed integer to handle the
-1 case.

Fixes: 75ec81a35c2e0 (mptcp: Fix Bug #41: Handle missing entries in the fullmesh-path-manager)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ipv4.c
@@ -92,6 +92,7 @@ static int mptcp_v4_join_init_req(struct request_sock *req, struct sock *sk,
 	struct mptcp_request_sock *mtreq = mptcp_rsk(req);
 	struct mptcp_cb *mpcb = tcp_sk(sk)->mpcb;
 	union inet_addr addr;
+	int loc_id;
 
 	tcp_request_sock_ipv4_ops.init_req(req, sk, skb);
 
@@ -100,9 +101,10 @@ static int mptcp_v4_join_init_req(struct request_sock *req, struct sock *sk,
 						    tcp_hdr(skb)->source,
 						    tcp_hdr(skb)->dest);
 	addr.ip = inet_rsk(req)->ir_loc_addr;
-	mtreq->loc_id = mpcb->pm_ops->get_local_id(AF_INET, &addr, sock_net(sk));
-	if (mtreq->loc_id == -1)
+	loc_id = mpcb->pm_ops->get_local_id(AF_INET, &addr, sock_net(sk));
+	if (loc_id == -1)
 		return -1;
+	mtreq->loc_id = loc_id;
 
 	mptcp_join_reqsk_init(mpcb, req, skb);
 

net/mptcp/mptcp_ipv6.c
@@ -122,6 +122,7 @@ static int mptcp_v6_join_init_req(struct request_sock *req, struct sock *sk,
 	struct mptcp_request_sock *mtreq = mptcp_rsk(req);
 	struct mptcp_cb *mpcb = tcp_sk(sk)->mpcb;
 	union inet_addr addr;
+	int loc_id;
 
 	tcp_request_sock_ipv6_ops.init_req(req, sk, skb);
 
@@ -130,9 +131,10 @@ static int mptcp_v6_join_init_req(struct request_sock *req, struct sock *sk,
 						    tcp_hdr(skb)->source,
 						    tcp_hdr(skb)->dest);
 	addr.in6 = inet_rsk(req)->ir_v6_loc_addr;
-	mtreq->loc_id = mpcb->pm_ops->get_local_id(AF_INET6, &addr, sock_net(sk));
-	if (mtreq->loc_id == -1)
+	loc_id = mpcb->pm_ops->get_local_id(AF_INET6, &addr, sock_net(sk));
+	if (loc_id == -1)
 		return -1;
+	mtreq->loc_id = loc_id;
 
 	mptcp_join_reqsk_init(mpcb, req, skb);
 