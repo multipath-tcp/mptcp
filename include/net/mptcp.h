multipath-tcp/mptcp

commit 2292b7ee133d19af43d427a909702bcfde2af53d

mptcp: Remove pending request-sockets
In MPTCP, we do not handle retransmissions of SYN/ACK+MP_JOIN, because
in regular TCP this (ab)uses the keepalive timer. So, for MPTCP we
decided to not retransmit these SYN/ACKs, because the reliable third ack
will ensure that SYN/ACK+MP_JOINs reach us.
We did this, because otherwise we cannot use TCP-keepalive in MPTCP, as
the same timer is used for multiple purposes.

However, by doing this, when a request-socket is created it will never
ever be removed from the request-socket queue if the 3-way handshake
never completes. We need to handle this correctly.

Fixes: ba1582e13 (mptcp: Correct support of TCP-Keepalive)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/mptcp.h
@@ -332,6 +332,9 @@ struct mptcp_cb {
 	int orig_sk_rcvbuf;
 	int orig_sk_sndbuf;
 	u32 orig_window_clamp;
+
+	/* Timer for retransmitting SYN/ACK+MP_JOIN */
+	struct timer_list synack_timer;
 };
 
 #define MPTCP_SUB_CAPABLE			0
@@ -850,6 +853,18 @@ void mptcp_cleanup_scheduler(struct mptcp_cb *mpcb);
 void mptcp_get_default_scheduler(char *name);
 int mptcp_set_default_scheduler(const char *name);
 extern struct mptcp_sched_ops mptcp_sched_default;
+
+static inline void mptcp_reset_synack_timer(struct sock *meta_sk,
+					    unsigned long len)
+{
+	sk_reset_timer(meta_sk, &tcp_sk(meta_sk)->mpcb->synack_timer,
+		       jiffies + len);
+}
+
+static inline void mptcp_delete_synack_timer(struct sock *meta_sk)
+{
+	sk_stop_timer(meta_sk, &tcp_sk(meta_sk)->mpcb->synack_timer);
+}
 
 static inline bool is_mptcp_enabled(const struct sock *sk)
 {

net/ipv4/inet_connection_sock.c
@@ -651,7 +651,7 @@ void inet_csk_reqsk_queue_prune(struct sock *parent,
 
 	lopt->clock_hand = i;
 
-	if (lopt->qlen)
+	if (lopt->qlen && !is_meta_sk(parent))
 		inet_csk_reset_keepalive_timer(parent, interval);
 }
 EXPORT_SYMBOL_GPL(inet_csk_reqsk_queue_prune);

net/ipv4/tcp_minisocks.c
@@ -816,7 +816,8 @@ embryonic_reset:
 			 * avoid ending up in inet_csk_reqsk_queue_removed ...
 			 */
 			inet_csk_reqsk_queue_unlink(sk, req, prev);
-			reqsk_queue_removed(&inet_csk(sk)->icsk_accept_queue, req);
+			if (reqsk_queue_removed(&inet_csk(sk)->icsk_accept_queue, req) == 0)
+				mptcp_delete_synack_timer(sk);
 			reqsk_free(req);
 		} else {
 			inet_csk_reqsk_queue_drop(sk, req, prev);

net/mptcp/mptcp_ctrl.c
@@ -539,6 +539,8 @@ void mptcp_destroy_sock(struct sock *sk)
 
 			mptcp_sub_close(sk_it, 0);
 		}
+
+		mptcp_delete_synack_timer(sk);
 	} else {
 		mptcp_del_sock(sk);
 	}
@@ -901,6 +903,36 @@ static int mptcp_inherit_sk(const struct sock *sk, struct sock *newsk,
 	return 0;
 }
 
+static void mptcp_synack_timer_handler(unsigned long data)
+{
+	struct sock *meta_sk = (struct sock *) data;
+	struct listen_sock *lopt = inet_csk(meta_sk)->icsk_accept_queue.listen_opt;
+
+	/* Only process if socket is not in use. */
+	bh_lock_sock(meta_sk);
+
+	if (sock_owned_by_user(meta_sk)) {
+		/* Try again later. */
+		mptcp_reset_synack_timer(meta_sk, HZ/20);
+		goto out;
+	}
+
+	/* May happen if the queue got destructed in mptcp_close */
+	if (!lopt)
+		goto out;
+
+	inet_csk_reqsk_queue_prune(meta_sk, TCP_SYNQ_INTERVAL,
+				   TCP_TIMEOUT_INIT, TCP_RTO_MAX);
+
+	if (lopt->qlen)
+		mptcp_reset_synack_timer(meta_sk, TCP_SYNQ_INTERVAL);
+
+out:
+	bh_unlock_sock(meta_sk);
+	sock_put(meta_sk);
+}
+
+
 static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key, u32 window)
 {
 	struct mptcp_cb *mpcb;
@@ -1101,6 +1133,9 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key, u32 window)
 
 	mptcp_init_path_manager(mpcb);
 	mptcp_init_scheduler(mpcb);
+
+	setup_timer(&mpcb->synack_timer, mptcp_synack_timer_handler,
+		    (unsigned long)meta_sk);
 
 	mptcp_debug("%s: created mpcb with token %#x\n",
 		    __func__, mpcb->mptcp_loc_token);
@@ -1710,6 +1745,8 @@ void mptcp_disconnect(struct sock *sk)
 {
 	struct sock *subsk, *tmpsk;
 	struct tcp_sock *tp = tcp_sk(sk);
+
+	mptcp_delete_synack_timer(sk);
 
 	__skb_queue_purge(&tp->mpcb->reinject_queue);
 

net/mptcp/mptcp_ipv4.c
@@ -143,7 +143,8 @@ static void mptcp_v4_reqsk_queue_hash_add(struct sock *meta_sk,
 				     lopt->hash_rnd, lopt->nr_table_entries);
 
 	reqsk_queue_hash_req(&meta_icsk->icsk_accept_queue, h2, req, timeout);
-	reqsk_queue_added(&meta_icsk->icsk_accept_queue);
+	if (reqsk_queue_added(&meta_icsk->icsk_accept_queue) == 0)
+		mptcp_reset_synack_timer(meta_sk, timeout);
 
 	rcu_read_lock();
 	spin_lock(&mptcp_reqsk_hlock);

net/mptcp/mptcp_ipv6.c
@@ -174,7 +174,8 @@ static void mptcp_v6_reqsk_queue_hash_add(struct sock *meta_sk,
 				      lopt->hash_rnd, lopt->nr_table_entries);
 
 	reqsk_queue_hash_req(&meta_icsk->icsk_accept_queue, h2, req, timeout);
-	reqsk_queue_added(&meta_icsk->icsk_accept_queue);
+	if (reqsk_queue_added(&meta_icsk->icsk_accept_queue) == 0)
+		mptcp_reset_synack_timer(meta_sk, timeout);
 
 	rcu_read_lock();
 	spin_lock(&mptcp_reqsk_hlock);

commit 9d2e8597f8e2e270cbdadc13a5b8f6622e29c4f8

mptcp: Build-fix
net/ipv4/tcp_minisocks.c: In function ‘tcp_check_req’:
net/ipv4/tcp_minisocks.c:820:5: error: implicit declaration of function ‘mptcp_delete_synack_timer’ [-Werror=implicit-function-declaration]
cc1: some warnings being treated as errors
make[3]: *** [net/ipv4/tcp_minisocks.o] Error 1
make[2]: *** [net/ipv4] Error 2
make[1]: *** [net] Error 2
make[1]: *** Waiting for unfinished jobs....
make: *** [sub-make] Error 2

Fixes: 2292b7ee133d19 (mptcp: Remove pending request-sockets)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

include/net/mptcp.h
@@ -1503,6 +1503,7 @@ static inline void mptcp_reqsk_new_mptcp(struct request_sock *req,
 					 const struct sk_buff *skb) {}
 static inline void mptcp_remove_shortcuts(const struct mptcp_cb *mpcb,
 					  const struct sk_buff *skb) {}
+static inline void mptcp_delete_synack_timer(struct sock *meta_sk) {}
 #endif /* CONFIG_MPTCP */
 
 #endif /* _MPTCP_H */