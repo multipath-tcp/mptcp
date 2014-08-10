multipath-tcp/mptcp

commit eabc34bfa1d3b32fc7081f9bc6da1621c28feaff

Make the tk-table rcu-style
That way, read-access to the tk-table is lock-less. This is necessary,
because the address-event handler takes first the tk-table-lock and then
the lock-sock although mptcp_alloc_mpcb is holding the lock-sock before
accessing the tk-table.

Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ctrl.c
@@ -691,13 +691,17 @@ int mptcp_alloc_mpcb(struct sock *master_sk, __u64 remote_key, u32 window)
 	meta_icsk->icsk_rto *= 2; /* Double of master - rto */
 	tcp_init_xmit_timers(meta_sk);
 
-	/* Adding the mpcb in the token hashtable */
+	/* Adding the meta_tp in the token hashtable */
+	rcu_read_lock();
 	spin_lock(&mptcp_tk_hashlock);
 	__mptcp_hash_insert(meta_tp, mpcb->mptcp_loc_token);
 	/* This is necessary, because the master has been added in tcp_connect_init */
-	if (!list_empty(&master_tp->tk_table))
-		list_del_init(&master_tp->tk_table);
+	if (master_tp->inside_tk_table) {
+		hlist_nulls_del_init_rcu(&master_tp->tk_table);
+		master_tp->inside_tk_table = 0;
+	}
 	spin_unlock(&mptcp_tk_hashlock);
+	rcu_read_unlock();
 
 	mptcp_mpcb_inherit_sockopts(meta_sk, master_sk);
 
@@ -1155,7 +1159,7 @@ void mptcp_close(struct sock *meta_sk, long timeout)
 			sock_rps_reset_flow(sk_it);
 	}
 
-	if (!list_empty(&meta_tp->tk_table)) {
+	if (meta_tp->inside_tk_table) {
 		/* Detach the mpcb from the token hashtable */
 		mptcp_hash_remove(meta_tp);
 		reqsk_queue_destroy(&((struct inet_connection_sock *)mpcb)->icsk_accept_queue);
@@ -1355,8 +1359,6 @@ int mptcp_check_req_master(struct sock *sk, struct sock *child,
 	struct mptcp_cb *mpcb;
 	struct mptcp_request_sock *mtreq;
 
-	INIT_LIST_HEAD(&child_tp->tk_table);
-
 	if (!tcp_rsk(req)->saw_mpc)
 		return 1;
 
@@ -1435,7 +1437,7 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk, struct sock *child,
 	struct mptcp_cb *mpcb = mtreq->mpcb;
 	u8 hash_mac_check[20];
 
-	INIT_LIST_HEAD(&child_tp->tk_table);
+	child_tp->inside_tk_table = 0;
 
 	if (!mpcb->rx_opt.join_ack)
 		goto teardown;

net/mptcp/mptcp_ipv4.c
@@ -319,7 +319,7 @@ void mptcp_v4_do_rcv_join_syn(struct sock *meta_sk, struct sk_buff *skb,
 	 * Check for close-state is necessary, because we may have been closed
 	 * without passing by mptcp_close().
 	 */
-	if (meta_sk->sk_state == TCP_CLOSE || list_empty(&mpcb->collide_tk))
+	if (meta_sk->sk_state == TCP_CLOSE || !tcp_sk(meta_sk)->inside_tk_table)
 		goto reset;
 
 	if (mptcp_v4_add_raddress(&mpcb->rx_opt,
@@ -349,7 +349,7 @@ int mptcp_v4_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 	 * Check for close-state is necessary, because we may have been closed
 	 * without passing by mptcp_close().
 	 */
-	if (meta_sk->sk_state == TCP_CLOSE || list_empty(&tcp_sk(meta_sk)->tk_table))
+	if (meta_sk->sk_state == TCP_CLOSE || !tcp_sk(meta_sk)->inside_tk_table)
 		goto reset_and_discard;
 
 	child = tcp_v4_hnd_req(meta_sk, skb);

net/mptcp/mptcp_ipv6.c
@@ -324,7 +324,7 @@ void mptcp_v6_do_rcv_join_syn(struct sock *meta_sk, struct sk_buff *skb,
 	 * Check for close-state is necessary, because we may have been closed
 	 * without passing by mptcp_close().
 	 */
-	if (meta_sk->sk_state == TCP_CLOSE || list_empty(&mpcb->collide_tk))
+	if (meta_sk->sk_state == TCP_CLOSE || !tcp_sk(meta_sk)->inside_tk_table)
 		goto reset;
 
 	if (mptcp_v6_add_raddress(&mpcb->rx_opt,
@@ -354,7 +354,7 @@ int mptcp_v6_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 	 * Check for close-state is necessary, because we may have been closed
 	 * without passing by mptcp_close().
 	 */
-	if (meta_sk->sk_state == TCP_CLOSE || list_empty(&tcp_sk(meta_sk)->tk_table))
+	if (meta_sk->sk_state == TCP_CLOSE || !tcp_sk(meta_sk)->inside_tk_table)
 		goto reset_and_discard;
 
 	child = tcp_v6_hnd_req(meta_sk, skb);

include/linux/tcp.h
@@ -529,6 +529,7 @@ struct tcp_sock {
 	 * rather than adding ifdefs everywhere.
 	 */
 	u8      mpc:1,          /* Other end is multipath capable */
+		inside_tk_table:1, /* Is the tcp_sock inside the token-table? */
 		send_mp_fclose:1,
 		request_mptcp:1, /* Did we send out an MP_CAPABLE?
 				    * (this speeds up mptcp_doit() in tcp_recvmsg)
@@ -542,7 +543,7 @@ struct tcp_sock {
 		was_meta_sk:1; /* This was a meta sk (in case of reuse) */
 	struct mptcp_tcp_sock *mptcp;
 #ifdef CONFIG_MPTCP
-	struct list_head tk_table;
+	struct hlist_nulls_node tk_table;
 	u32		mptcp_loc_token;
 	u64		mptcp_loc_key;
 #endif /* CONFIG_MPTCP */

include/net/mptcp.h
@@ -73,7 +73,7 @@ struct mptcp_request_sock {
 	 * request_sock.
 	 */
 	struct list_head		collide_tuple;
-	struct list_head		collide_tk;
+	struct hlist_nulls_node		collide_tk;
 	u32				mptcp_rem_nonce;
 	u32				mptcp_loc_token;
 	u64				mptcp_loc_key;
@@ -758,17 +758,38 @@ static inline int mptcp_req_sk_saw_mpc(const struct request_sock *req)
 
 static inline void mptcp_hash_request_remove(struct request_sock *req)
 {
-	spin_lock_bh(&mptcp_reqsk_hlock);
+	int in_softirq = 0;
+
+	if (in_softirq()) {
+		spin_lock(&mptcp_reqsk_hlock);
+		in_softirq = 1;
+	} else {
+		spin_lock_bh(&mptcp_reqsk_hlock);
+	}
+
 	list_del(&mptcp_rsk(req)->collide_tuple);
-	spin_unlock_bh(&mptcp_reqsk_hlock);
+
+	if (in_softirq)
+		spin_unlock(&mptcp_reqsk_hlock);
+	else
+		spin_unlock_bh(&mptcp_reqsk_hlock);
 }
 
 static inline void mptcp_reqsk_destructor(struct request_sock *req)
 {
-	if (!mptcp_rsk(req)->mpcb)
-		mptcp_reqsk_remove_tk(req);
-	else
+	if (!mptcp_rsk(req)->mpcb) {
+		if (in_softirq()) {
+			mptcp_reqsk_remove_tk(req);
+		} else {
+			rcu_read_lock_bh();
+			spin_lock(&mptcp_tk_hashlock);
+			hlist_nulls_del_rcu(&mptcp_rsk(req)->collide_tk);
+			spin_unlock(&mptcp_tk_hashlock);
+			rcu_read_unlock_bh();
+		}
+	} else {
 		mptcp_hash_request_remove(req);
+	}
 }
 
 static inline void mptcp_init_mp_opt(struct multipath_options *mopt)

net/ipv4/tcp.c
@@ -2251,7 +2251,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 
 		__skb_queue_purge(&tp->mpcb->reinject_queue);
 
-		if (!list_empty(&tp->tk_table)) {
+		if (tp->inside_tk_table) {
 			mptcp_hash_remove(tp);
 			reqsk_queue_destroy(&((struct inet_connection_sock *)tp->mpcb)->icsk_accept_queue);
 		}
@@ -2280,7 +2280,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 		tp->mpc = 0;
 		tp->mpcb = NULL;
 	} else {
-		if (!list_empty(&tp->tk_table))
+		if (tp->inside_tk_table)
 			mptcp_hash_remove(tp);
 	}
 #endif

net/ipv4/tcp_input.c
@@ -5818,10 +5818,13 @@ static int tcp_rcv_synsent_state_process(struct sock *sk, struct sk_buff *skb,
 		} else {
 			tp->request_mptcp = 0;
 
-			if (!list_empty(&tp->tk_table)) {
+			if (tp->inside_tk_table) {
+				rcu_read_lock();
 				spin_lock(&mptcp_tk_hashlock);
-				list_del_init(&tp->tk_table);
+				hlist_nulls_del_rcu(&tp->tk_table);
+				tp->inside_tk_table = 0;
 				spin_unlock(&mptcp_tk_hashlock);
+				rcu_read_unlock();
 			}
 		}
 		mptcp_include_mpc(tp);

net/ipv4/tcp_ipv4.c
@@ -2083,10 +2083,6 @@ static int tcp_v4_init_sock(struct sock *sk)
 	percpu_counter_inc(&tcp_sockets_allocated);
 	local_bh_enable();
 
-#ifdef CONFIG_MPTCP
-	INIT_LIST_HEAD(&tp->tk_table);
-#endif
-
 	return 0;
 }
 
@@ -2107,10 +2103,13 @@ void tcp_v4_destroy_sock(struct sock *sk)
 		mptcp_purge_ofo_queue(tp);
 	} else {
 		mptcp_del_sock(sk);
-		if (!list_empty(&tp->tk_table)) {
+		if (tp->inside_tk_table) {
+			rcu_read_lock();
 			spin_lock(&mptcp_tk_hashlock);
-			list_del_init(&tp->tk_table);
+			hlist_nulls_del_rcu(&tp->tk_table);
+			tp->inside_tk_table = 0;
 			spin_unlock(&mptcp_tk_hashlock);
+			rcu_read_unlock();
 		}
 		__skb_queue_purge(&tp->out_of_order_queue);
 	}

commit b293f6818ad72256eaf2267601a8e4aa0ed781a9

mptcp: Correctly search for TCP/request-sockets even if they get recycled
It may be that a TCP/request-socket gets recycled and then ends up in
another part of the hash-list. If this happens while we are looking up
for a token in the hash-list, we have been moved to the wrong list and
will never find what we are looking for.

So, we need to do the same thing as in __inet_lookup_establish, check
the nulls-value and restart if necessary.

Fixes: eabc34bfa1d3b (Make the tk-table rcu-style)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ctrl.c
@@ -181,11 +181,20 @@ static int mptcp_reqsk_find_tk(u32 token)
 	struct mptcp_request_sock *mtreqsk;
 	const struct hlist_nulls_node *node;
 
+begin:
 	hlist_nulls_for_each_entry_rcu(mtreqsk, node,
 				       &mptcp_reqsk_tk_htb[hash], hash_entry) {
 		if (token == mtreqsk->mptcp_loc_token)
 			return 1;
 	}
+	/* A request-socket is destroyed by RCU. So, it might have been recycled
+	 * and put into another hash-table list. So, after the lookup we may
+	 * end up in a different list. So, we may need to restart.
+	 *
+	 * See also the comment in __inet_lookup_established.
+	 */
+	if (get_nulls_value(node) != hash)
+		goto begin;
 	return 0;
 }
 
@@ -236,10 +245,19 @@ static int mptcp_find_token(u32 token)
 	struct tcp_sock *meta_tp;
 	const struct hlist_nulls_node *node;
 
+begin:
 	hlist_nulls_for_each_entry_rcu(meta_tp, node, &tk_hashtable[hash], tk_table) {
 		if (token == meta_tp->mptcp_loc_token)
 			return 1;
 	}
+	/* A TCP-socket is destroyed by RCU. So, it might have been recycled
+	 * and put into another hash-table list. So, after the lookup we may
+	 * end up in a different list. So, we may need to restart.
+	 *
+	 * See also the comment in __inet_lookup_established.
+	 */
+	if (get_nulls_value(node) != hash)
+		goto begin;
 	return 0;
 }
 
@@ -341,15 +359,25 @@ struct sock *mptcp_hash_find(struct net *net, u32 token)
 	struct hlist_nulls_node *node;
 
 	rcu_read_lock();
+begin:
 	hlist_nulls_for_each_entry_rcu(meta_tp, node, &tk_hashtable[hash],
 				       tk_table) {
 		meta_sk = (struct sock *)meta_tp;
 		if (token == meta_tp->mptcp_loc_token &&
 		    net_eq(net, sock_net(meta_sk)) &&
 		    atomic_inc_not_zero(&meta_sk->sk_refcnt))
-			break;
+			goto out;
 		meta_sk = NULL;
 	}
+	/* A TCP-socket is destroyed by RCU. So, it might have been recycled
+	 * and put into another hash-table list. So, after the lookup we may
+	 * end up in a different list. So, we may need to restart.
+	 *
+	 * See also the comment in __inet_lookup_established.
+	 */
+	if (get_nulls_value(node) != hash)
+		goto begin;
+out:
 	rcu_read_unlock();
 	return meta_sk;
 }

net/mptcp/mptcp_ipv4.c
@@ -281,6 +281,7 @@ struct sock *mptcp_v4_search_req(const __be16 rport, const __be32 raddr,
 	const u32 hash = inet_synq_hash(raddr, rport, 0, MPTCP_HASH_SIZE);
 
 	rcu_read_lock();
+begin:
 	hlist_nulls_for_each_entry_rcu(mtreq, node, &mptcp_reqsk_htb[hash],
 				       hash_entry) {
 		struct inet_request_sock *ireq = inet_rsk(rev_mptcp_rsk(mtreq));
@@ -291,10 +292,19 @@ struct sock *mptcp_v4_search_req(const __be16 rport, const __be32 raddr,
 		    ireq->ir_loc_addr == laddr &&
 		    rev_mptcp_rsk(mtreq)->rsk_ops->family == AF_INET &&
 		    net_eq(net, sock_net(meta_sk)))
-			break;
+			goto found;
 		meta_sk = NULL;
 	}
+	/* A request-socket is destroyed by RCU. So, it might have been recycled
+	 * and put into another hash-table list. So, after the lookup we may
+	 * end up in a different list. So, we may need to restart.
+	 *
+	 * See also the comment in __inet_lookup_established.
+	 */
+	if (get_nulls_value(node) != hash)
+		goto begin;
 
+found:
 	if (meta_sk && unlikely(!atomic_inc_not_zero(&meta_sk->sk_refcnt)))
 		meta_sk = NULL;
 	rcu_read_unlock();

net/mptcp/mptcp_ipv6.c
@@ -502,6 +502,7 @@ struct sock *mptcp_v6_search_req(const __be16 rport, const struct in6_addr *radd
 	const u32 hash = inet6_synq_hash(raddr, rport, 0, MPTCP_HASH_SIZE);
 
 	rcu_read_lock();
+begin:
 	hlist_nulls_for_each_entry_rcu(mtreq, node, &mptcp_reqsk_htb[hash],
 				       hash_entry) {
 		struct inet_request_sock *treq = inet_rsk(rev_mptcp_rsk(mtreq));
@@ -512,10 +513,19 @@ struct sock *mptcp_v6_search_req(const __be16 rport, const struct in6_addr *radd
 		    ipv6_addr_equal(&treq->ir_v6_rmt_addr, raddr) &&
 		    ipv6_addr_equal(&treq->ir_v6_loc_addr, laddr) &&
 		    net_eq(net, sock_net(meta_sk)))
-			break;
+			goto found;
 		meta_sk = NULL;
 	}
+	/* A request-socket is destroyed by RCU. So, it might have been recycled
+	 * and put into another hash-table list. So, after the lookup we may
+	 * end up in a different list. So, we may need to restart.
+	 *
+	 * See also the comment in __inet_lookup_established.
+	 */
+	if (get_nulls_value(node) != hash)
+		goto begin;
 
+found:
 	if (meta_sk && unlikely(!atomic_inc_not_zero(&meta_sk->sk_refcnt)))
 		meta_sk = NULL;
 	rcu_read_unlock();