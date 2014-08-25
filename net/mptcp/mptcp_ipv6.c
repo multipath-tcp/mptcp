multipath-tcp/mptcp

commit 560443b12b7312b75062b9eaae843c44f959d645

Don't use get_random_bytes
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ipv6.c
@@ -47,6 +47,48 @@
 static int mptcp_v6v4_send_synack(struct sock *meta_sk, struct request_sock *req,
 				  struct request_values *rvp, u16 queue_mapping);
 
+__u32 mptcp_v6_get_nonce(const __be32 *saddr, const __be32 *daddr,
+			 __be16 sport, __be16 dport, u32 seq)
+{
+	u32 secret[MD5_MESSAGE_BYTES / 4];
+	u32 hash[MD5_DIGEST_WORDS];
+	u32 i;
+
+	memcpy(hash, saddr, 16);
+	for (i = 0; i < 4; i++)
+		secret[i] = mptcp_secret[i] + (__force u32)daddr[i];
+	secret[4] = mptcp_secret[4] +
+		    (((__force u16)sport << 16) + (__force u16)dport);
+	secret[5] = seq;
+	for (i = 6; i < MD5_MESSAGE_BYTES / 4; i++)
+		secret[i] = mptcp_secret[i];
+
+	md5_transform(hash, secret);
+
+	return hash[0];
+}
+
+u64 mptcp_v6_get_key(const __be32 *saddr, const __be32 *daddr,
+		     __be16 sport, __be16 dport)
+{
+	u32 secret[MD5_MESSAGE_BYTES / 4];
+	u32 hash[MD5_DIGEST_WORDS];
+	u32 i;
+
+	memcpy(hash, saddr, 16);
+	for (i = 0; i < 4; i++)
+		secret[i] = mptcp_secret[i] + (__force u32)daddr[i];
+	secret[4] = mptcp_secret[4] +
+		    (((__force u16)sport << 16) + (__force u16)dport);
+	secret[5] = mptcp_key_seed++;
+	for (i = 5; i < MD5_MESSAGE_BYTES / 4; i++)
+		secret[i] = mptcp_secret[i];
+
+	md5_transform(hash, secret);
+
+	return *((u64 *)hash);
+}
+
 static void mptcp_v6_reqsk_destructor(struct request_sock *req)
 {
 	mptcp_reqsk_destructor(req);
@@ -297,23 +339,6 @@ static void mptcp_v6_join_request(struct sock *meta_sk, struct sk_buff *skb)
 	tmp_opt.tstamp_ok = tmp_opt.saw_tstamp;
 	tcp_openreq_init(req, &tmp_opt, skb);
 
-	mtreq = mptcp_rsk(req);
-	mtreq->mpcb = mpcb;
-	INIT_LIST_HEAD(&mtreq->collide_tuple);
-	mtreq->mptcp_rem_nonce = mopt.mptcp_recv_nonce;
-	mtreq->mptcp_rem_key = mpcb->mptcp_rem_key;
-	mtreq->mptcp_loc_key = mpcb->mptcp_loc_key;
-	get_random_bytes(&mtreq->mptcp_loc_nonce,
-			 sizeof(mtreq->mptcp_loc_nonce));
-	mptcp_hmac_sha1((u8 *)&mtreq->mptcp_loc_key,
-			(u8 *)&mtreq->mptcp_rem_key,
-			(u8 *)&mtreq->mptcp_loc_nonce,
-			(u8 *)&mtreq->mptcp_rem_nonce, (u32 *)mptcp_hash_mac);
-	mtreq->mptcp_hash_tmac = *(u64 *)mptcp_hash_mac;
-	mtreq->rem_id = mopt.rem_id;
-	mtreq->low_prio = mopt.low_prio;
-	tcp_rsk(req)->saw_mpc = 1;
-
 	treq = inet6_rsk(req);
 	treq->rmt_addr = ipv6_hdr(skb)->saddr;
 	treq->loc_addr = ipv6_hdr(skb)->daddr;
@@ -385,6 +410,25 @@ static void mptcp_v6_join_request(struct sock *meta_sk, struct sk_buff *skb)
 
 	tcp_rsk(req)->snt_isn = isn;
 	tcp_rsk(req)->snt_synack = tcp_time_stamp;
+
+	mtreq = mptcp_rsk(req);
+	mtreq->mpcb = mpcb;
+	INIT_LIST_HEAD(&mtreq->collide_tuple);
+	mtreq->mptcp_rem_nonce = mopt.mptcp_recv_nonce;
+	mtreq->mptcp_rem_key = mpcb->mptcp_rem_key;
+	mtreq->mptcp_loc_key = mpcb->mptcp_loc_key;
+	mtreq->mptcp_loc_nonce = mptcp_v6_get_nonce(ipv6_hdr(skb)->daddr.s6_addr32,
+						    ipv6_hdr(skb)->saddr.s6_addr32,
+						    tcp_hdr(skb)->dest,
+						    tcp_hdr(skb)->source, isn);
+	mptcp_hmac_sha1((u8 *)&mtreq->mptcp_loc_key,
+			(u8 *)&mtreq->mptcp_rem_key,
+			(u8 *)&mtreq->mptcp_loc_nonce,
+			(u8 *)&mtreq->mptcp_rem_nonce, (u32 *)mptcp_hash_mac);
+	mtreq->mptcp_hash_tmac = *(u64 *)mptcp_hash_mac;
+	mtreq->rem_id = mopt.rem_id;
+	mtreq->low_prio = mopt.low_prio;
+	tcp_rsk(req)->saw_mpc = 1;
 
 	if (meta_sk->sk_family == AF_INET6) {
 		if (tcp_v6_send_synack(meta_sk, req, NULL, skb_get_queue_mapping(skb)))
@@ -684,18 +728,18 @@ int mptcp_init6_subsockets(struct sock *meta_sk, const struct mptcp_loc6 *loc,
 	loc_in.sin6_addr = loc->addr;
 	rem_in.sin6_addr = rem->addr;
 
-	mptcp_debug("%s: token %#x pi %d src_addr:%pI6:%d dst_addr:%pI6:%d\n",
-		    __func__, tcp_sk(meta_sk)->mpcb->mptcp_loc_token,
-		    tp->mptcp->path_index, &loc_in.sin6_addr,
-		    ntohs(loc_in.sin6_port), &rem_in.sin6_addr,
-		    ntohs(rem_in.sin6_port));
-
 	ret = sock.ops->bind(&sock, (struct sockaddr *)&loc_in, ulid_size);
 	if (ret < 0) {
 		mptcp_debug("%s: MPTCP subsocket bind()failed, error %d\n",
 			    __func__, ret);
 		goto error;
 	}
+
+	mptcp_debug("%s: token %#x pi %d src_addr:%pI6:%d dst_addr:%pI6:%d\n",
+		    __func__, tcp_sk(meta_sk)->mpcb->mptcp_loc_token,
+		    tp->mptcp->path_index, &loc_in.sin6_addr,
+		    ntohs(loc_in.sin6_port), &rem_in.sin6_addr,
+		    ntohs(rem_in.sin6_port));
 
 	ret = sock.ops->connect(&sock, (struct sockaddr *)&rem_in,
 				ulid_size, O_NONBLOCK);

include/net/mptcp.h
@@ -147,6 +147,7 @@ struct mptcp_tcp_sock {
 	u32	map_subseq;
 	u16	map_data_len;
 	u16	slave_sk:1,
+		nonce_set:1, /* Is the nonce set? (in order to support 0-nonce) */
 		fully_established:1,
 		attached:1,
 		csum_error:1,
@@ -610,6 +611,12 @@ extern struct workqueue_struct *mptcp_wq;
 
 extern struct lock_class_key meta_key;
 extern struct lock_class_key meta_slock_key;
+extern u32 mptcp_secret[MD5_MESSAGE_BYTES / 4];
+
+/* This is needed to ensure that two subsequent key-generation result in
+ * different keys if the IPs and ports are the same.
+ */
+extern u32 mptcp_key_seed;
 
 void mptcp_data_ready(struct sock *sk, int bytes);
 void mptcp_write_space(struct sock *sk);

include/net/mptcp_pm.h
@@ -111,8 +111,9 @@ int mptcp_do_join_short(struct sk_buff *skb, struct mptcp_options_received *mopt
 void mptcp_reqsk_remove_tk(struct request_sock *reqsk);
 void mptcp_reqsk_new_mptcp(struct request_sock *req,
 			   const struct tcp_options_received *rx_opt,
-			   const struct mptcp_options_received *mopt);
-void mptcp_connect_init(struct tcp_sock *tp);
+			   const struct mptcp_options_received *mopt,
+			   const struct sk_buff *skb);
+void mptcp_connect_init(struct sock *sk);
 void mptcp_set_addresses(struct sock *meta_sk);
 int mptcp_check_req(struct sk_buff *skb, struct net *net);
 void mptcp_address_worker(struct work_struct *work);
@@ -123,7 +124,8 @@ void mptcp_pm_undo(void);
 #else /* CONFIG_MPTCP */
 static inline void mptcp_reqsk_new_mptcp(struct request_sock *req,
 					 const struct tcp_options_received *rx_opt,
-					 const struct mptcp_options_received *mopt)
+					 const struct mptcp_options_received *mopt,
+					 const struct sk_buff *skb)
 {}
 static inline void mptcp_hash_remove(struct tcp_sock *meta_tp) {}
 #endif /* CONFIG_MPTCP */

include/net/mptcp_v4.h
@@ -57,6 +57,9 @@ void mptcp_pm_addr4_event_handler(struct in_ifaddr *ifa, unsigned long event,
 int mptcp_pm_v4_init(void);
 void mptcp_pm_v4_undo(void);
 void mptcp_v4_send_add_addr(int loc_id, struct mptcp_cb *mpcb);
+u32 mptcp_v4_get_nonce(__be32 saddr, __be32 daddr, __be16 sport, __be16 dport,
+		       u32 seq);
+u64 mptcp_v4_get_key(__be32 saddr, __be32 daddr, __be16 sport, __be16 dport);
 
 #else
 

include/net/mptcp_v6.h
@@ -70,6 +70,10 @@ void mptcp_v6_send_add_addr(int loc_id, struct mptcp_cb *mpcb);
 struct sock *mptcp_v6v4_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 				      struct request_sock *req,
 				      struct dst_entry *dst);
+__u32 mptcp_v6_get_nonce(const __be32 *saddr, const __be32 *daddr,
+			 __be16 sport, __be16 dport, u32 seq);
+u64 mptcp_v6_get_key(const __be32 *saddr, const __be32 *daddr,
+		     __be16 sport, __be16 dport);
 
 #else /* CONFIG_MPTCP */
 

net/ipv4/tcp_ipv4.c
@@ -1428,7 +1428,7 @@ int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb)
 	tcp_openreq_init(req, &tmp_opt, skb);
 
 	if (mopt.saw_mpc)
-		mptcp_reqsk_new_mptcp(req, &tmp_opt, &mopt);
+		mptcp_reqsk_new_mptcp(req, &tmp_opt, &mopt, skb);
 
 	ireq = inet_rsk(req);
 	ireq->loc_addr = daddr;

net/ipv4/tcp_output.c
@@ -2723,7 +2723,7 @@ void tcp_connect_init(struct sock *sk)
 	if (mptcp_doit(sk)) {
 		if (is_master_tp(tp)) {
 			tp->request_mptcp = 1;
-			mptcp_connect_init(tp);
+			mptcp_connect_init(sk);
 		} else {
 			tp->mptcp->snt_isn = tp->write_seq;
 			tp->mptcp->init_rcv_wnd = tp->rcv_wnd;

net/ipv6/tcp_ipv6.c
@@ -1197,7 +1197,7 @@ static int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 	tcp_openreq_init(req, &tmp_opt, skb);
 
 	if (mopt.saw_mpc)
-		mptcp_reqsk_new_mptcp(req, &tmp_opt, &mopt);
+		mptcp_reqsk_new_mptcp(req, &tmp_opt, &mopt, skb);
 
 	treq = inet6_rsk(req);
 	treq->rmt_addr = ipv6_hdr(skb)->saddr;

commit 0cecdcf21f73a7b0dd07c22d6b06d9fcfca7a940

mptcp: Do not overwrite mptcp_seed in mptcp_v6_get_key
mptcp_seed is set in secret[5]. The loop right after it, starts again at
5 to set mptcp_secret there. Thus, mptcp_seed is always overwritten and
if we have a token-collision we do not have the necessary entropy to
actually generate a different key.

Reported-by: John Ronan <jpronans@gmail.com>
Fixes: 560443b12b731 (Don't use get_random_bytes)
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

net/mptcp/mptcp_ipv6.c
@@ -81,7 +81,7 @@ u64 mptcp_v6_get_key(const __be32 *saddr, const __be32 *daddr,
 	secret[4] = mptcp_secret[4] +
 		    (((__force u16)sport << 16) + (__force u16)dport);
 	secret[5] = mptcp_seed++;
-	for (i = 5; i < MD5_MESSAGE_BYTES / 4; i++)
+	for (i = 6; i < MD5_MESSAGE_BYTES / 4; i++)
 		secret[i] = mptcp_secret[i];
 
 	md5_transform(hash, secret);