49a2706b3c0856dfdf9ad3e5440ae2d3e3eb66ee
Christoph Paasch <christoph.paasch@gmail.com>
2014-12-23 08:36:16 -0800

    Maintenance Release v0.89.4
    Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2476,7 +2476,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.89.3");
+	pr_info("MPTCP: Stable release v0.89.4");
 
 	mptcp_init_failed = false;
 
