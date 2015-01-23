8900eefe389e0e3e4b68125bcb3a46508e95cc66
Christoph Paasch <cpaasch@apple.com>
2015-01-23 08:14:11 -0800

    mptcp: When falling back, close all other subflows
    We must make sure that all other subflows are closed when falling back
    to infinite mapping/regular TCP. Otherwise, these open subflows may
    still receive data and all kind of funny things may happen. Especially,
    when receiving a RST, the RST gets propagated up to the meta-sk through
    mptcp_sock_def_error_report.
    
    Fixes Github-issue: https://github.com/multipath-tcp/mptcp/issues/60
    
    Reported-by: https://github.com/andywu106
    Fixes: Zero-day bug
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/mptcp/mptcp_input.c
+++ net/mptcp/mptcp_input.c
@@ -552,6 +552,9 @@ static int mptcp_prevalidate_skb(struct sock *sk, struct sk_buff *skb)
 
 		tp->mpcb->infinite_mapping_snd = 1;
 		tp->mpcb->infinite_mapping_rcv = 1;
+
+		mptcp_sub_force_close_all(tp->mpcb, sk);
+
 		/* We do a seamless fallback and should not send a inf.mapping. */
 		tp->mpcb->send_infinite_mapping = 0;
 		tp->mptcp->fully_established = 1;
@@ -658,7 +661,8 @@ static int mptcp_detect_mapping(struct sock *sk, struct sk_buff *skb)
 		data_len = skb->len + (mptcp_is_data_fin(skb) ? 1 : 0);
 		sub_seq = tcb->seq;
 
-		/* TODO kill all other subflows than this one */
+		mptcp_sub_force_close_all(mpcb, sk);
+
 		/* data_seq and so on are set correctly */
 
 		/* At this point, the meta-ofo-queue has to be emptied,
@@ -1986,8 +1990,6 @@ static inline int mptcp_mp_fail_rcvd(struct sock *sk, const struct tcphdr *th)
 		mptcp->rx_opt.mp_fail = 0;
 
 		if (!th->rst && !mpcb->infinite_mapping_snd) {
-			struct sock *sk_it;
-
 			mpcb->send_infinite_mapping = 1;
 			/* We resend everything that has not been acknowledged */
 			meta_sk->sk_send_head = tcp_write_queue_head(meta_sk);
@@ -2010,18 +2012,13 @@ static inline int mptcp_mp_fail_rcvd(struct sock *sk, const struct tcphdr *th)
 			/* Trigger a sending on the meta. */
 			mptcp_push_pending_frames(meta_sk);
 
-			mptcp_for_each_sk(mpcb, sk_it) {
-				if (sk != sk_it)
-					mptcp_sub_force_close(sk_it);
-			}
+			mptcp_sub_force_close_all(mpcb, sk);
 		}
 
 		return 0;
 	}
 
 	if (unlikely(mptcp->rx_opt.mp_fclose)) {
-		struct sock *sk_it, *tmpsk;
-
 		mptcp->rx_opt.mp_fclose = 0;
 		if (mptcp->rx_opt.mptcp_key != mpcb->mptcp_loc_key)
 			return 0;
@@ -2029,8 +2026,7 @@ static inline int mptcp_mp_fail_rcvd(struct sock *sk, const struct tcphdr *th)
 		if (tcp_need_reset(sk->sk_state))
 			tcp_sk(sk)->ops->send_active_reset(sk, GFP_ATOMIC);
 
-		mptcp_for_each_sk_safe(mpcb, sk_it, tmpsk)
-			mptcp_sub_force_close(sk_it);
+		mptcp_sub_force_close_all(mpcb, NULL);
 
 		tcp_reset(meta_sk);
 
