multipath-tcp/mptcp

commit e5ad28f0ac251f0d26b9aff1f0c17219d6fba3d1

mptcp: ipv6: fix join when no listen socket
If there is no listen socket anymore, additional subflows will be reset.
This patch changes tcp_v6_rcv to be similar to tcp_v4_rcv, i.e., perform
the goto no_tcp_socket after the mptcp_lookup_join.

Fixes: 8e500482401f (Merge tag 'v3.18' into mptcp_trunk)
Signed-off-by: Gregory Detal <gregory.detal@tessares.net>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

net/ipv6/tcp_ipv6.c
@@ -1493,8 +1493,6 @@ static int tcp_v6_rcv(struct sk_buff *skb)
 
 	sk = __inet6_lookup_skb(&tcp_hashinfo, skb, th->source, th->dest,
 				tcp_v6_iif(skb));
-	if (!sk)
-		goto no_tcp_socket;
 
 process:
 	if (sk && sk->sk_state == TCP_TIME_WAIT)

commit 68e9980824b473038aacba32e066e8e7ad97e4f4

mptcp: ipv6: fix join when master is v6-mapped
As the af_ops are inherited from the meta socket we need to make sure that
ipv6 ops are used and not v6-mapped ops for IPv6 (on the wire) subflows.

Fixes: e5ad28f0ac25 (mptcp: ipv6: fix join when no listen socket)
Signed-off-by: Gregory Detal <gregory.detal@tessares.net>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

net/ipv6/tcp_ipv6.c
@@ -1183,6 +1183,14 @@ struct sock *tcp_v6_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 	if (newsk == NULL)
 		goto out_nonewsk;
 
+#ifdef CONFIG_MPTCP
+	/* If the meta_sk is v6-mapped we can end up here with the wrong af_ops.
+	 * Just make sure that this subflow is v6.
+	 */
+	if (is_meta_sk(sk))
+		inet_csk(newsk)->icsk_af_ops = &mptcp_v6_specific;
+#endif
+
 	/*
 	 * No need to charge this sock to the relevant IPv6 refcnt debug socks
 	 * count here, tcp_create_openreq_child now does this for us, see the