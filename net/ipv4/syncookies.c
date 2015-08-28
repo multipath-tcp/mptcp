multipath-tcp/mptcp

commit bb7a28fe4e57832c89aa1c1d74ca0c819e0e3696

mptcp: Generate correct socket when using SYN-cookies
Upon reception of the third ACK, we regenerate the state of the
request-socket as well as the keys and token (as the keys are echoed in
the third ACK).

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Gregory Detal <gregory.detal@tessares.net>

net/ipv4/syncookies.c
@@ -16,6 +16,8 @@
 #include <linux/cryptohash.h>
 #include <linux/kernel.h>
 #include <linux/export.h>
+#include <net/mptcp.h>
+#include <net/mptcp_v4.h>
 #include <net/tcp.h>
 #include <net/route.h>
 
@@ -203,8 +205,19 @@ static inline struct sock *get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 {
 	struct inet_connection_sock *icsk = inet_csk(sk);
 	struct sock *child;
+	int ret;
 
 	child = icsk->icsk_af_ops->syn_recv_sock(sk, skb, req, dst);
+
+#ifdef CONFIG_MPTCP
+	ret = mptcp_check_req_master(sk, child, req, NULL);
+	if (ret < 0)
+		return NULL;
+
+	if (!ret)
+		return tcp_sk(child)->mpcb->master_sk;
+#endif
+
 	if (child)
 		inet_csk_reqsk_queue_add(sk, req, child);
 	else
@@ -259,6 +272,7 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 {
 	struct ip_options *opt = &TCP_SKB_CB(skb)->header.h4.opt;
 	struct tcp_options_received tcp_opt;
+	struct mptcp_options_received mopt;
 	struct inet_request_sock *ireq;
 	struct tcp_request_sock *treq;
 	struct tcp_sock *tp = tcp_sk(sk);
@@ -285,13 +299,19 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 
 	/* check for timestamp cookie support */
 	memset(&tcp_opt, 0, sizeof(tcp_opt));
-	tcp_parse_options(skb, &tcp_opt, NULL, 0, NULL);
+	mptcp_init_mp_opt(&mopt);
+	tcp_parse_options(skb, &tcp_opt, &mopt, 0, NULL);
 
 	if (!cookie_check_timestamp(&tcp_opt, sock_net(sk), &ecn_ok))
 		goto out;
 
 	ret = NULL;
-	req = inet_reqsk_alloc(&tcp_request_sock_ops); /* for safety */
+#ifdef CONFIG_MPTCP
+	if (mopt.saw_mpc)
+		req = inet_reqsk_alloc(&mptcp_request_sock_ops); /* for safety */
+	else
+#endif
+		req = inet_reqsk_alloc(&tcp_request_sock_ops); /* for safety */
 	if (!req)
 		goto out;
 
@@ -310,9 +330,14 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 	ireq->sack_ok		= tcp_opt.sack_ok;
 	ireq->wscale_ok		= tcp_opt.wscale_ok;
 	ireq->tstamp_ok		= tcp_opt.saw_tstamp;
+	ireq->mptcp_rqsk	= 0;
+	ireq->saw_mpc		= 0;
 	req->ts_recent		= tcp_opt.saw_tstamp ? tcp_opt.rcv_tsval : 0;
 	treq->snt_synack	= tcp_opt.saw_tstamp ? tcp_opt.rcv_tsecr : 0;
 	treq->listener		= NULL;
+
+	if (mopt.saw_mpc)
+		mptcp_cookies_reqsk_init(req, &mopt, skb);
 
 	/* We throwed the options of the initial SYN away, so we hope
 	 * the ACK carries the same options again (see RFC1122 4.2.3.8)

net/ipv6/syncookies.c
@@ -19,6 +19,8 @@
 #include <linux/cryptohash.h>
 #include <linux/kernel.h>
 #include <net/ipv6.h>
+#include <net/mptcp.h>
+#include <net/mptcp_v6.h>
 #include <net/tcp.h>
 
 #define COOKIEBITS 24	/* Upper bits store count */
@@ -47,8 +49,19 @@ static inline struct sock *get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 {
 	struct inet_connection_sock *icsk = inet_csk(sk);
 	struct sock *child;
+	int ret;
 
 	child = icsk->icsk_af_ops->syn_recv_sock(sk, skb, req, dst);
+
+#ifdef CONFIG_MPTCP
+	ret = mptcp_check_req_master(sk, child, req, NULL);
+	if (ret < 0)
+		return NULL;
+
+	if (!ret)
+		return tcp_sk(child)->mpcb->master_sk;
+#endif
+
 	if (child)
 		inet_csk_reqsk_queue_add(sk, req, child);
 	else
@@ -156,6 +169,7 @@ EXPORT_SYMBOL_GPL(__cookie_v6_check);
 struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 {
 	struct tcp_options_received tcp_opt;
+	struct mptcp_options_received mopt;
 	struct inet_request_sock *ireq;
 	struct tcp_request_sock *treq;
 	struct ipv6_pinfo *np = inet6_sk(sk);
@@ -182,19 +196,33 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 
 	/* check for timestamp cookie support */
 	memset(&tcp_opt, 0, sizeof(tcp_opt));
-	tcp_parse_options(skb, &tcp_opt, NULL, 0, NULL);
+	mptcp_init_mp_opt(&mopt);
+	tcp_parse_options(skb, &tcp_opt, &mopt, 0, NULL);
 
 	if (!cookie_check_timestamp(&tcp_opt, sock_net(sk), &ecn_ok))
 		goto out;
 
 	ret = NULL;
-	req = inet_reqsk_alloc(&tcp6_request_sock_ops);
+#ifdef CONFIG_MPTCP
+	if (mopt.saw_mpc)
+		req = inet_reqsk_alloc(&mptcp6_request_sock_ops);
+	else
+#endif
+		req = inet_reqsk_alloc(&tcp6_request_sock_ops);
 	if (!req)
 		goto out;
 
 	ireq = inet_rsk(req);
+	ireq->mptcp_rqsk = 0;
+	ireq->saw_mpc = 0;
 	treq = tcp_rsk(req);
 	treq->listener = NULL;
+
+	/* Must be done before anything else, as it initializes
+	 * hash_entry of the MPTCP request-sock.
+	 */
+	if (mopt.saw_mpc)
+		mptcp_cookies_reqsk_init(req, &mopt, skb);
 
 	if (security_inet_conn_request(sk, skb, req))
 		goto out_free;

include/net/mptcp.h
@@ -821,6 +821,9 @@ void mptcp_enable_sock(struct sock *sk);
 void mptcp_disable_sock(struct sock *sk);
 void mptcp_enable_static_key(void);
 void mptcp_disable_static_key(void);
+void mptcp_cookies_reqsk_init(struct request_sock *req,
+			      struct mptcp_options_received *mopt,
+			      struct sk_buff *skb);
 
 /* MPTCP-path-manager registration/initialization functions */
 int mptcp_register_path_manager(struct mptcp_pm_ops *pm);
@@ -1451,6 +1454,9 @@ static inline void mptcp_remove_shortcuts(const struct mptcp_cb *mpcb,
 static inline void mptcp_delete_synack_timer(struct sock *meta_sk) {}
 static inline void mptcp_init_tcp_sock(struct sock *sk) {}
 static inline void mptcp_disable_static_key(void) {}
+static inline void mptcp_cookies_reqsk_init(struct request_sock *req,
+					    struct mptcp_options_received *mopt,
+					    struct sk_buff *skb) {}
 #endif /* CONFIG_MPTCP */
 
 #endif /* _MPTCP_H */

net/mptcp/mptcp_ctrl.c
@@ -2008,8 +2008,13 @@ int mptcp_check_req_master(struct sock *sk, struct sock *child,
 	if (ret)
 		return ret;
 
-	inet_csk_reqsk_queue_unlink(sk, req, prev);
-	inet_csk_reqsk_queue_removed(sk, req);
+	/* prev indicates that we come from tcp_check_req and thus need to
+	 * handle the request-socket fully.
+	 */
+	if (prev) {
+		inet_csk_reqsk_queue_unlink(sk, req, prev);
+		inet_csk_reqsk_queue_removed(sk, req);
+	}
 	inet_csk_reqsk_queue_add(sk, req, meta_sk);
 
 	return 0;
@@ -2286,6 +2291,39 @@ void mptcp_reqsk_init(struct request_sock *req, const struct sk_buff *skb,
 	}
 
 	mptcp_reqsk_new_mptcp(req, &mopt, skb);
+}
+
+void mptcp_cookies_reqsk_init(struct request_sock *req,
+			      struct mptcp_options_received *mopt,
+			      struct sk_buff *skb)
+{
+	struct mptcp_request_sock *mtreq = mptcp_rsk(req);
+
+	/* Absolutely need to always initialize this. */
+	mtreq->hash_entry.pprev = NULL;
+
+	mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
+	mtreq->mptcp_loc_key = mopt->mptcp_receiver_key;
+
+	/* Generate the token */
+	mptcp_key_sha1(mtreq->mptcp_loc_key, &mtreq->mptcp_loc_token, NULL);
+
+	rcu_read_lock();
+	spin_lock(&mptcp_tk_hashlock);
+
+	/* Check, if the key is still free */
+	if (mptcp_reqsk_find_tk(mtreq->mptcp_loc_token) ||
+	    mptcp_find_token(mtreq->mptcp_loc_token))
+		goto out;
+
+	inet_rsk(req)->saw_mpc = 1;
+	mtreq->is_sub = 0;
+	inet_rsk(req)->mptcp_rqsk = 1;
+	mtreq->dss_csum = mopt->dss_csum;
+
+out:
+	spin_unlock(&mptcp_tk_hashlock);
+	rcu_read_unlock();
 }
 
 int mptcp_conn_request(struct sock *sk, struct sk_buff *skb)

commit dd5dd4e558f38953862c0c2671d44d02cbc26fd4

mptcp: Fix SYN-cookies when syn_recv_sock fails
We need to check whether child is set before continuing (same stuff as
in tcp_check_req()). So, we need to goto listen_overflow in case
syn_recv_sock returns NULL.

Fixes: bb7a28fe4e578 (mptcp: Generate correct socket when using SYN-cookies)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Gregory Detal <gregory.detal@tessares.net>

net/ipv4/syncookies.c
@@ -210,12 +210,17 @@ static inline struct sock *get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 	child = icsk->icsk_af_ops->syn_recv_sock(sk, skb, req, dst);
 
 #ifdef CONFIG_MPTCP
+	if (!child)
+		goto listen_overflow;
+
 	ret = mptcp_check_req_master(sk, child, req, NULL);
 	if (ret < 0)
 		return NULL;
 
 	if (!ret)
 		return tcp_sk(child)->mpcb->master_sk;
+
+listen_overflow:
 #endif
 
 	if (child)

net/ipv6/syncookies.c
@@ -54,12 +54,17 @@ static inline struct sock *get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 	child = icsk->icsk_af_ops->syn_recv_sock(sk, skb, req, dst);
 
 #ifdef CONFIG_MPTCP
+	if (!child)
+		goto listen_overflow;
+
 	ret = mptcp_check_req_master(sk, child, req, NULL);
 	if (ret < 0)
 		return NULL;
 
 	if (!ret)
 		return tcp_sk(child)->mpcb->master_sk;
+
+listen_overflow:
 #endif
 
 	if (child)