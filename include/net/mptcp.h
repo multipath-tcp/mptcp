0968d2e24db273e8d97af787b36c06cf096791b2
Christoph Paasch <christoph.paasch@gmail.com>
2016-01-17 15:03:56 -0800

    mptcp: Build-Fix
    MPTCP_INC_STATS_BH is not defined when CONFIG_MPTCP is off.
    
    Fixes: 5f6aee55f9 (mptcp: Use correct MIB-handler in softirq context)
    Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -1241,11 +1241,6 @@ static inline void mptcp_set_rto(struct sock *sk)
 	}
 }
 
-static inline int mptcp_sysctl_syn_retries(void)
-{
-	return sysctl_mptcp_syn_retries;
-}
-
 static inline void mptcp_sub_close_passive(struct sock *sk)
 {
 	struct sock *meta_sk = mptcp_meta_sk(sk);
@@ -1449,10 +1444,6 @@ static inline int mptcp_check_rtt(const struct tcp_sock *tp, int time)
 	return 0;
 }
 static inline int mptcp_check_snd_buf(const struct tcp_sock *tp)
-{
-	return 0;
-}
-static inline int mptcp_sysctl_syn_retries(void)
 {
 	return 0;
 }
--- net/ipv4/tcp_timer.c
+++ net/ipv4/tcp_timer.c
@@ -173,13 +173,16 @@ int tcp_write_timeout(struct sock *sk)
 		}
 		retry_until = icsk->icsk_syn_retries ? : sysctl_tcp_syn_retries;
 		syn_set = true;
+
+#ifdef CONFIG_MPTCP
 		/* Stop retransmitting MP_CAPABLE options in SYN if timed out. */
 		if (tcp_sk(sk)->request_mptcp &&
-		    icsk->icsk_retransmits >= mptcp_sysctl_syn_retries()) {
+		    icsk->icsk_retransmits >= sysctl_mptcp_syn_retries) {
 			tcp_sk(sk)->request_mptcp = 0;
 
 			MPTCP_INC_STATS_BH(sock_net(sk), MPTCP_MIB_MPCAPABLERETRANSFALLBACK);
 		}
+#endif /* CONFIG_MPTCP */
 	} else {
 		if (retransmits_timed_out(sk, sysctl_tcp_retries1, 0, 0)) {
 			/* Black hole detection */
