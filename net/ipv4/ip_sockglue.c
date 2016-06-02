a39f1da1c902ce51828f9d32508906554bc50b2b
Sebastien DUPONCHEEL <sebastien.duponcheel@ovh.net>
2016-06-02 16:27:09 +0200

    Update TOS on established mptcp subflow
    When setting the TOS-option after a connection became MPTCP-established,
    we need to pass down the option on to the different subflows.
    
    Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

--- net/ipv4/ip_sockglue.c
+++ net/ipv4/ip_sockglue.c
@@ -43,6 +43,8 @@
 #endif
 #include <net/ip_fib.h>
 
+#include <net/mptcp.h>
+
 #include <linux/errqueue.h>
 #include <asm/uaccess.h>
 
@@ -709,6 +711,17 @@ static int do_ip_setsockopt(struct sock *sk, int level,
 			inet->tos = val;
 			sk->sk_priority = rt_tos2priority(val);
 			sk_dst_reset(sk);
+			/* Update TOS on mptcp subflow */
+			if (is_meta_sk(sk)) {
+				struct sock *sk_it;
+				mptcp_for_each_sk(tcp_sk(sk)->mpcb, sk_it) {
+					if (inet_sk(sk_it)->tos != inet_sk(sk)->tos) {
+						inet_sk(sk_it)->tos = inet_sk(sk)->tos;
+						sk_it->sk_priority = sk->sk_priority;
+						sk_dst_reset(sk_it);
+					}
+				}
+			}
 		}
 		break;
 	case IP_TTL:
