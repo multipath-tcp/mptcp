multipath-tcp/mptcp

commit 8e86cf262108513296ac0952f2ffef717dc12f4b

mptcp: Add IPv6 support for ADD_ADDR2
IPv6 addresses can now be advertised with ADD_ADDR2.

MPTCP options DSS and MP_PRIO are excluded in case of ADD_ADDR2
advertising an IPv6 address. This guarantees that ADD_ADDR2 fits
in the TCP options (if the port value is not present).
Plain TCP options are not affected.

Signed-off-by: Fabrizio Demaria <fabrizio.demaria@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

net/mptcp/mptcp_fullmesh.c
@@ -1584,12 +1584,14 @@ static void full_mesh_addr_signal(struct sock *sk, unsigned *size,
 
 	if (meta_v4)
 		goto skip_ipv6;
-
 skip_ipv4:
 	/* IPv6 */
 	unannouncedv6 = (~fmp->announced_addrs_v6) & mptcp_local->loc6_bits;
 	if (unannouncedv6 &&
-	    MAX_TCP_OPTION_SPACE - *size >= MPTCP_SUB_LEN_ADD_ADDR6_ALIGN) {
+	    ((mpcb->mptcp_ver == MPTCP_VERSION_0 &&
+	    MAX_TCP_OPTION_SPACE - *size >= MPTCP_SUB_LEN_ADD_ADDR6_ALIGN) ||
+	    (mpcb->mptcp_ver >= MPTCP_VERSION_1 &&
+	    MAX_TCP_OPTION_SPACE - *size >= MPTCP_SUB_LEN_ADD_ADDR6_ALIGN_VER1))) {
 		int ind = mptcp_find_free_index(~unannouncedv6);
 
 		opts->options |= OPTION_MPTCP;
@@ -1597,12 +1599,27 @@ skip_ipv4:
 		opts->add_addr6.addr_id = mptcp_local->locaddr6[ind].loc6_id;
 		opts->add_addr6.addr = mptcp_local->locaddr6[ind].addr;
 		opts->add_addr_v6 = 1;
+		if (mpcb->mptcp_ver >= MPTCP_VERSION_1) {
+			u8 mptcp_hash_mac[20];
+			u8 no_key[8];
+
+			*(u64 *)no_key = 0;
+			mptcp_hmac_sha1((u8 *)&mpcb->mptcp_loc_key,
+					(u8 *)no_key,
+					(u32 *)mptcp_hash_mac, 2,
+					1, (u8 *)&mptcp_local->locaddr6[ind].loc6_id,
+					16, (u8 *)&opts->add_addr6.addr.s6_addr);
+			opts->add_addr6.trunc_mac = *(u64 *)mptcp_hash_mac;
+		}
 
 		if (skb) {
 			fmp->announced_addrs_v6 |= (1 << ind);
 			fmp->add_addr--;
 		}
-		*size += MPTCP_SUB_LEN_ADD_ADDR6_ALIGN;
+		if (mpcb->mptcp_ver < MPTCP_VERSION_1)
+			*size += MPTCP_SUB_LEN_ADD_ADDR6_ALIGN;
+		if (mpcb->mptcp_ver >= MPTCP_VERSION_1)
+			*size += MPTCP_SUB_LEN_ADD_ADDR6_ALIGN_VER1;
 	}
 
 skip_ipv6:

net/mptcp/mptcp_output.c
@@ -977,6 +977,13 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 		*size += MPTCP_SUB_LEN_JOIN_ACK_ALIGN;
 	}
 
+	if (unlikely(mpcb->addr_signal) && mpcb->pm_ops->addr_signal) {
+		mpcb->pm_ops->addr_signal(sk, size, opts, skb);
+		if (opts->add_addr_v6)
+			/* Skip subsequent options */
+			return;
+	}
+
 	if (!tp->mptcp->include_mpc && !tp->mptcp->pre_established) {
 		opts->options |= OPTION_MPTCP;
 		opts->mptcp_options |= OPTION_DATA_ACK;
@@ -995,9 +1002,6 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 
 		*size += MPTCP_SUB_LEN_DSS_ALIGN;
 	}
-
-	if (unlikely(mpcb->addr_signal) && mpcb->pm_ops->addr_signal)
-		mpcb->pm_ops->addr_signal(sk, size, opts, skb);
 
 	if (unlikely(tp->mptcp->send_mp_prio) &&
 	    MAX_TCP_OPTION_SPACE - *size >= MPTCP_SUB_LEN_PRIO_ALIGN) {
@@ -1052,7 +1056,6 @@ void mptcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 		mpc->rsv = 0;
 		mpc->h = 1;
 	}
-
 	if (unlikely(OPTION_MP_JOIN & opts->mptcp_options)) {
 		struct mp_join *mpj = (struct mp_join *)ptr;
 
@@ -1102,13 +1105,20 @@ void mptcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 				ptr += MPTCP_SUB_LEN_ADD_ADDR4_ALIGN_VER1 >> 2;
 			}
 		} else if (opts->add_addr_v6) {
-			mpadd->len = MPTCP_SUB_LEN_ADD_ADDR6;
 			mpadd->sub = MPTCP_SUB_ADD_ADDR;
 			mpadd->ipver = 6;
 			mpadd->addr_id = opts->add_addr6.addr_id;
 			memcpy(&mpadd->u.v6.addr, &opts->add_addr6.addr,
 			       sizeof(mpadd->u.v6.addr));
-			ptr += MPTCP_SUB_LEN_ADD_ADDR6_ALIGN >> 2;
+			if (mpcb->mptcp_ver < MPTCP_VERSION_1) {
+				mpadd->len = MPTCP_SUB_LEN_ADD_ADDR6;
+				ptr += MPTCP_SUB_LEN_ADD_ADDR6_ALIGN >> 2;
+			} else {
+				memcpy((char *)mpadd->u.v6.mac - 2,
+				       (char *)&opts->add_addr6.trunc_mac, 8);
+				mpadd->len = MPTCP_SUB_LEN_ADD_ADDR6_VER1;
+				ptr += MPTCP_SUB_LEN_ADD_ADDR6_ALIGN_VER1 >> 2;
+			}
 		}
 
 		MPTCP_INC_STATS_BH(sock_net((struct sock *)tp), MPTCP_MIB_ADDADDRTX);

include/linux/tcp.h
@@ -111,7 +111,9 @@ struct tcp_out_options {
 	} add_addr4;
 
 	struct {
+		__u64 trunc_mac;
 		struct in6_addr addr;
+		u16 port;
 		u8 addr_id;
 	} add_addr6;
 

include/net/mptcp.h
@@ -400,9 +400,11 @@ struct mptcp_cb {
 #define MPTCP_SUB_LEN_ADD_ADDR4		8
 #define MPTCP_SUB_LEN_ADD_ADDR4_VER1	16
 #define MPTCP_SUB_LEN_ADD_ADDR6		20
+#define MPTCP_SUB_LEN_ADD_ADDR6_VER1	28
 #define MPTCP_SUB_LEN_ADD_ADDR4_ALIGN	8
 #define MPTCP_SUB_LEN_ADD_ADDR4_ALIGN_VER1	16
 #define MPTCP_SUB_LEN_ADD_ADDR6_ALIGN	20
+#define MPTCP_SUB_LEN_ADD_ADDR6_ALIGN_VER1	28
 
 #define MPTCP_SUB_REMOVE_ADDR	4
 #define MPTCP_SUB_LEN_REMOVE_ADDR	4
@@ -571,6 +573,7 @@ struct mp_add_addr {
 		struct {
 			struct in6_addr	addr;
 			__be16		port;
+			__u8		mac[8];
 		} v6;
 	} u;
 } __attribute__((__packed__));

net/mptcp/mptcp_input.c
@@ -1611,7 +1611,8 @@ static inline bool is_valid_addropt_opsize(u8 mptcp_ver,
 		       opsize == MPTCP_SUB_LEN_ADD_ADDR6 + 2;
 	}
 	if (mptcp_ver >= MPTCP_VERSION_1 && mpadd->ipver == 6)
-		return false; /* IPv6 with ADD_ADDR2 not supported yet */
+		return opsize == MPTCP_SUB_LEN_ADD_ADDR6_VER1 ||
+		       opsize == MPTCP_SUB_LEN_ADD_ADDR6_VER1 + 2;
 #endif
 	if (mptcp_ver < MPTCP_VERSION_1 && mpadd->ipver == 4) {
 		return opsize == MPTCP_SUB_LEN_ADD_ADDR4 ||
@@ -1954,7 +1955,36 @@ skip_hmac_v4:
 		addr.in = mpadd->u.v4.addr;
 #if IS_ENABLED(CONFIG_IPV6)
 	} else if (mpadd->ipver == 6) {
-		if (mpadd->len == MPTCP_SUB_LEN_ADD_ADDR6 + 2)
+		char *recv_hmac;
+		u8 hash_mac_check[20];
+		u8 no_key[8];
+		int msg_parts;
+
+		if (mpcb->mptcp_ver < MPTCP_VERSION_1)
+			goto skip_hmac_v6;
+
+		*(u64 *)no_key = 0;
+		recv_hmac = (char *)mpadd->u.v6.mac;
+		if (mpadd->len == MPTCP_SUB_LEN_ADD_ADDR6_VER1) {
+			recv_hmac -= sizeof(mpadd->u.v6.port);
+			msg_parts = 2;
+		} else if (mpadd->len == MPTCP_SUB_LEN_ADD_ADDR6_VER1 + 2) {
+			msg_parts = 3;
+		}
+		mptcp_hmac_sha1((u8 *)&mpcb->mptcp_rem_key,
+				(u8 *)no_key,
+				(u32 *)hash_mac_check, msg_parts,
+				1, (u8 *)&mpadd->addr_id,
+				16, (u8 *)&mpadd->u.v6.addr.s6_addr,
+				2, (u8 *)&mpadd->u.v6.port);
+		if (memcmp(hash_mac_check, recv_hmac, 8) != 0)
+			/* ADD_ADDR2 discarded */
+			return;
+skip_hmac_v6:
+		if ((mpcb->mptcp_ver == MPTCP_VERSION_0 &&
+		     mpadd->len == MPTCP_SUB_LEN_ADD_ADDR6 + 2) ||
+		     (mpcb->mptcp_ver == MPTCP_VERSION_1 &&
+		     mpadd->len == MPTCP_SUB_LEN_ADD_ADDR6_VER1 + 2))
 			port  = mpadd->u.v6.port;
 		family = AF_INET6;
 		addr.in6 = mpadd->u.v6.addr;

commit e2a7906124c5201ed3d053ca70df980ef72ca98b

mptcp: Fix IPv6 address announcments
Two issues in here:
1. Don't announce a v4 and a v6 address at the same time. There is not
   enough space to fit an v4+v6 ADD_ADDR and a DATA_ACK.
2. Only put a MPTCP-v1 ADD_ADDR if there is no data. Because, due to the
   HMAC there is no space anymore.
3. Restore pre-8e86cf26210 behavior for MPTCP-v0. Because, we want the
   DATA_ACK to take precedence over the ADD_ADDR. The DATA_ACK is more
   important, because a missing DATA_ACK may trigger a fallback to
   regular TCP.

Issue: https://github.com/multipath-tcp/mptcp/issues/134

Cc: Thomas Dreibholz <dreibh@simula.no>
Fixes: 8e86cf26210 (mptcp: Add IPv6 support for ADD_ADDR2)
Reported-by: Thomas Dreibholz <dreibh@simula.no>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_fullmesh.c
@@ -1598,6 +1598,8 @@ static void full_mesh_addr_signal(struct sock *sk, unsigned *size,
 			*size += MPTCP_SUB_LEN_ADD_ADDR4_ALIGN;
 		if (mpcb->mptcp_ver >= MPTCP_VERSION_1)
 			*size += MPTCP_SUB_LEN_ADD_ADDR4_ALIGN_VER1;
+
+		goto skip_ipv6;
 	}
 
 	if (meta_v4)

net/mptcp/mptcp_output.c
@@ -981,8 +981,10 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 		*size += MPTCP_SUB_LEN_JOIN_ACK_ALIGN;
 	}
 
-	if (unlikely(mpcb->addr_signal) && mpcb->pm_ops->addr_signal) {
+	if (unlikely(mpcb->addr_signal) && mpcb->pm_ops->addr_signal &&
+	    mpcb->mptcp_ver >= MPTCP_VERSION_1 && skb && !mptcp_is_data_seq(skb)) {
 		mpcb->pm_ops->addr_signal(sk, size, opts, skb);
+
 		if (opts->add_addr_v6)
 			/* Skip subsequent options */
 			return;
@@ -1006,6 +1008,10 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 
 		*size += MPTCP_SUB_LEN_DSS_ALIGN;
 	}
+
+	if (unlikely(mpcb->addr_signal) && mpcb->pm_ops->addr_signal &&
+	    mpcb->mptcp_ver < MPTCP_VERSION_1)
+		mpcb->pm_ops->addr_signal(sk, size, opts, skb);
 
 	if (unlikely(tp->mptcp->send_mp_prio) &&
 	    MAX_TCP_OPTION_SPACE - *size >= MPTCP_SUB_LEN_PRIO_ALIGN) {