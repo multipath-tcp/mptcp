multipath-tcp/mptcp

commit d90a354ad6aa701fd4e46a64eeb59fb6c04e881f

Ensure the Data FIN is in sequence before queuing
If the data-fin arrives before other segments it will be queued via
mptcp_queue_skb without testing tp->rcv_nxt. This can end the stream
abruptly or can cause the subflow to be reset.
This version of the patch incorporates suggestions from Christoph Paasch
(tcp_ofo_queue needs to be amended so as not to remove the data-fin).

Signed-off-by: Catalin Nicutar <catalin.nicutar@gmail.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/ipv4/tcp_input.c
@@ -4441,7 +4441,11 @@ static void tcp_ofo_queue(struct sock *sk)
 			tcp_dsack_extend(sk, TCP_SKB_CB(skb)->seq, dsack);
 		}
 
-		if (!after(TCP_SKB_CB(skb)->end_seq, tp->rcv_nxt)) {
+		/* In case of MPTCP, the segment may be empty if it's a
+		 * non-data DATA_FIN. (see beginning of tcp_data_queue)
+		 */
+		if (!after(TCP_SKB_CB(skb)->end_seq, tp->rcv_nxt) &&
+		    !(tp->mpc && TCP_SKB_CB(skb)->end_seq == TCP_SKB_CB(skb)->seq)) {
 			SOCK_DEBUG(sk, "ofo packet was already received\n");
 			__skb_unlink(skb, &tp->out_of_order_queue);
 			__kfree_skb(skb);
@@ -4507,23 +4511,9 @@ static void tcp_data_queue(struct sock *sk, struct sk_buff *skb)
 		meta_tp = mpcb_meta_tp(mpcb);
 
 	/* If no data is present, but a data_fin is in the options, we still
-	 * have to call mptcp_queue_skb. */
-	if (tp->mpc && mptcp_is_data_fin(skb) &&
-	    TCP_SKB_CB(skb)->seq == TCP_SKB_CB(skb)->end_seq) {
-		__skb_pull(skb, th->doff * 4);
-
-		eaten = mptcp_queue_skb(sk, skb);
-		if (eaten < 0)
-			return;
-
-		if (eaten > 0)
-			__kfree_skb(skb);
-		else if (!sock_flag(sk, SOCK_DEAD) && !tp->mpc)
-			sk->sk_data_ready(sk, 0);
-		return;
-	}
-
-	if (TCP_SKB_CB(skb)->seq == TCP_SKB_CB(skb)->end_seq)
+	 * have to call mptcp_queue_skb later on. */
+	if (TCP_SKB_CB(skb)->seq == TCP_SKB_CB(skb)->end_seq &&
+	    !(tp->mpc && mptcp_is_data_fin(skb)))
 		goto drop;
 
 	/* If mptcp, we need dst for sending the reset in mptcp_queue_skb */
@@ -4698,7 +4688,9 @@ drop:
 
 		/* Do skb overlap to previous one? */
 		if (skb1 && before(seq, TCP_SKB_CB(skb1)->end_seq)) {
-			if (!after(end_seq, TCP_SKB_CB(skb1)->end_seq)) {
+			/* MPTCP allows non-data data-fin to be in the ofo-queue */
+			if (!after(end_seq, TCP_SKB_CB(skb1)->end_seq) &&
+			    !(tp->mpc && end_seq == seq)) {
 				/* All the bits are present. Drop. */
 				__kfree_skb(skb);
 				tcp_dsack_set(sk, seq, end_seq);
@@ -4734,6 +4726,9 @@ drop:
 						 end_seq);
 				break;
 			}
+			/* MPTCP allows non-data data-fin to be in the ofo-queue */
+			if (tp->mpc && TCP_SKB_CB(skb1)->seq == TCP_SKB_CB(skb1)->end_seq)
+				continue;
 			__skb_unlink(skb1, &tp->out_of_order_queue);
 			tcp_dsack_extend(sk, TCP_SKB_CB(skb1)->seq,
 					 TCP_SKB_CB(skb1)->end_seq);

commit b0fde6dc07a032458b41b973f91eb285e49cd3b9

mptcp: Correctly handle empty data-fins in the ofo-queue
MPTCP allows to send an empty DATA_FIN. We need to put this one in the
subflow's out-of-order queue (if it is out-of-order at the
subflow-level), as the current design requires it.

When looking for overlapping mappings, we should properly skip these
skb's. Also, we should not send SACK-blocks for them.
If we don't skip these skb's, we are basically stuck in an infinite
loop, because we "continue", without advancing skb to the next segment.

Long-term it would be better to revisit this whole DATA_FIN-handling. We
should not put this segment in the ofo-queue. But, for backporting this
patch is probably best.

Fixes: d90a354ad (Ensure the Data FIN is in sequence before queuing)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp_input.c
@@ -4446,8 +4446,10 @@ static void tcp_data_queue_ofo(struct sock *sk, struct sk_buff *skb)
 			break;
 		}
 		/* MPTCP allows non-data data-fin to be in the ofo-queue */
-		if (mptcp(tp) && TCP_SKB_CB(skb1)->seq == TCP_SKB_CB(skb1)->end_seq)
+		if (mptcp(tp) && TCP_SKB_CB(skb1)->seq == TCP_SKB_CB(skb1)->end_seq) {
+			skb = skb1;
 			continue;
+		}
 		__skb_unlink(skb1, &tp->out_of_order_queue);
 		tcp_dsack_extend(sk, TCP_SKB_CB(skb1)->seq,
 				 TCP_SKB_CB(skb1)->end_seq);
@@ -4456,7 +4458,7 @@ static void tcp_data_queue_ofo(struct sock *sk, struct sk_buff *skb)
 	}
 
 add_sack:
-	if (tcp_is_sack(tp))
+	if (tcp_is_sack(tp) && seq != end_seq)
 		tcp_sack_new_ofo_skb(sk, seq, end_seq);
 end:
 	if (skb) {