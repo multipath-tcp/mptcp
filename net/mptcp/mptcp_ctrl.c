cb665bc00f1c6bb342629cf7a76c1e19fb87b404
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2016-10-25 13:35:49 +0200

    mptcp: Maintenance release 0.90.3
    Signed-off-by: Baptiste Jonglez <baptiste.jonglez@imag.fr>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2646,7 +2646,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.90.0");
+	pr_info("MPTCP: Stable release v0.90.3");
 
 	mptcp_init_failed = false;
 
