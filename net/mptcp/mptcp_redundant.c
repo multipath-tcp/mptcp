multipath-tcp/mptcp

commit f15a343d382ee4dd96f346a01171aa11d9500265

mptcp_redundant: redundant scheduler for latency sensitive traffic
Signed-off-by: Christian Pinedo <christian.pinedo@ehu.eus>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

net/mptcp/mptcp_redundant.c
@@ -1,0 +1,265 @@
+/*
+ *	MPTCP Scheduler to reduce latency and jitter.
+ *
+ *	This scheduler sends all packets redundantly on all available subflows.
+ *
+ *	Initial Design & Implementation:
+ *	Tobias Erbshaeusser <erbshauesser@dvs.tu-darmstadt.de>
+ *	Alexander Froemmgen <froemmge@dvs.tu-darmstadt.de>
+ *
+ *	Initial corrections & modifications:
+ *	Christian Pinedo <christian.pinedo@ehu.eus>
+ *	Igor Lopez <igor.lopez@ehu.eus>
+ *
+ *	This program is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation; either version
+ *      2 of the License, or (at your option) any later version.
+ */
+
+#include <linux/module.h>
+#include <net/mptcp.h>
+
+/* Struct to store the data of a single subflow */
+struct redsched_sock_data {
+	/* The skb or NULL */
+	struct sk_buff *skb;
+	/* End sequence number of the skb. This number should be checked
+	 * to be valid before the skb field is used
+	 */
+	u32 skb_end_seq;
+};
+
+/* Struct to store the data of the control block */
+struct redsched_cb_data {
+	/* The next subflow where a skb should be sent or NULL */
+	struct tcp_sock *next_subflow;
+};
+
+/* Returns the socket data from a given subflow socket */
+static struct redsched_sock_data *redsched_get_sock_data(struct tcp_sock *tp)
+{
+	return (struct redsched_sock_data *)&tp->mptcp->mptcp_sched[0];
+}
+
+/* Returns the control block data from a given meta socket */
+static struct redsched_cb_data *redsched_get_cb_data(struct tcp_sock *tp)
+{
+	return (struct redsched_cb_data *)&tp->mpcb->mptcp_sched[0];
+}
+
+static bool redsched_get_active_valid_sks(struct sock *meta_sk)
+{
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+	struct mptcp_cb *mpcb = meta_tp->mpcb;
+	struct sock *sk;
+	int active_valid_sks = 0;
+
+	mptcp_for_each_sk(mpcb, sk) {
+		if (subflow_is_active((struct tcp_sock *)sk) &&
+		    !mptcp_is_def_unavailable(sk))
+			active_valid_sks++;
+	}
+
+	return active_valid_sks;
+}
+
+static bool redsched_use_subflow(struct sock *meta_sk,
+				 int active_valid_sks,
+				 struct tcp_sock *tp,
+				 struct sk_buff *skb)
+{
+	if (!skb || !mptcp_is_available((struct sock *)tp, skb, false))
+		return false;
+
+	if (TCP_SKB_CB(skb)->path_mask != 0)
+		return subflow_is_active(tp);
+
+	if (TCP_SKB_CB(skb)->path_mask == 0) {
+		if (active_valid_sks == -1)
+			active_valid_sks = redsched_get_active_valid_sks(meta_sk);
+
+		if (subflow_is_backup(tp) && active_valid_sks > 0)
+			return false;
+		else
+			return true;
+	}
+
+	return false;
+}
+
+static struct sock *redundant_get_subflow(struct sock *meta_sk,
+					  struct sk_buff *skb,
+					  bool zero_wnd_test)
+{
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+	struct mptcp_cb *mpcb = meta_tp->mpcb;
+	struct redsched_cb_data *cb_data = redsched_get_cb_data(meta_tp);
+	struct tcp_sock *first_tp = cb_data->next_subflow;
+	struct sock *sk;
+	struct tcp_sock *tp;
+
+	/* Answer data_fin on same subflow */
+	if (meta_sk->sk_shutdown & RCV_SHUTDOWN &&
+	    skb && mptcp_is_data_fin(skb)) {
+		mptcp_for_each_sk(mpcb, sk) {
+			if (tcp_sk(sk)->mptcp->path_index ==
+				mpcb->dfin_path_index &&
+			    mptcp_is_available(sk, skb, zero_wnd_test))
+				return sk;
+		}
+	}
+
+	if (!first_tp)
+		first_tp = mpcb->connection_list;
+	tp = first_tp;
+
+	/* Search for any subflow to send it */
+	do {
+		if (mptcp_is_available((struct sock *)tp, skb,
+				       zero_wnd_test)) {
+			cb_data->next_subflow = tp->mptcp->next;
+			return (struct sock *)tp;
+		}
+
+		tp = tp->mptcp->next;
+		if (!tp)
+			tp = mpcb->connection_list;
+	} while (tp != first_tp);
+
+	/* No space */
+	return NULL;
+}
+
+/* Corrects the stored skb pointers if they are invalid */
+static void redsched_correct_skb_pointers(struct sock *meta_sk,
+					  struct redsched_sock_data *sk_data)
+{
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+
+	if (sk_data->skb && !after(sk_data->skb_end_seq, meta_tp->snd_una))
+		sk_data->skb = NULL;
+}
+
+/* Returns the next skb from the queue */
+static struct sk_buff *redundant_next_skb_from_queue(struct sk_buff_head *queue,
+						     struct sk_buff *previous)
+{
+	if (skb_queue_empty(queue))
+		return NULL;
+
+	if (!previous)
+		return skb_peek(queue);
+
+	if (skb_queue_is_last(queue, previous))
+		return NULL;
+
+	return skb_queue_next(queue, previous);
+}
+
+static struct sk_buff *redundant_next_segment(struct sock *meta_sk,
+					      int *reinject,
+					      struct sock **subsk,
+					      unsigned int *limit)
+{
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+	struct mptcp_cb *mpcb = meta_tp->mpcb;
+	struct redsched_cb_data *cb_data = redsched_get_cb_data(meta_tp);
+	struct tcp_sock *first_tp = cb_data->next_subflow;
+	struct tcp_sock *tp;
+	struct sk_buff *skb;
+	int active_valid_sks = -1;
+
+	if (skb_queue_empty(&mpcb->reinject_queue) &&
+	    skb_queue_empty(&meta_sk->sk_write_queue))
+		/* Nothing to send */
+		return NULL;
+
+	/* First try reinjections */
+	skb = skb_peek(&mpcb->reinject_queue);
+	if (skb) {
+		*subsk = get_available_subflow(meta_sk, skb, false);
+		if (!*subsk)
+			return NULL;
+		*reinject = 1;
+		return skb;
+	}
+
+	/* Then try indistinctly redundant and normal skbs */
+
+	if (!first_tp)
+		first_tp = mpcb->connection_list;
+	tp = first_tp;
+
+	*reinject = 0;
+	active_valid_sks = redsched_get_active_valid_sks(meta_sk);
+	do {
+		struct redsched_sock_data *sk_data;
+
+		/* Correct the skb pointers of the current subflow */
+		sk_data = redsched_get_sock_data(tp);
+		redsched_correct_skb_pointers(meta_sk, sk_data);
+
+		skb = redundant_next_skb_from_queue(&meta_sk->sk_write_queue,
+						    sk_data->skb);
+		if (skb && redsched_use_subflow(meta_sk, active_valid_sks, tp,
+						skb)) {
+			sk_data->skb = skb;
+			sk_data->skb_end_seq = TCP_SKB_CB(skb)->end_seq;
+			cb_data->next_subflow = tp->mptcp->next;
+			*subsk = (struct sock *)tp;
+			return skb;
+		}
+
+		tp = tp->mptcp->next;
+		if (!tp)
+			tp = mpcb->connection_list;
+	} while (tp != first_tp);
+
+	/* Nothing to send */
+	return NULL;
+}
+
+static void redundant_release(struct sock *sk)
+{
+	struct tcp_sock *tp = tcp_sk(sk);
+	struct redsched_cb_data *cb_data = redsched_get_cb_data(tp);
+
+	/* Check if the next subflow would be the released one. If yes correct
+	 * the pointer
+	 */
+	if (cb_data->next_subflow == tp)
+		cb_data->next_subflow = tp->mptcp->next;
+}
+
+struct mptcp_sched_ops mptcp_sched_redundant = {
+	.get_subflow = redundant_get_subflow,
+	.next_segment = redundant_next_segment,
+	.release = redundant_release,
+	.name = "redundant",
+	.owner = THIS_MODULE,
+};
+
+static int __init redundant_register(void)
+{
+	BUILD_BUG_ON(sizeof(struct redsched_sock_data) > MPTCP_SCHED_SIZE);
+	BUILD_BUG_ON(sizeof(struct redsched_cb_data) > MPTCP_SCHED_DATA_SIZE);
+
+	if (mptcp_register_scheduler(&mptcp_sched_redundant))
+		return -1;
+
+	return 0;
+}
+
+static void redundant_unregister(void)
+{
+	mptcp_unregister_scheduler(&mptcp_sched_redundant);
+}
+
+module_init(redundant_register);
+module_exit(redundant_unregister);
+
+MODULE_AUTHOR("Tobias Erbshaeusser, Alexander Froemmgen");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("REDUNDANT MPTCP");
+MODULE_VERSION("0.90");

include/net/mptcp.h
@@ -189,7 +189,7 @@ struct mptcp_tcp_sock {
 	u8	loc_id;
 	u8	rem_id;
 
-#define MPTCP_SCHED_SIZE 4
+#define MPTCP_SCHED_SIZE 16
 	u8	mptcp_sched[MPTCP_SCHED_SIZE] __aligned(8);
 
 	struct sk_buff  *shortcut_ofoqueue; /* Shortcut to the current modified
@@ -254,6 +254,7 @@ struct mptcp_sched_ops {
 						struct sock **subsk,
 						unsigned int *limit);
 	void			(*init)(struct sock *sk);
+	void			(*release)(struct sock *sk);
 
 	char			name[MPTCP_SCHED_NAME_MAX];
 	struct module		*owner;
@@ -286,6 +287,8 @@ struct mptcp_cb {
 	u8 cnt_subflows;
 	u8 cnt_established;
 
+#define MPTCP_SCHED_DATA_SIZE 8
+	u8 mptcp_sched[MPTCP_SCHED_DATA_SIZE] __aligned(8);
 	struct mptcp_sched_ops *sched_ops;
 
 	struct sk_buff_head reinject_queue;
@@ -907,6 +910,13 @@ void mptcp_init_scheduler(struct mptcp_cb *mpcb);
 void mptcp_cleanup_scheduler(struct mptcp_cb *mpcb);
 void mptcp_get_default_scheduler(char *name);
 int mptcp_set_default_scheduler(const char *name);
+bool mptcp_is_available(struct sock *sk, const struct sk_buff *skb,
+			bool zero_wnd_test);
+bool mptcp_is_def_unavailable(struct sock *sk);
+bool subflow_is_active(const struct tcp_sock *tp);
+bool subflow_is_backup(const struct tcp_sock *tp);
+struct sock *get_available_subflow(struct sock *meta_sk, struct sk_buff *skb,
+				   bool zero_wnd_test);
 extern struct mptcp_sched_ops mptcp_sched_default;
 
 /* Initializes function-pointers and MPTCP-flags */

net/mptcp/Kconfig
@@ -85,6 +85,13 @@ config MPTCP_ROUNDROBIN
 	  This is a very simple round-robin scheduler. Probably has bad performance
 	  but might be interesting for researchers.
 
+config MPTCP_REDUNDANT
+	tristate "MPTCP Redundant"
+	depends on (MPTCP=y)
+	---help---
+	  This scheduler sends all packets redundantly over all subflows to decreases
+	  latency and jitter on the cost of lower throughput.
+
 choice
 	prompt "Default MPTCP Scheduler"
 	default DEFAULT
@@ -103,6 +110,12 @@ choice
 		  This is the round-rob scheduler, sending in a round-robin
 		  fashion..
 
+	config DEFAULT_REDUNDANT
+		bool "Redundant" if MPTCP_REDUNDANT=y
+		---help---
+		  This is the redundant scheduler, sending packets redundantly over
+		  all the subflows.
+
 endchoice
 endif
 
@@ -111,5 +124,6 @@ config DEFAULT_MPTCP_SCHED
 	depends on (MPTCP=y)
 	default "default" if DEFAULT_SCHEDULER
 	default "roundrobin" if DEFAULT_ROUNDROBIN
+	default "redundant" if DEFAULT_REDUNDANT
 	default "default"
 

net/mptcp/Makefile
@@ -16,6 +16,7 @@ obj-$(CONFIG_MPTCP_FULLMESH) += mptcp_fullmesh.o
 obj-$(CONFIG_MPTCP_NDIFFPORTS) += mptcp_ndiffports.o
 obj-$(CONFIG_MPTCP_BINDER) += mptcp_binder.o
 obj-$(CONFIG_MPTCP_ROUNDROBIN) += mptcp_rr.o
+obj-$(CONFIG_MPTCP_REDUNDANT) += mptcp_redundant.o
 
 mptcp-$(subst m,y,$(CONFIG_IPV6)) += mptcp_ipv6.o
 

net/mptcp/mptcp_ctrl.c
@@ -1322,6 +1322,9 @@ void mptcp_del_sock(struct sock *sk)
 	mpcb = tp->mpcb;
 	tp_prev = mpcb->connection_list;
 
+	if (mpcb->sched_ops->release)
+		mpcb->sched_ops->release(sk);
+
 	mptcp_debug("%s: Removing subsock tok %#x pi:%d state %d is_meta? %d\n",
 		    __func__, mpcb->mptcp_loc_token, tp->mptcp->path_index,
 		    sk->sk_state, is_meta_sk(sk));

net/mptcp/mptcp_sched.c
@@ -15,7 +15,7 @@ static struct defsched_priv *defsched_get_priv(const struct tcp_sock *tp)
 	return (struct defsched_priv *)&tp->mptcp->mptcp_sched[0];
 }
 
-static bool mptcp_is_def_unavailable(struct sock *sk)
+bool mptcp_is_def_unavailable(struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
 
@@ -34,6 +34,7 @@ static bool mptcp_is_def_unavailable(struct sock *sk)
 
 	return false;
 }
+EXPORT_SYMBOL_GPL(mptcp_is_def_unavailable);
 
 static bool mptcp_is_temp_unavailable(struct sock *sk,
 				      const struct sk_buff *skb,
@@ -101,12 +102,13 @@ static bool mptcp_is_temp_unavailable(struct sock *sk,
 }
 
 /* Is the sub-socket sk available to send the skb? */
-static bool mptcp_is_available(struct sock *sk, const struct sk_buff *skb,
-			       bool zero_wnd_test)
+bool mptcp_is_available(struct sock *sk, const struct sk_buff *skb,
+			bool zero_wnd_test)
 {
 	return !mptcp_is_def_unavailable(sk) &&
 	       !mptcp_is_temp_unavailable(sk, skb, zero_wnd_test);
 }
+EXPORT_SYMBOL_GPL(mptcp_is_available);
 
 /* Are we not allowed to reinject this skb on tp? */
 static int mptcp_dont_reinject_skb(const struct tcp_sock *tp, const struct sk_buff *skb)
@@ -119,15 +121,17 @@ static int mptcp_dont_reinject_skb(const struct tcp_sock *tp, const struct sk_bu
 		mptcp_pi_to_flag(tp->mptcp->path_index) & TCP_SKB_CB(skb)->path_mask;
 }
 
-static bool subflow_is_backup(const struct tcp_sock *tp)
+bool subflow_is_backup(const struct tcp_sock *tp)
 {
 	return tp->mptcp->rcv_low_prio || tp->mptcp->low_prio;
 }
+EXPORT_SYMBOL_GPL(subflow_is_backup);
 
-static bool subflow_is_active(const struct tcp_sock *tp)
+bool subflow_is_active(const struct tcp_sock *tp)
 {
 	return !tp->mptcp->rcv_low_prio && !tp->mptcp->low_prio;
 }
+EXPORT_SYMBOL_GPL(subflow_is_active);
 
 /* Generic function to iterate over used and unused subflows and to select the
  * best one
@@ -214,9 +218,8 @@ static struct sock
  *
  * Additionally, this function is aware of the backup-subflows.
  */
-static struct sock *get_available_subflow(struct sock *meta_sk,
-					  struct sk_buff *skb,
-					  bool zero_wnd_test)
+struct sock *get_available_subflow(struct sock *meta_sk, struct sk_buff *skb,
+				   bool zero_wnd_test)
 {
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct sock *sk;
@@ -261,6 +264,7 @@ static struct sock *get_available_subflow(struct sock *meta_sk,
 		TCP_SKB_CB(skb)->path_mask = 0;
 	return sk;
 }
+EXPORT_SYMBOL_GPL(get_available_subflow);
 
 static struct sk_buff *mptcp_rcv_buf_optimization(struct sock *sk, int penal)
 {

commit a8405ec882b52e0689732080dbb5aee7ff265492

mptcp: Avoid over-counting packets_out with the redundant scheduler
The redundant scheduler is reinjecting data constantly. Thus, we have to
set the reinject-indicator correctly to avoid increasing
meta_tp->packets_out for each of these reinjected packets.

Otherwise we will crash with:
[   70.243585] ------------[ cut here ]------------
[   70.362167] kernel BUG at net/mptcp/mptcp_output.c:1421!
[   70.459047] invalid opcode: 0000 [#1] SMP
[   70.459504] Modules linked in: mptcp_redundant fuse vboxsf(O) tun snd_intel8x0 snd_ac97_codec snd_pcm crct10dif_pclmul crc32_pclmul joydev snd_seq_midi snd_seq_midi_event snd_rawmidi ip6table_filter aesni_intel aes_x86_64 lrw vboxvideo(O) snd_seq ip6_tables snd_seq_device snd_timer gf128mul ipt_MASQUERADE glue_helper ablk_helper snd nf_nat_masquerade_ipv4 cryptd serio_raw sg iptable_nat ttm 8250_fintek evdev soundcore nf_conntrack_ipv4 battery nf_defrag_ipv4 drm_kms_helper nf_nat_ipv4 drm ac ac97_bus nf_nat vboxguest(O) nf_conntrack iptable_filter i2c_piix4 acpi_cpufreq processor ip_tables x_tables parport_pc ppdev lp parport autofs4 ext4 crc16 mbcache jbd2 hid_generic usbhid hid sr_mod cdrom ata_generic sd_mod ohci_pci ehci_pci ohci_hcd ehci_hcd ata_piix ahci libahci crc32c_intel libata psmouse scsi_mod video usbcore thermal_sys e1000 usb_common button
[   70.467906] CPU: 1 PID: 0 Comm: swapper/1 Tainted: G           O    4.1.26.mptcp #2
[   70.469678] Hardware name: innotek GmbH VirtualBox/VirtualBox, BIOS VirtualBox 12/01/2006
[   70.472563] task: ffff88011afbaa10 ti: ffff88011afc8000 task.ti: ffff88011afc8000
[   70.474300] RIP: 0010:[<ffffffff815691b8>]  [<ffffffff815691b8>] mptcp_retransmit_skb+0x1b8/0x1f0
[   70.475134] RSP: 0018:ffff88011fd03de8  EFLAGS: 00010246
[   70.475636] RAX: 0000000000000594 RBX: ffff8800d6b45800 RCX: 0000000000000000
[   70.476290] RDX: 0000000000000000 RSI: 0000000000000000 RDI: ffff8800d9222800
[   70.476936] RBP: ffff8800daf2a000 R08: ffff8800d73c5ac0 R09: 0000000000000000
[   70.477582] R10: 0000000000000003 R11: 0000000000000020 R12: ffff8800d9222800
[   70.478228] R13: 0000000000000594 R14: ffff8800d6b45800 R15: 0000000000000000
[   70.478882] FS:  0000000000000000(0000) GS:ffff88011fd00000(0000) knlGS:0000000000000000
[   70.481707] CS:  0010 DS: 0000 ES: 0000 CR0: 000000008005003b
[   70.483298] CR2: 0000558224ed1d50 CR3: 00000000d73e5000 CR4: 00000000000406e0
[   70.483930] Stack:
[   70.484118]  ffff88011fd168c0 ffff8800d6b45800 ffff8800d6b45930 ffff8800d6b45878
[   70.484825]  ffffffff814da030 ffffffff8156936a ffff88011fd168c0 0000000000000096
[   70.485545]  0000000000000000 ffff8800d6b45800 ffff8800d6b45874 ffffffff814d9ed5
[   70.486286] Call Trace:
[   70.517886]  <IRQ>
[   70.575014]  [<ffffffff814da030>] ? tcp_write_timer_handler+0x210/0x210
[   70.747294]  [<ffffffff8156936a>] ? mptcp_meta_retransmit_timer+0x17a/0x410
[   70.807764]  [<ffffffff814d9ed5>] ? tcp_write_timer_handler+0xb5/0x210
[   70.808582]  [<ffffffff814da0dc>] ? tcp_write_timer+0xac/0xb0
[   70.842805]  [<ffffffff810d81b0>] ? call_timer_fn+0x30/0x140
[   70.844462]  [<ffffffff814da030>] ? tcp_write_timer_handler+0x210/0x210
[   70.845520]  [<ffffffff810d9fb7>] ? run_timer_softirq+0x267/0x340
[   70.847869]  [<ffffffff8101e375>] ? read_tsc+0x5/0x10
[   70.848750]  [<ffffffff810795c4>] ? __do_softirq+0x124/0x2d0
[   70.850483]  [<ffffffff810798e5>] ? irq_exit+0x95/0xa0
[   70.851701]  [<ffffffff815925fe>] ? smp_apic_timer_interrupt+0x3e/0x50
[   70.853747]  [<ffffffff815906be>] ? apic_timer_interrupt+0x6e/0x80
[   70.962474]  <EOI>
[   71.006753]  [<ffffffff8101f7f0>] ? idle_notifier_unregister+0x20/0x20
[   71.076477]  [<ffffffff8105c102>] ? native_safe_halt+0x2/0x10
[   71.095392]  [<ffffffff8101f80c>] ? default_idle+0x1c/0xf0
[   71.096232]  [<ffffffff810b4d23>] ? cpu_startup_entry+0x393/0x460
[   71.098164]  [<ffffffff8158f118>] ? _raw_spin_unlock_irqrestore+0x18/0x50
[   71.099096]  [<ffffffff8104bd35>] ? start_secondary+0x185/0x1b0
[   71.099953] Code: 85 c0 75 09 8b 45 14 89 83 4c 07 00 00 ba 04 00 00 00 44 89 ee 4c 89 e7 e8 06 e4 f6 ff 31 c0 e9 02 ff ff ff 0f 1f 80 00 00 00 00 <0f> 0b 66 0f 1f 44 00 00 b8 ff ff ff ff 31 d2 41 b8 01 00 00 00
[   71.150504] RIP  [<ffffffff815691b8>] mptcp_retransmit_skb+0x1b8/0x1f0
[   71.327359]  RSP <ffff88011fd03de8>
[   71.410490] ------------[ cut here ]------------

Issue: https://github.com/multipath-tcp/mptcp/issues/138

Fixes: f15a343d382ee (mptcp_redundant: redundant scheduler for latency sensitive traffic)

net/mptcp/mptcp_redundant.c
@@ -208,6 +208,9 @@ static struct sk_buff *redundant_next_segment(struct sock *meta_sk,
 			sk_data->skb_end_seq = TCP_SKB_CB(skb)->end_seq;
 			cb_data->next_subflow = tp->mptcp->next;
 			*subsk = (struct sock *)tp;
+
+			if (TCP_SKB_CB(skb)->path_mask)
+				*reinject = -1;
 			return skb;
 		}
 