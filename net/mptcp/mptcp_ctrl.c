multipath-tcp/mptcp

commit 3660726597f31d287fdb899e2ae6e8bfa89b99c5

Fix: Handle error-signals correctly while in fallback-mode
If we have fallen back to regular TCP, we should interpret a RST or any
other error-signal (e.g., coming from tcp_v4_err) as an error on the
whole connection.

This patch modifies mptcp_sock_def_error_report, to check on the
infinite-flags of the mpcb and propagate the error up to the
meta-socket.

Reported-by: Krishna Khanal <krishna.khanal2@citrix.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
Tested-by: Krishna Khanal <krishna.khanal2@citrix.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_ctrl.c
@@ -197,8 +197,22 @@ struct sock *mptcp_select_ack_sock(const struct sock *meta_sk, int copied)
 
 static void mptcp_sock_def_error_report(struct sock *sk)
 {
+	struct mptcp_cb *mpcb = tcp_sk(sk)->mpcb;
+
 	if (!sock_flag(sk, SOCK_DEAD))
 		mptcp_sub_close(sk, 0);
+
+	if (mpcb->infinite_mapping_rcv || mpcb->infinite_mapping_snd) {
+		struct sock *meta_sk = mptcp_meta_sk(sk);
+
+		meta_sk->sk_err = sk->sk_err;
+		meta_sk->sk_err_soft = sk->sk_err_soft;
+
+		if (!sock_flag(meta_sk, SOCK_DEAD))
+			meta_sk->sk_error_report(meta_sk);
+
+		tcp_done(meta_sk);
+	}
 
 	sk->sk_err = 0;
 	return;

commit 678a37d90c977e3436ebabb628f0d1b2c842d950

mptcp: Avoid call to tcp_done on an already closed socket
It is very dangerous to call tcp_done on a socket that is already
closed. We will be corrupting memory. So, prevent this at all cost and
add some debugging to trace down such issues.

I have the feeling that this one here might fix some of the memory
corruptions we see in a bunch of github issues:

https://github.com/multipath-tcp/mptcp/issues/133
https://github.com/multipath-tcp/mptcp/issues/130
https://github.com/multipath-tcp/mptcp/issues/99
https://github.com/multipath-tcp/mptcp/issues/97

Fixes: 3660726597f31 (Fix: Handle error-signals correctly while in fallback-mode)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -623,7 +623,13 @@ static void mptcp_sock_def_error_report(struct sock *sk)
 		if (!sock_flag(meta_sk, SOCK_DEAD))
 			meta_sk->sk_error_report(meta_sk);
 
-		tcp_done(meta_sk);
+		WARN(meta_sk->sk_state == TCP_CLOSE,
+		     "Meta already closed i_rcv %u i_snd %u send_i %u flags %#lx\n",
+		     mpcb->infinite_mapping_rcv, mpcb->infinite_mapping_snd,
+		     mpcb->send_infinite_mapping, meta_sk->sk_flags);
+
+		if (meta_sk->sk_state != TCP_CLOSE)
+			tcp_done(meta_sk);
 	}
 
 	sk->sk_err = 0;

net/mptcp/mptcp_output.c
@@ -1261,7 +1261,8 @@ void mptcp_send_active_reset(struct sock *meta_sk, gfp_t priority)
 	sk = mptcp_select_ack_sock(meta_sk);
 
 	/* May happen if no subflow is in an appropriate state, OR
-	 * we are in infinite mode or about to go there - just send a reset */
+	 * we are in infinite mode or about to go there - just send a reset
+	 */
 	if (!sk || mpcb->infinite_mapping_snd || mpcb->send_infinite_mapping ||
 	    mpcb->infinite_mapping_rcv) {
 