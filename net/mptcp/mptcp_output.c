multipath-tcp/mptcp

commit 09beb64a8fb7025af33365d466f938f5821043e0

mptcp: remove mptso_fragment
In the MPTCP case mptso_fragment can be called for frames that already
leaved the meta-sock so we must ajust the packet counters. This makes
mptso_fragment very similar with mptcp_fragment.

To remove some code duplication lets use mptcp_fragment instead of
mptso_fragment, the overhead should be very small.

Depends-on: e5d878af34b2e (tcp: add gfp parameter to tcp_fragment)
Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_output.c
@@ -582,7 +582,7 @@ const int mptcp_dss_len = MPTCP_SUB_LEN_DSS_ALIGN + MPTCP_SUB_LEN_ACK_ALIGN +
  * our sends out a subset of the initial mapping).
  *
  * Furthermore, the skb checksum is not always preserved across splits
- * (e.g. mptso_fragment) which would mean that we need to recompute
+ * (e.g. mptcp_fragment) which would mean that we need to recompute
  * the DSS checksum in this case.
  *
  * To avoid this we save the initial DSS mapping which allows us to
@@ -844,83 +844,6 @@ int mptcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 	 * adjust the various packet counters.
 	 */
 	if (!before(tp->snd_nxt, TCP_SKB_CB(buff)->end_seq) && reinject != 1) {
-		int diff = old_factor - tcp_skb_pcount(skb) -
-			tcp_skb_pcount(buff);
-
-		if (diff)
-			tcp_adjust_pcount(sk, skb, diff);
-	}
-
-	/* Link BUFF into the send queue. */
-	skb_header_release(buff);
-	if (reinject == 1)
-		__skb_queue_after(&tcp_sk(sk)->mpcb->reinject_queue, skb, buff);
-	else
-		tcp_insert_write_queue_after(skb, buff, sk);
-
-	return 0;
-}
-
-int mptso_fragment(struct sock *sk, struct sk_buff *skb, unsigned int len,
-		   unsigned int mss_now, gfp_t gfp, int reinject)
-{
-	struct sk_buff *buff;
-	int nlen = skb->len - len, old_factor;
-	u8 flags;
-
-	/* All of a TSO frame must be composed of paged data.  */
-	if (skb->len != skb->data_len)
-		return mptcp_fragment(sk, skb, len, mss_now, reinject);
-
-	buff = sk_stream_alloc_skb(sk, 0, gfp);
-	if (unlikely(buff == NULL))
-		return -ENOMEM;
-
-	/* See below - if reinject == 1, the buff will be added to the reinject-
-	 * queue, which is currently not part of the memory-accounting.
-	 */
-	if (reinject != 1) {
-		sk->sk_wmem_queued += buff->truesize;
-		sk_mem_charge(sk, buff->truesize);
-	}
-	buff->truesize += nlen;
-	skb->truesize -= nlen;
-
-	/* Correct the sequence numbers. */
-	TCP_SKB_CB(buff)->seq = TCP_SKB_CB(skb)->seq + len;
-	TCP_SKB_CB(buff)->end_seq = TCP_SKB_CB(skb)->end_seq;
-	TCP_SKB_CB(skb)->end_seq = TCP_SKB_CB(buff)->seq;
-
-	/* PSH and FIN should only be set in the second packet. */
-	flags = TCP_SKB_CB(skb)->tcp_flags;
-	TCP_SKB_CB(skb)->tcp_flags = flags & ~(TCPHDR_FIN | TCPHDR_PSH);
-	TCP_SKB_CB(buff)->tcp_flags = flags;
-
-	flags = TCP_SKB_CB(skb)->mptcp_flags;
-	TCP_SKB_CB(skb)->mptcp_flags = flags & ~(MPTCPHDR_FIN);
-	TCP_SKB_CB(buff)->mptcp_flags = flags;
-
-	/* This packet was never sent out yet, so no SACK bits. */
-	TCP_SKB_CB(buff)->sacked = 0;
-
-	buff->ip_summed = CHECKSUM_PARTIAL;
-	skb->ip_summed = CHECKSUM_PARTIAL;
-	skb_split(skb, buff, len);
-
-	/* We lost the dss-option when creating buff - put it back! */
-	if (!is_meta_sk(sk))
-		memcpy(TCP_SKB_CB(buff)->dss, TCP_SKB_CB(skb)->dss, mptcp_dss_len);
-
-	old_factor = tcp_skb_pcount(skb);
-
-	/* Fix up tso_factor for both original and new SKB.  */
-	tcp_set_skb_tso_segs(sk, skb, mss_now);
-	tcp_set_skb_tso_segs(sk, buff, mss_now);
-
-	/* If this packet has been sent out already, we must
-	 * adjust the various packet counters.
-	 */
-	if (!before(tcp_sk(sk)->snd_nxt, TCP_SKB_CB(buff)->end_seq) && reinject != 1) {
 		int diff = old_factor - tcp_skb_pcount(skb) -
 			tcp_skb_pcount(buff);
 
@@ -1229,7 +1152,7 @@ retry:
 			 * If it is not possible to send the TSO segment on the
 			 * best subflow right now try to look for another subflow.
 			 * If there is no subflow available defer the segment to avoid
-			 * the call to mptso_fragment.
+			 * the call to mptcp_fragment.
 			 */
 			if (!push_one && !reinject && tcp_tso_should_defer(subsk, skb)) {
 				mpcb->noneligible |= mptcp_pi_to_flag(subtp->mptcp->path_index);
@@ -1246,7 +1169,7 @@ retry:
 						    nonagle);
 
 		if (skb->len > limit &&
-		    unlikely(mptso_fragment(meta_sk, skb, limit, mss_now, gfp, reinject)))
+		    unlikely(mptcp_fragment(meta_sk, skb, limit, mss_now, gfp, reinject)))
 			break;
 
 		subskb = mptcp_skb_entail(subsk, skb, reinject);
@@ -1965,7 +1888,7 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 					          TCP_NAGLE_OFF);
 
 	if (skb->len > limit &&
-	    unlikely(mptso_fragment(meta_sk, skb, limit, mss_now,
+	    unlikely(mptcp_fragment(meta_sk, skb, limit, mss_now,
 				    GFP_ATOMIC, 0)))
 		goto failed;
 

include/net/mptcp.h
@@ -775,8 +775,6 @@ void __init mptcp_init(void);
 int mptcp_trim_head(struct sock *sk, struct sk_buff *skb, u32 len);
 int mptcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 		   unsigned int mss_now, gfp_t gfp, int reinject);
-int mptso_fragment(struct sock *sk, struct sk_buff *skb, unsigned int len,
-		   unsigned int mss_now, gfp_t gfp, int reinject);
 void mptcp_destroy_sock(struct sock *sk);
 int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 				    struct sk_buff *skb,
@@ -1398,12 +1396,6 @@ static inline int mptcp_trim_head(struct sock *sk, struct sk_buff *skb, u32 len)
 }
 static inline int mptcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 				 unsigned int mss_now, gfp_t gfp, int reinject)
-{
-	return 0;
-}
-static inline int mptso_fragment(struct sock *sk, struct sk_buff *skb,
-				 unsigned int len, unsigned int mss_now,
-				 gfp_t gfp, int reinject)
 {
 	return 0;
 }

net/ipv4/tcp_output.c
@@ -1659,7 +1659,7 @@ static int tso_fragment(struct sock *sk, struct sk_buff *skb, unsigned int len,
 	u8 flags;
 
 	if (tcp_sk(sk)->mpc && mptcp_is_data_seq(skb))
-		mptso_fragment(sk, skb, len, mss_now, gfp, 0);
+		mptcp_fragment(sk, skb, len, mss_now, gfp, 0);
 
 	/* All of a TSO frame must be composed of paged data.  */
 	if (skb->len != skb->data_len)

commit 04cd2d22d8c832e3bc6f57f2251f288399b56ab9

mptcp: Reset TCP-fields in tcp_skb_cb when reinjecting data
When we are reinjecting, the skb might have been sacked or even an
ECN-bit might have been set on it by the TCP-subflow. Thus, we need to
zero out these fields, as this is a fresh skb that will hit a new
subflow.

Having tcp_skb_cb->sacked set might cause a whole lot of trouble, because in
tcp_adjust_pcount, we might end up changing sacked_out of the meta-sk,
which will completely confuse the stack. So, to make sure of this we
added a WARN statement to catch these kind of corruptions.

Fixes: 09beb64a8fb (mptcp: remove mptso_fragment)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_output.c
@@ -145,6 +145,10 @@ static void __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk
 
 	skb->sk = meta_sk;
 
+	/* Reset subflow-specific TCP control-data */
+	TCP_SKB_CB(skb)->sacked = 0;
+	TCP_SKB_CB(skb)->tcp_flags &= (TCPHDR_ACK | TCPHDR_PSH);
+
 	/* If it reached already the destination, we don't have to reinject it */
 	if (!after(TCP_SKB_CB(skb)->end_seq, meta_tp->snd_una)) {
 		__kfree_skb(skb);
@@ -468,10 +472,7 @@ static bool mptcp_skb_entail(struct sock *sk, struct sk_buff *skb, int reinject)
 	mptcp_save_dss_data_seq(tp, subskb);
 
 	tcb->seq = tp->write_seq;
-	tcb->sacked = 0; /* reset the sacked field: from the point of view
-			  * of this subflow, we are sending a brand new
-			  * segment
-			  */
+
 	/* Take into account seg len */
 	tp->write_seq += subskb->len + ((tcb->tcp_flags & TCPHDR_FIN) ? 1 : 0);
 	tcb->end_seq = tp->write_seq;
@@ -582,6 +583,9 @@ int mptcp_write_wakeup(struct sock *meta_sk)
 		unsigned int seg_size = tcp_wnd_end(meta_tp) - TCP_SKB_CB(skb)->seq;
 		struct sock *subsk = meta_tp->mpcb->sched_ops->get_subflow(meta_sk, skb, true);
 		struct tcp_sock *subtp;
+
+		WARN_ON(TCP_SKB_CB(skb)->sacked);
+
 		if (!subsk)
 			goto window_probe;
 		subtp = tcp_sk(subsk);
@@ -660,6 +664,9 @@ bool mptcp_write_xmit(struct sock *meta_sk, unsigned int mss_now, int nonagle,
 	while ((skb = mpcb->sched_ops->next_segment(meta_sk, &reinject, &subsk,
 						    &sublimit))) {
 		unsigned int limit;
+
+		WARN(TCP_SKB_CB(skb)->sacked, "sacked: %u reinject: %u",
+		     TCP_SKB_CB(skb)->sacked, reinject);
 
 		subtp = tcp_sk(subsk);
 		mss_now = tcp_current_mss(subsk);
@@ -1391,6 +1398,8 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 	struct sock *subsk;
 	unsigned int limit, mss_now;
 	int err = -1;
+
+	WARN_ON(TCP_SKB_CB(skb)->sacked);
 
 	/* Do not sent more than we queued. 1/4 is reserved for possible
 	 * copying overhead: fragmentation, tunneling, mangling etc.