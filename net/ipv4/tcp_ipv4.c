b1fb9d144d5ee49bd6411aeaa717e448f06f85fb
Christoph Paasch <cpaasch@apple.com>
2016-12-22 15:49:08 -0800

    mptcp: Need to correctly copy MPTCP-sockets
    Regular TCP does a call to sk_clone_lock, when inheriting from the
    listener. In there, the socket gets allocated and everything relevant set to 0
    by sock_prot_alloc (taking care that the hlist won't get zero'd out).
    
    Then, the call to sock_clone copies from the listener and again pays
    attention to not copy into the hlist memory-region. Finally, it
    initializes the hlist with a call to sk_node_init() which sets pprev to
    NULL.
    
    All of this to maintain RCU list-processing.
    
    For MPTCP, we absolutely need to do the same. Meaning, when we copy we
    have to not overwrite the tk_table hlist. Additionally, we have to make
    sure that pprev is initialized to NULL.
    
    We do this in the patch here by adding a new callback "copy_sk" that
    allows to call our custom copy function.
    
    This here probably fixes some of the memory-corruption issues people
    have been reporting on the github.
    
    Fixes: Zero-day bug
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -2421,6 +2421,22 @@ static void tcp_v4_clear_sk(struct sock *sk, int size)
 
 	memset(&tp->tk_table.pprev, 0, size - offsetof(struct tcp_sock, tk_table.pprev));
 }
+
+void tcp_copy_sk(struct sock *nsk, const struct sock *osk)
+{
+	struct tcp_sock *ntp = tcp_sk(nsk);
+	struct tcp_sock *otp = tcp_sk(osk);
+
+	memcpy(nsk, osk, offsetof(struct sock, sk_dontcopy_begin));
+
+	memcpy(&nsk->sk_dontcopy_end, &osk->sk_dontcopy_end,
+	       offsetof(struct tcp_sock, tk_table.next) - offsetof(struct sock, sk_dontcopy_end));
+
+	memcpy(&ntp->tk_table.pprev, &otp->tk_table.pprev,
+	       osk->sk_prot->obj_size - offsetof(struct tcp_sock, tk_table.pprev));
+
+	ntp->tk_table.pprev = NULL;
+}
 #endif
 
 struct proto tcp_prot = {
@@ -2471,6 +2487,7 @@ struct proto tcp_prot = {
 #endif
 #ifdef CONFIG_MPTCP
 	.clear_sk		= tcp_v4_clear_sk,
+	.copy_sk		= tcp_copy_sk,
 #endif
 };
 EXPORT_SYMBOL(tcp_prot);
