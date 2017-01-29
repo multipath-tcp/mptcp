multipath-tcp/mptcp

commit 800305afc267d3e7f9326ac00bffa1bd71321372

MPTCP : fix if_idx in ndiffports
if_idx was not initialized

Fixes: 9e763119cccc6 (mptcp: Check interface index in fullmesh pm)
Signed-off-by: Benjamin Hesmans <benjamin.hesmans@uclouvain.be>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
(cherry picked from commit a2ba2dac2d419c274d86f1221a49f99f004262a6)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

net/mptcp/mptcp_ndiffports.c
@@ -62,6 +62,7 @@ next_subflow:
 			loc.addr.s_addr = inet_sk(meta_sk)->inet_saddr;
 			loc.loc4_id = 0;
 			loc.low_prio = 0;
+			loc.if_idx = mpcb->master_sk->sk_bound_dev_if;
 
 			rem.addr.s_addr = inet_sk(meta_sk)->inet_daddr;
 			rem.port = inet_sk(meta_sk)->inet_dport;
@@ -76,6 +77,7 @@ next_subflow:
 			loc.addr = inet6_sk(meta_sk)->saddr;
 			loc.loc6_id = 0;
 			loc.low_prio = 0;
+			loc.if_idx = mpcb->master_sk->sk_bound_dev_if;
 
 			rem.addr = meta_sk->sk_v6_daddr;
 			rem.port = inet_sk(meta_sk)->inet_dport;

commit a6e1173d3849d1cdb8fb8b65eb1bb7743cc2a021

mptcp: Avoid accessing master_sk if it is NULL
We can have:
[ 1009.560781] BUG: unable to handle kernel NULL pointer dereference at 0000000000000014
[ 1009.561021] IP: [<ffffffff815f8dea>] create_subflow_worker+0x12a/0x1b0
[ 1009.561021] PGD 3881f067 PUD 3c28c067 PMD 0
[ 1009.561021] Oops: 0000 [#1] SMP
[ 1009.561021] Modules linked in:
[ 1009.561021] CPU: 2 PID: 6474 Comm: kworker/u8:2 Not tainted 4.1.38.mptcp #156
[ 1009.561021] Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS rel-1.7.5-0-ge51488c-20140602_164612-nilsson.home.kraxel.org 04/01/2014
[ 1009.561021] Workqueue: mptcp_wq create_subflow_worker
[ 1009.575395] task: ffff88003fa93d80 ti: ffff88003da44000 task.ti: ffff88003da44000
[ 1009.575395] RIP: 0010:[<ffffffff815f8dea>]  [<ffffffff815f8dea>] create_subflow_worker+0x12a/0x1b0
[ 1009.575395] RSP: 0018:ffff88003da47da8  EFLAGS: 00010282
[ 1009.575395] RAX: 000000000101010a RBX: ffff880039acd800 RCX: 0000000000000000
[ 1009.575395] RDX: ffff88003da47dac RSI: ffff88003da47dc0 RDI: ffff880039acd800
[ 1009.575395] RBP: ffff88003da47df8 R08: ffff88003da44000 R09: 0000000000000004
[ 1009.575395] R10: ffff88003fd14b28 R11: 0000000000004786 R12: 0000000000000006
[ 1009.575395] R13: ffff88003f8eca80 R14: ffff88003f8ecb00 R15: ffff88003f80d000
[ 1009.575395] FS:  0000000000000000(0000) GS:ffff88003fd00000(0000) knlGS:0000000000000000
[ 1009.575395] CS:  0010 DS: 0000 ES: 0000 CR0: 000000008005003b
[ 1009.575395] CR2: 0000000000000014 CR3: 0000000039c74000 CR4: 00000000000006e0
[ 1009.575395] Stack:
[ 1009.575395]  500088003da47dc8 ffff88000101020a ffff88003f961180 000000053fb79200
[ 1009.575395]  000000000101010a ffff88003f80d000 ffff88003f8ecb50 ffff88003f961180
[ 1009.575395]  ffff88003fb79200 0000000000000000 ffff88003da47e48 ffffffff81067a92
[ 1009.575395] Call Trace:
[ 1009.575395]  [<ffffffff81067a92>] process_one_work+0x142/0x440
[ 1009.575395]  [<ffffffff81067ea2>] worker_thread+0x112/0x510
[ 1009.575395]  [<ffffffff8160f526>] ? _raw_spin_unlock_irqrestore+0x16/0x30
[ 1009.575395]  [<ffffffff81067d90>] ? process_one_work+0x440/0x440
[ 1009.575395]  [<ffffffff8106d584>] kthread+0xc4/0xe0
[ 1009.575395]  [<ffffffff8106d4c0>] ? kthread_create_on_node+0x170/0x170
[ 1009.575395]  [<ffffffff8160fe12>] ret_from_fork+0x42/0x70
[ 1009.575395]  [<ffffffff8106d4c0>] ? kthread_create_on_node+0x170/0x170
[ 1009.575395] Code: 70 0f b7 43 10 66 83 f8 02 0f 85 22 ff ff ff 8b 83 b0 02 00 00 80 65 c9 fe 48 8d 55 b4 c6 45 c8 00 48 8d 75 c8 48 89 df 89 45 d0 <8b> 41 14 89 45 cc 8b 03 89 45 b8 0f b7 43 0c c6 45 b4 00 66 89
[ 1009.575395] RIP  [<ffffffff815f8dea>] create_subflow_worker+0x12a/0x1b0
[ 1009.575395]  RSP <ffff88003da47da8>
[ 1009.575395] CR2: 0000000000000014
[ 1009.575395] ---[ end trace ef173051a9770390 ]---

We neeed to avoid access the master_sk if it is already gone.

Cc: Benjamin Hesmans <benjamin.hesmans@uclouvain.be>
Fixes: 800305afc267 (MPTCP : fix if_idx in ndiffports)

net/mptcp/mptcp_ndiffports.c
@@ -62,7 +62,10 @@ next_subflow:
 			loc.addr.s_addr = inet_sk(meta_sk)->inet_saddr;
 			loc.loc4_id = 0;
 			loc.low_prio = 0;
-			loc.if_idx = mpcb->master_sk->sk_bound_dev_if;
+			if (mpcb->master_sk)
+				loc.if_idx = mpcb->master_sk->sk_bound_dev_if;
+			else
+				loc.if_idx = 0;
 
 			rem.addr.s_addr = inet_sk(meta_sk)->inet_daddr;
 			rem.port = inet_sk(meta_sk)->inet_dport;
@@ -77,7 +80,10 @@ next_subflow:
 			loc.addr = inet6_sk(meta_sk)->saddr;
 			loc.loc6_id = 0;
 			loc.low_prio = 0;
-			loc.if_idx = mpcb->master_sk->sk_bound_dev_if;
+			if (mpcb->master_sk)
+				loc.if_idx = mpcb->master_sk->sk_bound_dev_if;
+			else
+				loc.if_idx = 0;
 
 			rem.addr = meta_sk->sk_v6_daddr;
 			rem.port = inet_sk(meta_sk)->inet_dport;