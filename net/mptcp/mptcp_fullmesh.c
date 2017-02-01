multipath-tcp/mptcp

commit de7ad274c31df739ec0aef3ea4cc2380b3dba2ea

mptcp: Update bitfields when removing subflows
When we try to recreate subflows upon a timeout, we first need to make
sure that this address-pair actually has been removed from the bitfield.

Reported-by: Max <list_mptcp@4ext.com>
Tested-by: Max <list_mptcp@4ext.com>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_fullmesh.c
@@ -1696,6 +1696,65 @@ static void full_mesh_rem_raddr(struct mptcp_cb *mpcb, u8 rem_id)
 	mptcp_v6_rem_raddress(mpcb, rem_id);
 }
 
+static void full_mesh_delete_subflow(struct sock *sk)
+{
+	struct fullmesh_priv *fmp = fullmesh_get_priv(tcp_sk(sk)->mpcb);
+	struct mptcp_fm_ns *fm_ns = fm_get_ns(sock_net(sk));
+	struct mptcp_loc_addr *mptcp_local;
+	int index, i;
+
+	if (!create_on_err)
+		return;
+
+	rcu_read_lock_bh();
+	mptcp_local = rcu_dereference_bh(fm_ns->local);
+
+	if (sk->sk_family == AF_INET || mptcp_v6_is_v4_mapped(sk)) {
+		union inet_addr saddr;
+
+		saddr.ip = inet_sk(sk)->inet_saddr;
+		index = mptcp_find_address(mptcp_local, AF_INET, &saddr,
+					   sk->sk_bound_dev_if);
+		if (index < 0)
+			goto out;
+
+		mptcp_for_each_bit_set(fmp->rem4_bits, i) {
+			struct fullmesh_rem4 *rem4 = &fmp->remaddr4[i];
+
+			if (rem4->addr.s_addr != sk->sk_daddr)
+				continue;
+
+			if (rem4->port && rem4->port != inet_sk(sk)->inet_dport)
+				continue;
+
+			rem4->bitfield &= ~(1 << index);
+		}
+	} else {
+		union inet_addr saddr;
+
+		saddr.in6 = inet6_sk(sk)->saddr;
+		index = mptcp_find_address(mptcp_local, AF_INET6, &saddr,
+					   sk->sk_bound_dev_if);
+		if (index < 0)
+			goto out;
+
+		mptcp_for_each_bit_set(fmp->rem6_bits, i) {
+			struct fullmesh_rem6 *rem6 = &fmp->remaddr6[i];
+
+			if (!ipv6_addr_equal(&rem6->addr, &sk->sk_v6_daddr))
+				continue;
+
+			if (rem6->port && rem6->port != inet_sk(sk)->inet_dport)
+				continue;
+
+			rem6->bitfield &= ~(1 << index);
+		}
+	}
+
+out:
+	rcu_read_unlock_bh();
+}
+
 /* Output /proc/net/mptcp_fullmesh */
 static int mptcp_fm_seq_show(struct seq_file *seq, void *v)
 {
@@ -1826,6 +1885,7 @@ static struct mptcp_pm_ops full_mesh __read_mostly = {
 	.addr_signal = full_mesh_addr_signal,
 	.add_raddr = full_mesh_add_raddr,
 	.rem_raddr = full_mesh_rem_raddr,
+	.delete_subflow = full_mesh_delete_subflow,
 	.name = "fullmesh",
 	.owner = THIS_MODULE,
 };

include/net/mptcp.h
@@ -235,6 +235,7 @@ struct mptcp_pm_ops {
 	void (*rem_raddr)(struct mptcp_cb *mpcb, u8 rem_id);
 	void (*init_subsocket_v4)(struct sock *sk, struct in_addr addr);
 	void (*init_subsocket_v6)(struct sock *sk, struct in6_addr addr);
+	void (*delete_subflow)(struct sock *sk);
 
 	char		name[MPTCP_PM_NAME_MAX];
 	struct module	*owner;

net/mptcp/mptcp_ctrl.c
@@ -1351,6 +1351,9 @@ void mptcp_del_sock(struct sock *sk)
 	if (mpcb->sched_ops->release)
 		mpcb->sched_ops->release(sk);
 
+	if (mpcb->pm_ops->delete_subflow)
+		mpcb->pm_ops->delete_subflow(sk);
+
 	mptcp_debug("%s: Removing subsock tok %#x pi:%d state %d is_meta? %d\n",
 		    __func__, mpcb->mptcp_loc_token, tp->mptcp->path_index,
 		    sk->sk_state, is_meta_sk(sk));

commit 395311c302624d156b22d15d47b49c814a8f05cf

mptcp: Build-Fix
Fixes: de7ad274c31d (mptcp: Update bitfields when removing subflows)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

net/mptcp/mptcp_fullmesh.c
@@ -1729,6 +1729,7 @@ static void full_mesh_delete_subflow(struct sock *sk)
 
 			rem4->bitfield &= ~(1 << index);
 		}
+#if IS_ENABLED(CONFIG_IPV6)
 	} else {
 		union inet_addr saddr;
 
@@ -1749,6 +1750,7 @@ static void full_mesh_delete_subflow(struct sock *sk)
 
 			rem6->bitfield &= ~(1 << index);
 		}
+#endif
 	}
 
 out: