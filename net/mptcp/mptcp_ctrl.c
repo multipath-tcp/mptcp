4b7df55ef0fd6e3863477477f0b9567555d66644
Christoph Paasch <cpaasch@apple.com>
2017-03-22 23:37:18 -0700

    mptcp: Release-candidate v0.92
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2879,7 +2879,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.89.0-rc");
+	pr_info("MPTCP: Stable release v0.92-rc");
 
 	mptcp_init_failed = false;
 
