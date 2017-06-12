multipath-tcp/mptcp

commit 0a71d1778f1ac91b3506b00d8843db8739fbc3dd

mptcp: fastopen client support
This patch enables the support of fastopen in the MPTCP stack. As fastopen
sends data in the SYN this data is not covered by a DSS. However, when
receiving the SYN+ACK the client knows the number of bytes of the SYN that
were received. Mapping this data to data sequence number is therefore not
mandatory as the SYN / SYN+ACK exchange is sequential. Unacknowledged data
must however be mapped to data sequence number in order to retransmit this
data at the MPTCP-level.

To deal with middleboxes that can add or remove bytes in the SYN. We decided
to change the semantics of the relative subflow sequence number in the DSS.
It now starts at the sequence number of the first unacknowledged byte - 1.
We use -1 to have the same behavior as regular MPTCP, i.e., the first data
sent will have a ssn of 1 (as the SYN accounts for 1 byte).
(see also https://datatracker.ietf.org/doc/draft-barre-mptcp-tfo/)

To understand how remaping works here are two examples (output syntax is
from packetdrill):

1. Server only acknowledge the SYN (ex if it does not supports fastopen)

> S 0:20(20) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable a>
< S. 0:0(0) ack 1 win 5792 <mss 1460,sackOK,TS val 700 ecr 100,nop,wscale 7,mp_capable b>
> . 1:1(0) ack 1 <nop,nop,TS val 100 ecr 700,mp_capable a b, dss dack4=1>
> P. 1:21(20) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4=1 dsn4=1 ssn=1 dll=20>
< . 1:1(0) ack 21 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4=21>

2. Server acknowledge the SYN and the data

> S 0:20(20) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable a>
< S. 0:0(0) ack 21 win 5792 <mss 1460,sackOK,TS val 700 ecr 100,nop,wscale 7,mp_capable b>
> . 1:1(0) ack 1 <nop,nop,TS val 100 ecr 700,mp_capable a b, dss dack4=1>
write(3, ..., 1000) = 1000
> P. 21:1021(1000) ack 1 <nop,nop,TS val 100 ecr 700,dss dack4=1 dsn4=1 ssn=1 dll=1000>

Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

net/ipv4/tcp_input.c
@@ -5456,7 +5456,11 @@ static bool tcp_rcv_fastopen_synack(struct sock *sk, struct sk_buff *synack,
 
 	tcp_fastopen_cache_set(sk, mss, cookie, syn_drop);
 
-	if (data) { /* Retransmit unacked data in SYN */
+	/* In mptcp case, we do not rely on "retransmit", but instead on
+	 * "transmit", because if fastopen data is not acked, the retransmission
+	 * becomes the first MPTCP data (see mptcp_rcv_synsent_fastopen).
+	 */
+	if (data && !mptcp(tp)) { /* Retransmit unacked data in SYN */
 		tcp_for_write_queue_from(data, sk) {
 			if (data == tcp_send_head(sk) ||
 			    __tcp_retransmit_skb(sk, data))

net/mptcp/mptcp_input.c
@@ -2131,6 +2131,67 @@ int mptcp_handle_options(struct sock *sk, const struct tcphdr *th,
 	return 0;
 }
 
+/* In case of fastopen, some data can already be in the write queue.
+ * We need to update the sequence number of the segments as they
+ * were initially TCP sequence numbers.
+ */
+static void mptcp_rcv_synsent_fastopen(struct sock *meta_sk)
+{
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+	struct tcp_sock *master_tp = tcp_sk(meta_tp->mpcb->master_sk);
+	struct sk_buff *skb;
+	u32 new_mapping = meta_tp->write_seq - master_tp->snd_una;
+
+	/* There should only be one skb in write queue: the data not
+	 * acknowledged in the SYN+ACK. In this case, we need to map
+	 * this data to data sequence numbers.
+	 */
+	skb_queue_walk(&meta_sk->sk_write_queue, skb) {
+		/* If the server only acknowledges partially the data sent in
+		 * the SYN, we need to trim the acknowledged part because
+		 * we don't want to retransmit this already received data.
+		 * When we reach this point, tcp_ack() has already cleaned up
+		 * fully acked segments. However, tcp trims partially acked
+		 * segments only when retransmitting. Since MPTCP comes into
+		 * play only now, we will fake an initial transmit, and
+		 * retransmit_skb() will not be called. The following fragment
+		 * comes from __tcp_retransmit_skb().
+		 */
+		if (before(TCP_SKB_CB(skb)->seq, master_tp->snd_una)) {
+			BUG_ON(before(TCP_SKB_CB(skb)->end_seq,
+				      master_tp->snd_una));
+			/* tcp_trim_head can only returns ENOMEM if skb is
+			 * cloned. It is not the case here (see
+			 * tcp_send_syn_data).
+			 */
+			BUG_ON(tcp_trim_head(meta_sk, skb, master_tp->snd_una -
+					     TCP_SKB_CB(skb)->seq));
+		}
+
+		TCP_SKB_CB(skb)->seq += new_mapping;
+		TCP_SKB_CB(skb)->end_seq += new_mapping;
+	}
+
+	/* We can advance write_seq by the number of bytes unacknowledged
+	 * and that were mapped in the previous loop.
+	 */
+	meta_tp->write_seq += master_tp->write_seq - master_tp->snd_una;
+
+	/* The packets from the master_sk will be entailed to it later
+	 * Until that time, its write queue is empty, and
+	 * write_seq must align with snd_una
+	 */
+	master_tp->snd_nxt = master_tp->write_seq = master_tp->snd_una;
+	master_tp->packets_out = 0;
+
+	/* Although these data have been sent already over the subsk,
+	 * They have never been sent over the meta_sk, so we rewind
+	 * the send_head so that tcp considers it as an initial send
+	 * (instead of retransmit).
+	 */
+	meta_sk->sk_send_head = tcp_write_queue_head(meta_sk);
+}
+
 /* The skptr is needed, because if we become MPTCP-capable, we have to switch
  * from meta-socket to master-socket.
  *
@@ -2172,6 +2233,8 @@ int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 				(u32 *)&tp->mptcp->sender_mac[0]);
 
 	} else if (mopt->saw_mpc) {
+		struct sock *meta_sk = sk;
+
 		if (mptcp_create_master_sk(sk, mopt->mptcp_key,
 					   ntohs(tcp_hdr(skb)->window)))
 			return 2;
@@ -2180,10 +2243,22 @@ int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 		*skptr = sk;
 		tp = tcp_sk(sk);
 
-		/* snd_nxt - 1, because it has been incremented
-		 * by tcp_connect for the SYN
+		/* If fastopen was used data might be in the send queue. We
+		 * need to update their sequence number to MPTCP-level seqno.
+		 * Note that it can happen in rare cases that fastopen_req is
+		 * NULL and syn_data is 0 but fastopen indeed occurred and
+		 * data has been queued in the write queue (but not sent).
+		 * Example of such rare cases: connect is non-blocking and
+		 * TFO is configured to work without cookies.
 		 */
-		tp->mptcp->snt_isn = tp->snd_nxt - 1;
+		if (!skb_queue_empty(&meta_sk->sk_write_queue))
+			mptcp_rcv_synsent_fastopen(meta_sk);
+
+		/* -1, because the SYN consumed 1 byte. In case of TFO, we
+		 * start the subflow-sequence number as if the data of the SYN
+		 * is not part of any mapping.
+		 */
+		tp->mptcp->snt_isn = tp->snd_una - 1;
 		tp->mpcb->dss_csum = mopt->dss_csum;
 		tp->mptcp->include_mpc = 1;
 

commit bc318c3f1d9eb958a4f1b802c91330f5a2f91e64

mptcp: fix syn_loss fastopen cache
In MPTCP we move data from the master socket to the meta socket when
checking whether a SYN was lost fastopen looks at the write queue.
We have to look at the meta socket write queue instead of the master
socket write queue.

Example packetdrill script to reproduce the issue. Unfortunately,
packetdrill crashes, the MPTCP version needs to be patched to allow
supporting more than one socket and others.

--------------
0	`sysctl net.ipv4.tcp_fastopen=5`

+0	socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
+0	fcntl(3, F_GETFL) = 0x2 (flags O_RDWR)
+0	fcntl(3, F_SETFL, O_RDWR|O_NONBLOCK) = 0

+0	sendto(3, ..., 20, MSG_FASTOPEN, ..., ...) = 20
+0	> S 0:20(20) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable a> sock(3)
+1	> S 0:0(0) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable a> sock(3)
+0	< S. 0:0(0) ack 1 win 5792 <mss 1460,sackOK,TS val 700 ecr 100,nop,wscale 7,mp_capable b> sock(3)
+0	> . 1:1(0) ack 1 <nop,nop,TS val 100 ecr 700,mp_capable a b, dss dack4=trunc_r64_hmac(b)+1> sock(3)

+0	> P. 1:21(20) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4> sock(3)
+0	< . 1:1(0) ack 21 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4> sock(3)

+0	write(3, ..., 10) = 10

+0	> P. 21:31(10) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4> sock(3)
+0	< . 1:1(0) ack 31 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4> sock(3)

+0	close(3) = 0
+0	> F. 31:31(0) ack 1 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4 FIN> sock(3)
+0	< F. 1:1(0) ack 32 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4=trunc_r64_hmac(a)+32 dsn4 FIN> sock(3)
+0	> . 32:32(0) ack 2 <nop,nop,TS val 150 ecr 700,dss dack4> sock(3)
+0	> . 32:32(0) ack 2 <nop,nop,TS val 150 ecr 700,dss dack4> sock(3)

/* The cache should now indicate there is SYN which has been dropped but we will retry to send data */

+0	socket(..., SOCK_STREAM, IPPROTO_TCP) = 5
+0	setsockopt(5, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0	fcntl(5, F_GETFL) = 0x2 (flags O_RDWR)
+0	fcntl(5, F_SETFL, O_RDWR|O_NONBLOCK) = 0

+0	sendto(5, ..., 20, MSG_FASTOPEN, ..., ...) = 20
+0	> S 0:20(20) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable c> sock(5)
+1	> S 0:0(0) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable c> sock(5)
+0	< S. 0:0(0) ack 1 win 5792 <mss 1460,sackOK,TS val 700 ecr 100,nop,wscale 7,mp_capable d> sock(5)
+0	> . 1:1(0) ack 1 <nop,nop,TS val 100 ecr 700,mp_capable c d, dss dack4=trunc_r64_hmac(d)+1> sock(5)

+0	> P. 1:21(20) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4> sock(5)
+0	< . 1:1(0) ack 21 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4> sock(5)

+0	write(5, ..., 10) = 10

+0	> P. 21:31(10) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4> sock(5)
+0	< . 1:1(0) ack 31 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4> sock(5)

+0	close(5) = 0
+0	> F. 31:31(0) ack 1 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4 FIN> sock(5)
+0	< F. 1:1(0) ack 32 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4 dsn4 FIN> sock(5)
+0	> . 32:32(0) ack 2 <nop,nop,TS val 150 ecr 700,dss dack4> sock(5)

/* Thank to the cache, we will no longer try to use SYN with data */

+0	socket(..., SOCK_STREAM, IPPROTO_TCP) = 7
+0	setsockopt(7, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0	fcntl(7, F_GETFL) = 0x2 (flags O_RDWR)
+0	fcntl(7, F_SETFL, O_RDWR|O_NONBLOCK) = 0

+0	sendto(7, ..., 20, MSG_FASTOPEN, ..., ...) = 20
+0	> S 0:0(0) win 29200 <mss 1460,sackOK,TS val 100 ecr 0,nop,wscale 7,mp_capable e> sock(7)
+0	< S. 0:0(0) ack 1 win 5792 <mss 1460,sackOK,TS val 700 ecr 100,nop,wscale 7,mp_capable f> sock(7)
+0	> . 1:1(0) ack 1 <nop,nop,TS val 100 ecr 700,mp_capable e f, dss dack4=trunc_r64_hmac(f)+1> sock(7)

+0	> P. 1:21(20) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4> sock(7)
+0	< . 1:1(0) ack 21 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4> sock(7)

+0	write(7, ..., 10) = 10

+0	> P. 21:31(10) ack 1 win 229 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4> sock(7)
+0	< . 1:1(0) ack 31 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4> sock(7)

+0	close(7) = 0
+0	> F. 31:31(0) ack 1 <nop,nop,TS val 150 ecr 700,dss dack4 dsn4 FIN> sock(7)
+0	< F. 1:1(0) ack 32 win 5792 <nop,nop,TS val 700 ecr 150,dss dack4=trunc_r64_hmac(f)+32 dsn4 FIN> sock(7)
+0	> . 32:32(0) ack 2 <nop,nop,TS val 150 ecr 700,dss dack4> sock(7)

+0	`sysctl net.ipv4.tcp_fastopen=0`
--------------

Fixes: 0a71d1778f1ac91 (mptcp: fastopen client support)

net/ipv4/tcp_input.c
@@ -5560,7 +5560,8 @@ static bool tcp_rcv_fastopen_synack(struct sock *sk, struct sk_buff *synack,
 				    struct tcp_fastopen_cookie *cookie)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct sk_buff *data = tp->syn_data ? tcp_write_queue_head(sk) : NULL;
+	struct sock *meta_sk = mptcp(tp) ? mptcp_meta_sk(sk) : sk;
+	struct sk_buff *data = tp->syn_data ? tcp_write_queue_head(meta_sk) : NULL;
 	u16 mss = tp->rx_opt.mss_clamp, try_exp = 0;
 	bool syn_drop = false;
 