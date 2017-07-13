multipath-tcp/mptcp

commit de09a83186666c67c9831057a83ba426f91fbea3

mptcp: Add ADD_ADDR2 option
ADD_ADDR2 implementation adds the truncated HMAC value of the
advertised information as a field in the ADD_ADDR option. The
rationale for this is to prevent injection of unauthorized
ADD_ADDR2 packets.

ADD_ADDR2 is used only if MPTCP version 1 is in use for the
MPTCP session. If version 0 is set, ADD_ADDR is used instead,
guaranteeing retro-compatibility.

This implementation complies with RFC6824bis-04.

Signed-off-by: Fabrizio Demaria <fabrizio.demaria@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

net/mptcp/mptcp_input.c
@@ -1601,9 +1601,33 @@ static void mptcp_send_reset_rem_id(const struct mptcp_cb *mpcb, u8 rem_id)
 	}
 }
 
+static inline bool is_valid_addropt_opsize(u8 mptcp_ver,
+					   struct mp_add_addr *mpadd,
+					   int opsize)
+{
+#if IS_ENABLED(CONFIG_IPV6)
+	if (mptcp_ver < MPTCP_VERSION_1 && mpadd->ipver == 6) {
+		return opsize == MPTCP_SUB_LEN_ADD_ADDR6 ||
+		       opsize == MPTCP_SUB_LEN_ADD_ADDR6 + 2;
+	}
+	if (mptcp_ver >= MPTCP_VERSION_1 && mpadd->ipver == 6)
+		return false; /* IPv6 with ADD_ADDR2 not supported yet */
+#endif
+	if (mptcp_ver < MPTCP_VERSION_1 && mpadd->ipver == 4) {
+		return opsize == MPTCP_SUB_LEN_ADD_ADDR4 ||
+		       opsize == MPTCP_SUB_LEN_ADD_ADDR4 + 2;
+	}
+	if (mptcp_ver >= MPTCP_VERSION_1 && mpadd->ipver == 4) {
+		return opsize == MPTCP_SUB_LEN_ADD_ADDR4_VER1 ||
+		       opsize == MPTCP_SUB_LEN_ADD_ADDR4_VER1 + 2;
+	}
+	return false;
+}
+
 void mptcp_parse_options(const uint8_t *ptr, int opsize,
 			 struct mptcp_options_received *mopt,
-			 const struct sk_buff *skb)
+			 const struct sk_buff *skb,
+			 struct tcp_sock *tp)
 {
 	const struct mptcp_option *mp_opt = (struct mptcp_option *)ptr;
 
@@ -1754,17 +1778,10 @@ void mptcp_parse_options(const uint8_t *ptr, int opsize,
 	}
 	case MPTCP_SUB_ADD_ADDR:
 	{
-#if IS_ENABLED(CONFIG_IPV6)
-		const struct mp_add_addr *mpadd = (struct mp_add_addr *)ptr;
+		struct mp_add_addr *mpadd = (struct mp_add_addr *)ptr;
 
-		if ((mpadd->ipver == 4 && opsize != MPTCP_SUB_LEN_ADD_ADDR4 &&
-		     opsize != MPTCP_SUB_LEN_ADD_ADDR4 + 2) ||
-		    (mpadd->ipver == 6 && opsize != MPTCP_SUB_LEN_ADD_ADDR6 &&
-		     opsize != MPTCP_SUB_LEN_ADD_ADDR6 + 2)) {
-#else
-		if (opsize != MPTCP_SUB_LEN_ADD_ADDR4 &&
-		    opsize != MPTCP_SUB_LEN_ADD_ADDR4 + 2) {
-#endif /* CONFIG_IPV6 */
+		if (!is_valid_addropt_opsize(tp->mpcb->mptcp_ver,
+					     mpadd, opsize)) {
 			mptcp_debug("%s: mp_add_addr: bad option size %d\n",
 				    __func__, opsize);
 			break;
@@ -1864,7 +1881,7 @@ void tcp_parse_mptcp_options(const struct sk_buff *skb,
 			if (opsize > length)
 				return;	/* don't parse partial options */
 			if (opcode == TCPOPT_MPTCP)
-				mptcp_parse_options(ptr - 2, opsize, mopt, skb);
+				mptcp_parse_options(ptr - 2, opsize, mopt, skb, NULL);
 		}
 		ptr += opsize - 2;
 		length -= opsize;
@@ -1902,7 +1919,36 @@ static void mptcp_handle_add_addr(const unsigned char *ptr, struct sock *sk)
 	sa_family_t family;
 
 	if (mpadd->ipver == 4) {
-		if (mpadd->len == MPTCP_SUB_LEN_ADD_ADDR4 + 2)
+		char *recv_hmac;
+		u8 hash_mac_check[20];
+		u8 no_key[8];
+		int msg_parts;
+
+		if (mpcb->mptcp_ver < MPTCP_VERSION_1)
+			goto skip_hmac_v4;
+
+		*(u64 *)no_key = 0;
+		recv_hmac = (char *)mpadd->u.v4.mac;
+		if (mpadd->len == MPTCP_SUB_LEN_ADD_ADDR4_VER1) {
+			recv_hmac -= sizeof(mpadd->u.v4.port);
+			msg_parts = 2;
+		} else if (mpadd->len == MPTCP_SUB_LEN_ADD_ADDR4_VER1 + 2) {
+			msg_parts = 3;
+		}
+		mptcp_hmac_sha1((u8 *)&mpcb->mptcp_rem_key,
+				(u8 *)no_key,
+				(u32 *)hash_mac_check, msg_parts,
+				1, (u8 *)&mpadd->addr_id,
+				4, (u8 *)&mpadd->u.v4.addr.s_addr,
+				2, (u8 *)&mpadd->u.v4.port);
+		if (memcmp(hash_mac_check, recv_hmac, 8) != 0)
+			/* ADD_ADDR2 discarded */
+			return;
+skip_hmac_v4:
+		if ((mpcb->mptcp_ver == MPTCP_VERSION_0 &&
+		     mpadd->len == MPTCP_SUB_LEN_ADD_ADDR4 + 2) ||
+		     (mpcb->mptcp_ver == MPTCP_VERSION_1 &&
+		     mpadd->len == MPTCP_SUB_LEN_ADD_ADDR4_VER1 + 2))
 			port  = mpadd->u.v4.port;
 		family = AF_INET;
 		addr.in = mpadd->u.v4.addr;
@@ -1969,16 +2015,11 @@ static void mptcp_parse_addropt(const struct sk_buff *skb, struct sock *sk)
 				return;  /* don't parse partial options */
 			if (opcode == TCPOPT_MPTCP &&
 			    ((struct mptcp_option *)ptr)->sub == MPTCP_SUB_ADD_ADDR) {
-#if IS_ENABLED(CONFIG_IPV6)
+				u8 mptcp_ver = tcp_sk(sk)->mpcb->mptcp_ver;
 				struct mp_add_addr *mpadd = (struct mp_add_addr *)ptr;
-				if ((mpadd->ipver == 4 && opsize != MPTCP_SUB_LEN_ADD_ADDR4 &&
-				     opsize != MPTCP_SUB_LEN_ADD_ADDR4 + 2) ||
-				    (mpadd->ipver == 6 && opsize != MPTCP_SUB_LEN_ADD_ADDR6 &&
-				     opsize != MPTCP_SUB_LEN_ADD_ADDR6 + 2))
-#else
-				if (opsize != MPTCP_SUB_LEN_ADD_ADDR4 &&
-				    opsize != MPTCP_SUB_LEN_ADD_ADDR4 + 2)
-#endif /* CONFIG_IPV6 */
+
+				if (!is_valid_addropt_opsize(mptcp_ver, mpadd,
+							     opsize))
 					goto cont;
 
 				mptcp_handle_add_addr(ptr, sk);

drivers/infiniband/hw/cxgb4/cm.c
@@ -3549,7 +3549,7 @@ static void build_cpl_pass_accept_req(struct sk_buff *skb, int stid , u8 tos)
 	 */
 	memset(&tmp_opt, 0, sizeof(tmp_opt));
 	tcp_clear_options(&tmp_opt);
-	tcp_parse_options(skb, &tmp_opt, NULL, 0, NULL);
+	tcp_parse_options(skb, &tmp_opt, NULL, 0, NULL, NULL);
 
 	req = (struct cpl_pass_accept_req *)__skb_push(skb, sizeof(*req));
 	memset(req, 0, sizeof(*req));

include/linux/tcp.h
@@ -104,7 +104,9 @@ struct tcp_out_options {
 	};
 
 	struct {
+		__u64 trunc_mac;
 		struct in_addr addr;
+		u16 port;
 		u8 addr_id;
 	} add_addr4;
 

include/net/mptcp.h
@@ -345,6 +345,9 @@ struct mptcp_cb {
 	u32 orig_window_clamp;
 };
 
+#define MPTCP_VERSION_0 0
+#define MPTCP_VERSION_1 1
+
 #define MPTCP_SUB_CAPABLE			0
 #define MPTCP_SUB_LEN_CAPABLE_SYN		12
 #define MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN		12
@@ -395,8 +398,10 @@ struct mptcp_cb {
 
 #define MPTCP_SUB_ADD_ADDR		3
 #define MPTCP_SUB_LEN_ADD_ADDR4		8
+#define MPTCP_SUB_LEN_ADD_ADDR4_VER1	16
 #define MPTCP_SUB_LEN_ADD_ADDR6		20
 #define MPTCP_SUB_LEN_ADD_ADDR4_ALIGN	8
+#define MPTCP_SUB_LEN_ADD_ADDR4_ALIGN_VER1	16
 #define MPTCP_SUB_LEN_ADD_ADDR6_ALIGN	20
 
 #define MPTCP_SUB_REMOVE_ADDR	4
@@ -561,6 +566,7 @@ struct mp_add_addr {
 		struct {
 			struct in_addr	addr;
 			__be16		port;
+			__u8		mac[8];
 		} v4;
 		struct {
 			struct in6_addr	addr;
@@ -790,7 +796,8 @@ void tcp_parse_mptcp_options(const struct sk_buff *skb,
 			     struct mptcp_options_received *mopt);
 void mptcp_parse_options(const uint8_t *ptr, int opsize,
 			 struct mptcp_options_received *mopt,
-			 const struct sk_buff *skb);
+			 const struct sk_buff *skb,
+			 struct tcp_sock *tp);
 void mptcp_syn_options(const struct sock *sk, struct tcp_out_options *opts,
 		       unsigned *remaining);
 void mptcp_synack_options(struct request_sock *req,
@@ -1383,8 +1390,9 @@ static inline void mptcp_sub_close(struct sock *sk, unsigned long delay) {}
 static inline void mptcp_set_rto(const struct sock *sk) {}
 static inline void mptcp_send_fin(const struct sock *meta_sk) {}
 static inline void mptcp_parse_options(const uint8_t *ptr, const int opsize,
-				       const struct mptcp_options_received *mopt,
-				       const struct sk_buff *skb) {}
+				       struct mptcp_options_received *mopt,
+				       const struct sk_buff *skb
+				       const struct tcp_sock *tp) {}
 static inline void mptcp_syn_options(const struct sock *sk,
 				     struct tcp_out_options *opts,
 				     unsigned *remaining) {}

include/net/tcp.h
@@ -559,7 +559,8 @@ int tcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len, int nonblock,
 void tcp_parse_options(const struct sk_buff *skb,
 		       struct tcp_options_received *opt_rx,
 		       struct mptcp_options_received *mopt_rx,
-		       int estab, struct tcp_fastopen_cookie *foc);
+		       int estab, struct tcp_fastopen_cookie *foc,
+		       struct tcp_sock *tp);
 const u8 *tcp_parse_md5sig_option(const struct tcphdr *th);
 
 /*

net/ipv4/syncookies.c
@@ -342,7 +342,7 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 	/* check for timestamp cookie support */
 	memset(&tcp_opt, 0, sizeof(tcp_opt));
 	mptcp_init_mp_opt(&mopt);
-	tcp_parse_options(skb, &tcp_opt, &mopt, 0, NULL);
+	tcp_parse_options(skb, &tcp_opt, &mopt, 0, NULL, NULL);
 
 	if (!cookie_timestamp_decode(&tcp_opt))
 		goto out;

net/ipv4/tcp_input.c
@@ -3664,7 +3664,8 @@ static void tcp_parse_fastopen_option(int len, const unsigned char *cookie,
 void tcp_parse_options(const struct sk_buff *skb,
 		       struct tcp_options_received *opt_rx,
 		       struct mptcp_options_received *mopt,
-		       int estab, struct tcp_fastopen_cookie *foc)
+		       int estab, struct tcp_fastopen_cookie *foc,
+		       struct tcp_sock *tp)
 {
 	const unsigned char *ptr;
 	const struct tcphdr *th = tcp_hdr(skb);
@@ -3748,7 +3749,7 @@ void tcp_parse_options(const struct sk_buff *skb,
 				break;
 #endif
 			case TCPOPT_MPTCP:
-				mptcp_parse_options(ptr - 2, opsize, mopt, skb);
+				mptcp_parse_options(ptr - 2, opsize, mopt, skb, tp);
 				break;
 
 			case TCPOPT_FASTOPEN:
@@ -3813,8 +3814,8 @@ static bool tcp_fast_parse_options(const struct sk_buff *skb,
 		if (tcp_parse_aligned_timestamp(tp, th))
 			return true;
 	}
-	tcp_parse_options(skb, &tp->rx_opt, mptcp(tp) ? &tp->mptcp->rx_opt : NULL,
-			  1, NULL);
+	tcp_parse_options(skb, &tp->rx_opt,
+			  mptcp(tp) ? &tp->mptcp->rx_opt : NULL, 1, NULL, tp);
 	if (tp->rx_opt.saw_tstamp && tp->rx_opt.rcv_tsecr)
 		tp->rx_opt.rcv_tsecr -= tp->tsoffset;
 
@@ -5502,7 +5503,7 @@ static bool tcp_rcv_fastopen_synack(struct sock *sk, struct sk_buff *synack,
 		/* Get original SYNACK MSS value if user MSS sets mss_clamp */
 		tcp_clear_options(&opt);
 		opt.user_mss = opt.mss_clamp = 0;
-		tcp_parse_options(synack, &opt, NULL, 0, NULL);
+		tcp_parse_options(synack, &opt, NULL, 0, NULL, NULL);
 		mss = opt.mss_clamp;
 	}
 
@@ -5557,7 +5558,7 @@ static int tcp_rcv_synsent_state_process(struct sock *sk, struct sk_buff *skb,
 	mptcp_init_mp_opt(&mopt);
 
 	tcp_parse_options(skb, &tp->rx_opt,
-			  mptcp(tp) ? &tp->mptcp->rx_opt : &mopt, 0, &foc);
+			  mptcp(tp) ? &tp->mptcp->rx_opt : &mopt, 0, &foc, tp);
 	if (tp->rx_opt.saw_tstamp && tp->rx_opt.rcv_tsecr)
 		tp->rx_opt.rcv_tsecr -= tp->tsoffset;
 
@@ -6286,7 +6287,7 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 	tcp_clear_options(&tmp_opt);
 	tmp_opt.mss_clamp = af_ops->mss_clamp;
 	tmp_opt.user_mss  = tp->rx_opt.user_mss;
-	tcp_parse_options(skb, &tmp_opt, NULL, 0, want_cookie ? NULL : &foc);
+	tcp_parse_options(skb, &tmp_opt, NULL, 0, want_cookie ? NULL : &foc, NULL);
 
 	if (want_cookie && !tmp_opt.saw_tstamp)
 		tcp_clear_options(&tmp_opt);

net/ipv4/tcp_minisocks.c
@@ -111,7 +111,7 @@ tcp_timewait_state_process(struct inet_timewait_sock *tw, struct sk_buff *skb,
 	if (th->doff > (sizeof(*th) >> 2) && tcptw->tw_ts_recent_stamp) {
 		mptcp_init_mp_opt(&mopt);
 
-		tcp_parse_options(skb, &tmp_opt, &mopt, 0, NULL);
+		tcp_parse_options(skb, &tmp_opt, &mopt, 0, NULL, NULL);
 
 		if (tmp_opt.saw_tstamp) {
 			tmp_opt.rcv_tsecr	-= tcptw->tw_ts_offset;
@@ -609,7 +609,7 @@ struct sock *tcp_check_req(struct sock *sk, struct sk_buff *skb,
 	mptcp_init_mp_opt(&mopt);
 
 	if (th->doff > (sizeof(struct tcphdr)>>2)) {
-		tcp_parse_options(skb, &tmp_opt, &mopt, 0, NULL);
+		tcp_parse_options(skb, &tmp_opt, &mopt, 0, NULL, NULL);
 
 		if (tmp_opt.saw_tstamp) {
 			tmp_opt.ts_recent = req->ts_recent;

commit 3fc787d80217c2cc25fe17da798ef313335e100c

mptcp: Handle incoming ADD_ADDR on non-established sockets
If a server sends us an ADD_ADDR (with valid ACK-number) instead of the SYN/ACK,
we will parse this ADD_ADDR and access tp->mpcb. However, at this stage tp->mpcb
is not yet set and thus we will panic:

[   31.845802] BUG: unable to handle kernel NULL pointer dereference at 000000000000033d
[   31.846784] IP: [<ffffffff815a86c1>] mptcp_parse_options+0x2e1/0x540
[   31.846784] PGD 3d72d067 PUD 3dbcd067 PMD 0
[   31.846784] Oops: 0000 [#1] SMP
[   31.846784] Modules linked in:
[   31.846784] CPU: 2 PID: 0 Comm: swapper/2 Not tainted 4.4.0.mptcp #3
[   31.846784] Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS rel-1.7.5-0-ge51488c-20140602_164612-nilsson.home.kraxel.org 04/01/2014
[   31.846784] task: ffff88003f87ee00 ti: ffff88003f8c4000 task.ti: ffff88003f8c4000
[   31.846784] RIP: 0010:[<ffffffff815a86c1>]  [<ffffffff815a86c1>] mptcp_parse_options+0x2e1/0x540
[   31.846784] RSP: 0018:ffff88003fd039a8  EFLAGS: 00010286
[   31.846784] RAX: 0000000000000000 RBX: 0000000000000010 RCX: ffff88003d469500
[   31.846784] RDX: ffff88003fd03a70 RSI: 0000000000000008 RDI: ffff88003c521482
[   31.846784] RBP: ffff88003fd039a8 R08: ffff88003d8d8000 R09: ffff88003c521484
[   31.846784] R10: ffff88003d8d85a8 R11: ffff88003fd03a3e R12: ffff88003c521462
[   31.846784] R13: 0000000000000008 R14: 0000000000000008 R15: ffff88003d469500
[   31.846784] FS:  0000000000000000(0000) GS:ffff88003fd00000(0000) knlGS:0000000000000000
[   31.846784] CS:  0010 DS: 0000 ES: 0000 CR0: 000000008005003b
[   31.846784] CR2: 000000000000033d CR3: 000000003c608000 CR4: 00000000000006e0
[   31.846784] Stack:
[   31.846784]  ffff88003fd03a20 ffffffff81504c1f ffff88003fd039c8 ffff88003fd03a3e
[   31.846784]  ffff88003d8d85a8 ffff88003c521484 ffff88003fd03a3f ffff88003d8d8000
[   31.846784]  000000006967fb40 ffff88003fd03a70 ffff88003d8d8000 ffff88003d8d8000
[   31.846784] Call Trace:
[   31.846784]  <IRQ>
[   31.846784]  [<ffffffff81504c1f>] tcp_parse_options+0x25f/0x420
[   31.846784]  [<ffffffff8150be35>] tcp_rcv_synsent_state_process+0x95/0x990
[   31.846784]  [<ffffffff8154680a>] ? ipt_do_table+0x2da/0x3a0
[   31.846784]  [<ffffffff8150c7d9>] tcp_rcv_state_process+0xa9/0x7e0
[   31.846784]  [<ffffffff81515d5a>] tcp_v4_do_rcv+0x16a/0x280
[   31.846784]  [<ffffffff8151714e>] tcp_v4_rcv+0xa6e/0xaf0
[   31.846784]  [<ffffffff814daf90>] ? ip_vs_local_request4+0x30/0x40
[   31.846784]  [<ffffffff814dafa9>] ? ip_vs_remote_request4+0x9/0x10
[   31.846784]  [<ffffffff814f1a2e>] ip_local_deliver_finish+0x4e/0x170
[   31.846784]  [<ffffffff814f1d58>] ip_local_deliver+0xb8/0xd0
[   31.846784]  [<ffffffff814f19e0>] ? ip_rcv_finish+0x2a0/0x2a0
[   31.846784]  [<ffffffff814f17bb>] ip_rcv_finish+0x7b/0x2a0
[   31.846784]  [<ffffffff814f2021>] ip_rcv+0x2b1/0x360
[   31.846784]  [<ffffffff814f1740>] ? inet_del_offload+0x40/0x40
[   31.846784]  [<ffffffff8149da12>] __netif_receive_skb_core+0x2d2/0x9b0
[   31.846784]  [<ffffffff8151c078>] ? tcp4_gro_receive+0x128/0x1d0
[   31.846784]  [<ffffffff8152ae7a>] ? inet_gro_receive+0x18a/0x200
[   31.846784]  [<ffffffff8149e108>] __netif_receive_skb+0x18/0x60
[   31.846784]  [<ffffffff8149e178>] netif_receive_skb_internal+0x28/0x90
[   31.846784]  [<ffffffff8149ebdb>] napi_gro_receive+0x6b/0x90
[   31.846784]  [<ffffffff813d2c00>] virtnet_receive+0x430/0x790
[   31.846784]  [<ffffffff813d2f78>] virtnet_poll+0x18/0x80
[   31.846784]  [<ffffffff8149e581>] net_rx_action+0x1b1/0x2f0
[   31.846784]  [<ffffffff81055402>] __do_softirq+0xf2/0x210
[   31.846784]  [<ffffffff8105569e>] irq_exit+0xae/0xb0
[   31.846784]  [<ffffffff8100508f>] do_IRQ+0x4f/0xd0
[   31.846784]  [<ffffffff815c15c2>] common_interrupt+0x82/0x82

Fixes: de09a8318666 (mptcp: Add ADD_ADDR2 option)

net/mptcp/mptcp_input.c
@@ -1752,7 +1752,7 @@ void mptcp_parse_options(const uint8_t *ptr, int opsize,
 		/* If tcp_sock is not available, MPTCP version can't be
 		 * retrieved and ADD_ADDR opsize validation is not possible.
 		 */
-		if (!tp)
+		if (!tp || !tp->mpcb)
 			break;
 
 		if (!is_valid_addropt_opsize(tp->mpcb->mptcp_ver,