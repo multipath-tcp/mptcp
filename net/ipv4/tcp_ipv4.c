20a6ea4fbcd64b7891a2addd7fbb01e171e2ce2c
Christoph Paasch <cpaasch@apple.com>
2017-08-19 14:27:01 -0700

    mptcp: Always send a DATA_ACK on MPTCP subflows in TIME_WAIT-state
    The check in tcp_v4_timewait_ack() checks if the meta-socket is in
    time-wait state before deciding to send a DATA_ACK. So, if the
    meta-socket is not in time-wait state we won't send a DATA_ACK, if there
    was an incoming FIN with DATA_FIN.
    
    This means, that we send a pure ACK upon incoming data, which makes the
    peer consider this subflow as non-MPTCP if there wasn't previous data on
    this subflow to bring it into "fully_established"-state.
    
    We should always send a DATA_ACK on these time-wait sockets. Just grab
    the rcv_nxt that we had when the subflow went into time-wait state. If
    we receive a DATA_FIN, and the meta-socket is not yet in time-wait
    state, the DATA_ACK we send will simply be a dup-ack triggering the
    server to retransmit the DATA_FIN on another subflow eventually.
    
    Sooner or later, the meta-socket will go properly in time-wait state and
    the mptw->rcv_nxt will be properly set.
    
    Fixes: 9a725addbb3 (Correctly data-ack on time-wait sockets)
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 03cd1a24b42a242e677c74132e046563351ce7ef)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -819,7 +819,7 @@ static void tcp_v4_timewait_ack(struct sock *sk, struct sk_buff *skb)
 	u32 data_ack = 0;
 	int mptcp = 0;
 
-	if (tcptw->mptcp_tw && tcptw->mptcp_tw->meta_tw) {
+	if (tcptw->mptcp_tw) {
 		data_ack = (u32)tcptw->mptcp_tw->rcv_nxt;
 		mptcp = 1;
 	}
