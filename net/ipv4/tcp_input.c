e791cf4354d1f4ce915551d9bcc6c56d62b67403
Christoph Paasch <cpaasch@apple.com>
2017-10-17 15:35:01 -0700

    mptcp: Don't call tcp_fin on the meta-socket
    We have our own version, mptcp_fin(). Now since we do the ofo-queuing
    with the TCP-functions we need to make sure that they call mptcp_fin()
    instead.
    
    Cc: Sebastian Foss <sebastian.foss.mail@googlemail.com>
    Cc: Schleindlhuber@gmx.net
    Reported-by: Sebastian Foss <sebastian.foss.mail@googlemail.com>
    Reported-by: Schleindlhuber@gmx.net
    Fixes: 050453aea3c3 (Merge tag 'v4.9' into mptcp_trunk)
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 588aa45235c7b8550e672764f53e1da75f326900)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4100,6 +4100,11 @@ void tcp_fin(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 
+	if (is_meta_sk(sk)) {
+		mptcp_fin(sk);
+		return;
+	}
+
 	inet_csk_schedule_ack(sk);
 
 	sk->sk_shutdown |= RCV_SHUTDOWN;
