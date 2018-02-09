multipath-tcp/mptcp

commit 1285e3945aee27cd22a8cf56091f7972c8365086

mptcp: Build-Fix
Fixes: c61bc63e8f37 ("Merge tag 'v4.15-rc3' into mptcp_trunk")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

net/mptcp/mptcp_output.c
@@ -94,20 +94,26 @@ static bool mptcp_is_reinjected(const struct sk_buff *skb)
 	return TCP_SKB_CB(skb)->mptcp_flags & MPTCP_REINJECT;
 }
 
-static void mptcp_find_and_set_pathmask(const struct sock *meta_sk, struct sk_buff *skb)
+static void mptcp_find_and_set_pathmask(struct sock *meta_sk, struct sk_buff *skb)
 {
+	struct rb_node **p = &meta_sk->tcp_rtx_queue.rb_node;
+	struct rb_node *parent;
 	struct sk_buff *skb_it;
 
-	skb_it = tcp_write_queue_head(meta_sk);
-
-	tcp_for_write_queue_from(skb_it, meta_sk) {
-		if (skb_it == tcp_send_head(meta_sk))
-			break;
-
-		if (TCP_SKB_CB(skb_it)->seq == TCP_SKB_CB(skb)->seq) {
-			TCP_SKB_CB(skb)->path_mask = TCP_SKB_CB(skb_it)->path_mask;
-			break;
+	while (*p) {
+		parent = *p;
+		skb = rb_to_skb(parent);
+		if (before(TCP_SKB_CB(skb)->seq, TCP_SKB_CB(skb_it)->seq)) {
+			p = &parent->rb_left;
+			continue;
 		}
+		if (after(TCP_SKB_CB(skb)->seq, TCP_SKB_CB(skb_it)->seq)) {
+			p = &parent->rb_right;
+			continue;
+		}
+
+		TCP_SKB_CB(skb)->path_mask = TCP_SKB_CB(skb_it)->path_mask;
+		break;
 	}
 }
 
@@ -158,23 +164,44 @@ static void __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk
 	/* Only reinject segments that are fully covered by the mapping */
 	if (skb->len + (mptcp_is_data_fin(skb) ? 1 : 0) !=
 	    TCP_SKB_CB(skb)->end_seq - TCP_SKB_CB(skb)->seq) {
-		u32 seq = TCP_SKB_CB(skb)->seq;
+		struct rb_node *parent, **p = &meta_sk->tcp_rtx_queue.rb_node;
 		u32 end_seq = TCP_SKB_CB(skb)->end_seq;
+		u32 seq = TCP_SKB_CB(skb)->seq;
 
 		__kfree_skb(skb);
 
 		/* Ok, now we have to look for the full mapping in the meta
 		 * send-queue :S
 		 */
-		tcp_for_write_queue(skb, meta_sk) {
-			/* Not yet at the mapping? */
-			if (before(TCP_SKB_CB(skb)->seq, seq))
-				continue;
-			/* We have passed by the mapping */
-			if (after(TCP_SKB_CB(skb)->end_seq, end_seq))
-				return;
 
-			__mptcp_reinject_data(skb, meta_sk, NULL, 1);
+		/* First, find the first skb that covers us */
+		while (*p) {
+			parent = *p;
+			skb = rb_to_skb(parent);
+
+			/* Not yet at the mapping? */
+			if (!after(end_seq, TCP_SKB_CB(skb)->seq)) {
+				p = &parent->rb_left;
+				continue;
+			}
+
+			if (!before(seq, TCP_SKB_CB(skb)->end_seq)) {
+				p = &parent->rb_right;
+				continue;
+			}
+
+			break;
+		}
+
+		if (*p) {
+			/* We found it, now let's reinject everything */
+			skb = rb_to_skb(*p);
+
+			skb_rbtree_walk_from(skb) {
+				if (after(TCP_SKB_CB(skb)->end_seq, end_seq))
+					return;
+				__mptcp_reinject_data(skb, meta_sk, NULL, 1);
+			}
 		}
 		return;
 	}
@@ -518,7 +545,8 @@ static bool mptcp_skb_entail(struct sock *sk, struct sk_buff *skb, int reinject)
 /* Fragment an skb and update the mptcp meta-data. Due to reinject, we
  * might need to undo some operations done by tcp_fragment.
  */
-static int mptcp_fragment(struct sock *meta_sk, struct sk_buff *skb, u32 len,
+static int mptcp_fragment(struct sock *meta_sk, enum tcp_queue tcp_queue,
+			  struct sk_buff *skb, u32 len,
 			  gfp_t gfp, int reinject)
 {
 	int ret, diff, old_factor;
@@ -536,7 +564,7 @@ static int mptcp_fragment(struct sock *meta_sk, struct sk_buff *skb, u32 len,
 	 * care about is that it is set to 1 for accurate packets_out
 	 * accounting.
 	 */
-	ret = tcp_fragment(meta_sk, skb, len, UINT_MAX, gfp);
+	ret = tcp_fragment(meta_sk, tcp_queue, skb, len, UINT_MAX, gfp);
 	if (ret)
 		return ret;
 
@@ -611,8 +639,8 @@ int mptcp_write_wakeup(struct sock *meta_sk, int mib)
 		    skb->len > mss) {
 			seg_size = min(seg_size, mss);
 			TCP_SKB_CB(skb)->tcp_flags |= TCPHDR_PSH;
-			if (mptcp_fragment(meta_sk, skb, seg_size,
-					   GFP_ATOMIC, 0))
+			if (mptcp_fragment(meta_sk, TCP_FRAG_IN_WRITE_QUEUE,
+					   skb, seg_size, GFP_ATOMIC, 0))
 				return -1;
 		} else if (!tcp_skb_pcount(skb)) {
 			/* see mptcp_write_xmit on why we use UINT_MAX */
@@ -740,7 +768,8 @@ bool mptcp_write_xmit(struct sock *meta_sk, unsigned int mss_now, int nonagle,
 			limit = min(limit, sublimit);
 
 		if (skb->len > limit &&
-		    unlikely(mptcp_fragment(meta_sk, skb, limit, gfp, reinject)))
+		    unlikely(mptcp_fragment(meta_sk, TCP_FRAG_IN_WRITE_QUEUE,
+					    skb, limit, gfp, reinject)))
 			break;
 
 		if (!mptcp_skb_entail(subsk, skb, reinject))
@@ -1378,9 +1407,10 @@ static void mptcp_ack_retransmit_timer(struct sock *sk)
 out:;
 }
 
-void mptcp_ack_handler(unsigned long data)
+void mptcp_ack_handler(struct timer_list *t)
 {
-	struct sock *sk = (struct sock *)data;
+	struct mptcp_tcp_sock *mptcp = from_timer(mptcp, t, mptcp_ack_timer);
+	struct sock *sk = (struct sock *)mptcp->tp;
 	struct sock *meta_sk = mptcp_meta_sk(sk);
 
 	bh_lock_sock(meta_sk);
@@ -1464,8 +1494,8 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 					    TCP_NAGLE_OFF);
 
 	if (skb->len > limit &&
-	    unlikely(mptcp_fragment(meta_sk, skb, limit,
-				    GFP_ATOMIC, 0)))
+	    unlikely(mptcp_fragment(meta_sk, TCP_FRAG_IN_WRITE_QUEUE, skb,
+				    limit, GFP_ATOMIC, 0)))
 		goto failed;
 
 	if (!mptcp_skb_entail(subsk, skb, -1))
@@ -1539,7 +1569,7 @@ void mptcp_meta_retransmit_timer(struct sock *meta_sk)
 			return;
 		}
 
-		mptcp_retransmit_skb(meta_sk, tcp_write_queue_head(meta_sk));
+		mptcp_retransmit_skb(meta_sk, tcp_rtx_queue_head(meta_sk));
 		goto out_reset_timer;
 	}
 
@@ -1551,7 +1581,7 @@ void mptcp_meta_retransmit_timer(struct sock *meta_sk)
 
 	meta_icsk->icsk_ca_state = TCP_CA_Loss;
 
-	err = mptcp_retransmit_skb(meta_sk, tcp_write_queue_head(meta_sk));
+	err = mptcp_retransmit_skb(meta_sk, tcp_rtx_queue_head(meta_sk));
 	if (err > 0) {
 		/* Retransmission failed because of local congestion,
 		 * do not backoff.
@@ -1593,7 +1623,7 @@ out_reset_timer:
 	 * linear-timeout retransmissions into a black hole
 	 */
 	if (meta_sk->sk_state == TCP_ESTABLISHED &&
-	    (meta_tp->thin_lto || sysctl_tcp_thin_linear_timeouts) &&
+	    (meta_tp->thin_lto || sock_net(meta_sk)->ipv4.sysctl_tcp_thin_linear_timeouts) &&
 	    tcp_stream_is_thin(meta_tp) &&
 	    meta_icsk->icsk_retransmits <= TCP_THIN_LINEAR_RETRIES) {
 		meta_icsk->icsk_backoff = 0;
@@ -1623,18 +1653,18 @@ void mptcp_sub_retransmit_timer(struct sock *sk)
 }
 
 /* Modify values to an mptcp-level for the initial window of new subflows */
-void mptcp_select_initial_window(int __space, __u32 mss, __u32 *rcv_wnd,
-				__u32 *window_clamp, int wscale_ok,
-				__u8 *rcv_wscale, __u32 init_rcv_wnd,
-				 const struct sock *sk)
+void mptcp_select_initial_window(const struct sock *sk, int __space, __u32 mss,
+				 __u32 *rcv_wnd, __u32 *window_clamp,
+				 int wscale_ok, __u8 *rcv_wscale,
+				 __u32 init_rcv_wnd)
 {
 	const struct mptcp_cb *mpcb = tcp_sk(sk)->mpcb;
 
 	*window_clamp = mpcb->orig_window_clamp;
-	__space = tcp_win_from_space(mpcb->orig_sk_rcvbuf);
+	__space = tcp_win_from_space(sk, mpcb->orig_sk_rcvbuf);
 
-	tcp_select_initial_window(__space, mss, rcv_wnd, window_clamp,
-				  wscale_ok, rcv_wscale, init_rcv_wnd, sk);
+	tcp_select_initial_window(sk, __space, mss, rcv_wnd, window_clamp,
+				  wscale_ok, rcv_wscale, init_rcv_wnd);
 }
 
 static inline u64 mptcp_calc_rate(const struct sock *meta_sk, unsigned int mss,

include/linux/tcp.h
@@ -84,13 +84,13 @@ struct tcp_sack_block {
 };
 
 struct tcp_out_options {
-	u16	options;	/* bit field of OPTION_* */
-	u8	ws;		/* window scale, 0 to disable */
-	u8	num_sack_blocks;/* number of SACK blocks to include */
-	u8	hash_size;	/* bytes in hash_location */
-	u16	mss;		/* 0 to disable */
-	__u8	*hash_location;	/* temporary pointer, overloaded */
-	__u32	tsval, tsecr;	/* need to include OPTION_TS */
+	u16 options;		/* bit field of OPTION_* */
+	u16 mss;		/* 0 to disable */
+	u8 ws;			/* window scale, 0 to disable */
+	u8 num_sack_blocks;	/* number of SACK blocks to include */
+	u8 hash_size;		/* bytes in hash_location */
+	__u8 *hash_location;	/* temporary pointer, overloaded */
+	__u32 tsval, tsecr;	/* need to include OPTION_TS */
 	struct tcp_fastopen_cookie *fastopen_cookie;	/* Fast open cookie */
 #ifdef CONFIG_MPTCP
 	u16	mptcp_options;	/* bit field of MPTCP related OPTION_* */

include/net/mptcp.h
@@ -194,7 +194,7 @@ struct mptcp_tcp_sock {
 	u32	infinite_cutoff_seq;
 	struct delayed_work work;
 	u32	mptcp_loc_nonce;
-	struct tcp_sock *tp; /* Where is my daddy? */
+	struct tcp_sock *tp;
 	u32	last_end_data_seq;
 
 	/* MP_JOIN subflow: timer for retransmitting the 3rd ack */
@@ -816,10 +816,10 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk,
 				   struct sk_buff *skb,
 				   const struct mptcp_options_received *mopt);
 u32 __mptcp_select_window(struct sock *sk);
-void mptcp_select_initial_window(int __space, __u32 mss, __u32 *rcv_wnd,
-					__u32 *window_clamp, int wscale_ok,
-					__u8 *rcv_wscale, __u32 init_rcv_wnd,
-					const struct sock *sk);
+void mptcp_select_initial_window(const struct sock *sk, int __space, __u32 mss,
+				 __u32 *rcv_wnd, __u32 *window_clamp,
+				 int wscale_ok, __u8 *rcv_wscale,
+				 __u32 init_rcv_wnd);
 unsigned int mptcp_current_mss(struct sock *meta_sk);
 int mptcp_select_size(const struct sock *meta_sk, bool sg, bool first_skb);
 void mptcp_key_sha1(u64 key, u32 *token, u64 *idsn);
@@ -835,7 +835,7 @@ void mptcp_sub_close(struct sock *sk, unsigned long delay);
 struct sock *mptcp_select_ack_sock(const struct sock *meta_sk);
 void mptcp_fallback_meta_sk(struct sock *meta_sk);
 int mptcp_backlog_rcv(struct sock *meta_sk, struct sk_buff *skb);
-void mptcp_ack_handler(unsigned long);
+void mptcp_ack_handler(struct timer_list *t);
 bool mptcp_check_rtt(const struct tcp_sock *tp, int time);
 int mptcp_check_snd_buf(const struct tcp_sock *tp);
 bool mptcp_handle_options(struct sock *sk, const struct tcphdr *th,

include/net/tcp.h
@@ -1421,8 +1421,7 @@ static inline void tcp_slow_start_after_idle_check(struct sock *sk)
 void tcp_select_initial_window(const struct sock *sk, int __space,
 			       __u32 mss, __u32 *rcv_wnd,
 			       __u32 *window_clamp, int wscale_ok,
-			       __u8 *rcv_wscale, __u32 init_rcv_wnd,
-			       const struct sock *sk);
+			       __u8 *rcv_wscale, __u32 init_rcv_wnd);
 
 static inline int tcp_win_from_space(const struct sock *sk, int space)
 {
@@ -1981,10 +1980,10 @@ struct tcp_sock_af_ops {
 struct tcp_sock_ops {
 	u32 (*__select_window)(struct sock *sk);
 	u16 (*select_window)(struct sock *sk);
-	void (*select_initial_window)(int __space, __u32 mss, __u32 *rcv_wnd,
+	void (*select_initial_window)(const struct sock *sk, int __space,
+				      __u32 mss, __u32 *rcv_wnd,
 				      __u32 *window_clamp, int wscale_ok,
-				      __u8 *rcv_wscale, __u32 init_rcv_wnd,
-				      const struct sock *sk);
+				      __u8 *rcv_wscale, __u32 init_rcv_wnd);
 	int (*select_size)(const struct sock *sk, bool sg, bool first_skb);
 	void (*init_buffer_space)(struct sock *sk);
 	void (*set_rto)(struct sock *sk);

net/ipv4/syncookies.c
@@ -421,7 +421,7 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 	tp->ops->select_initial_window(sk, tcp_full_space(sk), req->mss,
 				       &req->rsk_rcv_wnd, &req->rsk_window_clamp,
 				       ireq->wscale_ok, &rcv_wscale,
-				       dst_metric(&rt->dst, RTAX_INITRWND), sk);
+				       dst_metric(&rt->dst, RTAX_INITRWND));
 
 	ireq->rcv_wscale  = rcv_wscale;
 	ireq->ecn_ok = cookie_ecn_ok(&tcp_opt, sock_net(sk), &rt->dst);

net/ipv4/tcp.c
@@ -491,7 +491,7 @@ void tcp_init_transfer(struct sock *sk, int bpf_op)
 	tcp_init_metrics(sk);
 	tcp_call_bpf(sk, bpf_op);
 	tcp_init_congestion_control(sk);
-	tp->ops->init_buffer_space(sk);
+	tcp_sk(sk)->ops->init_buffer_space(sk);
 }
 
 static void tcp_tx_timestamp(struct sock *sk, u16 tsflags)

net/ipv4/tcp_output.c
@@ -194,7 +194,7 @@ u32 tcp_default_init_rwnd(u32 mss)
 void tcp_select_initial_window(const struct sock *sk, int __space, __u32 mss,
 			       __u32 *rcv_wnd, __u32 *window_clamp,
 			       int wscale_ok, __u8 *rcv_wscale,
-			       __u32 init_rcv_wnd, const struct sock *sk)
+			       __u32 init_rcv_wnd)
 {
 	unsigned int space = (__space < 0 ? 0 : __space);
 
@@ -428,18 +428,6 @@ static void smc_options_write(__be32 *ptr, u16 *options)
 	}
 #endif
 }
-
-struct tcp_out_options {
-	u16 options;		/* bit field of OPTION_* */
-	u16 mss;		/* 0 to disable */
-	u8 ws;			/* window scale, 0 to disable */
-	u8 num_sack_blocks;	/* number of SACK blocks to include */
-	u8 hash_size;		/* bytes in hash_location */
-	__u8 *hash_location;	/* temporary pointer, overloaded */
-	__u32 tsval, tsecr;	/* need to include OPTION_TS */
-	struct tcp_fastopen_cookie *fastopen_cookie;	/* Fast open cookie */
-};
->>>>>>> v4.15-rc3
 
 /* Write previously computed TCP options to the packet.
  *

net/mptcp/mptcp_ctrl.c
@@ -1622,12 +1622,15 @@ void mptcp_update_sndbuf(const struct tcp_sock *tp)
 
 		new_sndbuf += sk->sk_sndbuf;
 
-		if (new_sndbuf > sysctl_tcp_wmem[2] || new_sndbuf < 0) {
-			new_sndbuf = sysctl_tcp_wmem[2];
+		if (new_sndbuf > sock_net(meta_sk)->ipv4.sysctl_tcp_wmem[2] ||
+		    new_sndbuf < 0) {
+			new_sndbuf = sock_net(meta_sk)->ipv4.sysctl_tcp_wmem[2];
 			break;
 		}
 	}
-	meta_sk->sk_sndbuf = max(min(new_sndbuf, sysctl_tcp_wmem[2]), meta_sk->sk_sndbuf);
+	meta_sk->sk_sndbuf = max(min(new_sndbuf,
+				     sock_net(meta_sk)->ipv4.sysctl_tcp_wmem[2]),
+				 meta_sk->sk_sndbuf);
 
 	/* The subflow's call to sk_write_space in tcp_new_space ends up in
 	 * mptcp_write_space.

commit 5a1dbd70f0f61c43b1c4a699f3f838541cdca580

mptcp: Set correct skb in mptcp_find_and_set_pathmask
We need to set the iterator skb_it instead of skb.

Fixes: 1285e3945aee ("mptcp: Build-Fix")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_output.c
@@ -102,7 +102,7 @@ static void mptcp_find_and_set_pathmask(struct sock *meta_sk, struct sk_buff *sk
 
 	while (*p) {
 		parent = *p;
-		skb = rb_to_skb(parent);
+		skb_it = rb_to_skb(parent);
 		if (before(TCP_SKB_CB(skb)->seq, TCP_SKB_CB(skb_it)->seq)) {
 			p = &parent->rb_left;
 			continue;