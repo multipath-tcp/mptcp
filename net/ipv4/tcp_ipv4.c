multipath-tcp/mptcp

commit f9bafcf74ea2891aa17519e0ad501c5b795f5e2a

mptcp: Check for closed meta-socket before tcp_check_req
As now request-sockets are full fledged sockets in the hash-table,
calling mptcp_close() is no more cleaning up the half-open
request-sockets from new subflows.

These will only be cleaned up as the 3-way handshake finishes. At that
point, we need to check whether the meta-socket is still in a state
where it can establish new subflows. If we don't do this, refcounting
gets messed up and we start accessing garbage.

Fixes: b568f578baca (Merge tag 'v4.4' into mptcp_trunk)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
(cherry picked from commit 84e18404349ecdc8e3e006da562ef9439d20eb79)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp_ipv4.c
@@ -1717,6 +1717,14 @@ process:
 		if (is_meta_sk(sk)) {
 			bh_lock_sock(sk);
 
+			if (!mptcp_can_new_subflow(sk)) {
+				inet_csk_reqsk_queue_drop_and_put(sk, req);
+				bh_unlock_sock(sk);
+				sock_put(sk);
+
+				return 0;
+			}
+
 			if (sock_owned_by_user(sk)) {
 				skb->sk = sk;
 				if (unlikely(sk_add_backlog(sk, skb,

include/net/mptcp.h
@@ -1312,6 +1312,21 @@ static inline bool mptcp_v6_is_v4_mapped(const struct sock *sk)
 	       ipv6_addr_type(&inet6_sk(sk)->saddr) == IPV6_ADDR_MAPPED;
 }
 
+static inline bool mptcp_can_new_subflow(const struct sock *meta_sk)
+{
+	/* Has been removed from the tk-table. Thus, no new subflows.
+	 *
+	 * Check for close-state is necessary, because we may have been closed
+	 * without passing by mptcp_close().
+	 *
+	 * When falling back, no new subflows are allowed either.
+	 */
+	return meta_sk->sk_state != TCP_CLOSE &&
+	       tcp_sk(meta_sk)->inside_tk_table &&
+	       !tcp_sk(meta_sk)->mpcb->infinite_mapping_rcv &&
+	       !tcp_sk(meta_sk)->mpcb->send_infinite_mapping;
+}
+
 /* TCP and MPTCP mpc flag-depending functions */
 u16 mptcp_select_window(struct sock *sk);
 void mptcp_init_buffer_space(struct sock *sk);

net/mptcp/mptcp_ipv4.c
@@ -186,7 +186,6 @@ int mptcp_finish_handshake(struct sock *child, struct sk_buff *skb)
  */
 int mptcp_v4_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	const struct tcphdr *th = tcp_hdr(skb);
 	const struct iphdr *iph = ip_hdr(skb);
 	struct sock *child, *rsk = NULL, *sk;
@@ -213,7 +212,11 @@ int mptcp_v4_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 	if (sk->sk_state == TCP_NEW_SYN_RECV) {
 		struct request_sock *req = inet_reqsk(sk);
 
+		if (!mptcp_can_new_subflow(meta_sk))
+			goto reset_and_discard;
+
 		local_bh_disable();
+
 		child = tcp_check_req(meta_sk, skb, req, false);
 		if (!child) {
 			reqsk_put(req);
@@ -246,15 +249,7 @@ int mptcp_v4_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 	return ret;
 
 new_subflow:
-	/* Has been removed from the tk-table. Thus, no new subflows.
-	 *
-	 * Check for close-state is necessary, because we may have been closed
-	 * without passing by mptcp_close().
-	 *
-	 * When falling back, no new subflows are allowed either.
-	 */
-	if (meta_sk->sk_state == TCP_CLOSE || !tcp_sk(meta_sk)->inside_tk_table ||
-	    mpcb->infinite_mapping_rcv || mpcb->send_infinite_mapping)
+	if (!mptcp_can_new_subflow(meta_sk))
 		goto reset_and_discard;
 
 	child = tcp_v4_cookie_check(meta_sk, skb);

net/mptcp/mptcp_ipv6.c
@@ -179,7 +179,6 @@ static int mptcp_v6_join_request(struct sock *meta_sk, struct sk_buff *skb)
 
 int mptcp_v6_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	const struct tcphdr *th = tcp_hdr(skb);
 	const struct ipv6hdr *ip6h = ipv6_hdr(skb);
 	struct sock *child, *rsk = NULL, *sk;
@@ -207,6 +206,9 @@ int mptcp_v6_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 
 	if (sk->sk_state == TCP_NEW_SYN_RECV) {
 		struct request_sock *req = inet_reqsk(sk);
+
+		if (!mptcp_can_new_subflow(meta_sk))
+			goto reset_and_discard;
 
 		local_bh_disable();
 		child = tcp_check_req(meta_sk, skb, req, false);
@@ -241,15 +243,7 @@ int mptcp_v6_do_rcv(struct sock *meta_sk, struct sk_buff *skb)
 	return ret;
 
 new_subflow:
-	/* Has been removed from the tk-table. Thus, no new subflows.
-	 *
-	 * Check for close-state is necessary, because we may have been closed
-	 * without passing by mptcp_close().
-	 *
-	 * When falling back, no new subflows are allowed either.
-	 */
-	if (meta_sk->sk_state == TCP_CLOSE || !tcp_sk(meta_sk)->inside_tk_table ||
-	    mpcb->infinite_mapping_rcv || mpcb->send_infinite_mapping)
+	if (!mptcp_can_new_subflow(meta_sk))
 		goto reset_and_discard;
 
 	child = tcp_v6_cookie_check(meta_sk, skb);

commit d7d1db1fd55380ea3f3f702d26b87a318b20b3f6

mptcp: Properly free skb when exiting tcp_v4(6)_rcv
commit f9bafcf74ea2 ("mptcp: Check for closed meta-socket before tcp_check_req")
started checking for the meta-socket's state before accepting an
incoming subflow.

However, we missed to free the skb when exiting tcp_v4_rcv.

Also, the code wasn't added to IPv6.

Fixes: f9bafcf74ea2 ("mptcp: Check for closed meta-socket before tcp_check_req")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp_ipv4.c
@@ -1746,9 +1746,8 @@ process:
 			if (!mptcp_can_new_subflow(sk)) {
 				inet_csk_reqsk_queue_drop_and_put(sk, req);
 				bh_unlock_sock(sk);
-				sock_put(sk);
 
-				return 0;
+				goto discard_and_relse;
 			}
 
 			if (sock_owned_by_user(sk)) {

net/ipv6/tcp_ipv6.c
@@ -1527,6 +1527,13 @@ process:
 		if (is_meta_sk(sk)) {
 			bh_lock_sock(sk);
 
+			if (!mptcp_can_new_subflow(sk)) {
+				inet_csk_reqsk_queue_drop_and_put(sk, req);
+				bh_unlock_sock(sk);
+
+				goto discard_and_relse;
+			}
+
 			if (sock_owned_by_user(sk)) {
 				skb->sk = sk;
 				if (unlikely(sk_add_backlog(sk, skb,