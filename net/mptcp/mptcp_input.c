multipath-tcp/mptcp

commit 32bc8fb196eebc4bc67b213e232dd35aff423d8f

mptcp: Don't parse DATA_ACK when closed
mptcp_data_ack accesses some fields in the meta-sk which could be
pointing to garbage if the socket is already closed. In particular,
sk->sk_send_head will be set but the send-queue has been free'd in
tcp_v4_destroy_sock.

So, don't enter mptcp_data_ack when the meta is closed.

Github-issue: https://github.com/multipath-tcp/mptcp/issues/226

Cc: Kostas <kpeletidis@gmail.com>
Reported-by: Kostas <kpeletidis@gmail.com>
Fixes: Zero-day bug
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
(cherry picked from commit ac6cddc5fb571dd5b67c61d1560b3bf6cb8b4b93)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_input.c
@@ -1446,6 +1446,9 @@ static void mptcp_data_ack(struct sock *sk, const struct sk_buff *skb)
 	u32 nwin, data_ack, data_seq;
 	u16 data_len = 0;
 
+	if (meta_sk->sk_state == TCP_CLOSE)
+		return;
+
 	/* A valid packet came in - subflow is operational again */
 	tp->pf = 0;
 

commit f894d97e1c29dc5eea392f73721ffebfdf543a29

mptcp: Mark subflow as fully established upon DATA_ACK when meta is closed
It can be, that a MPTCP-connection is already in CLOSED-state before we
ever receive a DATA_ACK on a subflow. This can be triggered with
the packetdrill script:

+0` ip tcp_metrics flush`

+0   socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
+0  setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0  bind(3, ..., ...) = 0
+0  listen(3, 1) = 0

+0  socket(..., SOCK_STREAM, IPPROTO_TCP) = 5
+0  setsockopt(5, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0  bind(5, {sa_family = AF_INET, sin_port = htons(8080), sin_addr = inet_addr("192.168.0.1")}, ...) = 0
+0  listen(5,1) = 0

+0  < S 0:0(0) win 32792 <mss 1460,nop,wscale 7,mp_capable a> sock(3)
+0  > S. 0:0(0) ack 1 <mss 1460,nop,wscale 7, mp_capable b> sock(3)
+0.1  < . 1:1(0) ack 1 win 257 <mp_capable a b, dss dack4> sock(3)
+0  accept(3, ..., ...) = 4

+0 < P. 1:1001(1000) ack 1 win 4242 <dss dack4=1 dsn4=1> sock(4)
+0 > . 1:1(0) ack 1001 <dss dack4=1001> sock(4)
+0 read(4, ..., 1000) = 1000

+0  write(4, ..., 1000) = 1000
+0  > P. 1:1001(1000) ack 1001 <dss dack4=1001 dsn4=1> sock(4)

+0  < S 0:0(0) win 32792 <mss 1460,sackOK,nop,nop,nop,wscale 7,mp_join_syn backup=0 address_id=0 token=sha1_32(b)> sock(5)
+0  > S. 0:0(0) ack 1 win 28800 <mss 1460,nop,nop,sackOK,nop,wscale 7, mp_join_syn_ack backup=0 address_id=0 sender_hmac=trunc_l64_hmac(b a) > sock(5)

+0  < . 1:1(0) ack 1 win 32792 <mp_join_ack sender_hmac=full_160_hmac(a b)> sock(5)
+0 mp_join_accept(5) = 6

+0 > . 1:1(0) ack 1 <dss dack4=1001> sock(6) // reliably mp_join_ack

+0 close(4) = 0
+0 > . 1:1(0) ack 1 <dss dack4=1001 dsn4=1001 FIN> sock(6)
+0 < . 1:1(0) ack 1 win 4242 <dss dack4=1002 dsn4=1001 FIN> sock(6)

+0 > F. 1:1(0) ack 1 <dss dack4=1001> sock(6)
+0 > F. 1001:1001(0) ack 1001 <dss dack4=1001> sock(4)
+0 > . 2:2(0) ack 1 <dss dack4=1002> sock(6)

+0 < F. 1:1(0) ack 2 win 4242 <dss dack4=1002> sock(6)
+0 > . 2:2(0) ack 2 <dss dack4=1002> sock(6)

/* Finally, the DATA_ACK comes in, but meta is already closed. This checks
 * whether we still process the DATA_ACK here (aka., make subflow fully established)
 */
+0 < . 1001:1001(0) ack 1001 win 4242 <dss dack4=1001> sock(4)
+0 < F. 1001:1001(0) ack 1002 win 4242 <dss dack4=1002> sock(4)

+0 > . 1002:1002(0) ack 1002 <dss dack4=1002> sock(4)

We need to allow processing this DATA_ACK even if meta is closed.
Because, otherwise we will trigger a fallback to regular TCP on this
subflow, which will be incorrect.
Also, a subsequent FIN will then be processed as if it was a DATA_FIN.
But, the connection is already closed and then the warning in
mptcp_fin() about incorrect state will be displayed.

Fixes: 32bc8fb196ee ("mptcp: Don't parse DATA_ACK when closed")

net/mptcp/mptcp_input.c
@@ -1428,9 +1428,6 @@ static void mptcp_data_ack(struct sock *sk, const struct sk_buff *skb)
 	u32 nwin, data_ack, data_seq;
 	u16 data_len = 0;
 
-	if (meta_sk->sk_state == TCP_CLOSE)
-		return;
-
 	/* A valid packet came in - subflow is operational again */
 	tp->pf = 0;
 
@@ -1446,9 +1443,7 @@ static void mptcp_data_ack(struct sock *sk, const struct sk_buff *skb)
 	 * set by mptcp_clean_rtx_infinite.
 	 */
 	if (!(tcb->mptcp_flags & MPTCPHDR_ACK) && !tp->mpcb->infinite_mapping_snd)
-		goto exit;
-
-	data_ack = tp->mptcp->rx_opt.data_ack;
+		return;
 
 	if (unlikely(!tp->mptcp->fully_established) &&
 	    tp->mptcp->snt_isn + 1 != TCP_SKB_CB(skb)->ack_seq)
@@ -1457,6 +1452,13 @@ static void mptcp_data_ack(struct sock *sk, const struct sk_buff *skb)
 		 */
 		mptcp_become_fully_estab(sk);
 
+	/* After we did the subflow-only processing (stopping timer and marking
+	 * subflow as established), check if we can proceed with MPTCP-level
+	 * processing.
+	 */
+	if (meta_sk->sk_state == TCP_CLOSE)
+		return;
+
 	/* Get the data_seq */
 	if (mptcp_is_data_seq(skb)) {
 		data_seq = tp->mptcp->rx_opt.data_seq;
@@ -1464,6 +1466,8 @@ static void mptcp_data_ack(struct sock *sk, const struct sk_buff *skb)
 	} else {
 		data_seq = meta_tp->snd_wl1;
 	}
+
+	data_ack = tp->mptcp->rx_opt.data_ack;
 
 	/* If the ack is older than previous acks
 	 * then we can probably ignore it.