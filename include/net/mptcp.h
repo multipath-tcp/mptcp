multipath-tcp/mptcp

commit 57192ffda92ba5e1a23b4df944623b504d2c208d

mptcp: Correct initial meta-RTO estimation after 3-way handshake
Since the merge with v4.4, we call mptcp_set_rto coming from
tcp_synack_rtt_meas() in mptcp_check_req_master() before the subflow has
transitioned to ESTABLISHED state. This means that we will be ignoring
the RTT-estimation from the initial subflow that has been measured
during the 3-way handshake.

This patch fixes this by accepting RTT-estimations from subflows that
are in SYN_RECV-state. Subflows in the MPTCP's subflow-list will only be
in there if their 3-way handshake has completed, so it's safe to do this
here.

Fixes: b568f578baca (Merge tag 'v4.4' into mptcp_trunk)
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -1219,7 +1219,7 @@ static inline void mptcp_set_rto(struct sock *sk)
 		return;
 
 	mptcp_for_each_sk(tp->mpcb, sk_it) {
-		if (mptcp_sk_can_send(sk_it) &&
+		if ((mptcp_sk_can_send(sk_it) || sk->sk_state == TCP_SYN_RECV) &&
 		    inet_csk(sk_it)->icsk_rto > max_rto)
 			max_rto = inet_csk(sk_it)->icsk_rto;
 	}

commit 92a6e05825aa069d409d188f42ab62aa9f83c167

mptcp: Pick correct sk when iterating in mptcp_set_rto
We are iterating with sk_it as the iterator, so pick this one!

Fixes: 57192ffda92b ("mptcp: Correct initial meta-RTO estimation after 3-way handshake")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -1212,7 +1212,7 @@ static inline void mptcp_set_rto(struct sock *sk)
 	mptcp_for_each_sub(tp->mpcb, mptcp) {
 		struct sock *sk_it = mptcp_to_sock(mptcp);
 
-		if ((mptcp_sk_can_send(sk_it) || sk->sk_state == TCP_SYN_RECV) &&
+		if ((mptcp_sk_can_send(sk_it) || sk_it->sk_state == TCP_SYN_RECV) &&
 		    inet_csk(sk_it)->icsk_rto > max_rto)
 			max_rto = inet_csk(sk_it)->icsk_rto;
 	}