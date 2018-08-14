c7ca416f56bb380af980f4b96db308f623a733ca
Gregory Detal <gregory.detal@tessares.net>
2018-08-14 18:09:38 +0200

    mptcp: correct refcnt for sk in tcp_v{4,6}_rcv
    When we receive an skb in tcp_vX_rcv if the sock is hold by user we copy
    the sk inside the skb before putting the skb inside the backlog, the sk
    will be used after from mptcp_backlog_rcv. If the sk refcnt is not done
    properly the sk read from mptcp_backlog_rcv could point to another sk
    because it may have been freed and reused in the mean time.
    
    In mptcp_backlog_rcv, the call to skb_oprhan will call the destructor
    that we set for the skb and decrement the sk refcnt. Note that we call
    skb_oprhan after the increase of sk_refcnt. This ensures that we never
    free the sk too quickly.
    
    We also remove unneeded skb->sk assignments, a line is there for that in
    mptcp_backlog_rcv and if we set it, we should also assign a destructor.
    
    Notes about backports/forwardport:
    - in mptcp_trunk, changes in mptcp_input.c should no longer be needed.
      In tcp_ipv{4,6}, the first additions should no longer be needed as
      well.
    - before v4.4, we need to remove skb->sk = meta_sk in mptcp_check_req().
    
    Fixes: Zero-day-bug
    Signed-off-by: Gregory Detal <gregory.detal@tessares.net>
    Signed-off-by: Benjamin Hesmans <benjamin.hesmans@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    (cherry picked from commit 952bd28cc5be67feb578cbb5b671488a5313e016)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    
    Conflicts:
    	net/mptcp/mptcp_ctrl.c

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1728,7 +1728,7 @@ process:
 			}
 
 			if (sock_owned_by_user(sk)) {
-				skb->sk = sk;
+				mptcp_prepare_for_backlog(sk, skb);
 				if (unlikely(sk_add_backlog(sk, skb,
 							    sk->sk_rcvbuf + sk->sk_sndbuf))) {
 					reqsk_put(req);
@@ -1797,7 +1797,7 @@ process:
 
 		bh_lock_sock_nested(meta_sk);
 		if (sock_owned_by_user(meta_sk))
-			skb->sk = sk;
+			mptcp_prepare_for_backlog(sk, skb);
 	} else {
 		meta_sk = sk;
 		bh_lock_sock_nested(sk);
