multipath-tcp/mptcp

commit 17683a61dd6de05cdbc3a909786c7ae73e5bb932

mptcp: Don't clone empty DATA_FIN in tcp_transmit_skb
We did not put the empty DATA_FIN on the subflow's send-queue. Thus, we
should not clone it in tcp_transmit_skb.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_output.c
@@ -518,20 +518,16 @@ static bool mptcp_skb_entail(struct sock *sk, struct sk_buff *skb, int reinject)
 		sk->sk_wmem_queued += subskb->truesize;
 		sk_mem_charge(sk, subskb->truesize);
 	} else {
-		int err;
-
 		/* Necessary to initialize for tcp_transmit_skb. mss of 1, as
 		 * skb->len = 0 will force tso_segs to 1.
 		 */
 		tcp_init_tso_segs(subskb, 1);
+
 		/* Empty data-fins are sent immediatly on the subflow */
-		err = tcp_transmit_skb(sk, subskb, 1, GFP_ATOMIC);
-
-		/* It has not been queued, we can free it now. */
-		kfree_skb(subskb);
-
-		if (err)
+		if (tcp_transmit_skb(sk, subskb, 0, GFP_ATOMIC) != 0) {
+			kfree_skb(subskb);
 			return false;
+		}
 	}
 
 	if (!tp->mptcp->fully_established) {

commit db9347ff01f604e60f0199faa2847337c3f829cb

mptcp: Don't free skb when tcp_transmit_skb without cloning
Because, tcp_transmit_skb will already take care of the free'ing.

Fixes: 17683a61dd6d ("mptcp: Don't clone empty DATA_FIN in tcp_transmit_skb")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_output.c
@@ -562,10 +562,8 @@ static bool mptcp_skb_entail(struct sock *sk, struct sk_buff *skb, int reinject)
 		tcp_init_tso_segs(subskb, 1);
 
 		/* Empty data-fins are sent immediatly on the subflow */
-		if (tcp_transmit_skb(sk, subskb, 0, GFP_ATOMIC) != 0) {
-			kfree_skb(subskb);
+		if (tcp_transmit_skb(sk, subskb, 0, GFP_ATOMIC))
 			return false;
-		}
 	}
 
 	if (!tp->mptcp->fully_established) {