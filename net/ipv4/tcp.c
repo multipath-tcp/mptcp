multipath-tcp/mptcp

commit f2632fa4ee58f2d375e38119633b5739b6d43b2e

mptcp: Use tcp_abort correctly for MPTCP
With MPTCP, we have a modified version of tcp_send_active_reset(). Let's
use it if needed.

Fixes: 10c57b6f07ac (Merge tag 'v4.5' into mptcp_trunk)
Fixes: 07abdbda3bd6 (mptcp: Use tcp_abort correctly for MPTCP)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

net/ipv4/tcp.c
@@ -3991,7 +3991,7 @@ int tcp_abort(struct sock *sk, int err)
 		smp_wmb();
 		sk->sk_error_report(sk);
 		if (tcp_need_reset(sk->sk_state))
-			tcp_send_active_reset(sk, GFP_ATOMIC);
+			tp->ops->send_active_reset(sk, GFP_ATOMIC);
 		tcp_done(sk);
 	}
 

commit 4b41ddb989aea141dd7308964e6d8a171211899e

mptcp: Build-Fix
Fixes: f2632fa4ee58 ("mptcp: Use tcp_abort correctly for MPTCP")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

net/ipv4/tcp.c
@@ -3991,7 +3991,7 @@ int tcp_abort(struct sock *sk, int err)
 		smp_wmb();
 		sk->sk_error_report(sk);
 		if (tcp_need_reset(sk->sk_state))
-			tp->ops->send_active_reset(sk, GFP_ATOMIC);
+			tcp_sk(sk)->ops->send_active_reset(sk, GFP_ATOMIC);
 		tcp_done(sk);
 	}
 