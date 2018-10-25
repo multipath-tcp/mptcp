68ec6c2b751c5ba5aa115459097e8d4cce9592f0
Matthieu Baerts <matthieu.baerts@tessares.net>
2018-10-25 10:26:18 +0200

    mptcp: Stable Release v0.93.2
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2901,7 +2901,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.93.1");
+	pr_info("MPTCP: Stable release v0.93.2");
 
 	mptcp_init_failed = false;
 
