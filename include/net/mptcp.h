multipath-tcp/mptcp

commit 133537deb63d04e1dfb5af7fd82ed51ba243e518

mptcp: Recreate subflows after a timeout
It has been reported that (very) longlived connections can experience a
NAT-timeout if they are idle for too long. The subflow will then
eventually timeout if he tries to send data again.

Thus, eventually such longlived connections will end up only using a
single subflow.

Our path-manager should reestablish subflows when this happens.

Currently, we control this with a module-parameter "create_on_err" for
the fullmesh path-manager.

Github-issue: https://github.com/multipath-tcp/mptcp/issues/153

Reported-by: https://github.com/wapsi
Reported-by: https://github.com/titer
Reported-by: Max <list_mptcp@4ext.com>
Tested-by: https://github.com/djbobo
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -225,6 +225,7 @@ struct mptcp_pm_ops {
 	void (*release_sock)(struct sock *meta_sk);
 	void (*fully_established)(struct sock *meta_sk);
 	void (*new_remote_address)(struct sock *meta_sk);
+	void (*subflow_error)(struct sock *meta_sk, struct sock *sk);
 	int  (*get_local_id)(sa_family_t family, union inet_addr *addr,
 			     struct net *net, bool *low_prio);
 	void (*addr_signal)(struct sock *sk, unsigned *size,

net/mptcp/mptcp_ctrl.c
@@ -604,13 +604,13 @@ EXPORT_SYMBOL(mptcp_select_ack_sock);
 static void mptcp_sock_def_error_report(struct sock *sk)
 {
 	const struct mptcp_cb *mpcb = tcp_sk(sk)->mpcb;
+	struct sock *meta_sk = mptcp_meta_sk(sk);
 
 	if (!sock_flag(sk, SOCK_DEAD))
 		mptcp_sub_close(sk, 0);
 
 	if (mpcb->infinite_mapping_rcv || mpcb->infinite_mapping_snd ||
 	    mpcb->send_infinite_mapping) {
-		struct sock *meta_sk = mptcp_meta_sk(sk);
 
 		meta_sk->sk_err = sk->sk_err;
 		meta_sk->sk_err_soft = sk->sk_err_soft;
@@ -626,6 +626,9 @@ static void mptcp_sock_def_error_report(struct sock *sk)
 		if (meta_sk->sk_state != TCP_CLOSE)
 			tcp_done(meta_sk);
 	}
+
+	if (mpcb->pm_ops->subflow_error)
+		mpcb->pm_ops->subflow_error(meta_sk, sk);
 
 	sk->sk_err = 0;
 	return;

net/mptcp/mptcp_fullmesh.c
@@ -95,6 +95,10 @@ static int num_subflows __read_mostly = 1;
 module_param(num_subflows, int, 0644);
 MODULE_PARM_DESC(num_subflows, "choose the number of subflows per pair of IP addresses of MPTCP connection");
 
+static int create_on_err __read_mostly;
+module_param(create_on_err, int, 0644);
+MODULE_PARM_DESC(create_on_err, "recreate the subflow upon a timeout");
+
 static struct mptcp_pm_ops full_mesh __read_mostly;
 
 static void full_mesh_create_subflows(struct sock *meta_sk);
@@ -1370,6 +1374,24 @@ static void full_mesh_create_subflows(struct sock *meta_sk)
 	}
 }
 
+static void full_mesh_subflow_error(struct sock *meta_sk, struct sock *sk)
+{
+	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
+
+	if (!create_on_err)
+		return;
+
+	if (mpcb->infinite_mapping_snd || mpcb->infinite_mapping_rcv ||
+	    mpcb->send_infinite_mapping ||
+	    mpcb->server_side || sock_flag(meta_sk, SOCK_DEAD))
+		return;
+
+	if (sk->sk_err != ETIMEDOUT)
+		return;
+
+	full_mesh_create_subflows(meta_sk);
+}
+
 /* Called upon release_sock, if the socket was owned by the user during
  * a path-management event.
  */
@@ -1799,6 +1821,7 @@ static struct mptcp_pm_ops full_mesh __read_mostly = {
 	.release_sock = full_mesh_release_sock,
 	.fully_established = full_mesh_create_subflows,
 	.new_remote_address = full_mesh_create_subflows,
+	.subflow_error = full_mesh_subflow_error,
 	.get_local_id = full_mesh_get_local_id,
 	.addr_signal = full_mesh_addr_signal,
 	.add_raddr = full_mesh_add_raddr,

commit ed3036438443a7bcd8d2d0169a012d5595baed75

mptcp:fm: recreate subflows after an error
If a subflow is closed because of an error, it will be recreated if
create_on_err parameter is set.

For that, we need to store the previously recorded error (sk->sk_err)
and look at it when closing the subflow.

Note that here, we limit the retry on ETIMEDOUT error, like it was done
before. Otherwise, we might end up looping on subflow-creations, if we
are sending a SYN+MP_JOIN that results in a TCP-RST (ECONNREFUSED).

Fixes: 133537deb63d (mptcp: Recreate subflows after a timeout)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

include/net/mptcp.h
@@ -185,6 +185,7 @@ struct mptcp_tcp_sock {
 	u8	path_index;
 	u8	loc_id;
 	u8	rem_id;
+	u8	sk_err;
 
 #define MPTCP_SCHED_SIZE 16
 	u8	mptcp_sched[MPTCP_SCHED_SIZE] __aligned(8);

net/mptcp/mptcp_ctrl.c
@@ -620,6 +620,9 @@ static void mptcp_sock_def_error_report(struct sock *sk)
 		}
 	}
 
+	/* record this info that can be used by PM after the sf close */
+	tp->mptcp->sk_err = sk->sk_err;
+
 	if (mpcb->infinite_mapping_rcv || mpcb->infinite_mapping_snd ||
 	    mpcb->send_infinite_mapping) {
 		struct sock *meta_sk = mptcp_meta_sk(sk);

net/mptcp/mptcp_fullmesh.c
@@ -1686,10 +1686,14 @@ static void full_mesh_delete_subflow(struct sock *sk)
 {
 	struct fullmesh_priv *fmp = fullmesh_get_priv(tcp_sk(sk)->mpcb);
 	struct mptcp_fm_ns *fm_ns = fm_get_ns(sock_net(sk));
+	struct sock *meta_sk = mptcp_meta_sk(sk);
 	struct mptcp_loc_addr *mptcp_local;
 	int index, i;
 
 	if (!create_on_err)
+		return;
+
+	if (!mptcp_can_new_subflow(meta_sk))
 		return;
 
 	rcu_read_lock_bh();
@@ -1741,6 +1745,10 @@ static void full_mesh_delete_subflow(struct sock *sk)
 
 out:
 	rcu_read_unlock_bh();
+
+	/* re-schedule the creation of failed subflows */
+	if (tcp_sk(sk)->mptcp->sk_err == ETIMEDOUT || sk->sk_err == ETIMEDOUT)
+		full_mesh_create_subflows(meta_sk);
 }
 
 /* Output /proc/net/mptcp_fullmesh */