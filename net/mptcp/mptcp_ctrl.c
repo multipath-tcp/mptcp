fb2dec520372e990059bd622533b567ce5e75563
Christoph Paasch <cpaasch@apple.com>
2019-03-27 19:07:49 +0100

    mptcp: Move subflows to time-wait when we are in infinite mapping mode
    When in infinite mapping mode, we are effectively a TCP-connection.
    The MPTCP-state and the TCP-subflow state thus need to move in tandem.
    Which means that when we are in mptcp_time_wait, we need to also apply
    this to the subflow.
    
    Fixes: Zero-day bug
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 3c8383f51e63b8af4b5c5415af2922bc0064d583)
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    (cherry picked from commit c1ae1d34f11240a68ce41f223e7888d2c53d1daa)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit f01d6c9863d49aa47d7f4006f92358a940a7fbc6)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -641,9 +641,7 @@ static void mptcp_sock_def_error_report(struct sock *sk)
 		}
 	}
 
-	if (!tp->tcp_disconnect &&
-	    (mpcb->infinite_mapping_rcv || mpcb->infinite_mapping_snd ||
-	     mpcb->send_infinite_mapping)) {
+	if (!tp->tcp_disconnect && mptcp_in_infinite_mapping_weak(mpcb)) {
 		meta_sk->sk_err = sk->sk_err;
 		meta_sk->sk_err_soft = sk->sk_err_soft;
 
@@ -2327,6 +2325,17 @@ void mptcp_time_wait(struct sock *meta_sk, int state, int timeo)
 {
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 	struct mptcp_tw *mptw;
+
+	if (mptcp_in_infinite_mapping_weak(meta_tp->mpcb)) {
+		struct sock *sk_it, *tmp;
+
+		mptcp_for_each_sk_safe(meta_tp->mpcb, sk_it, tmp) {
+			if (sk_it->sk_state == TCP_CLOSE)
+				continue;
+
+			tcp_sk(sk_it)->ops->time_wait(sk_it, state, timeo);
+		}
+	}
 
 	/* Used for sockets that go into tw after the meta
 	 * (see mptcp_init_tw_sock())
--- net/mptcp/mptcp_fullmesh.c
+++ net/mptcp/mptcp_fullmesh.c
@@ -870,9 +870,7 @@ duno:
 				goto next;
 
 			if (!mptcp(meta_tp) || !is_meta_sk(meta_sk) ||
-			    mpcb->infinite_mapping_snd ||
-			    mpcb->infinite_mapping_rcv ||
-			    mpcb->send_infinite_mapping)
+			    mptcp_in_infinite_mapping_weak(mpcb))
 				goto next;
 
 			/* May be that the pm has changed in-between */
@@ -1413,8 +1411,7 @@ static void full_mesh_create_subflows(struct sock *meta_sk)
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct fullmesh_priv *fmp = fullmesh_get_priv(mpcb);
 
-	if (mpcb->infinite_mapping_snd || mpcb->infinite_mapping_rcv ||
-	    mpcb->send_infinite_mapping ||
+	if (mptcp_in_infinite_mapping_weak(mpcb) ||
 	    mpcb->server_side || sock_flag(meta_sk, SOCK_DEAD))
 		return;
 
@@ -1436,8 +1433,7 @@ static void full_mesh_subflow_error(struct sock *meta_sk, struct sock *sk)
 	if (!create_on_err)
 		return;
 
-	if (mpcb->infinite_mapping_snd || mpcb->infinite_mapping_rcv ||
-	    mpcb->send_infinite_mapping ||
+	if (mptcp_in_infinite_mapping_weak(mpcb) ||
 	    mpcb->server_side || sock_flag(meta_sk, SOCK_DEAD))
 		return;
 
