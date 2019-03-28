multipath-tcp/mptcp

commit 78bab8daa281db54967a6d42922404a5c4185716

mptcp: Rework mptcp_disconnect
Looking at how we close the subflows, this is very buggy. We are
"cheating" by marking them as non-MPTCP, forcing removal,... It is much
cleaner to reproduce the work from inet_child_forget and properly
disconnect the subflows.

Btw., the way we are doing it at the moment can lead to a deadlock,
because tcp_disconnect can be called while holding the master-socket's
lock (thus, we deadlock in mptcp_disconnect):

INFO: rcu_sched self-detected stall on CPU
	1-...: (20999 ticks this GP) idle=caa/140000000000001/0 softirq=230760/230765 fqs=0
	 (t=21000 jiffies g=98517 c=98516 q=79)
rcu_sched kthread starved for 21000 jiffies! g98517 c98516 f0x0 RCU_GP_WAIT_FQS(3) ->state=0x402 ->cpu=0
rcu_sched       I20840     8      2 0x80000000
[...snip...]
RIP: 0010:rep_nop arch/x86/include/asm/processor.h:655 [inline]
RIP: 0010:cpu_relax arch/x86/include/asm/processor.h:660 [inline]
RIP: 0010:virt_spin_lock arch/x86/include/asm/qspinlock.h:87 [inline]
RIP: 0010:queued_spin_lock_slowpath+0x1d0/0xde0 kernel/locking/qspinlock.c:313
RSP: 0018:ffff88806d1064b8 EFLAGS: 00000202 ORIG_RAX: ffffffffffffff10
RAX: 0000000000000001 RBX: ffff888064dd6f88 RCX: 1ffff1100d8f5ca8
RDX: 0000000000000004 RSI: 0000000000000001 RDI: ffff888064dd6f88
RBP: ffffed100da20cb7 R08: ffff88806a028000 R09: 00000000d1095007
R10: 0000000048bafb09 R11: ffff88804858ff58 R12: 1ffff1100da20c9b
R13: 0000000000000003 R14: dffffc0000000000 R15: ffff88806d1065b8
 spin_lock include/linux/spinlock.h:317 [inline]
 mptcp_disconnect+0x2be/0x610 net/mptcp/mptcp_ctrl.c:1870
 tcp_disconnect+0xf27/0x1230 net/ipv4/tcp.c:2426
 inet_child_forget+0x70/0x310 net/ipv4/inet_connection_sock.c:910
 inet_csk_reqsk_queue_add+0x29f/0x320 net/ipv4/inet_connection_sock.c:939
 inet_csk_complete_hashdance+0xd2/0x1c0 net/ipv4/inet_connection_sock.c:962
 mptcp_check_req_master+0x245/0x2a0 net/mptcp/mptcp_ctrl.c:2071
 tcp_check_req+0x1383/0x17c0 net/ipv4/tcp_minisocks.c:817

net/mptcp/mptcp_ctrl.c
@@ -1619,13 +1619,6 @@ static void mptcp_sub_close_doit(struct sock *sk)
 	if (sock_flag(sk, SOCK_DEAD))
 		return;
 
-	/* We come from tcp_disconnect. We are sure that meta_sk is set */
-	if (!mptcp(tp)) {
-		tp->closing = 1;
-		tcp_close(sk, 0);
-		return;
-	}
-
 	if (meta_sk->sk_shutdown == SHUTDOWN_MASK || sk->sk_state == TCP_CLOSE) {
 		tp->closing = 1;
 		tcp_close(sk, 0);
@@ -1933,47 +1926,34 @@ out:
 	sock_put(meta_sk); /* Taken by sock_hold */
 }
 
-void mptcp_disconnect(struct sock *sk)
+void mptcp_disconnect(struct sock *meta_sk)
 {
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 	struct mptcp_tcp_sock *mptcp;
-	struct tcp_sock *tp = tcp_sk(sk);
 	struct hlist_node *tmp;
 
-	__skb_queue_purge(&tp->mpcb->reinject_queue);
+	__skb_queue_purge(&meta_tp->mpcb->reinject_queue);
 
-	if (tp->inside_tk_table)
-		mptcp_hash_remove_bh(tp);
+	if (meta_tp->inside_tk_table)
+		mptcp_hash_remove_bh(meta_tp);
 
 	local_bh_disable();
-	mptcp_for_each_sub_safe(tp->mpcb, mptcp, tmp) {
+	mptcp_for_each_sub_safe(meta_tp->mpcb, mptcp, tmp) {
 		struct sock *subsk = mptcp_to_sock(mptcp);
 
-		/* The socket will get removed from the subsocket-list
-		 * and made non-mptcp by setting mpc to 0.
-		 *
-		 * This is necessary, because tcp_disconnect assumes
-		 * that the connection is completly dead afterwards.
-		 * Thus we need to do a mptcp_del_sock. Due to this call
-		 * we have to make it non-mptcp.
-		 *
-		 * We have to lock the socket, because we set mpc to 0.
-		 * An incoming packet would take the subsocket's lock
-		 * and go on into the receive-path.
-		 * This would be a race.
-		 */
+		meta_sk->sk_prot->disconnect(subsk, O_NONBLOCK);
 
-		bh_lock_sock(subsk);
-		mptcp_del_sock(subsk);
-		tcp_sk(subsk)->mpc = 0;
-		tcp_sk(subsk)->ops = &tcp_specific;
-		mptcp_sub_force_close(subsk);
-		bh_unlock_sock(subsk);
+		sock_orphan(subsk);
+
+		percpu_counter_inc(meta_sk->sk_prot->orphan_count);
+
+		inet_csk_destroy_sock(subsk);
 	}
 	local_bh_enable();
 
-	tp->was_meta_sk = 1;
-	tp->mpc = 0;
-	tp->ops = &tcp_specific;
+	meta_tp->was_meta_sk = 1;
+	meta_tp->mpc = 0;
+	meta_tp->ops = &tcp_specific;
 }
 
 

include/net/mptcp.h
@@ -844,7 +844,7 @@ unsigned int mptcp_xmit_size_goal(const struct sock *meta_sk, u32 mss_now,
 int mptcp_init_tw_sock(struct sock *sk, struct tcp_timewait_sock *tw);
 void mptcp_twsk_destructor(struct tcp_timewait_sock *tw);
 void mptcp_time_wait(struct sock *sk, int state, int timeo);
-void mptcp_disconnect(struct sock *sk);
+void mptcp_disconnect(struct sock *meta_sk);
 bool mptcp_should_expand_sndbuf(const struct sock *sk);
 int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb);
 void mptcp_tsq_flags(struct sock *sk);
@@ -1458,7 +1458,7 @@ static inline int mptcp_init_tw_sock(struct sock *sk,
 	return 0;
 }
 static inline void mptcp_twsk_destructor(struct tcp_timewait_sock *tw) {}
-static inline void mptcp_disconnect(struct sock *sk) {}
+static inline void mptcp_disconnect(struct sock *meta_sk) {}
 static inline void mptcp_tsq_flags(struct sock *sk) {}
 static inline void mptcp_tsq_sub_deferred(struct sock *meta_sk) {}
 static inline void mptcp_hash_remove_bh(struct tcp_sock *meta_tp) {}

commit 3cf601a85f6c29c24e9c5254d9109b4163603d0e

mptcp: Don't close meta in mptcp_sock_def_error_report upon tcp_disconnect
Because, at this point the meta-sk is already CLOSED and thus we will
hit the WARN_ON in mptcp_sock_def_error_report.

Otherwise we get:
------------[ cut here ]------------
WARNING: CPU: 0 PID: 6603 at net/mptcp/mptcp_ctrl.c:646 mptcp_sock_def_error_report+0x478/0x570 net/mptcp/mptcp_ctrl.c:643
Kernel panic - not syncing: panic_on_warn set ...

CPU: 0 PID: 6603 Comm: syz-executor.0 Not tainted 4.14.105 #3
Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS 0.5.1 01/01/2011
Call Trace:
 __dump_stack lib/dump_stack.c:17 [inline]
 dump_stack+0x10a/0x1d1 lib/dump_stack.c:53
 panic+0x1bc/0x3c6 kernel/panic.c:182
 __warn+0x1c1/0x1e0 kernel/panic.c:546
 report_bug+0x1ee/0x2b0 lib/bug.c:186
 fixup_bug.part.9+0x32/0x80 arch/x86/kernel/traps.c:177
 fixup_bug arch/x86/kernel/traps.c:246 [inline]
 do_error_trap+0x248/0x320 arch/x86/kernel/traps.c:295
 invalid_op+0x1b/0x40 arch/x86/entry/entry_64.S:944
RIP: 0010:mptcp_sock_def_error_report+0x478/0x570 net/mptcp/mptcp_ctrl.c:643
RSP: 0018:ffff888052557738 EFLAGS: 00010282
RAX: 000000000000003c RBX: ffff888046d3f300 RCX: 1ffff1100a4aae7a
RDX: 0000000000000000 RSI: 0000000000000000 RDI: ffff88806ce1e798
RBP: ffff888046a60b80 R08: 1ffff1100a4aae4d R09: 0000000000000000
R10: 0000000000000003 R11: 0000000000000000 R12: ffff888049b05c80
R13: ffff888046d3f698 R14: ffff888049b05ca0 R15: ffff888046a60b92
 tcp_disconnect+0xbf5/0x1250 net/ipv4/tcp.c:2476
 mptcp_disconnect+0x352/0x760 net/mptcp/mptcp_ctrl.c:1893
 tcp_disconnect+0xf41/0x1250 net/ipv4/tcp.c:2432
 mptcp_close+0x436/0x1600 net/mptcp/mptcp_ctrl.c:1763
 tcp_close+0xf96/0x12c0 net/ipv4/tcp.c:2222
 inet_release+0xea/0x1c0 net/ipv4/af_inet.c:428
 inet6_release+0x4b/0x70 net/ipv6/af_inet6.c:449
 __sock_release+0xce/0x280 net/socket.c:602
 sock_close+0x15/0x20 net/socket.c:1138
 __fput+0x30e/0x7b0 fs/file_table.c:210
 task_work_run+0x179/0x240 kernel/task_work.c:113
 tracehook_notify_resume include/linux/tracehook.h:191 [inline]
 exit_to_usermode_loop+0x1db/0x210 arch/x86/entry/common.c:163
 prepare_exit_to_usermode arch/x86/entry/common.c:198 [inline]
 syscall_return_slowpath arch/x86/entry/common.c:267 [inline]
 do_syscall_64+0x4f4/0x6d0 arch/x86/entry/common.c:294

Fixes: 78bab8daa281 ("mptcp: Rework mptcp_disconnect")

include/linux/tcp.h
@@ -479,7 +479,8 @@ struct tcp_sock {
 		mptcp_ver:4,
 		mptcp_sched_setsockopt:1,
 		mptcp_pm_setsockopt:1,
-		record_master_info:1;
+		record_master_info:1,
+		tcp_disconnect:1;
 	struct mptcp_tcp_sock *mptcp;
 #ifdef CONFIG_MPTCP
 #define MPTCP_SCHED_NAME_MAX 16

net/mptcp/mptcp_ctrl.c
@@ -637,8 +637,9 @@ static void mptcp_sock_def_error_report(struct sock *sk)
 	/* record this info that can be used by PM after the sf close */
 	tp->mptcp->sk_err = sk->sk_err;
 
-	if (mpcb->infinite_mapping_rcv || mpcb->infinite_mapping_snd ||
-	    mpcb->send_infinite_mapping) {
+	if (!tp->tcp_disconnect &&
+	    (mpcb->infinite_mapping_rcv || mpcb->infinite_mapping_snd ||
+	     mpcb->send_infinite_mapping)) {
 		struct sock *meta_sk = mptcp_meta_sk(sk);
 
 		meta_sk->sk_err = sk->sk_err;
@@ -1984,6 +1985,8 @@ void mptcp_disconnect(struct sock *meta_sk)
 
 		if (spin_is_locked(&subsk->sk_lock.slock))
 			bh_unlock_sock(subsk);
+
+		tcp_sk(subsk)->tcp_disconnect = 1;
 
 		meta_sk->sk_prot->disconnect(subsk, O_NONBLOCK);
 