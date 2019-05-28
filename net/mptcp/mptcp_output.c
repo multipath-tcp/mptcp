833ff9085b8c2135dda04f0b818482b2cac1fafc
Christoph Paasch <cpaasch@apple.com>
2019-05-28 15:16:16 -0700

    mptcp: Fix zero-window probes at the meta-level
    When the subflow hits a zero-window with no outstanding data at the
    subflow-level we rely on the meta-retransmission timer to trigger the
    zero-window-probes.
    
    Which is why zero_wnd_probe is set to false in mptcp_retransmit_skb.
    
    However, that test is not done correctly when checking if the subflow is
    temporarily unavailable.
    
    Also, when we retransmit, try to fit it inside the receive-window!
    Otherwise we might end up just retransmitting data...
    
    Easy to repro with:
    +0      socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
    +0      setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
    +0      bind(3, ..., ...) = 0
    +0      listen(3, 1) = 0
    
    +0      < S 0:0(0) win 2000 <mss 1460,mp_capable a> sock(3)
    +0      > S. 0:0(0) ack 1 win 28800 <mss 1460,mp_capable b> sock(3)
    +0      < . 1:1(0) ack 1 win 2000 <mp_capable a b> sock(3)
    
    +0      accept(3, ..., ...) = 4
    
    /* Client send 1000 bytes */
    +0      write(4, ..., 1000) = 1000
    +0      > P. 1:1001(1000) ack 1 <dss dack4 dsn4=1> sock(4)
    +0      write(4, ..., 2000) = 2000
    +0      > . 1001:2001(1000) ack 1 <dss dack4 dsn4=1001> sock(4)
    
    +0.01   < . 1:1(0) ack 1001 win 0 <dss dack4=1001> sock(4)
    +0      < . 1:1(0) ack 2001 win 0 <dss dack4=1001> sock(4)
    
    +0.2~+0.22      > . 2000:2000(0) ack 1 <dss dack4> sock(4)
    
    +0.01   < . 1:1(0) ack 2001 win 4000 <dss dack4=1001> sock(4)
    +0      > P. 2001:3001(1000) ack 1 <dss dack4 dsn4=2001> sock(4)
    +0      < . 1:1(0) ack 3001 win 4000 <dss dack4=1001> sock(4)
    +0.19   > . 3001:4001(1000) ack 1 <dss dack4 dsn4=1001> sock(4)
    
    Fixes: 72b0c869786f ("mptcp: fallback to zero-window-probes if subflow's window is 0")
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 859cdc7ef717fd5750ecddc90564f0d50d7aaa81)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -1573,6 +1573,8 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 					    UINT_MAX / mss_now,
 					    TCP_NAGLE_OFF);
 
+	limit = min(limit, tcp_wnd_end(meta_tp) - TCP_SKB_CB(skb)->seq);
+
 	if (skb->len > limit &&
 	    unlikely(mptcp_fragment(meta_sk, TCP_FRAG_IN_RTX_QUEUE, skb,
 				    limit, GFP_ATOMIC, 0)))
