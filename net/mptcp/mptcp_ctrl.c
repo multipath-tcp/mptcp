multipath-tcp/mptcp

commit ece771ffb87e7caab3b532106d5c80027449b315

mptcp: Fully cleanup meta-socket in mptcp_disconnect
We now handle subflows correctly in mptcp_disconnect, we also need to
handle the Meta-socket correctly.

The goal of tcp_disconnect() is to basically create again a "clean"
socket. Thus, we need to revert everything that has been done in
mptcp_alloc_mpcb(). Notably, setting the meta_sk,mpcb pointers to NULL,
cleaning up the tw_list, freeing the mpcb, changing the
function-pointers,...

Because, this socket could get reused and then for example the mpcb
could be overwritten and we would be leaking memory. Or, we could
trigger the warning in mptcp_send_active_reset because send_mp_fclose is
still set.

Thus, clean it all up!

This means we need to call the part that takes care of meta-sockets in
mptcp_sock_destruct, thus I created mptcp_mpcb_cleanup().
As we change the function-pointers, including sk_destruct we don't need
tp->was_meta_sk anymore. We would never end up there on an old meta-sk.

Because we do a mptcp_mpcb_put() in mptcp_disconnect(), we need to make
sure that we hold the refcount when taking the mpcb_mutex because
otherwise that mpcb could get free'd when we still need to release the
mutex. We should never hit a case where the refcount is already zero,
thus I BUG(); if that happens.

This also means that we can't dereference mpcb without holding the
socket-lock (cfr., getsockopt).

Fixes: Zero-day bug
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -661,7 +661,7 @@ static void mptcp_sock_def_error_report(struct sock *sk)
 	return;
 }
 
-static void mptcp_mpcb_put(struct mptcp_cb *mpcb)
+void mptcp_mpcb_put(struct mptcp_cb *mpcb)
 {
 	if (refcount_dec_and_test(&mpcb->mpcb_refcnt)) {
 		mptcp_cleanup_path_manager(mpcb);
@@ -670,12 +670,33 @@ static void mptcp_mpcb_put(struct mptcp_cb *mpcb)
 		kmem_cache_free(mptcp_cb_cache, mpcb);
 	}
 }
+EXPORT_SYMBOL(mptcp_mpcb_put);
 
-void mptcp_sock_destruct(struct sock *sk)
+static void mptcp_mpcb_cleanup(struct mptcp_cb *mpcb)
+{
+	struct mptcp_tw *mptw;
+
+	/* The mpcb is disappearing - we can make the final
+	 * update to the rcv_nxt of the time-wait-sock and remove
+	 * its reference to the mpcb.
+	 */
+	spin_lock_bh(&mpcb->mpcb_list_lock);
+	list_for_each_entry_rcu(mptw, &mpcb->tw_list, list) {
+		list_del_rcu(&mptw->list);
+		mptw->in_list = 0;
+		mptcp_mpcb_put(mpcb);
+		rcu_assign_pointer(mptw->mpcb, NULL);
+	}
+	spin_unlock_bh(&mpcb->mpcb_list_lock);
+
+	mptcp_mpcb_put(mpcb);
+}
+
+static void mptcp_sock_destruct(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 
-	if (!is_meta_sk(sk) && !tp->was_meta_sk) {
+	if (!is_meta_sk(sk)) {
 		BUG_ON(!hlist_unhashed(&tp->mptcp->cb_list));
 
 		kmem_cache_free(mptcp_sock_cache, tp->mptcp);
@@ -685,26 +706,10 @@ void mptcp_sock_destruct(struct sock *sk)
 		sock_put(mptcp_meta_sk(sk));
 		mptcp_mpcb_put(tp->mpcb);
 	} else {
-		struct mptcp_cb *mpcb = tp->mpcb;
-		struct mptcp_tw *mptw;
-
-		/* The mpcb is disappearing - we can make the final
-		 * update to the rcv_nxt of the time-wait-sock and remove
-		 * its reference to the mpcb.
-		 */
-		spin_lock_bh(&mpcb->mpcb_list_lock);
-		list_for_each_entry_rcu(mptw, &mpcb->tw_list, list) {
-			list_del_rcu(&mptw->list);
-			mptw->in_list = 0;
-			mptcp_mpcb_put(mpcb);
-			rcu_assign_pointer(mptw->mpcb, NULL);
-		}
-		spin_unlock_bh(&mpcb->mpcb_list_lock);
-
 		mptcp_debug("%s destroying meta-sk token %#x\n", __func__,
 			    tcp_sk(sk)->mpcb->mptcp_loc_token);
 
-		mptcp_mpcb_put(mpcb);
+		mptcp_mpcb_cleanup(tp->mpcb);
 	}
 
 	WARN_ON(!static_key_false(&mptcp_static_key));
@@ -1318,8 +1323,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	meta_tp->mpcb = mpcb;
 	meta_tp->meta_sk = meta_sk;
 
-	meta_tp->was_meta_sk = 0;
-
 	/* Initialize the queues */
 	master_tp->out_of_order_queue = RB_ROOT;
 	master_sk->tcp_rtx_queue = RB_ROOT;
@@ -1674,15 +1677,17 @@ void mptcp_sub_close_wq(struct work_struct *work)
 {
 	struct tcp_sock *tp = container_of(work, struct mptcp_tcp_sock, work.work)->tp;
 	struct sock *sk = (struct sock *)tp;
+	struct mptcp_cb *mpcb = tp->mpcb;
 	struct sock *meta_sk = mptcp_meta_sk(sk);
 
-	mutex_lock(&tp->mpcb->mpcb_mutex);
+	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
 	mptcp_sub_close_doit(sk);
 
 	release_sock(meta_sk);
-	mutex_unlock(&tp->mpcb->mpcb_mutex);
+	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(sk);
 }
 
@@ -1728,6 +1733,7 @@ void mptcp_sub_close(struct sock *sk, unsigned long delay)
 	}
 
 	sock_hold(sk);
+	refcount_inc(&tp->mpcb->mpcb_refcnt);
 	queue_delayed_work(mptcp_wq, work, delay);
 }
 
@@ -1798,6 +1804,7 @@ void mptcp_close(struct sock *meta_sk, long timeout)
 	mptcp_debug("%s: Close of meta_sk with tok %#x\n",
 		    __func__, mpcb->mptcp_loc_token);
 
+	WARN_ON(refcount_inc_not_zero(&mpcb->mpcb_refcnt) == 0);
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
@@ -1965,6 +1972,7 @@ out:
 	bh_unlock_sock(meta_sk);
 	local_bh_enable();
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk); /* Taken by sock_hold */
 }
 
@@ -1998,9 +2006,21 @@ void mptcp_disconnect(struct sock *meta_sk)
 	}
 	local_bh_enable();
 
-	meta_tp->was_meta_sk = 1;
+	mptcp_mpcb_cleanup(meta_tp->mpcb);
+	meta_tp->meta_sk = NULL;
+
+	meta_tp->send_mp_fclose = 0;
 	meta_tp->mpc = 0;
 	meta_tp->ops = &tcp_specific;
+#if IS_ENABLED(CONFIG_IPV6)
+	if (meta_sk->sk_family == AF_INET6)
+		meta_sk->sk_backlog_rcv = tcp_v6_do_rcv;
+	else
+		meta_sk->sk_backlog_rcv = tcp_v4_do_rcv;
+#else
+	meta_sk->sk_backlog_rcv = tcp_v4_do_rcv;
+#endif
+	meta_sk->sk_destruct = inet_sock_destruct;
 }
 
 
@@ -2456,8 +2476,6 @@ void mptcp_tsq_sub_deferred(struct sock *meta_sk)
 	struct mptcp_tcp_sock *mptcp;
 	struct hlist_node *tmp;
 
-	BUG_ON(!is_meta_sk(meta_sk) && !meta_tp->was_meta_sk);
-
 	__sock_put(meta_sk);
 	hlist_for_each_entry_safe(mptcp, tmp, &meta_tp->mpcb->callback_list, cb_list) {
 		struct tcp_sock *tp = mptcp->tp;
@@ -2694,6 +2712,10 @@ int mptcp_get_info(const struct sock *meta_sk, char __user *optval, int optlen)
 	struct mptcp_info m_info;
 
 	unsigned int info_len;
+
+	/* Check again with the lock held */
+	if (!mptcp(meta_tp))
+		return -EINVAL;
 
 	if (copy_from_user(&m_info, optval, optlen))
 		return -EFAULT;

include/linux/tcp.h
@@ -472,7 +472,6 @@ struct tcp_sock {
 		       * stop using the subflow
 		       */
 		mp_killed:1, /* Killed with a tcp_done in mptcp? */
-		was_meta_sk:1,	/* This was a meta sk (in case of reuse) */
 		is_master_sk:1,
 		close_it:1,	/* Must close socket in mptcp_data_ready? */
 		closing:1,

include/net/mptcp.h
@@ -871,7 +871,7 @@ void mptcp_disable_static_key(void);
 void mptcp_cookies_reqsk_init(struct request_sock *req,
 			      struct mptcp_options_received *mopt,
 			      struct sk_buff *skb);
-void mptcp_sock_destruct(struct sock *sk);
+void mptcp_mpcb_put(struct mptcp_cb *mpcb);
 int mptcp_finish_handshake(struct sock *child, struct sk_buff *skb);
 int mptcp_get_info(const struct sock *meta_sk, char __user *optval, int optlen);
 void mptcp_clear_sk(struct sock *sk, int size);

net/ipv4/inet_connection_sock.c
@@ -991,9 +991,11 @@ void inet_csk_listen_stop(struct sock *sk)
 	while ((req = reqsk_queue_remove(queue, sk)) != NULL) {
 		struct sock *child = req->sk;
 		bool mutex_taken = false;
+		struct mptcp_cb *mpcb = tcp_sk(child)->mpcb;
 
 		if (is_meta_sk(child)) {
-			mutex_lock(&tcp_sk(child)->mpcb->mpcb_mutex);
+			WARN_ON(refcount_inc_not_zero(&mpcb->mpcb_refcnt) == 0);
+			mutex_lock(&mpcb->mpcb_mutex);
 			mutex_taken = true;
 		}
 		local_bh_disable();
@@ -1005,8 +1007,10 @@ void inet_csk_listen_stop(struct sock *sk)
 		reqsk_put(req);
 		bh_unlock_sock(child);
 		local_bh_enable();
-		if (mutex_taken)
-			mutex_unlock(&tcp_sk(child)->mpcb->mpcb_mutex);
+		if (mutex_taken) {
+			mutex_unlock(&mpcb->mpcb_mutex);
+			mptcp_mpcb_put(mpcb);
+		}
 		sock_put(child);
 
 		cond_resched();

net/ipv4/tcp.c
@@ -3737,16 +3737,22 @@ static int do_tcp_getsockopt(struct sock *sk, int level,
 		if (put_user(len, optlen))
 			return -EFAULT;
 
+		lock_sock(sk);
 		if (mptcp(tcp_sk(sk))) {
 			struct mptcp_cb *mpcb = tcp_sk(mptcp_meta_sk(sk))->mpcb;
 
-			if (copy_to_user(optval, mpcb->sched_ops->name, len))
+			if (copy_to_user(optval, mpcb->sched_ops->name, len)) {
+				release_sock(sk);
 				return -EFAULT;
+			}
 		} else {
 			if (copy_to_user(optval, tcp_sk(sk)->mptcp_sched_name,
-					 len))
+					 len)) {
+				release_sock(sk);
 				return -EFAULT;
+			}
 		}
+		release_sock(sk);
 		return 0;
 
 	case MPTCP_PATH_MANAGER:
@@ -3756,16 +3762,22 @@ static int do_tcp_getsockopt(struct sock *sk, int level,
 		if (put_user(len, optlen))
 			return -EFAULT;
 
+		lock_sock(sk);
 		if (mptcp(tcp_sk(sk))) {
 			struct mptcp_cb *mpcb = tcp_sk(mptcp_meta_sk(sk))->mpcb;
 
-			if (copy_to_user(optval, mpcb->pm_ops->name, len))
+			if (copy_to_user(optval, mpcb->pm_ops->name, len)) {
+				release_sock(sk);
 				return -EFAULT;
+			}
 		} else {
 			if (copy_to_user(optval, tcp_sk(sk)->mptcp_pm_name,
-					 len))
+					 len)) {
+				release_sock(sk);
 				return -EFAULT;
+			}
 		}
+		release_sock(sk);
 		return 0;
 
 	case MPTCP_ENABLED:

net/mptcp/mptcp_binder.c
@@ -317,6 +317,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -347,6 +350,7 @@ next_subflow:
 exit:
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -383,6 +387,7 @@ static void binder_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&pm_priv->subflow_work)) {
 		sock_hold(meta_sk);
+		refcount_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &pm_priv->subflow_work);
 	}
 }

net/mptcp/mptcp_fullmesh.c
@@ -379,6 +379,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -435,6 +438,7 @@ exit:
 	kfree(mptcp_local);
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -477,7 +481,7 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
-	if (sock_flag(meta_sk, SOCK_DEAD))
+	if (sock_flag(meta_sk, SOCK_DEAD) || !mptcp(tcp_sk(meta_sk)))
 		goto exit;
 
 	if (mpcb->master_sk &&
@@ -585,6 +589,7 @@ next_subflow:
 
 	if (retry && !delayed_work_pending(&fmp->subflow_retry_work)) {
 		sock_hold(meta_sk);
+		refcount_inc(&mpcb->mpcb_refcnt);
 		queue_delayed_work(mptcp_wq, &fmp->subflow_retry_work,
 				   msecs_to_jiffies(MPTCP_SUBFLOW_RETRY_DELAY));
 	}
@@ -593,6 +598,7 @@ exit:
 	kfree(mptcp_local);
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -1351,7 +1357,7 @@ fallback:
 
 static void full_mesh_create_subflows(struct sock *meta_sk)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
+	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct fullmesh_priv *fmp = fullmesh_get_priv(mpcb);
 
 	if (mpcb->infinite_mapping_snd || mpcb->infinite_mapping_rcv ||
@@ -1365,6 +1371,7 @@ static void full_mesh_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&fmp->subflow_work)) {
 		sock_hold(meta_sk);
+		refcount_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &fmp->subflow_work);
 	}
 }

net/mptcp/mptcp_ndiffports.c
@@ -44,6 +44,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -98,6 +101,7 @@ next_subflow:
 exit:
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -113,7 +117,7 @@ static void ndiffports_new_session(const struct sock *meta_sk)
 
 static void ndiffports_create_subflows(struct sock *meta_sk)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
+	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct ndiffports_priv *pm_priv = (struct ndiffports_priv *)&mpcb->mptcp_pm[0];
 
 	if (mpcb->infinite_mapping_snd || mpcb->infinite_mapping_rcv ||
@@ -123,6 +127,7 @@ static void ndiffports_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&pm_priv->subflow_work)) {
 		sock_hold(meta_sk);
+		refcount_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &pm_priv->subflow_work);
 	}
 }

commit 528be7477e4e10ef7a2882c2306284f1ba831169

mptcp: mpcb-put when canceling a workqueue for closing subflows
Commit ece771ffb87e ("mptcp: Fully cleanup meta-socket in
mptcp_disconnect") added a refcount hold on the mpcb when closing
subflows (among other cases). We forgot to put the reference when the
work-queue is being canceled. Otherwise, we will end up leaking the
mpcb, and we get log-splashs like:

BUG: memory leak
unreferenced object 0xffff888053724000 (size 1056):
  comm "softirq", pid 0, jiffies 4295075668 (age 18.015s)
  hex dump (first 32 bytes):
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
    d1 d8 ee 60 d0 d8 ee 60 55 f5 51 cd 56 f5 51 cd  ...`...`U.Q.V.Q.
  backtrace:
    [<ffffffff8333b9b3>] kmem_cache_zalloc include/linux/slab.h:651 [inline]
    [<ffffffff8333b9b3>] mptcp_alloc_mpcb net/mptcp/mptcp_ctrl.c:1105 [inline]
    [<ffffffff8333b9b3>] mptcp_create_master_sk+0x203/0x2980 net/mptcp/mptcp_ctrl.c:1960
    [<ffffffff83368f50>] mptcp_rcv_synsent_state_process+0x580/0x1120 net/mptcp/mptcp_input.c:2300
    [<ffffffff83006345>] tcp_rcv_synsent_state_process+0x19b5/0x2d30 net/ipv4/tcp_input.c:5828
    [<ffffffff83007ef3>] tcp_rcv_state_process+0x833/0x2e43 net/ipv4/tcp_input.c:6074
    [<ffffffff832d09e9>] tcp_v6_do_rcv+0x789/0x12d0 net/ipv6/tcp_ipv6.c:1381
    [<ffffffff82d00088>] sk_backlog_rcv include/net/sock.h:913 [inline]
    [<ffffffff82d00088>] __release_sock+0x138/0x360 net/core/sock.c:2289
    [<ffffffff82d00350>] release_sock+0xa0/0x280 net/core/sock.c:2804
    [<ffffffff830a3951>] inet_wait_for_connect net/ipv4/af_inet.c:560 [inline]
    [<ffffffff830a3951>] __inet_stream_connect+0x621/0xe60 net/ipv4/af_inet.c:646
    [<ffffffff830a41e3>] inet_stream_connect+0x53/0xa0 net/ipv4/af_inet.c:685
    [<ffffffff82ce95a6>] SYSC_connect+0x1e6/0x420 net/socket.c:1655
    [<ffffffff8100693f>] do_syscall_64+0x23f/0x6f0 arch/x86/entry/common.c:292
    [<ffffffff83a00086>] entry_SYSCALL_64_after_hwframe+0x42/0xb7
    [<ffffffffffffffff>] 0xffffffffffffffff

Reported-by: Franz Schleindlhuber <Schleindlhuber@gmx.net>
Fixes: ece771ffb87e ("mptcp: Fully cleanup meta-socket in mptcp_disconnect")

net/mptcp/mptcp_ctrl.c
@@ -1707,6 +1707,7 @@ void mptcp_sub_close(struct sock *sk, unsigned long delay)
 		if (!cancel_delayed_work(work))
 			return;
 		sock_put(sk);
+		mptcp_mpcb_put(tp->mpcb);
 	}
 
 	if (!delay) {