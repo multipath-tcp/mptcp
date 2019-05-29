multipath-tcp/mptcp

commit 6a9aee5df8922a27643f30eb2d726972f7df829a

mptcp: Fix tsorted_anchor usage
Some missing pieces to handle the anchor and rb-tree correctly.

Fixes: c61bc63e8f37 ("Merge tag 'v4.15-rc3' into mptcp_trunk")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_input.c
@@ -123,7 +123,6 @@ static void mptcp_clean_rtx_queue(struct sock *meta_sk, u32 prior_snd_una)
 			break;
 
 		next = skb_rb_next(skb);
-		tcp_rtx_queue_unlink(skb, meta_sk);
 
 		if (mptcp_is_data_fin(skb)) {
 			struct mptcp_tcp_sock *mptcp;
@@ -147,7 +146,7 @@ static void mptcp_clean_rtx_queue(struct sock *meta_sk, u32 prior_snd_una)
 				mptcp_sub_close(sk_it, delay);
 			}
 		}
-		sk_wmem_free_skb(meta_sk, skb);
+		tcp_rtx_queue_unlink_and_free(skb, meta_sk);
 	}
 	/* Remove acknowledged data from the reinject queue */
 	skb_queue_walk_safe(&mpcb->reinject_queue, skb, tmp) {
@@ -595,6 +594,7 @@ static void mptcp_restart_sending(struct sock *meta_sk)
 	 */
 	wq_head = tcp_write_queue_head(meta_sk);
 	skb_rbtree_walk_safe(skb, &meta_sk->tcp_rtx_queue, tmp) {
+		list_del(&skb->tcp_tsorted_anchor);
 		tcp_rtx_queue_unlink(skb, meta_sk);
 
 		if (wq_head)

net/mptcp/mptcp_output.c
@@ -138,10 +138,12 @@ static void __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk
 			skb = pskb_copy_for_clone(orig_skb, GFP_ATOMIC);
 		} tcp_skb_tsorted_restore(orig_skb);
 	} else {
-		if (tcp_queue == TCP_FRAG_IN_WRITE_QUEUE)
+		if (tcp_queue == TCP_FRAG_IN_WRITE_QUEUE) {
 			__skb_unlink(orig_skb, &sk->sk_write_queue);
-		else
-			rb_erase(&orig_skb->rbnode, &sk->tcp_rtx_queue);
+		} else {
+			list_del(&orig_skb->tcp_tsorted_anchor);
+			tcp_rtx_queue_unlink(orig_skb, sk);
+		}
 		sock_set_flag(sk, SOCK_QUEUE_SHRUNK);
 		sk->sk_wmem_queued -= orig_skb->truesize;
 		sk_mem_uncharge(sk, orig_skb->truesize);
@@ -150,15 +152,15 @@ static void __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk
 	if (unlikely(!skb))
 		return;
 
+	/* Make sure that this list is clean */
+	tcp_skb_tsorted_anchor_cleanup(skb);
+
 	if (sk && !mptcp_reconstruct_mapping(skb)) {
 		__kfree_skb(skb);
 		return;
 	}
 
 	skb->sk = meta_sk;
-
-	/* Make sure that this list is clean */
-	tcp_skb_tsorted_anchor_cleanup(skb);
 
 	/* Reset subflow-specific TCP control-data */
 	TCP_SKB_CB(skb)->sacked = 0;
@@ -1320,8 +1322,8 @@ void mptcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 /* Sends the datafin */
 void mptcp_send_fin(struct sock *meta_sk)
 {
+	struct sk_buff *skb, *tskb = tcp_write_queue_tail(meta_sk);
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
-	struct sk_buff *skb = tcp_write_queue_tail(meta_sk);
 	int mss_now;
 
 	if ((1 << meta_sk->sk_state) & (TCPF_CLOSE_WAIT | TCPF_LAST_ACK))
@@ -1333,9 +1335,9 @@ void mptcp_send_fin(struct sock *meta_sk)
 	 */
 	mss_now = mptcp_current_mss(meta_sk);
 
-	if (tcp_send_head(meta_sk) != NULL) {
-		TCP_SKB_CB(skb)->mptcp_flags |= MPTCPHDR_FIN;
-		TCP_SKB_CB(skb)->end_seq++;
+	if (tskb) {
+		TCP_SKB_CB(tskb)->mptcp_flags |= MPTCPHDR_FIN;
+		TCP_SKB_CB(tskb)->end_seq++;
 		meta_tp->write_seq++;
 	} else {
 		/* Socket is locked, keep trying until memory is available. */
@@ -1347,6 +1349,7 @@ void mptcp_send_fin(struct sock *meta_sk)
 			yield();
 		}
 		/* Reserve space for headers and prepare control bits. */
+		INIT_LIST_HEAD(&skb->tcp_tsorted_anchor);
 		skb_reserve(skb, MAX_TCP_HEADER);
 
 		tcp_init_nondata_skb(skb, meta_tp->write_seq, TCPHDR_ACK);

commit 5919dd53cdd13718cafeeccf1418d75b8eedc672

mptcp: Correctly set the tcp_tsorted_anchor on skbs
Since v4.15, the stack is using a list in skbs to handle the
retransmissions. MPTCP does not really need it, but it needs to
set/reset the list correctly on the skb.

Thus, make sure we add and remove the skbs from the list with a call to
tcp_update_skb_after_send() and list_del(&skb->tcp_tsorted_anchor);.

We also need to call INIT_LIST_HEAD after calls to
tcp_rtx_queue_unlink() because the latter sets all pointers to NULL.

Fixes: 6a9aee5df892 ("mptcp: Fix tsorted_anchor usage")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/linux/skbuff.h
@@ -3267,11 +3267,6 @@ static inline int __skb_grow_rcsum(struct sk_buff *skb, unsigned int len)
 		for (skb = skb_rb_first(root); skb != NULL;			\
 		     skb = skb_rb_next(skb))
 
-#define skb_rbtree_walk_safe(skb, root, tmp)					\
-		for (skb = skb_rb_first(root);					\
-		     tmp = skb ? skb_rb_next(skb) : NULL, (skb != NULL);	\
-		     skb = tmp)
-
 #define skb_rbtree_walk_from(skb)						\
 		for (; skb != NULL;						\
 		     skb = skb_rb_next(skb))

include/net/tcp.h
@@ -378,6 +378,8 @@ bool retransmits_timed_out(struct sock *sk,
 			   unsigned int timeout);
 void tcp_write_err(struct sock *sk);
 void tcp_adjust_pcount(struct sock *sk, const struct sk_buff *skb, int decr);
+void tcp_update_skb_after_send(struct sock *sk, struct sk_buff *skb,
+			       u64 prior_wstamp);
 void tcp_set_skb_tso_segs(struct sk_buff *skb, unsigned int mss_now);
 
 void tcp_v4_reqsk_send_ack(const struct sock *sk, struct sk_buff *skb,

net/ipv4/tcp_output.c
@@ -1011,8 +1011,8 @@ enum hrtimer_restart tcp_pace_kick(struct hrtimer *timer)
 	return HRTIMER_NORESTART;
 }
 
-static void tcp_update_skb_after_send(struct sock *sk, struct sk_buff *skb,
-				      u64 prior_wstamp)
+void tcp_update_skb_after_send(struct sock *sk, struct sk_buff *skb,
+			       u64 prior_wstamp)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 

net/mptcp/mptcp_input.c
@@ -596,15 +596,19 @@ static void mptcp_restart_sending(struct sock *meta_sk)
 {
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 	struct mptcp_cb *mpcb = meta_tp->mpcb;
-	struct sk_buff *skb, *tmp, *wq_head;
+	struct sk_buff *wq_head, *skb, *tmp;
+
+	skb = tcp_rtx_queue_head(meta_sk);
 
 	/* We resend everything that has not been acknowledged, thus we need
 	 * to move it from the rtx-tree to the write-queue.
 	 */
 	wq_head = tcp_write_queue_head(meta_sk);
-	skb_rbtree_walk_safe(skb, &meta_sk->tcp_rtx_queue, tmp) {
+
+	skb_rbtree_walk_from_safe(skb, tmp) {
 		list_del(&skb->tcp_tsorted_anchor);
 		tcp_rtx_queue_unlink(skb, meta_sk);
+		INIT_LIST_HEAD(&skb->tcp_tsorted_anchor);
 
 		if (wq_head)
 			__skb_queue_before(&meta_sk->sk_write_queue, wq_head, skb);
@@ -2185,14 +2189,16 @@ static void mptcp_rcv_synsent_fastopen(struct sock *meta_sk)
 {
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 	struct tcp_sock *master_tp = tcp_sk(meta_tp->mpcb->master_sk);
-	struct sk_buff *skb, *tmp;
 	u32 new_mapping = meta_tp->write_seq - master_tp->snd_una;
+	struct sk_buff *skb, *tmp;
+
+	skb = tcp_rtx_queue_head(meta_sk);
 
 	/* There should only be one skb in write queue: the data not
 	 * acknowledged in the SYN+ACK. In this case, we need to map
 	 * this data to data sequence numbers.
 	 */
-	skb_rbtree_walk_safe(skb, &meta_sk->tcp_rtx_queue, tmp) {
+	skb_rbtree_walk_from_safe(skb, tmp) {
 		/* If the server only acknowledges partially the data sent in
 		 * the SYN, we need to trim the acknowledged part because
 		 * we don't want to retransmit this already received data.
@@ -2217,7 +2223,10 @@ static void mptcp_rcv_synsent_fastopen(struct sock *meta_sk)
 		TCP_SKB_CB(skb)->seq += new_mapping;
 		TCP_SKB_CB(skb)->end_seq += new_mapping;
 
+		list_del(&skb->tcp_tsorted_anchor);
 		tcp_rtx_queue_unlink(skb, meta_sk);
+		INIT_LIST_HEAD(&skb->tcp_tsorted_anchor);
+
 		tcp_add_write_queue_tail(meta_sk, skb);
 	}
 

net/mptcp/mptcp_output.c
@@ -143,6 +143,7 @@ static void __mptcp_reinject_data(struct sk_buff *orig_skb, struct sock *meta_sk
 		} else {
 			list_del(&orig_skb->tcp_tsorted_anchor);
 			tcp_rtx_queue_unlink(orig_skb, sk);
+			INIT_LIST_HEAD(&orig_skb->tcp_tsorted_anchor);
 		}
 		sock_set_flag(sk, SOCK_QUEUE_SHRUNK);
 		sk->sk_wmem_queued -= orig_skb->truesize;
@@ -319,8 +320,10 @@ void mptcp_reinject_data(struct sock *sk, int clone_it)
 				      TCP_FRAG_IN_WRITE_QUEUE);
 	}
 
-	skb_rbtree_walk_safe(skb_it, &sk->tcp_rtx_queue, tmp) {
+	skb_it = tcp_rtx_queue_head(sk);
+	skb_rbtree_walk_from_safe(skb_it, tmp) {
 		struct tcp_skb_cb *tcb = TCP_SKB_CB(skb_it);
+
 		/* Subflow syn's and fin's are not reinjected.
 		 *
 		 * As well as empty subflow-fins with a data-fin.
@@ -704,7 +707,7 @@ int mptcp_write_wakeup(struct sock *meta_sk, int mib)
 		tcp_event_new_data_sent(meta_sk, skb);
 
 		__tcp_push_pending_frames(subsk, mss, TCP_NAGLE_PUSH);
-		skb->skb_mstamp_ns = meta_tp->tcp_wstamp_ns;
+		tcp_update_skb_after_send(meta_sk, skb, meta_tp->tcp_wstamp_ns);
 		meta_tp->lsndtime = tcp_jiffies32;
 
 		return 0;
@@ -852,7 +855,8 @@ bool mptcp_write_xmit(struct sock *meta_sk, unsigned int mss_now, int nonagle,
 		 * always push on the subflow
 		 */
 		__tcp_push_pending_frames(subsk, mss_now, TCP_NAGLE_PUSH);
-		skb->skb_mstamp_ns = meta_tp->tcp_wstamp_ns;
+		if (reinject <= 0)
+			tcp_update_skb_after_send(meta_sk, skb, meta_tp->tcp_wstamp_ns);
 		meta_tp->lsndtime = tcp_jiffies32;
 
 		path_mask |= mptcp_pi_to_flag(subtp->mptcp->path_index);
@@ -1589,7 +1593,7 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 	}
 
 	__tcp_push_pending_frames(subsk, mss_now, TCP_NAGLE_PUSH);
-	skb->skb_mstamp_ns = meta_tp->tcp_wstamp_ns;
+	tcp_update_skb_after_send(meta_sk, skb, meta_tp->tcp_wstamp_ns);
 	meta_tp->lsndtime = tcp_jiffies32;
 
 	return 0;