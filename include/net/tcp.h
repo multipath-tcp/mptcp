multipath-tcp/mptcp

commit f4c33b39114799251bc8dab246876cb475ede44d

mptcp: Clean up select_size()
This commit removes the MPTCP code from select_size().

Changelog

v2: removed static definition for mptcp_select_size

Signed-off-by: Doru Gucea <doru.gucea@intel.com>
Signed-off-by: Christoph Paasch <christoph.paasch@gmail.com>

include/net/tcp.h
@@ -658,6 +658,7 @@ void tcp_send_loss_probe(struct sock *sk);
 bool tcp_schedule_loss_probe(struct sock *sk);
 
 u16 tcp_select_window(struct sock *sk);
+int select_size(const struct sock *sk, bool sg);
 bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 		int push_one, gfp_t gfp);
 
@@ -1792,6 +1793,7 @@ struct tcp_sock_ops {
 				      __u32 *window_clamp, int wscale_ok,
 				      __u8 *rcv_wscale, __u32 init_rcv_wnd,
 				      const struct sock *sk);
+	int (*select_size)(const struct sock *sk, bool sg);
 	void (*init_buffer_space)(struct sock *sk);
 	void (*set_rto)(struct sock *sk);
 	bool (*should_expand_sndbuf)(const struct sock *sk);

net/ipv4/tcp.c
@@ -376,6 +376,7 @@ const struct tcp_sock_ops tcp_specific = {
 	.__select_window		= __tcp_select_window,
 	.select_window			= tcp_select_window,
 	.select_initial_window		= tcp_select_initial_window,
+	.select_size			= select_size,
 	.init_buffer_space		= tcp_init_buffer_space,
 	.set_rto			= tcp_set_rto,
 	.should_expand_sndbuf		= tcp_should_expand_sndbuf,
@@ -1065,13 +1066,10 @@ int tcp_sendpage(struct sock *sk, struct page *page, int offset,
 }
 EXPORT_SYMBOL(tcp_sendpage);
 
-static inline int select_size(const struct sock *sk, bool sg)
+int select_size(const struct sock *sk, bool sg)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
 	int tmp = tp->mss_cache;
-
-	if (mptcp(tp))
-		return mptcp_select_size(sk, sg);
 
 	if (sg) {
 		if (sk_can_gso(sk)) {
@@ -1214,7 +1212,7 @@ new_segment:
 				goto wait_for_sndbuf;
 
 			skb = sk_stream_alloc_skb(sk,
-						  select_size(sk, sg),
+						  tp->ops->select_size(sk, sg),
 						  sk->sk_allocation);
 			if (!skb)
 				goto wait_for_memory;

include/net/mptcp.h
@@ -1423,10 +1423,6 @@ static inline unsigned int mptcp_current_mss(struct sock *meta_sk)
 {
 	return 0;
 }
-static inline int mptcp_select_size(const struct sock *meta_sk, bool sg)
-{
-	return 0;
-}
 static inline void mptcp_sub_close_passive(struct sock *sk) {}
 static inline bool mptcp_fallback_infinite(const struct sock *sk, int flag)
 {

net/mptcp/mptcp_ctrl.c
@@ -960,6 +960,7 @@ static const struct tcp_sock_ops mptcp_meta_specific = {
 	.__select_window		= __mptcp_select_window,
 	.select_window			= mptcp_select_window,
 	.select_initial_window		= mptcp_select_initial_window,
+	.select_size			= mptcp_select_size,
 	.init_buffer_space		= mptcp_init_buffer_space,
 	.set_rto			= mptcp_tcp_set_rto,
 	.should_expand_sndbuf		= mptcp_should_expand_sndbuf,
@@ -977,6 +978,7 @@ static const struct tcp_sock_ops mptcp_sub_specific = {
 	.__select_window		= __mptcp_select_window,
 	.select_window			= mptcp_select_window,
 	.select_initial_window		= mptcp_select_initial_window,
+	.select_size			= mptcp_select_size,
 	.init_buffer_space		= mptcp_init_buffer_space,
 	.set_rto			= mptcp_tcp_set_rto,
 	.should_expand_sndbuf		= mptcp_should_expand_sndbuf,

commit 992f18052c2d6578a0ed4d04a3b8be220bc1af2d

tcp: fix compilation error with CHELSIO drivers
When CONFIG_NET_VENDOR_CHELSIO is on, the compilation fails because
select_size() function is re-defined.

TCP' select_size() function doesn't need to be exposed so the simpler
fix for that is to reduce modifications in TCP code and go back to a
static function.

Fixes: f4c33b391147 (mptcp: Clean up select_size())
Github-Fixes: #339 (trouble compiling CHELSIO code)
Reported-by: Matthieu Coudron <coudron@iij.ad.jp>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

include/net/tcp.h
@@ -698,7 +698,6 @@ void tcp_skb_collapse_tstamp(struct sk_buff *skb,
 			     const struct sk_buff *next_skb);
 
 u16 tcp_select_window(struct sock *sk);
-int select_size(const struct sock *sk, bool first_skb, bool zc);
 bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 		int push_one, gfp_t gfp);
 

net/ipv4/tcp.c
@@ -400,6 +400,8 @@ static u64 tcp_compute_delivery_rate(const struct tcp_sock *tp)
 	return rate64;
 }
 
+static int select_size(const struct sock *sk, bool first_skb, bool zc);
+
 const struct tcp_sock_ops tcp_specific = {
 	.__select_window		= __tcp_select_window,
 	.select_window			= tcp_select_window,
@@ -1173,7 +1175,7 @@ int linear_payload_sz(bool first_skb)
 	return 0;
 }
 
-int select_size(const struct sock *sk, bool first_skb, bool zc)
+static int select_size(const struct sock *sk, bool first_skb, bool zc)
 {
 	if (zc)
 		return 0;