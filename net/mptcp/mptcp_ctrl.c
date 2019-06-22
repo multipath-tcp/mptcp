8bf32af5f92a94f064b324ec1abc3ae030baec56
Christoph Paasch <cpaasch@apple.com>
2019-06-22 11:46:23 -0700

    mptcp: New Release
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -3093,7 +3093,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.95-rc");
+	pr_info("MPTCP: Stable release v0.95");
 
 	mptcp_init_failed = false;
 
