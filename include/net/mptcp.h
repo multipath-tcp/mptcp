multipath-tcp/mptcp

commit fc29b3ada6441caeff5e8f90fde4eefbdc82b1b5

mptcp: Don't update meta-RTO from subflows that are retransmitting
Because, the RTO of these subflows is inflated. Thus, if you have one
bad subflow hanging around, the MPTCP RTO-timer can blow up to the order
of minutes. Better to ignore the RTO of these subflows.

The following packet-drill script illustrates the behavior:

--tolerance_usecs=100000
+0	`sysctl -w net.mptcp.mptcp_checksum=0`

+0	socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
+0	setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0	setsockopt(3, SOL_TCP, TCP_NODELAY, [1], 4) = 0
+0	fcntl(3, F_SETFL, O_RDWR|O_NONBLOCK) = 0
+0	bind(3, {sa_family = AF_INET, sin_port = htons(13000), sin_addr = inet_addr("192.168.0.1")}, ...) = 0
+0	listen(3, 1) = 0

+0	socket(..., SOCK_STREAM, IPPROTO_TCP) = 5
+0	setsockopt(5, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0	bind(5, {sa_family = AF_INET, sin_port = htons(13001), sin_addr = inet_addr("192.168.0.1")}, ...) = 0
+0	listen(5,1) = 0

+0	< S 0:0(0) win 32792 <mss 1460,sackOK,nop,nop,nop,wscale 7,mp_capable_no_cs key_a> sock(3)
+0	> S. 0:0(0) ack 1 win 28800 <mss 1460,nop,nop,sackOK,nop,wscale 7,mp_capable_no_cs key_b> sock(3)
+0	< . 1:1(0) ack 1 win 257 <mp_capable_no_cs key_a key_b> sock(3)
+0	accept(3, ..., ...) = 4
+0	fcntl(4, F_SETFL, O_RDWR|O_NONBLOCK) = 0

+0	write(4, ..., 1000) = 1000
+0	> P. 1:1001(1000) ack 1 <dss dack4 dsn4 nocs, nop, nop> sock(4)
+0	< . 1:1(0) ack 1001 win 4242 <dss dack4=1001> sock(4)
+0	write(4, ..., 1000) = 1000
+0	> P. 1001:2001(1000) ack 1 <dss dack4 dsn4 nocs, nop, nop> sock(4)
+0	write(4, ..., 1000) = 1000
+0	> P. 2001:3001(1000) ack 1 <dss dack4 dsn4 nocs, nop, nop> sock(4)
+0	write(4, ..., 1000) = 1000
+0	> P. 3001:4001(1000) ack 1 <dss dack4 dsn4 nocs, nop, nop> sock(4)
+0	write(4, ..., 1000) = 1000
+0	> P. 4001:5001(1000) ack 1 <dss dack4 dsn4 nocs, nop, nop> sock(4)

+0	< . 1:1(0) ack 5001 win 4242 <dss dack4=1001> sock(4)

/* ACK, without advencing dack: Now, the only way to recover is through a meta-retransmission timer */

include/net/mptcp.h
@@ -1195,6 +1195,7 @@ static inline void mptcp_set_rto(struct sock *sk)
 		struct sock *sk_it = mptcp_to_sock(mptcp);
 
 		if ((mptcp_sk_can_send(sk_it) || sk_it->sk_state == TCP_SYN_RECV) &&
+		    inet_csk(sk_it)->icsk_retransmits == 0 &&
 		    inet_csk(sk_it)->icsk_rto > max_rto)
 			max_rto = inet_csk(sk_it)->icsk_rto;
 	}

commit 3944acd04622902a072d0b88d8aaeea36c0536dc

mptcp: Only update meta-RTO when we have a valid RTT measurement
Commit fc29b3ada644 ("mptcp: Don't update meta-RTO from subflows that are
retransmitting") for the most part already addressed the issue where a
subflow with an inflated RTO due to retransmissions might push the
meta-RTO to huge values.

However, there is one possibility where icsk_retransmits is 0, while the
RTO is still inflated. When an ACK comes in that ends up not updating
the RTT (see tcp_ack_update_rtt) but is still acknowledging data, we end
up in a situation where icsk_retransmits is 0 but the subflow's RTO is
still huge. The one additional input to whether we have a valid RTO is
icsk_backoff (see comment in tcp_ack_update_rtt()).

So, we need to take this into account as well.

Fixes: fc29b3ada644 ("mptcp: Don't update meta-RTO from subflows that are retransmitting")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -1196,6 +1196,7 @@ static inline void mptcp_set_rto(struct sock *sk)
 
 		if ((mptcp_sk_can_send(sk_it) || sk_it->sk_state == TCP_SYN_RECV) &&
 		    inet_csk(sk_it)->icsk_retransmits == 0 &&
+		    inet_csk(sk_it)->icsk_backoff == 0 &&
 		    inet_csk(sk_it)->icsk_rto > max_rto)
 			max_rto = inet_csk(sk_it)->icsk_rto;
 	}