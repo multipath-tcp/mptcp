knbnaz/android_kernel_lge_sm8150

mptcp: Fully cleanup meta-socket in mptcp_disconnect
We now handle subflows correctly in mptcp_disconnect, we also need to
handle the Meta-socket correctly.

The goal of tcp_disconnect() is to basically create again a "clean"
socket. Thus, we need to revert everything that has been done in
mptcp_alloc_mpcb(). Notably, setting the meta_sk,mpcb pointers to NULL,
cleaning up the tw_list, freeing the mpcb, changing the
function-pointers,...

Because, this socket could get reused and then for example the mpcb
could be overwritten and we would be leaking memory. Or, we could
trigger the warning in mptcp_send_active_reset because send_mp_fclose is
still set.

Thus, clean it all up!

This means we need to call the part that takes care of meta-sockets in
mptcp_sock_destruct, thus I created mptcp_mpcb_cleanup().
As we change the function-pointers, including sk_destruct we don't need
tp->was_meta_sk anymore. We would never end up there on an old meta-sk.

Because we do a mptcp_mpcb_put() in mptcp_disconnect(), we need to make
sure that we hold the refcount when taking the mpcb_mutex because
otherwise that mpcb could get free'd when we still need to release the
mutex. We should never hit a case where the refcount is already zero,
thus I BUG(); if that happens.

This also means that we can't dereference mpcb without holding the
socket-lock (cfr., getsockopt).

Fixes: Zero-day bug
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_binder.c
@@ -317,6 +317,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -347,6 +350,7 @@ next_subflow:
 exit:
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -383,6 +387,7 @@ static void binder_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&pm_priv->subflow_work)) {
 		sock_hold(meta_sk);
+		refcount_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &pm_priv->subflow_work);
 	}
 }

net/mptcp/mptcp_ndiffports.c
@@ -44,6 +44,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -98,6 +101,7 @@ next_subflow:
 exit:
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -113,7 +117,7 @@ static void ndiffports_new_session(const struct sock *meta_sk)
 
 static void ndiffports_create_subflows(struct sock *meta_sk)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
+	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct ndiffports_priv *pm_priv = (struct ndiffports_priv *)&mpcb->mptcp_pm[0];
 
 	if (mpcb->infinite_mapping_snd || mpcb->infinite_mapping_rcv ||
@@ -123,6 +127,7 @@ static void ndiffports_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&pm_priv->subflow_work)) {
 		sock_hold(meta_sk);
+		refcount_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &pm_priv->subflow_work);
 	}
 }

multipath-tcp/mptcp

(cherry picked from commit ece771ffb87e7caab3b532106d5c80027449b315)
(cherry picked from commit f8c53660473d8e14f772ed0dc9c551d3c269badc)
(cherry picked from commit 975947f0c4b41d10ec128567bf6e278bc91335db)
(cherry picked from commit c39c9f96c5ab950a929b0e046632762f54c89ee5)

net/mptcp/mptcp_binder.c
@@ -313,6 +313,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -343,6 +346,7 @@ next_subflow:
 exit:
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -378,6 +382,7 @@ static void binder_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&pm_priv->subflow_work)) {
 		sock_hold(meta_sk);
+		atomic_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &pm_priv->subflow_work);
 	}
 }

net/mptcp/mptcp_ndiffports.c
@@ -44,6 +44,9 @@ next_subflow:
 	mutex_lock(&mpcb->mpcb_mutex);
 	lock_sock_nested(meta_sk, SINGLE_DEPTH_NESTING);
 
+	if (!mptcp(tcp_sk(meta_sk)))
+		goto exit;
+
 	iter++;
 
 	if (sock_flag(meta_sk, SOCK_DEAD))
@@ -98,6 +101,7 @@ next_subflow:
 exit:
 	release_sock(meta_sk);
 	mutex_unlock(&mpcb->mpcb_mutex);
+	mptcp_mpcb_put(mpcb);
 	sock_put(meta_sk);
 }
 
@@ -113,7 +117,7 @@ static void ndiffports_new_session(const struct sock *meta_sk)
 
 static void ndiffports_create_subflows(struct sock *meta_sk)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
+	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct ndiffports_priv *pm_priv = (struct ndiffports_priv *)&mpcb->mptcp_pm[0];
 
 	if (mptcp_in_infinite_mapping_weak(mpcb) ||
@@ -122,6 +126,7 @@ static void ndiffports_create_subflows(struct sock *meta_sk)
 
 	if (!work_pending(&pm_priv->subflow_work)) {
 		sock_hold(meta_sk);
+		atomic_inc(&mpcb->mpcb_refcnt);
 		queue_work(mptcp_wq, &pm_priv->subflow_work);
 	}
 }