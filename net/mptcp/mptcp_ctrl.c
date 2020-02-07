multipath-tcp/mptcp

commit 8d9eefd861fbd5c4fa71e1b24a712aa766db7c06

mptcp: fix possible use-after-free
In v4.1, reqsk_queue_alloc() can return an error, not in v4.4.

Fixes: 8ffbc42ad7b3 (mptcp: Reset icsk_bind_hash to NULL to avoid use-after-free in inet_put_port)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -1253,10 +1253,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	INIT_LIST_HEAD(&master_tp->tsq_node);
 
 	master_tp->tsq_flags = 0;
-	/* icsk_bind_hash inherited from the meta, but it will be properly set in
-	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
-	 */
-	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	mutex_init(&mpcb->mpcb_mutex);
 
@@ -1270,6 +1266,11 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 		sk_free(master_sk);
 		return -ENOMEM;
 	}
+
+	/* icsk_bind_hash inherited from the meta, but it will be properly set in
+	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
+	 */
+	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	if (!sock_flag(meta_sk, SOCK_MPTCP)) {
 		mptcp_enable_static_key_bh();

commit 94412a7b50f6f9b5bfb2e6568b0e3ba3bce95151

mptcp: fix possible double-free
Fixes: 8d9eefd861fb (mptcp: fix possible use-after-free)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -1253,6 +1253,10 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	INIT_LIST_HEAD(&master_tp->tsq_node);
 
 	master_tp->tsq_flags = 0;
+	/* icsk_bind_hash inherited from the meta, but it will be properly set in
+	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
+	 */
+	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	mutex_init(&mpcb->mpcb_mutex);
 
@@ -1261,16 +1265,10 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	 * pending subflow creations.
 	 */
 	if (reqsk_queue_alloc(&meta_icsk->icsk_accept_queue, 32, GFP_ATOMIC)) {
-		inet_put_port(master_sk);
 		kmem_cache_free(mptcp_cb_cache, mpcb);
 		sk_free(master_sk);
 		return -ENOMEM;
 	}
-
-	/* icsk_bind_hash inherited from the meta, but it will be properly set in
-	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
-	 */
-	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	if (!sock_flag(meta_sk, SOCK_MPTCP)) {
 		mptcp_enable_static_key_bh();