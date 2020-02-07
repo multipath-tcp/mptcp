multipath-tcp/mptcp

commit 8ffbc42ad7b3266bb557540bfc2bdd04749aad40

mptcp: Reset icsk_bind_hash to NULL to avoid use-after-free in inet_put_port
Normally, the master-sk's icsk_bind_hash is going to be properly set by
the call to __inet_inherit_port in mptcp_create_master_sk(). However, if
we don't get there, it will be pointing to the same as the meta-socket
as sk_clone_lock inherits it. That's why in inet_csk_clone_lock() it is
set to NULL. We need to do the same!

Otherwise, we can panic when we hit the error code-path in
mptcp_create_master_sk():

==================================================================
BUG: KASAN: use-after-free in __write_once_size include/linux/compiler.h:212 [inline]
BUG: KASAN: use-after-free in __hlist_del include/linux/list.h:649 [inline]
BUG: KASAN: use-after-free in __sk_del_bind_node include/net/sock.h:698 [inline]
BUG: KASAN: use-after-free in __inet_put_port net/ipv4/inet_hashtables.c:114 [inline]
BUG: KASAN: use-after-free in inet_put_port+0x413/0x450 net/ipv4/inet_hashtables.c:124
Write of size 8 at addr ffff888068572e38 by task syz-executor.5/10165

CPU: 0 PID: 10165 Comm: syz-executor.5 Not tainted 4.14.104 #1
Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS 0.5.1 01/01/2011
Call Trace:
 __dump_stack lib/dump_stack.c:17 [inline]
 dump_stack+0x7b/0xa6 lib/dump_stack.c:53
 print_address_description+0x6e/0x280 mm/kasan/report.c:252
 kasan_report_error mm/kasan/report.c:351 [inline]
 kasan_report+0x251/0x340 mm/kasan/report.c:409
 __write_once_size include/linux/compiler.h:212 [inline]
 __hlist_del include/linux/list.h:649 [inline]
 __sk_del_bind_node include/net/sock.h:698 [inline]
 __inet_put_port net/ipv4/inet_hashtables.c:114 [inline]
 inet_put_port+0x413/0x450 net/ipv4/inet_hashtables.c:124
 tcp_set_state+0x138/0x3c0 net/ipv4/tcp.c:2118
 tcp_done+0xf0/0x2a0 net/ipv4/tcp.c:3636
 mptcp_create_master_sk+0x1b5f/0x1f60 net/mptcp/mptcp_ctrl.c:1940
 mptcp_rcv_synsent_state_process+0x534/0x10c0 net/mptcp/mptcp_input.c:2291

net/mptcp/mptcp_ctrl.c
@@ -1228,6 +1228,10 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	INIT_LIST_HEAD(&master_tp->tsq_node);
 
 	master_tp->tsq_flags = 0;
+	/* icsk_bind_hash inherited from the meta, but it will be properly set in
+	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
+	 */
+	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	mutex_init(&mpcb->mpcb_mutex);
 

commit 8d9eefd861fbd5c4fa71e1b24a712aa766db7c06

mptcp: fix possible use-after-free
In v4.1, reqsk_queue_alloc() can return an error, not in v4.4.

Fixes: 8ffbc42ad7b3 (mptcp: Reset icsk_bind_hash to NULL to avoid use-after-free in inet_put_port)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -1253,10 +1253,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	INIT_LIST_HEAD(&master_tp->tsq_node);
 
 	master_tp->tsq_flags = 0;
-	/* icsk_bind_hash inherited from the meta, but it will be properly set in
-	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
-	 */
-	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	mutex_init(&mpcb->mpcb_mutex);
 
@@ -1270,6 +1266,11 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 		sk_free(master_sk);
 		return -ENOMEM;
 	}
+
+	/* icsk_bind_hash inherited from the meta, but it will be properly set in
+	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
+	 */
+	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	if (!sock_flag(meta_sk, SOCK_MPTCP)) {
 		mptcp_enable_static_key_bh();