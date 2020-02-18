multipath-tcp/mptcp

commit 2fe8702686271c501138b89d89433fe949fd8d60

mptcp: Make sure that inside_tk_table is 0 when creating a TCP-socket
We only set it to 0 inside MPTCP-functions. But that is way too late...
TCP-creation might fail earlier and then we end up in the
destroy-function thinking that the subflow is "inside_tk_table". So, we
remove it and will end up corrupting the hlist.

Fixes: Zero-day bug
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
(cherry picked from commit a794fa391b5c95d81606e4092c32851b34a96e55)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
(cherry picked from commit b86461666ede4e6da195431dcf26cd454bc547fe)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
(cherry picked from commit e00f7a7d2a23167cc2c1a438c72d0464a66a443e)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -1123,6 +1123,7 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 		goto err_alloc_master;
 
 	master_tp = tcp_sk(master_sk);
+	master_tp->inside_tk_table = 0;
 
 	mpcb = kmem_cache_zalloc(mptcp_cb_cache, GFP_ATOMIC);
 	if (!mpcb)
@@ -1193,7 +1194,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 		local_bh_enable();
 		rcu_read_unlock();
 	}
-	master_tp->inside_tk_table = 0;
 
 #if IS_ENABLED(CONFIG_IPV6)
 	if (meta_icsk->icsk_af_ops == &mptcp_v6_mapped) {
@@ -2173,7 +2173,6 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk,
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	u8 hash_mac_check[20];
 
-	child_tp->out_of_order_queue = RB_ROOT;
 	child_tp->inside_tk_table = 0;
 
 	if (!mopt->join_ack) {

net/ipv4/tcp_minisocks.c
@@ -591,6 +591,7 @@ struct sock *tcp_create_openreq_child(const struct sock *sk,
 		newtp->syn_data_acked = 0;
 		newtp->rack.mstamp.v64 = 0;
 		newtp->rack.advanced = 0;
+		newtp->inside_tk_table = 0;
 
 		__TCP_INC_STATS(sock_net(sk), TCP_MIB_PASSIVEOPENS);
 	}

commit afe4074aa9e241321eb3ad3ec7eed0b44c0c07f1

mptcp: fix bad conflict resolution
Fixes: 2fe870268627 (mptcp: Make sure that inside_tk_table is 0 when creating a TCP-socket)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -2173,7 +2173,7 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk,
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	u8 hash_mac_check[20];
 
-	child_tp->inside_tk_table = 0;
+	child_tp->out_of_order_queue = RB_ROOT;
 
 	if (!mopt->join_ack) {
 		MPTCP_INC_STATS(sock_net(meta_sk), MPTCP_MIB_JOINACKFAIL);