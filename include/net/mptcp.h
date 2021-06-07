bdb5b3644e955ad4daf30a8bdc36834fc6c57e4c
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-06-07 18:03:07 +0200

    mptcp: remove unused mptcp_prune_ofo_queue
    The MPTCP-specific ofo-queue has been removed in mptcp_v0.93, replaced
    by TCP RB-tree.
    
    mptcp_prune_ofo_queue() was defined in mptcp_ofo_queue.c which has been
    removed.
    
    Fixes: 15d1040668f9 ("mptcp: Build-Fix")
    Fixes: 050453aea3c3 ("Merge tag 'v4.9' into mptcp_trunk")
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit bf3a29789783ee82b5ecb9333c5e75b9bd7ec7f1)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -1315,9 +1315,6 @@ static inline int mptcp_subflow_count(const struct mptcp_cb *mpcb)
 u16 mptcp_select_window(struct sock *sk);
 void mptcp_tcp_set_rto(struct sock *sk);
 
-/* TCP and MPTCP flag-depending functions */
-bool mptcp_prune_ofo_queue(struct sock *sk);
-
 #else /* CONFIG_MPTCP */
 #define mptcp_debug(fmt, args...)	\
 	do {				\
