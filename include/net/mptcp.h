multipath-tcp/mptcp

commit 3f1f5123bfe19be335a34ef1bff84612a23650e0

mptcp: New MP_CAPABLE exchange - sender side
New MP_CAPABLE exchange allows to reduce the size of the option in the
SYN by making sure the keys are sent reliable after the 3-way handshake.

The first data-packet will also use the new format of the MP_CAPABLE
option. Thus, we store this info in the packets's tcp_skb_cb->flags.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -271,6 +271,7 @@ struct mptcp_cb {
 	u32 rcv_high_order[2];
 
 	u16	send_infinite_mapping:1,
+		send_mptcpv1_mpcapable:1,
 		in_time_wait:1,
 		list_rcvd:1, /* XXX TO REMOVE */
 		addr_signal:1, /* Path-manager wants us to call addr_signal */
@@ -352,6 +353,16 @@ struct mptcp_cb {
 #define MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN		12
 #define MPTCP_SUB_LEN_CAPABLE_ACK		20
 #define MPTCP_SUB_LEN_CAPABLE_ACK_ALIGN		20
+
+#define MPTCPV1_SUB_LEN_CAPABLE_SYN		4
+#define MPTCPV1_SUB_LEN_CAPABLE_SYN_ALIGN	4
+#define MPTCPV1_SUB_LEN_CAPABLE_SYNACK		12
+#define MPTCPV1_SUB_LEN_CAPABLE_SYNACK_ALIGN	12
+#define MPTCPV1_SUB_LEN_CAPABLE_ACK		20
+#define MPTCPV1_SUB_LEN_CAPABLE_ACK_ALIGN	20
+#define MPTCPV1_SUB_LEN_CAPABLE_DATA		22
+#define MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM	22
+#define MPTCPV1_SUB_LEN_CAPABLE_DATA_ALIGN	24
 
 #define MPTCP_SUB_JOIN			1
 #define MPTCP_SUB_LEN_JOIN_SYN		12
@@ -449,14 +460,15 @@ extern bool mptcp_init_failed;
 #define MPTCPHDR_SEQ		0x01 /* DSS.M option is present */
 #define MPTCPHDR_FIN		0x02 /* DSS.F option is present */
 #define MPTCPHDR_SEQ64_INDEX	0x04 /* index of seq in mpcb->snd_high_order */
+#define MPTCPHDR_MPC_DATA	0x08
 /* MPTCP flags: RX only */
-#define MPTCPHDR_ACK		0x08
-#define MPTCPHDR_SEQ64_SET	0x10 /* Did we received a 64-bit seq number?  */
-#define MPTCPHDR_SEQ64_OFO	0x20 /* Is it not in our circular array? */
-#define MPTCPHDR_DSS_CSUM	0x40
+#define MPTCPHDR_ACK		0x10
+#define MPTCPHDR_SEQ64_SET	0x20 /* Did we received a 64-bit seq number?  */
+#define MPTCPHDR_SEQ64_OFO	0x40 /* Is it not in our circular array? */
+#define MPTCPHDR_DSS_CSUM	0x80
 /* MPTCP flags: TX only */
-#define MPTCPHDR_INF		0x08
-#define MPTCP_REINJECT		0x10 /* Did we reinject this segment? */
+#define MPTCPHDR_INF		0x10
+#define MPTCP_REINJECT		0x20 /* Did we reinject this segment? */
 
 struct mptcp_option {
 	__u8	kind;
@@ -979,6 +991,11 @@ static inline void mptcp_sub_force_close_all(struct mptcp_cb *mpcb,
 		if (sk_it != except)
 			mptcp_send_reset(sk_it);
 	}
+}
+
+static inline bool mptcp_is_data_mpcapable(const struct sk_buff *skb)
+{
+	return TCP_SKB_CB(skb)->mptcp_flags & MPTCPHDR_MPC_DATA;
 }
 
 static inline bool mptcp_is_data_seq(const struct sk_buff *skb)

net/mptcp/mptcp_input.c
@@ -1467,6 +1467,9 @@ static bool mptcp_process_data_ack(struct sock *sk, const struct sk_buff *skb)
 	if (after(data_ack, meta_tp->snd_nxt))
 		goto exit;
 
+	/* First valid DATA_ACK, we can stop sending the special MP_CAPABLE */
+	tp->mpcb->send_mptcpv1_mpcapable = 0;
+
 	/*** Now, update the window  - inspired by tcp_ack_update_window ***/
 	nwin = ntohs(tcp_hdr(skb)->window);
 
@@ -2339,6 +2342,11 @@ int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 		if (mopt->mptcp_ver > tcp_sk(sk)->mptcp_ver)
 			/* TODO Consider adding new MPTCP_INC_STATS entry */
 			goto fallback;
+		if (tcp_sk(sk)->mptcp_ver == MPTCP_VERSION_1 &&
+		    mopt->mptcp_ver < MPTCP_VERSION_1)
+			/* TODO Consider adding new MPTCP_INC_STATS entry */
+			/* TODO - record this in the cache - use v0 next time */
+			goto fallback;
 
 		if (mptcp_create_master_sk(sk, mopt->mptcp_sender_key,
 					   mopt->mptcp_ver,
@@ -2367,6 +2375,9 @@ int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 		tp->mpcb->dss_csum = mopt->dss_csum;
 		if (tp->mpcb->dss_csum)
 			MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_CSUMENABLED);
+
+		if (tp->mpcb->mptcp_ver >= MPTCP_VERSION_1)
+			tp->mpcb->send_mptcpv1_mpcapable = 1;
 
 		tp->mptcp->include_mpc = 1;
 

net/mptcp/mptcp_output.c
@@ -479,6 +479,42 @@ static void mptcp_save_dss_data_seq(const struct tcp_sock *tp, struct sk_buff *s
 	ptr += mptcp_write_dss_mapping(tp, skb, ptr);
 }
 
+/* Write the MP_CAPABLE with data-option */
+static int mptcp_write_mpcapable_data(const struct tcp_sock *tp,
+				      struct sk_buff *skb,
+				      __be32 *ptr)
+{
+	struct mp_capable *mpc = (struct mp_capable *)ptr;
+	u8 length;
+
+	if (tp->mpcb->dss_csum)
+		length = MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM;
+	else
+		length = MPTCPV1_SUB_LEN_CAPABLE_DATA;
+
+	mpc->kind = TCPOPT_MPTCP;
+	mpc->len = length;
+	mpc->sub = MPTCP_SUB_CAPABLE;
+	mpc->ver = MPTCP_VERSION_1;
+	mpc->a = tp->mpcb->dss_csum;
+	mpc->b = 0;
+	mpc->rsv = 0;
+	mpc->h = 1;
+
+	ptr++;
+	memcpy(ptr, TCP_SKB_CB(skb)->dss, mptcp_dss_len);
+
+	mpc->sender_key = tp->mpcb->mptcp_loc_key;
+	mpc->receiver_key = tp->mpcb->mptcp_rem_key;
+
+	/* dss is in a union with inet_skb_parm and
+	 * the IP layer expects zeroed IPCB fields.
+	 */
+	memset(TCP_SKB_CB(skb)->dss, 0, mptcp_dss_len);
+
+	return MPTCPV1_SUB_LEN_CAPABLE_DATA_ALIGN / sizeof(*ptr);
+}
+
 /* Write the saved DSS mapping to the header */
 static int mptcp_write_dss_data_seq(const struct tcp_sock *tp, struct sk_buff *skb,
 				    __be32 *ptr)
@@ -502,7 +538,7 @@ static bool mptcp_skb_entail(struct sock *sk, struct sk_buff *skb, int reinject)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	const struct sock *meta_sk = mptcp_meta_sk(sk);
-	const struct mptcp_cb *mpcb = tp->mpcb;
+	struct mptcp_cb *mpcb = tp->mpcb;
 	struct tcp_skb_cb *tcb;
 	struct sk_buff *subskb = NULL;
 
@@ -543,6 +579,11 @@ static bool mptcp_skb_entail(struct sock *sk, struct sk_buff *skb, int reinject)
 		mptcp_combine_dfin(subskb, meta_sk, sk);
 
 	mptcp_save_dss_data_seq(tp, subskb);
+
+	if (mpcb->send_mptcpv1_mpcapable) {
+		TCP_SKB_CB(subskb)->mptcp_flags |= MPTCPHDR_MPC_DATA;
+		mpcb->send_mptcpv1_mpcapable = 0;
+	}
 
 	tcb->seq = tp->write_seq;
 
@@ -984,7 +1025,12 @@ void mptcp_syn_options(const struct sock *sk, struct tcp_out_options *opts,
 	if (is_master_tp(tp)) {
 		opts->mptcp_options |= OPTION_MP_CAPABLE | OPTION_TYPE_SYN;
 		opts->mptcp_ver = tp->mptcp_ver;
-		*remaining -= MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN;
+
+		if (tp->mptcp_ver >= MPTCP_VERSION_1)
+			*remaining -= MPTCPV1_SUB_LEN_CAPABLE_SYN_ALIGN;
+		else
+			*remaining -= MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN;
+
 		opts->mp_capable.sender_key = tp->mptcp_loc_key;
 		opts->dss_csum = !!sysctl_mptcp_checksum;
 	} else {
@@ -1012,7 +1058,11 @@ void mptcp_synack_options(struct request_sock *req,
 		opts->mptcp_ver = mtreq->mptcp_ver;
 		opts->mp_capable.sender_key = mtreq->mptcp_loc_key;
 		opts->dss_csum = !!sysctl_mptcp_checksum || mtreq->dss_csum;
-		*remaining -= MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN;
+		if (mtreq->mptcp_ver >= MPTCP_VERSION_1) {
+			*remaining -= MPTCPV1_SUB_LEN_CAPABLE_SYNACK_ALIGN;
+		} else {
+			*remaining -= MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN;
+		}
 	} else {
 		opts->mptcp_options |= OPTION_MP_JOIN | OPTION_TYPE_SYNACK;
 		opts->mp_join_syns.sender_truncated_mac =
@@ -1075,7 +1125,12 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 		opts->options |= OPTION_MPTCP;
 		opts->mptcp_options |= OPTION_MP_CAPABLE |
 				       OPTION_TYPE_ACK;
-		*size += MPTCP_SUB_LEN_CAPABLE_ACK_ALIGN;
+
+		if (mpcb->mptcp_ver >= MPTCP_VERSION_1)
+			*size += MPTCPV1_SUB_LEN_CAPABLE_ACK_ALIGN;
+		else
+			*size += MPTCP_SUB_LEN_CAPABLE_ACK_ALIGN;
+
 		opts->mptcp_ver = mpcb->mptcp_ver;
 		opts->mp_capable.sender_key = mpcb->mptcp_loc_key;
 		opts->mp_capable.receiver_key = mpcb->mptcp_rem_key;
@@ -1108,6 +1163,9 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 		 */
 		if (skb && !mptcp_is_data_seq(skb)) {
 			*size += MPTCP_SUB_LEN_ACK_ALIGN;
+		} else if ((skb && mptcp_is_data_mpcapable(skb)) ||
+			   (!skb && tp->mpcb->send_mptcpv1_mpcapable)) {
+			*size += MPTCPV1_SUB_LEN_CAPABLE_DATA_ALIGN;
 		} else {
 			/* Doesn't matter, if csum included or not. It will be
 			 * either 10 or 12, and thus aligned = 12
@@ -1166,18 +1224,36 @@ void mptcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 
 		mpc->kind = TCPOPT_MPTCP;
 
-		if ((OPTION_TYPE_SYN & opts->mptcp_options) ||
-		    (OPTION_TYPE_SYNACK & opts->mptcp_options)) {
-			mpc->sender_key = opts->mp_capable.sender_key;
-			mpc->len = MPTCP_SUB_LEN_CAPABLE_SYN;
+		if (OPTION_TYPE_SYN & opts->mptcp_options) {
 			mpc->ver = opts->mptcp_ver;
-			ptr += MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN >> 2;
-		} else if (OPTION_TYPE_ACK & opts->mptcp_options) {
+
+			if (mpc->ver >= MPTCP_VERSION_1) {
+				mpc->len = MPTCPV1_SUB_LEN_CAPABLE_SYN;
+				ptr += MPTCPV1_SUB_LEN_CAPABLE_SYN_ALIGN >> 2;
+			} else {
+				mpc->sender_key = opts->mp_capable.sender_key;
+				mpc->len = MPTCP_SUB_LEN_CAPABLE_SYN;
+				ptr += MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN >> 2;
+			}
+		} else if (OPTION_TYPE_SYNACK & opts->mptcp_options) {
+			mpc->ver = opts->mptcp_ver;
+
+			if (mpc->ver >= MPTCP_VERSION_1) {
+				mpc->len = MPTCPV1_SUB_LEN_CAPABLE_SYNACK;
+				ptr += MPTCPV1_SUB_LEN_CAPABLE_SYNACK_ALIGN >> 2;
+			} else {
+				mpc->len = MPTCP_SUB_LEN_CAPABLE_SYN;
+				ptr += MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN >> 2;
+			}
+
 			mpc->sender_key = opts->mp_capable.sender_key;
-			mpc->receiver_key = opts->mp_capable.receiver_key;
+		} else if (OPTION_TYPE_ACK & opts->mptcp_options) {
 			mpc->len = MPTCP_SUB_LEN_CAPABLE_ACK;
 			mpc->ver = opts->mptcp_ver;
 			ptr += MPTCP_SUB_LEN_CAPABLE_ACK_ALIGN >> 2;
+
+			mpc->sender_key = opts->mp_capable.sender_key;
+			mpc->receiver_key = opts->mp_capable.receiver_key;
 		}
 
 		mpc->sub = MPTCP_SUB_CAPABLE;
@@ -1309,6 +1385,8 @@ void mptcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 	if (OPTION_DATA_ACK & opts->mptcp_options) {
 		if (!mptcp_is_data_seq(skb))
 			ptr += mptcp_write_dss_data_ack(tp, skb, ptr);
+		else if (mptcp_is_data_mpcapable(skb))
+			ptr += mptcp_write_mpcapable_data(tp, skb, ptr);
 		else
 			ptr += mptcp_write_dss_data_seq(tp, skb, ptr);
 	}

commit 93569d23296f0ba17622be32a9f8d5897b8775ca

mptcp: Fix expected suboption length for MP_CAPABLE with data and checksum
RFC 8684 section 3.1 defines the MP_CAPABLE option as 22 bytes long,
with the optional checksum adding two more bytes. The existing code
does not account for the checksum bytes.

Note that this does not fully (or even mostly) resolve the MPTCPv1
MP_CAPABLE+DATA interop issues, as the correct header bytes are not
currently written to or parsed from the option headers.

Fixes: 3f1f5123bfe1 ("mptcp: New MP_CAPABLE exchange - sender side")
Github-Issue: https://github.com/multipath-tcp/mptcp/issues/427
Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -364,7 +364,7 @@ struct mptcp_cb {
 #define MPTCPV1_SUB_LEN_CAPABLE_ACK		20
 #define MPTCPV1_SUB_LEN_CAPABLE_ACK_ALIGN	20
 #define MPTCPV1_SUB_LEN_CAPABLE_DATA		22
-#define MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM	22
+#define MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM	24
 #define MPTCPV1_SUB_LEN_CAPABLE_DATA_ALIGN	24
 
 #define MPTCP_SUB_JOIN			1