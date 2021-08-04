multipath-tcp/mptcp

commit 0a0abb253259e8f478a39b2a84cfcca6fddb270f

mptcp: New MP_CAPABLE receiver side
Keys are now sent by the client after the 3-way handshake. Thus, we need
to delay some parts of the state initialization to that moment.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -321,7 +321,11 @@ static void mptcp_reqsk_new_mptcp(struct request_sock *req,
 	spin_unlock(&mptcp_tk_hashlock);
 	local_bh_enable();
 	rcu_read_unlock();
-	mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
+
+	if (mtreq->mptcp_ver == MPTCP_VERSION_0) {
+		mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
+		mtreq->rem_key_set = 1;
+	}
 }
 
 static int mptcp_reqsk_new_cookie(struct request_sock *req,
@@ -357,7 +361,10 @@ static int mptcp_reqsk_new_cookie(struct request_sock *req,
 	local_bh_enable();
 	rcu_read_unlock();
 
-	mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
+	if (mtreq->mptcp_ver == MPTCP_VERSION_0) {
+		mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
+		mtreq->rem_key_set = 1;
+	}
 
 	return true;
 }
@@ -1236,14 +1243,33 @@ static const struct tcp_sock_ops mptcp_sub_specific = {
 	.set_cong_ctrl                  = __tcp_set_congestion_control,
 };
 
+void mptcp_initialize_recv_vars(struct tcp_sock *meta_tp, struct mptcp_cb *mpcb,
+				__u64 remote_key)
+{
+	u64 idsn;
+
+	mpcb->mptcp_rem_key = remote_key;
+	mpcb->rem_key_set = 1;
+	mptcp_key_sha1(mpcb->mptcp_rem_key, &mpcb->mptcp_rem_token, &idsn);
+
+	idsn++;
+	mpcb->rcv_high_order[0] = idsn >> 32;
+	mpcb->rcv_high_order[1] = mpcb->rcv_high_order[0] + 1;
+	meta_tp->copied_seq = (u32)idsn;
+	meta_tp->rcv_nxt = (u32)idsn;
+	meta_tp->rcv_wup = (u32)idsn;
+
+	meta_tp->snd_wl1 = meta_tp->rcv_nxt - 1;
+}
+
 static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
-			    __u8 mptcp_ver, u32 window)
+			    int rem_key_set, __u8 mptcp_ver, u32 window)
 {
 	struct mptcp_cb *mpcb;
 	struct sock *master_sk;
 	struct inet_connection_sock *meta_icsk = inet_csk(meta_sk);
 	struct tcp_sock *master_tp, *meta_tp = tcp_sk(meta_sk);
-	u64 snd_idsn, rcv_idsn;
+	u64 snd_idsn;
 
 	dst_release(meta_sk->sk_rx_dst);
 	meta_sk->sk_rx_dst = NULL;
@@ -1275,12 +1301,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	snd_idsn++;
 	mpcb->snd_high_order[0] = snd_idsn >> 32;
 	mpcb->snd_high_order[1] = mpcb->snd_high_order[0] - 1;
-
-	mpcb->mptcp_rem_key = remote_key;
-	mptcp_key_sha1(mpcb->mptcp_rem_key, &mpcb->mptcp_rem_token, &rcv_idsn);
-	rcv_idsn++;
-	mpcb->rcv_high_order[0] = rcv_idsn >> 32;
-	mpcb->rcv_high_order[1] = mpcb->rcv_high_order[0] + 1;
 
 	mpcb->meta_sk = meta_sk;
 	mpcb->master_sk = master_sk;
@@ -1393,11 +1413,9 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	meta_tp->pushed_seq = meta_tp->write_seq;
 	meta_tp->snd_up = meta_tp->write_seq;
 
-	meta_tp->copied_seq = (u32)rcv_idsn;
-	meta_tp->rcv_nxt = (u32)rcv_idsn;
-	meta_tp->rcv_wup = (u32)rcv_idsn;
+	if (rem_key_set)
+		mptcp_initialize_recv_vars(meta_tp, mpcb, remote_key);
 
-	meta_tp->snd_wl1 = meta_tp->rcv_nxt - 1;
 	meta_tp->snd_wnd = window;
 	meta_tp->retrans_stamp = 0; /* Set in tcp_connect() */
 
@@ -2144,12 +2162,12 @@ bool mptcp_doit(struct sock *sk)
 }
 
 int mptcp_create_master_sk(struct sock *meta_sk, __u64 remote_key,
-			   __u8 mptcp_ver, u32 window)
+			   int rem_key_set, __u8 mptcp_ver, u32 window)
 {
 	struct tcp_sock *master_tp;
 	struct sock *master_sk;
 
-	if (mptcp_alloc_mpcb(meta_sk, remote_key, mptcp_ver, window))
+	if (mptcp_alloc_mpcb(meta_sk, remote_key, rem_key_set, mptcp_ver, window))
 		goto err_alloc_mpcb;
 
 	master_sk = tcp_sk(meta_sk)->mpcb->master_sk;
@@ -2177,6 +2195,7 @@ err_alloc_mpcb:
 }
 
 static int __mptcp_check_req_master(struct sock *child,
+				    const struct mptcp_options_received *mopt,
 				    struct request_sock *req)
 {
 	struct tcp_sock *child_tp = tcp_sk(child);
@@ -2187,6 +2206,8 @@ static int __mptcp_check_req_master(struct sock *child,
 	/* Never contained an MP_CAPABLE */
 	if (!inet_rsk(req)->mptcp_rqsk)
 		return 1;
+
+	mtreq = mptcp_rsk(req);
 
 	if (!inet_rsk(req)->saw_mpc) {
 		/* Fallback to regular TCP, because we saw one SYN without
@@ -2199,15 +2220,21 @@ static int __mptcp_check_req_master(struct sock *child,
 		return 1;
 	}
 
+	/* mopt can be NULL when coming from FAST-OPEN */
+	if (mopt && mopt->saw_mpc && mtreq->mptcp_ver == MPTCP_VERSION_1) {
+		mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
+		mtreq->rem_key_set = 1;
+	}
+
 	MPTCP_INC_STATS(sock_net(meta_sk), MPTCP_MIB_MPCAPABLEPASSIVEACK);
 
 	/* Just set this values to pass them to mptcp_alloc_mpcb */
-	mtreq = mptcp_rsk(req);
 	child_tp->mptcp_loc_key = mtreq->mptcp_loc_key;
 	child_tp->mptcp_loc_token = mtreq->mptcp_loc_token;
 
 	if (mptcp_create_master_sk(meta_sk, mtreq->mptcp_rem_key,
-				   mtreq->mptcp_ver, child_tp->snd_wnd)) {
+				   mtreq->rem_key_set, mtreq->mptcp_ver,
+				   child_tp->snd_wnd)) {
 		inet_csk_prepare_forced_close(meta_sk);
 		tcp_done(meta_sk);
 
@@ -2242,7 +2269,7 @@ int mptcp_check_req_fastopen(struct sock *child, struct request_sock *req)
 	u32 new_mapping;
 	int ret;
 
-	ret = __mptcp_check_req_master(child, req);
+	ret = __mptcp_check_req_master(child, NULL, req);
 	if (ret)
 		return ret;
 
@@ -2285,12 +2312,13 @@ int mptcp_check_req_fastopen(struct sock *child, struct request_sock *req)
 
 int mptcp_check_req_master(struct sock *sk, struct sock *child,
 			   struct request_sock *req, const struct sk_buff *skb,
+			   const struct mptcp_options_received *mopt,
 			   int drop, u32 tsoff)
 {
 	struct sock *meta_sk = child;
 	int ret;
 
-	ret = __mptcp_check_req_master(child, req);
+	ret = __mptcp_check_req_master(child, mopt, req);
 	if (ret)
 		return ret;
 	child = tcp_sk(child)->mpcb->master_sk;
@@ -2658,8 +2686,10 @@ void mptcp_cookies_reqsk_init(struct request_sock *req,
 	/* Absolutely need to always initialize this. */
 	mtreq->hash_entry.pprev = NULL;
 
+	mtreq->mptcp_ver = mopt->mptcp_ver;
 	mtreq->mptcp_rem_key = mopt->mptcp_sender_key;
 	mtreq->mptcp_loc_key = mopt->mptcp_receiver_key;
+	mtreq->rem_key_set = 1;
 
 	/* Generate the token */
 	mptcp_key_sha1(mtreq->mptcp_loc_key, &mtreq->mptcp_loc_token, NULL);

include/net/mptcp.h
@@ -102,7 +102,8 @@ struct mptcp_request_sock {
 
 	u8				loc_id;
 	u8				rem_id; /* Address-id in the MP_JOIN */
-	u8				dss_csum:1,
+	u16				dss_csum:1,
+					rem_key_set:1,
 					is_sub:1, /* Is this a new subflow? */
 					low_prio:1, /* Interface set to low-prio? */
 					rcv_low_prio:1,
@@ -272,6 +273,7 @@ struct mptcp_cb {
 
 	u16	send_infinite_mapping:1,
 		send_mptcpv1_mpcapable:1,
+		rem_key_set:1,
 		in_time_wait:1,
 		list_rcvd:1, /* XXX TO REMOVE */
 		addr_signal:1, /* Path-manager wants us to call addr_signal */
@@ -811,10 +813,11 @@ void mptcp_options_write(__be32 *ptr, struct tcp_sock *tp,
 void mptcp_close(struct sock *meta_sk, long timeout);
 bool mptcp_doit(struct sock *sk);
 int mptcp_create_master_sk(struct sock *meta_sk, __u64 remote_key,
-			   __u8 mptcp_ver, u32 window);
+			   int rem_key_set, __u8 mptcp_ver, u32 window);
 int mptcp_check_req_fastopen(struct sock *child, struct request_sock *req);
 int mptcp_check_req_master(struct sock *sk, struct sock *child,
 			   struct request_sock *req, const struct sk_buff *skb,
+			   const struct mptcp_options_received *mopt,
 			   int drop, u32 tsoff);
 struct sock *mptcp_check_req_child(struct sock *meta_sk,
 				   struct sock *child,
@@ -838,6 +841,8 @@ void mptcp_sub_close_wq(struct work_struct *work);
 void mptcp_sub_close(struct sock *sk, unsigned long delay);
 struct sock *mptcp_select_ack_sock(const struct sock *meta_sk);
 void mptcp_prepare_for_backlog(struct sock *sk, struct sk_buff *skb);
+void mptcp_initialize_recv_vars(struct tcp_sock *meta_tp, struct mptcp_cb *mpcb,
+				__u64 remote_key);
 int mptcp_backlog_rcv(struct sock *meta_sk, struct sk_buff *skb);
 void mptcp_ack_handler(struct timer_list *t);
 bool mptcp_check_rtt(const struct tcp_sock *tp, int time);
@@ -1415,6 +1420,7 @@ static inline int mptcp_check_req_master(const struct sock *sk,
 					 const struct sock *child,
 					 const struct request_sock *req,
 					 const struct sk_buff *skb,
+					 const struct mptcp_options_received *mopt,
 					 int drop,
 					 u32 tsoff)
 {

include/net/tcp.h
@@ -582,6 +582,7 @@ void inet_sk_rx_dst_set(struct sock *sk, const struct sk_buff *skb);
 /* From syncookies.c */
 struct sock *tcp_get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 				 struct request_sock *req,
+				 const struct mptcp_options_received *mopt,
 				 struct dst_entry *dst, u32 tsoff);
 int __cookie_v4_check(const struct iphdr *iph, const struct tcphdr *th,
 		      u32 cookie);

net/ipv4/syncookies.c
@@ -203,6 +203,7 @@ EXPORT_SYMBOL_GPL(__cookie_v4_check);
 
 struct sock *tcp_get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 				 struct request_sock *req,
+				 const struct mptcp_options_received *mopt,
 				 struct dst_entry *dst, u32 tsoff)
 {
 	struct inet_connection_sock *icsk = inet_csk(sk);
@@ -219,7 +220,7 @@ struct sock *tcp_get_cookie_sock(struct sock *sk, struct sk_buff *skb,
 	if (!child)
 		goto listen_overflow;
 
-	ret = mptcp_check_req_master(sk, child, req, skb, 0, tsoff);
+	ret = mptcp_check_req_master(sk, child, req, skb, mopt, 0, tsoff);
 	if (ret < 0)
 		return NULL;
 
@@ -428,7 +429,7 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 	ireq->rcv_wscale  = rcv_wscale;
 	ireq->ecn_ok = cookie_ecn_ok(&tcp_opt, sock_net(sk), &rt->dst);
 
-	ret = tcp_get_cookie_sock(sk, skb, req, &rt->dst, tsoff);
+	ret = tcp_get_cookie_sock(sk, skb, req, &mopt, &rt->dst, tsoff);
 	/* ip_queue_xmit() depends on our flow being setup
 	 * Normal sockets get it right from inet_csk_route_child_sock()
 	 */

net/ipv4/tcp_minisocks.c
@@ -828,7 +828,7 @@ struct sock *tcp_check_req(struct sock *sk, struct sk_buff *skb,
 		goto listen_overflow;
 
 	if (own_req && !is_meta_sk(sk)) {
-		int ret = mptcp_check_req_master(sk, child, req, skb, 1, 0);
+		int ret = mptcp_check_req_master(sk, child, req, skb, &mopt, 1, 0);
 		if (ret < 0)
 			goto listen_overflow;
 

net/ipv6/syncookies.c
@@ -267,7 +267,7 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 	ireq->rcv_wscale = rcv_wscale;
 	ireq->ecn_ok = cookie_ecn_ok(&tcp_opt, sock_net(sk), dst);
 
-	ret = tcp_get_cookie_sock(sk, skb, req, dst, tsoff);
+	ret = tcp_get_cookie_sock(sk, skb, req, &mopt, dst, tsoff);
 out:
 	return ret;
 out_free:

net/mptcp/mptcp_input.c
@@ -348,6 +348,17 @@ static int mptcp_verif_dss_csum(struct sock *sk)
 						sizeof(data_seq), csum_tcp);
 
 			dss_csum_added = 1; /* Just do it once */
+		} else if (mptcp_is_data_mpcapable(tmp) && !dss_csum_added) {
+			u32 offset = skb_transport_offset(tmp) + TCP_SKB_CB(tmp)->dss_off;
+			__be64 data_seq = htonll(tp->mptcp->map_data_seq);
+			__be32 rel_seq = htonl(tp->mptcp->map_subseq - tp->mptcp->rcv_isn);
+
+			csum_tcp = csum_partial(&data_seq, sizeof(data_seq), csum_tcp);
+			csum_tcp = csum_partial(&rel_seq, sizeof(rel_seq), csum_tcp);
+
+			csum_tcp = skb_checksum(tmp, offset, 4, csum_tcp);
+
+			dss_csum_added = 1;
 		}
 		last = tmp;
 		iter++;
@@ -558,6 +569,7 @@ static int mptcp_prevalidate_skb(struct sock *sk, struct sk_buff *skb)
 	 * this segment, this path has to fallback to infinite or be torn down.
 	 */
 	if (!tp->mptcp->fully_established && !mptcp_is_data_seq(skb) &&
+	    !mptcp_is_data_mpcapable(skb) &&
 	    !tp->mptcp->mapping_present && !mpcb->infinite_mapping_rcv) {
 		pr_debug("%s %#x will fallback - pi %d from %pS, seq %u mptcp-flags %#x\n",
 			 __func__, mpcb->mptcp_loc_token,
@@ -670,24 +682,35 @@ static int mptcp_detect_mapping(struct sock *sk, struct sk_buff *skb)
 		return 0;
 	}
 
-	/* No mapping here? Exit - it is either already set or still on its way */
-	if (!mptcp_is_data_seq(skb)) {
-		/* Too many packets without a mapping - this subflow is broken */
+	if (!tp->mptcp->mapping_present && mptcp_is_data_mpcapable(skb)) {
+		__u32 *ptr = (__u32 *)(skb_transport_header(skb) + TCP_SKB_CB(skb)->dss_off);
+
+		sub_seq = 1 + tp->mptcp->rcv_isn;
+		data_seq = meta_tp->rcv_nxt;
+		data_len = get_unaligned_be16(ptr);
+	} else if (!mptcp_is_data_seq(skb)) {
+		/* No mapping here?
+		 * Exit - it is either already set or still on its way
+		 */
 		if (!tp->mptcp->mapping_present &&
 		    tp->rcv_nxt - tp->copied_seq > 65536) {
+			/* Too many packets without a mapping,
+			 * this subflow is broken
+			 */
 			MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_NODSSWINDOW);
 			mptcp_send_reset(sk);
 			return 1;
 		}
 
 		return 0;
+	} else {
+		/* Well, then the DSS-mapping is there. So, read it! */
+		ptr = mptcp_skb_set_data_seq(skb, &data_seq, mpcb);
+		ptr++;
+		sub_seq = get_unaligned_be32(ptr) + tp->mptcp->rcv_isn;
+		ptr++;
+		data_len = get_unaligned_be16(ptr);
 	}
-
-	ptr = mptcp_skb_set_data_seq(skb, &data_seq, mpcb);
-	ptr++;
-	sub_seq = get_unaligned_be32(ptr) + tp->mptcp->rcv_isn;
-	ptr++;
-	data_len = get_unaligned_be16(ptr);
 
 	/* If it's an empty skb with DATA_FIN, sub_seq must get fixed.
 	 * The draft sets it to 0, but we really would like to have the
@@ -1616,6 +1639,7 @@ void mptcp_parse_options(const uint8_t *ptr, int opsize,
 			 struct tcp_sock *tp)
 {
 	const struct mptcp_option *mp_opt = (struct mptcp_option *)ptr;
+	const struct tcphdr *th = tcp_hdr(skb);
 
 	/* If the socket is mp-capable we would have a mopt. */
 	if (!mopt)
@@ -1626,9 +1650,21 @@ void mptcp_parse_options(const uint8_t *ptr, int opsize,
 	{
 		const struct mp_capable *mpcapable = (struct mp_capable *)ptr;
 
-		if (opsize != MPTCP_SUB_LEN_CAPABLE_SYN &&
-		    opsize != MPTCP_SUB_LEN_CAPABLE_ACK) {
-			mptcp_debug("%s: mp_capable: bad option size %d\n",
+		if (mpcapable->ver == MPTCP_VERSION_0 &&
+		    ((th->syn && opsize != MPTCP_SUB_LEN_CAPABLE_SYN) ||
+		     (!th->syn && th->ack && opsize != MPTCP_SUB_LEN_CAPABLE_ACK))) {
+			mptcp_debug("%s: mp_capable v0: bad option size %d\n",
+				    __func__, opsize);
+			break;
+		}
+
+		if (mpcapable->ver == MPTCP_VERSION_1 &&
+		    ((th->syn && !th->ack && opsize != MPTCPV1_SUB_LEN_CAPABLE_SYN) ||
+		     (th->syn && th->ack && opsize != MPTCPV1_SUB_LEN_CAPABLE_SYNACK) ||
+		     (!th->syn && th->ack && opsize != MPTCPV1_SUB_LEN_CAPABLE_ACK &&
+		      opsize != MPTCPV1_SUB_LEN_CAPABLE_DATA &&
+		      opsize != MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM))) {
+			mptcp_debug("%s: mp_capable v1: bad option size %d\n",
 				    __func__, opsize);
 			break;
 		}
@@ -1652,10 +1688,38 @@ void mptcp_parse_options(const uint8_t *ptr, int opsize,
 		mopt->saw_mpc = 1;
 		mopt->dss_csum = sysctl_mptcp_checksum || mpcapable->a;
 
-		if (opsize >= MPTCP_SUB_LEN_CAPABLE_SYN)
-			mopt->mptcp_sender_key = mpcapable->sender_key;
-		if (opsize == MPTCP_SUB_LEN_CAPABLE_ACK)
-			mopt->mptcp_receiver_key = mpcapable->receiver_key;
+		if (mpcapable->ver == MPTCP_VERSION_0) {
+			if (opsize == MPTCP_SUB_LEN_CAPABLE_SYN)
+				mopt->mptcp_sender_key = mpcapable->sender_key;
+
+			if (opsize == MPTCP_SUB_LEN_CAPABLE_ACK) {
+				mopt->mptcp_sender_key = mpcapable->sender_key;
+				mopt->mptcp_receiver_key = mpcapable->receiver_key;
+			}
+		} else if (mpcapable->ver == MPTCP_VERSION_1) {
+			if (opsize == MPTCPV1_SUB_LEN_CAPABLE_SYNACK)
+				mopt->mptcp_sender_key = mpcapable->sender_key;
+
+			if (opsize == MPTCPV1_SUB_LEN_CAPABLE_ACK) {
+				mopt->mptcp_sender_key = mpcapable->sender_key;
+				mopt->mptcp_receiver_key = mpcapable->receiver_key;
+			}
+
+			if (opsize == MPTCPV1_SUB_LEN_CAPABLE_DATA ||
+			    opsize == MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM) {
+				mopt->mptcp_sender_key = mpcapable->sender_key;
+				mopt->mptcp_receiver_key = mpcapable->receiver_key;
+
+				TCP_SKB_CB(skb)->mptcp_flags |= MPTCPHDR_MPC_DATA;
+
+				ptr += sizeof(struct mp_capable);
+				TCP_SKB_CB(skb)->dss_off = (ptr - skb_transport_header(skb));
+
+				/* Is a check-sum present? */
+				if (opsize == MPTCPV1_SUB_LEN_CAPABLE_DATA_CSUM)
+					TCP_SKB_CB(skb)->mptcp_flags |= MPTCPHDR_DSS_CSUM;
+			}
+		}
 
 		mopt->mptcp_ver = mpcapable->ver;
 		break;
@@ -2127,6 +2191,10 @@ bool mptcp_handle_options(struct sock *sk, const struct tcphdr *th,
 	if (sk->sk_state == TCP_RST_WAIT && !th->rst)
 		return true;
 
+	if (mopt->saw_mpc && !tp->mpcb->rem_key_set)
+		mptcp_initialize_recv_vars(mptcp_meta_tp(tp), tp->mpcb,
+					   mopt->mptcp_sender_key);
+
 	if (unlikely(mopt->mp_fail))
 		mptcp_mp_fail_rcvd(sk, th);
 
@@ -2134,7 +2202,8 @@ bool mptcp_handle_options(struct sock *sk, const struct tcphdr *th,
 	 * If a checksum is not present when its use has been negotiated, the
 	 * receiver MUST close the subflow with a RST as it is considered broken.
 	 */
-	if (mptcp_is_data_seq(skb) && tp->mpcb->dss_csum &&
+	if ((mptcp_is_data_seq(skb) || mptcp_is_data_mpcapable(skb)) &&
+	    tp->mpcb->dss_csum &&
 	    !(TCP_SKB_CB(skb)->mptcp_flags & MPTCPHDR_DSS_CSUM)) {
 		mptcp_send_reset(sk);
 		return true;
@@ -2348,7 +2417,7 @@ int mptcp_rcv_synsent_state_process(struct sock *sk, struct sock **skptr,
 			/* TODO - record this in the cache - use v0 next time */
 			goto fallback;
 
-		if (mptcp_create_master_sk(sk, mopt->mptcp_sender_key,
+		if (mptcp_create_master_sk(sk, mopt->mptcp_sender_key, 1,
 					   mopt->mptcp_ver,
 					   ntohs(tcp_hdr(skb)->window)))
 			return 2;

net/mptcp/mptcp_ipv4.c
@@ -106,6 +106,9 @@ static int mptcp_v4_join_init_req(struct request_sock *req, const struct sock *m
 	int loc_id;
 	bool low_prio = false;
 
+	if (!mpcb->rem_key_set)
+		return -1;
+
 	/* We need to do this as early as possible. Because, if we fail later
 	 * (e.g., get_local_id), then reqsk_free tries to remove the
 	 * request-socket from the htb in mptcp_hash_request_remove as pprev

commit f67b110b540d04d50945a426a7164b6b565fe0c4

mptcp: Fix incorrect version "negotiation"
RFC8684 actually "removes" version-negotiation due to the
incompatibility of v0 with v1. Thus, upon an incoming SYN that asks for
a version higher than what we support, we should fall back to regular
TCP.

Fixes: 0a0abb253259 ("mptcp: New MP_CAPABLE receiver side")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -306,15 +306,9 @@ static void mptcp_reqsk_new_mptcp(struct request_sock *req,
 				  const struct sk_buff *skb)
 {
 	struct mptcp_request_sock *mtreq = mptcp_rsk(req);
-	const struct tcp_sock *tp = tcp_sk(sk);
 
 	inet_rsk(req)->saw_mpc = 1;
-
-	/* MPTCP version agreement */
-	if (mopt->mptcp_ver >= tp->mptcp_ver)
-		mtreq->mptcp_ver = tp->mptcp_ver;
-	else
-		mtreq->mptcp_ver = mopt->mptcp_ver;
+	mtreq->mptcp_ver = mopt->mptcp_ver;
 
 	rcu_read_lock();
 	local_bh_disable();
@@ -341,11 +335,10 @@ static int mptcp_reqsk_new_cookie(struct request_sock *req,
 {
 	struct mptcp_request_sock *mtreq = mptcp_rsk(req);
 
-	/* MPTCP version agreement */
-	if (mopt->mptcp_ver >= tcp_sk(sk)->mptcp_ver)
-		mtreq->mptcp_ver = tcp_sk(sk)->mptcp_ver;
-	else
-		mtreq->mptcp_ver = mopt->mptcp_ver;
+	/* Must happen before mptcp_set_key_reqsk to generate the token with
+	 * the proper hash algo.
+	 */
+	mtreq->mptcp_ver = mopt->mptcp_ver;
 
 	rcu_read_lock();
 	local_bh_disable();
@@ -2754,6 +2747,10 @@ int mptcp_conn_request(struct sock *sk, struct sk_buff *skb)
 		goto drop;
 
 	if (!sock_flag(sk, SOCK_MPTCP))
+		mopt.saw_mpc = 0;
+
+	/* If the requested version is higher than what we support, fall back */
+	if (mopt.saw_mpc && mopt.mptcp_ver > tcp_sk(sk)->mptcp_ver)
 		mopt.saw_mpc = 0;
 
 	if (skb->protocol == htons(ETH_P_IP)) {