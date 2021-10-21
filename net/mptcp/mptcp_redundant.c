multipath-tcp/mptcp

commit 9796f92926e47cf4ffb5c7e43538dbfd78d9d13d

mptcp: Make sure only valid skb's are pointed to by the redundant scheduler
mptcp_restart_sending moves skbs from the rtx-queue to the send-queue.

Ideally we would need to update the skb-pointer in the redundant
scheduler. However, to be on the safe side, we can simply make sure that
the skb is within snd_una -> snd_nxt.

This needs to be backported to all the trees that have the rbnode usage
in the retransmit-queue.

Reported-by: https://github.com/gou4shi1
Github-Issue: https://github.com/multipath-tcp/mptcp/issues/385
Fixes: c61bc63e8f37 ("Merge tag 'v4.15-rc3' into mptcp_trunk")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_redundant.c
@@ -187,7 +187,9 @@ static void redsched_correct_skb_pointers(struct sock *meta_sk,
 {
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 
-	if (red_p->skb && !after(red_p->skb_end_seq, meta_tp->snd_una))
+	if (red_p->skb &&
+	    (!after(red_p->skb_end_seq, meta_tp->snd_una) ||
+	     after(red_p->skb_end_seq, meta_tp->snd_nxt)))
 		red_p->skb = NULL;
 }
 

commit 7835e7847026bd0aa943b0f0482444c77d927bb2

mptcp: Fix use-after-free in the redundant scheduler
There still is a use-after-free in the redundant scheduler. Just using
end_seq is definitely not enough. We need to make sure that the entire
skb is within snd_una -> snd_nxt.

Fixes: 9796f92926e4 ("mptcp: Make sure only valid skb's are pointed to by the redundant scheduler")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_redundant.c
@@ -24,9 +24,10 @@
 struct redsched_priv {
 	/* The skb or NULL */
 	struct sk_buff *skb;
-	/* End sequence number of the skb. This number should be checked
+	/* Start/end sequence number of the skb. This number should be checked
 	 * to be valid before the skb field is used
 	 */
+	u32 skb_start_seq;
 	u32 skb_end_seq;
 };
 
@@ -188,7 +189,7 @@ static void redsched_correct_skb_pointers(struct sock *meta_sk,
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 
 	if (red_p->skb &&
-	    (!after(red_p->skb_end_seq, meta_tp->snd_una) ||
+	    (!after(red_p->skb_start_seq, meta_tp->snd_una) ||
 	     after(red_p->skb_end_seq, meta_tp->snd_nxt)))
 		red_p->skb = NULL;
 }
@@ -306,6 +307,7 @@ static struct sk_buff *mptcp_red_next_segment(struct sock *meta_sk,
 		if (skb && redsched_use_subflow(meta_sk, active_valid_sks, tp,
 						skb)) {
 			red_p->skb = skb;
+			red_p->skb_start_seq = TCP_SKB_CB(skb)->seq;
 			red_p->skb_end_seq = TCP_SKB_CB(skb)->end_seq;
 			redsched_update_next_subflow(tp, red_cb);
 			*subsk = (struct sock *)tp;
@@ -333,6 +335,7 @@ static struct sk_buff *mptcp_red_next_segment(struct sock *meta_sk,
 		if (skb && redsched_use_subflow(meta_sk, active_valid_sks, tp,
 						skb)) {
 			red_p->skb = skb;
+			red_p->skb_start_seq = TCP_SKB_CB(skb)->seq;
 			red_p->skb_end_seq = TCP_SKB_CB(skb)->end_seq;
 			redsched_update_next_subflow(tp, red_cb);
 			*subsk = (struct sock *)tp;