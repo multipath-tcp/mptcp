575e7904af524073d767aaec255c11baead782a8
Christoph Paasch <cpaasch@apple.com>
2021-10-22 09:04:55 -0700

    mptcp: Avoid double-free of fastopen_req
    Setting tp->fastopen_req to NULL in mptcp_rcv_synsent_state_process may
    be too late, because the master-sk may get destroyed much earlier in
    case of any other of the error-conditions. Make sure that it is set to
    NULL as soon as possible.
    
    Fixes: e56ce5051a71 ("Merge tag 'v4.11' into mptcp_trunk")
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 5bc49377f405f01eb337d675bef0b94826046b3f)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit cbcde895cdd07f5f47047b2c7e2a65765f49e7ea)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -1358,6 +1358,8 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	INIT_LIST_HEAD(&master_tp->tsq_node);
 	INIT_LIST_HEAD(&master_tp->tsorted_sent_queue);
 
+	master_tp->fastopen_req = NULL;
+
 	master_sk->sk_tsq_flags = 0;
 	/* icsk_bind_hash inherited from the meta, but it will be properly set in
 	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
