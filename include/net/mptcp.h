multipath-tcp/mptcp

commit b0b64e79bff79aaeefba51d90d6f24b90d458d65

mptcp: Maintain a "Current Established" counter for MPTCP
Useful for monitoring, the same way as there is for TCP.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -728,6 +728,7 @@ static inline struct sock *mptcp_to_sock(const struct mptcp_tcp_sock *mptcp)
 	mptcp_for_each_bit_set(~b, i)
 
 #define MPTCP_INC_STATS(net, field)	SNMP_INC_STATS((net)->mptcp.mptcp_statistics, field)
+#define MPTCP_DEC_STATS(net, field)	SNMP_DEC_STATS((net)->mptcp.mptcp_statistics, field)
 #define MPTCP_INC_STATS_BH(net, field)	__SNMP_INC_STATS((net)->mptcp.mptcp_statistics, field)
 
 enum
@@ -774,6 +775,7 @@ enum
 	MPTCP_MIB_REMADDRRX,		/* Received a REMOVE_ADDR */
 	MPTCP_MIB_REMADDRTX,		/* Sent a REMOVE_ADDR */
 	MPTCP_MIB_JOINALTERNATEPORT,	/* Established a subflow on a different destination port-number */
+	MPTCP_MIB_CURRESTAB,		/* Current established MPTCP connections */
 	__MPTCP_MIB_MAX
 };
 

net/ipv4/tcp.c
@@ -2318,8 +2318,11 @@ void tcp_set_state(struct sock *sk, int state)
 
 	switch (state) {
 	case TCP_ESTABLISHED:
-		if (oldstate != TCP_ESTABLISHED)
+		if (oldstate != TCP_ESTABLISHED) {
 			TCP_INC_STATS(sock_net(sk), TCP_MIB_CURRESTAB);
+			if (is_meta_sk(sk))
+				MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+		}
 		break;
 
 	case TCP_CLOSE:
@@ -2332,8 +2335,11 @@ void tcp_set_state(struct sock *sk, int state)
 			inet_put_port(sk);
 		/* fall through */
 	default:
-		if (oldstate == TCP_ESTABLISHED)
+		if (oldstate == TCP_ESTABLISHED) {
 			TCP_DEC_STATS(sock_net(sk), TCP_MIB_CURRESTAB);
+			if (is_meta_sk(sk))
+				MPTCP_DEC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+		}
 	}
 
 	/* Change state AFTER socket is unhashed to avoid closed

net/mptcp/mptcp_ctrl.c
@@ -3038,6 +3038,7 @@ static const struct snmp_mib mptcp_snmp_list[] = {
 	SNMP_MIB_ITEM("RemAddrRx", MPTCP_MIB_REMADDRRX),
 	SNMP_MIB_ITEM("RemAddrTx", MPTCP_MIB_REMADDRTX),
 	SNMP_MIB_ITEM("MPJoinAlternatePort", MPTCP_MIB_JOINALTERNATEPORT),
+	SNMP_MIB_ITEM("MPCurrEstab", MPTCP_MIB_CURRESTAB),
 	SNMP_MIB_SENTINEL
 };
 

commit 7200d3e7b2392962de00861fad4cd219535f3154

mptcp: fix compilation error without CONFIG_MPTCP
MPTCP_DEC_STATS() is in tcp.c, outside MPTCP subsystem.

Instead of adding '#ifdef CONFIG_MPTCP' in multiple places in tcp.c, we
can define a dummy macro like we did for MPTCP_INC_STATS().

Fixes: b0b64e79bff7 ("mptcp: Maintain a "Current Established" counter for MPTCP")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -1407,6 +1407,10 @@ static inline struct sock *mptcp_to_sock(const struct mptcp_tcp_sock *mptcp)
 	do {				\
 	} while(0)
 
+#define MPTCP_DEC_STATS(net, field)	\
+	do {				\
+	} while(0)
+
 static inline bool mptcp_is_data_fin(const struct sk_buff *skb)
 {
 	return false;