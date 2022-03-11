3a365820aa0cd3277326cbbf084a9fec56c7196f
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-03-11 14:59:31 +0100

    mptcp: Stable Release v0.94.8
    Christoph Paasch <cpaasch@apple.com> (14):
          [0fde783554a0] Merge tag 'v4.14.150' into mptcp_v0.94
          [f81f16610ac8] mptcp: Only mpcb-put if the time-wait sk is still in the list
          [e00f7a7d2a23] mptcp: Make sure that inside_tk_table is 0 when creating a TCP-socket
          [830a817e79ac] mptcp: Avoid tcp_fin impossible state by stop processing upon fast-close
          [c83abf47b90e] mptcp: Avoid over-scheduling on a subflow
          [0bcd6a4f841d] mptcp: Correctly set cwnd_validated
          [32c794060ec2] mptcp: Make sure that we don't overfill subflows
          [1ddc334969d6] mptcp: Use correct MSS for space-computation
          [793f69a25fb8] mptcp: Avoid double-free of fastopen_req
          [f08ddc13e101] mptcp: Properly reset sockets in tcp_disconnect
          [d0b3246eb555] mptcp: Avoid tcp_done and mptcp_sock_def_error_report warnings
          [973acc185a98] mptcp: Don't leak request-sockets when socket-creation fails
          [d912d9e7fdd2] mptcp: Avoid last mptcp_sock_def_error_report warning
          [142af6db2ec1] mptcp: Restart sending missing data when falling back at the beginning
    
    Matthieu Baerts <matthieu.baerts@tessares.net> (25):
          [af1269bdd526] mptcp: fix bad conflict resolution
          [43aab7db75cd] mptcp: fix Kconfig warning
          [9df282933c92] Merge tag 'v4.14.172' into mptcp_v0.94
          [1b1d69d6d491] Merge tag 'v4.14.174' into mptcp_v0.94
          [d3eb721c7ab5] Merge tag 'v4.14.177' into mptcp_v0.94
          [17bdbd55df00] Merge tag 'v4.14.184' into mptcp_v0.94
          [36dba1f71328] Merge tag 'v4.14.205' into mptcp_v0.94
          [a8996d984917] Merge tag 'v4.14.209' into mptcp_v0.94
          [5956aaee1124] Merge tag 'v4.14.213' into mptcp_v0.94
          [1c78c1fec7cc] Merge tag 'v4.14.228' into mptcp_v0.94
          [6c699835f8bd] Merge tag 'v4.14.232' into mptcp_v0.94
          [9bfaf9830c13] mptcp: remove unused mptcp_prune_ofo_queue
          [cc5ddcbd823b] mptcp: disable BH before increase stats from rcu-critical sections
          [816796344f50] mptcp: sched: avoid calling tcp_current_mss() twice
          [8a137aff0bb6] Merge tag 'v4.14.238' into mptcp_v0.94
          [7aab281133c7] Revert "mptcp: sched: avoid calling tcp_current_mss() twice"
          [ea0f3dc91ad4] Revert "mptcp: correctly ensure to not overfill subflows"
          [89b1febdeb27] Revert "mptcp: Use correct MSS for space-computation"
          [cfe9141bde57] Revert "mptcp: Make sure that we don't overfill subflows"
          [067f5dfe1164] Merge tag 'v4.14.246' into mptcp_v0.94
          [d08fe228887d] Merge tag 'v4.14.261' into mptcp_v0.94
          [9e51c02d445f] Merge tag 'v4.14.265' into mptcp_v0.94
          [b4d562f60fc8] Revert "mptcp: Don't leak request-sockets when socket-creation fails"
          [ddf21bd9069e] Revert "mptcp: Restart sending missing data when falling back at the beginning"
          [3a8bed926f26] Merge tag 'v4.14.271' into mptcp_v0.94
    
    Tim Froidcoeur <tim.froidcoeur@tessares.net> (7):
          [a4980762c6cd] mptcp: fix delayed acks when ofo queue blocked
          [5d25660ca184] mptcp: correctly refcount congestion control module
          [29a2ad282c24] mptcp: propagate TCP_CONGESTION sockopt to subflows
          [4607ccf2a36f] mptcp: honor per destination congestion control
          [39650211485a] mptcp: correct chronos for meta and subflows
          [c41b6bd8cbb0] mptcp: avoid meta window shrink impacting subflow
          [088ed7dc1f39] mptcp: correctly ensure to not overfill subflows
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -3010,7 +3010,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.94.7");
+	pr_info("MPTCP: Stable release v0.94.8");
 
 	mptcp_init_failed = false;
 
