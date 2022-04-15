a7fbe8736944c47f19b8354906e71a5926906558
Christoph Paasch <cpaasch@apple.com>
2022-04-15 14:15:32 -0700

    mptcp: Avoid tcp_fin warning
    It can happen that our stack shows a tcp_fin warning:
    TCP: tcp_fin: Impossible, sk->sk_state=7
    This is because we may have a FIN in the out-of-order queue, when
    falling back and the connection has been closed with an active reset.
    
    +0      socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
    +0      setsockopt(3, SOL_TCP, TCP_NODELAY, [1], 4) = 0
    +0      setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
    +0      bind(3, ..., ...) = 0
    +0      listen(3, 1) = 0
    
    +0      < S 0:0(0) win 32792 <mss 1460,sackOK,nop,nop,nop,wscale 7,mp_capable key_a> sock(3)
    +0      > S. 0:0(0) ack 1 <mss 1460,nop,nop,sackOK,nop,wscale 7,mp_capable key_b> sock(3)
    +0.1    < . 1:1(0) ack 1 win 257 <mp_capable key_a key_b> sock(3)
    +0      accept(3, ..., ...) = 4
    
    +0      < P. 1:11(10) ack 1 win 257 <dss dack4 dsn4> sock(4)
    +0      > . 1:1(0) ack 11 <dss dack4=11> sock(4)
    
    +0      read(4, ..., 100) = 10
    
    +0      write(4, ..., 100) = 100
    +0      > P. 1:101(100) ack 11 <dss dack4=11 dsn4> sock(4)
    +0      write(4, ..., 100) = 100
    +0      > P. 101:201(100) ack 11 <dss dack4=11 dsn4> sock(4)
    
    /* out-of-order FIN */
    +0      < F. 12:12(0) ack 1 win 4242 <dss dack4=1> sock(4)
    +0      > . 201:201(0) ack 11 <dss dack4=11, nop, nop, sack 12:13> sock(4)
    
    +0      close(4) = 0
    +0      > . 201:201(0) ack 11 <nop, nop, sack 12:13, dss dack4=11 dsn4 FIN> sock(4)
    
    /* Trigger a fallback to infinite */
    +0      < . 11:12(1) ack 200 win 4242 sock(4)
    +0      > R. 201:201(0) ack 11 <nop, nop, sack 12:13> sock(4)
    
    We need to make sure that in those cases we stop processing any incoming
    packets.
    
    Fixes: Zero-day bug
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 2cb2658e51d2fb7b0464d98f5828f48e7aa2b4df)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 2c3b49d1c5f49ae8898e9d1b086228ac74529c4b)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 7d372191f7cca16ad5defcecd73f83c34989a6de)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 1c9734bb141656aaa9d42993285da76d7a1cb8e5)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_input.c
+++ net/mptcp/mptcp_input.c
@@ -1447,7 +1447,7 @@ static void mptcp_snd_una_update(struct tcp_sock *meta_tp, u32 data_ack)
 	meta_tp->snd_una = data_ack;
 }
 
-/* Handle the DATA_ACK */
+/* Return false if we can continue processing packets. True, otherwise */
 static bool mptcp_process_data_ack(struct sock *sk, const struct sk_buff *skb)
 {
 	struct sock *meta_sk = mptcp_meta_sk(sk);
@@ -1591,6 +1591,7 @@ no_queue:
 	return false;
 }
 
+/* Return false if we can continue processing packets. True, otherwise */
 bool mptcp_handle_ack_in_infinite(struct sock *sk, const struct sk_buff *skb,
 				  int flag)
 {
@@ -1675,9 +1676,8 @@ bool mptcp_handle_ack_in_infinite(struct sock *sk, const struct sk_buff *skb,
 	}
 
 exit:
-	mptcp_process_data_ack(sk, skb);
 
-	return false;
+	return mptcp_process_data_ack(sk, skb);
 }
 
 /**** static functions used by mptcp_parse_options */
