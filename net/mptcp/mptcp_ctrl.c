multipath-tcp/mptcp

commit 5c37f0c36b463ad8b92834e1892d08590500a258

mptcp: Properly handle failures in mptcp_create_master_sk
When allocatin the mptcp_tcp_sock fails in mptcp_add_sock, we have a
problem when coming from tcp_rcv_synsent_state_process ->
mptcp_rcv_synsent_state_process -> mptcp_craete_master_sk.

Because, when this happens we will end up leaving the "meta_sk"
initialized as a regular meta-sk. With sequence-numbers and all...
Then, in tcp_rcv_state_process we end up calling
mptcp_update_metasocket() and end up trying to access the
master_tp->mptcp, resulting in:

kasan: GPF could be caused by NULL-ptr deref or user memory access
general protection fault: 0000 [#1] SMP KASAN
CPU: 0 PID: 29354 Comm: syz-executor.2 Not tainted 5.4.184a32c74b31d01b8aecb4f0133e32c688fab1feb24 #155
Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS rel-1.13.0-0-gf21b5a4aeb02-prebuilt.qemu.org 04/01/2014
RIP: 0010:full_mesh_new_session+0x5a5/0x1bd0 net/mptcp/mptcp_fullmesh.c:1286
Code: 8b 4c 24 58 48 8d 04 5b 48 8d be 8e 00 00 00 48 89 fa 0f b6 44 81 01 48 c1 ea 03 83 e0 01 89 c3 48 b8 00 00 00 00 00 fc ff df <0f> b6 14 02 48 89 f8 83 e0 07 83 c0 01 38 d0 7c 08 84 d2 0f 85 74
RSP: 0018:ffff88811165f6c0 EFLAGS: 00010246
RAX: dffffc0000000000 RBX: 0000000000000000 RCX: ffff88810fc9ec00
RDX: 0000000000000011 RSI: 0000000000000000 RDI: 000000000000008e
RBP: ffff88811165f7e0 R08: 0000000000000000 R09: 0000000000000002
R10: 0000000000000003 R11: 0000000000000001 R12: ffff88811455f000
R13: 0000000000000002 R14: ffff888113663c00 R15: 0000000000000003
FS:  00007fac8b272700(0000) GS:ffff88811b400000(0000) knlGS:0000000000000000
CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
CR2: 000000000045e9f0 CR3: 0000000115f6b005 CR4: 0000000000160ef0
Call Trace:
 mptcp_update_metasocket+0x85/0xb0 net/mptcp/mptcp_ctrl.c:1682
 tcp_rcv_state_process+0xfda/0x2cd0 net/ipv4/tcp_input.c:6365
 tcp_v4_do_rcv+0x3bf/0x9c0 net/ipv4/tcp_ipv4.c:1639
 sk_backlog_rcv include/net/sock.h:962 [inline]
 __release_sock+0x1ad/0x320 net/core/sock.c:2492

net/mptcp/mptcp_ctrl.c
@@ -1297,19 +1297,44 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	meta_tp->is_master_sk = 1;
 	master_sk = sk_clone_lock(meta_sk, GFP_ATOMIC | __GFP_ZERO);
 	meta_tp->is_master_sk = 0;
-	if (!master_sk)
+	if (!master_sk) {
+		net_err_ratelimited("%s Could not allocate master_sk on meta %p\n",
+				    __func__, meta_sk);
 		goto err_alloc_master;
+	}
 
 	/* Same as in inet_csk_clone_lock - need to init to 0 */
 	memset(&inet_csk(master_sk)->icsk_accept_queue, 0,
 	       sizeof(inet_csk(master_sk)->icsk_accept_queue));
 
+	/* icsk_bind_hash inherited from the meta, but it will be properly set
+	 * in mptcp_create_master_sk. Same operation is done in
+	 * inet_csk_clone_lock.
+	 */
+	inet_csk(master_sk)->icsk_bind_hash = NULL;
+
 	master_tp = tcp_sk(master_sk);
 	master_tp->inside_tk_table = 0;
 
+	master_tp->mptcp = kmem_cache_zalloc(mptcp_sock_cache, GFP_ATOMIC);
+	if (!master_tp->mptcp) {
+		net_err_ratelimited("%s Could not allocate mptcp_tcp_sock on meta %p\n",
+				    __func__, meta_sk);
+		goto err_alloc_mptcp;
+	}
+
 	mpcb = kmem_cache_zalloc(mptcp_cb_cache, GFP_ATOMIC);
-	if (!mpcb)
+	if (!mpcb) {
+		net_err_ratelimited("%s Could not allocate mpcb on meta %p\n",
+				    __func__, meta_sk);
 		goto err_alloc_mpcb;
+	}
+
+	if (__inet_inherit_port(meta_sk, master_sk) < 0) {
+		net_err_ratelimited("%s Could not inherit port on meta %p\n",
+				    __func__, meta_sk);
+		goto err_inherit_port;
+	}
 
 	/* Store the mptcp version agreed on initial handshake */
 	mpcb->mptcp_ver = mptcp_ver;
@@ -1461,10 +1486,6 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	master_tp->fastopen_req = NULL;
 
 	master_sk->sk_tsq_flags = 0;
-	/* icsk_bind_hash inherited from the meta, but it will be properly set in
-	 * mptcp_create_master_sk. Same operation is done in inet_csk_clone_lock.
-	 */
-	inet_csk(master_sk)->icsk_bind_hash = NULL;
 
 	/* Init the accept_queue structure, we support a queue of 32 pending
 	 * connections, it does not need to be huge, since we only store  here
@@ -1511,7 +1532,21 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 err_insert_token:
 	kmem_cache_free(mptcp_cb_cache, mpcb);
 
+	kmem_cache_free(mptcp_sock_cache, master_tp->mptcp);
+	master_tp->mptcp = NULL;
+
+	inet_csk_prepare_forced_close(master_sk);
+	tcp_done(master_sk);
+	return -EINVAL;
+
+err_inherit_port:
+	kmem_cache_free(mptcp_cb_cache, mpcb);
+
 err_alloc_mpcb:
+	kmem_cache_free(mptcp_sock_cache, master_tp->mptcp);
+	master_tp->mptcp = NULL;
+
+err_alloc_mptcp:
 	inet_sk(master_sk)->inet_opt = NULL;
 	master_sk->sk_state = TCP_CLOSE;
 	sock_orphan(master_sk);
@@ -1545,13 +1580,19 @@ int mptcp_add_sock(struct sock *meta_sk, struct sock *sk, u8 loc_id, u8 rem_id,
 	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct tcp_sock *tp = tcp_sk(sk);
 
-	tp->mptcp = kmem_cache_zalloc(mptcp_sock_cache, flags);
-	if (!tp->mptcp)
-		return -ENOMEM;
+	/* Could have been allocated by mptcp_alloc_mpcb */
+	if (!tp->mptcp) {
+		tp->mptcp = kmem_cache_zalloc(mptcp_sock_cache, flags);
+
+		if (!tp->mptcp)
+			return -ENOMEM;
+	}
 
 	tp->mptcp->path_index = mptcp_set_new_pathindex(mpcb);
 	/* No more space for more subflows? */
 	if (!tp->mptcp->path_index) {
+		WARN_ON(is_master_tp(tp));
+
 		kmem_cache_free(mptcp_sock_cache, tp->mptcp);
 		return -EPERM;
 	}
@@ -2196,11 +2237,10 @@ int mptcp_create_master_sk(struct sock *meta_sk, __u64 remote_key,
 	master_sk = tcp_sk(meta_sk)->mpcb->master_sk;
 	master_tp = tcp_sk(master_sk);
 
-	if (mptcp_add_sock(meta_sk, master_sk, 0, 0, GFP_ATOMIC))
-		goto err_add_sock;
-
-	if (__inet_inherit_port(meta_sk, master_sk) < 0)
-		goto err_add_sock;
+	if (mptcp_add_sock(meta_sk, master_sk, 0, 0, GFP_ATOMIC)) {
+		WARN_ON(1);
+		return -EINVAL;
+	}
 
 	meta_sk->sk_prot->unhash(meta_sk);
 	inet_ehash_nolisten(master_sk, NULL);
@@ -2208,10 +2248,6 @@ int mptcp_create_master_sk(struct sock *meta_sk, __u64 remote_key,
 	master_tp->mptcp->init_rcv_wnd = master_tp->rcv_wnd;
 
 	return 0;
-
-err_add_sock:
-	inet_csk_prepare_forced_close(master_sk);
-	tcp_done(master_sk);
 
 err_alloc_mpcb:
 	return -ENOBUFS;

commit 54a51a5a2640867c34ba0ed665091d0c91d8773f

mptcp: Don't warn when creating 32 subflows
slave_sk is set only after mptcp_add_sock(). Thus is_master_tp() will
always return true, thus always resulting in a WARN when
mptcp_set_new_pathindex returns 0.

Fixes: 5c37f0c36b46 ("mptcp: Properly handle failures in mptcp_create_master_sk")
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_ctrl.c
@@ -1591,8 +1591,6 @@ int mptcp_add_sock(struct sock *meta_sk, struct sock *sk, u8 loc_id, u8 rem_id,
 	tp->mptcp->path_index = mptcp_set_new_pathindex(mpcb);
 	/* No more space for more subflows? */
 	if (!tp->mptcp->path_index) {
-		WARN_ON(is_master_tp(tp));
-
 		kmem_cache_free(mptcp_sock_cache, tp->mptcp);
 		return -EPERM;
 	}