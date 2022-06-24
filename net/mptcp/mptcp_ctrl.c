8ed60969d86ddcd10d79c6bc80ec0307b9fadb04
Christoph Paasch <cpaasch@apple.com>
2022-06-24 09:26:00 -0700

    mptcp: Do not race forced-closure with packet reception
    It is possible that we call inet_csk_prepare_forced_close() - which
    calls bh_unlock_sock() - on a socket that is already in the ehash table
    because it went through tcp_v4_syn_recv_sock.
    
    That is a problem. Because, the moment we unlock incoming packets may be
    processed on the socket, because this socket has not the mpc-flag set.
    
    So, that means that we are processing an incoming packet while at the
    same time being potentially inside tcp_done(). Only bad things can
    happen there...
    
    I'm hunting down a weird kernel-WARNING in __inet_hash_connect() and the
    current best guess is that this is the problem, as it can cause all kind
    of racy behavior.
    
    [858163.658887] ------------[ cut here ]------------
    [858163.661126] WARNING: CPU: 24 PID: 7966 at net/ipv4/inet_hashtables.c:740 __inet_hash_connect+0x419/0x440
    [...]
    
    Fixes: 1f2d95166ce9 ("Fix error-handling")
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 1d5fc78957a788eaaa4fab85d0ce7f6e0f978931)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit fe3634a6995e7c7c3f127e7e7342e06e24f927bc)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit b800164af09ebd88d17b39666918dff879ae0ee6)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit aea1af352e9f99ecfe1e71699889808ee13374ac)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 737e8ff993bcbb004881709f2b9fd05efe4e09c7)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit eb945274cb43818d7f2f0b3832a5d500e7807821)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -1118,6 +1118,28 @@ static const struct tcp_sock_ops mptcp_sub_specific = {
 	.set_cong_ctrl                  = __tcp_set_congestion_control,
 };
 
+/* Inspired by inet_csk_prepare_forced_close */
+static void mptcp_icsk_forced_close(struct sock *sk)
+{
+	/* The problem with inet_csk_prepare_forced_close is that it unlocks
+	 * before calling tcp_done. That is fine for sockets that are not
+	 * yet in the ehash table. But for us we already are there. Thus,
+	 * if we unlock we run the risk of processing packets while inside
+	 * tcp_done() and friends. That can cause all kind of problems...
+	 */
+
+	/* The below has to be done to allow calling inet_csk_destroy_sock */
+	sock_set_flag(sk, SOCK_DEAD);
+	percpu_counter_inc(sk->sk_prot->orphan_count);
+	inet_sk(sk)->inet_num = 0;
+
+	tcp_done(sk);
+
+	/* sk_clone_lock locked the socket and set refcnt to 2 */
+	bh_unlock_sock(sk);
+	sock_put(sk);
+}
+
 static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 			    __u8 mptcp_ver, u32 window)
 {
@@ -2030,10 +2052,8 @@ int mptcp_create_master_sk(struct sock *meta_sk, __u64 remote_key,
 err_add_sock:
 	kfree(inet_csk(meta_sk)->icsk_accept_queue.listen_opt);
 
-	inet_csk_prepare_forced_close(master_sk);
-	tcp_done(master_sk);
-	inet_csk_prepare_forced_close(meta_sk);
-	tcp_done(meta_sk);
+	mptcp_icsk_forced_close(master_sk);
+	mptcp_icsk_forced_close(meta_sk);
 
 err_alloc_mpcb:
 	return -ENOBUFS;
@@ -2246,8 +2266,9 @@ struct sock *mptcp_check_req_child(struct sock *meta_sk, struct sock *child,
 teardown:
 	/* Drop this request - sock creation failed. */
 	inet_csk_reqsk_queue_drop(meta_sk, req);
-	inet_csk_prepare_forced_close(child);
-	tcp_done(child);
+
+	mptcp_icsk_forced_close(child);
+
 	return meta_sk;
 }
 
