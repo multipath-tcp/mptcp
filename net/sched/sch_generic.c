7074b39d83f5d71fa4f0521b28bd4fb3a22152c1
Diederik de Haas <didi.debian@cknow.org>
2022-06-30 00:49:38 +0200

    net/sched: move NULL ptr check to qdisc_put() too
    In commit 92833e8b5db6c209e9311ac8c6a44d3bf1856659 titled
    "net: sched: rename qdisc_destroy() to qdisc_put()" part of the
    functionality of qdisc_destroy() was moved into a (for linux-4.19.y)
    new function qdisk_put(), and the previous calls to qdisc_destroy()
    were changed to qdisk_put().
    This made it similar to f.e. 5.10.y and current master.
    
    There was one part of qdisc_destroy() not moved over to qdisc_put() and
    that was the check for a NULL pointer, causing oopses.
    (See upstream commit: 6efb971ba8edfbd80b666f29de12882852f095ae)
    This patch fixes that.
    
    Fixes: 92833e8b5db6c209e9311ac8c6a44d3bf1856659
    Reported-by: Thorsten Glaser <tg@mirbsd.de>
    Link: https://bugs.debian.org/1013299
    Signed-off-by: Diederik de Haas <didi.debian@cknow.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -970,8 +970,6 @@ static void qdisc_destroy(struct Qdisc *qdisc)
 	const struct Qdisc_ops *ops;
 	struct sk_buff *skb, *tmp;
 
-	if (!qdisc)
-		return;
 	ops = qdisc->ops;
 
 #ifdef CONFIG_NET_SCHED
@@ -1003,6 +1001,9 @@ static void qdisc_destroy(struct Qdisc *qdisc)
 
 void qdisc_put(struct Qdisc *qdisc)
 {
+	if (!qdisc)
+		return;
+
 	if (qdisc->flags & TCQ_F_BUILTIN ||
 	    !refcount_dec_and_test(&qdisc->refcnt))
 		return;
