f4ba4cfab701f7c699ef48db478b8677c4a021fa
Christoph Paasch <cpaasch@apple.com>
2022-07-07 09:21:07 -0700

    mptcp: Avoid ever-increasing count of orphan sockets
    We have observed an ever-increasing count of orphan sockets on busy
    servers. These are MPTCP-connections that ran out of MPTCP subflows.
    
    What happens then is that when we try to retransmit the DATA_FIN, we fail in
    mptcp_retransmit_skb. In that case, retrans_stamp won't be set and
    remains 0.
    
    If that happens, the check in retransmits_timed_out() may always return
    false, depending on whether tcp_time_stamp(tcp_sk(sk)) is > 2^31 or not.
    Meaning, we never call tcp_write_err() on that MPTCP-socket and thus it
    will hang around forever.
    
    tcp_retransmit_skb handles that by always setting retrans_stamp -
    regardless of whether the retransmission succeeded or not.
    
    We should do the same.
    
    Fixes: Zero-day bug
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 3a4382f50a2a4b3a11c6dd15faf9ca263a22a001)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 95803d8122bcbc213512e6feb5e512d908902656)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 54147de78e9b66f177a301c1414107a378fcb09a)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit c35e7c2b6705b2cdee3fa2eae4bfac30e691cf25)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 20240f03f32b45dd5f997af325730d94fa764ca7)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit f8a17804123569ef3cee4eb07e285cded67ddca7)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -1456,7 +1456,9 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 	 */
 	if (atomic_read(&meta_sk->sk_wmem_alloc) >
 	    min(meta_sk->sk_wmem_queued + (meta_sk->sk_wmem_queued >> 2), meta_sk->sk_sndbuf)) {
-		return -EAGAIN;
+		err = -EAGAIN;
+
+		goto failed;
 	}
 
 	/* We need to make sure that the retransmitted segment can be sent on a
@@ -1517,6 +1519,10 @@ int mptcp_retransmit_skb(struct sock *meta_sk, struct sk_buff *skb)
 
 failed:
 	NET_INC_STATS_BH(sock_net(meta_sk), LINUX_MIB_TCPRETRANSFAIL);
+	/* Save stamp of the first attempted retransmit. */
+	if (!meta_tp->retrans_stamp)
+		meta_tp->retrans_stamp = tcp_skb_timestamp(skb);
+
 	return err;
 }
 
