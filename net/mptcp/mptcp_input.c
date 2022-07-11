8c178832dd138cbe632972f3abc588ddad6e4b07
Tim Froidcoeur <tim.froidcoeur@tessares.net>
2022-07-11 16:39:21 +0200

    mptcp: fix tcp-mem accounting
    sk_mem_charge shouldn't be done without making sure that sufficient
    memory has been scheduled onto sk_forward_alloc.
    
    The network stack will (should) always call __sk_mem_schedule or
    sk_forced_mem_schedule to make sure sufficient budget has been reserved
    in sk_forward_alloc. sk_forward_alloc serves as the socket's
    "cached/local" memory budget for global network memory accounting (like
    tcp_mem).
    This pattern can be seen in sk_stream_alloc_skb (called by
    tcp_sendmsg_locked for example). It allocates and skb and makes a
    reservation on the sk_forward_alloc. It is only later, in __skb_entail
    that the memory is actually charged to sk_forward_alloc by calling
    sk_mem_charge.
    
    The reservation in sk_forward_alloc is rounded up to a full page, so it
    often allows multiple skb to be handled with one reservation.
    
    Note that the stack will also sometimes return excess sk_forward_alloc
    to the global pool.
    
    The design idea behind this is probably twofold: 1) avoid the complexity
    of having to roll back some operations if no more (tcp_mem) memory is
    available and 2) to avoid accessing a global variable on SMP
    architectures every time.
    
    The impact of not doing sk_mem_schedule will be that a call to sk_mem_charge
    will make sk_forward_alloc negative, so memory will be used without
    being visible on the global memory accounting (tcp_mem).
    
    skb in the TCP OFOQ are not charged to the socket (maybe they should,
    but this will introduce complexities, like correctly signaling SACK and
    making sure to always accept the HOL packet for the OFOq).
    
    When a packet is transferred from the OFOq to the actual receive queue,
    tcp_try_coalesce will call sk_mem_charge, but no call to sk_mem_schedule
    is made to ensure there is enough reservation on sk_forward_alloc.
    
    Adding a 'forced' sk_mem_schedule because 1) Dropping the packet at this
    point is more complicated 2) the packet is in memory for a while
    already, just never been charged properly. A forced charge will never
    fail (and never has to drop the packet), but can cause the global memore
    accounting to exceed the limits that were set.
    
    Fixes: Zero-day Bug
    Signed-off-by: Nathan Atta <nathan.atta@tessares.net>
    Signed-off-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>
    Acked-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 8ebf89a5de81cc97bc74594b0517aa764049bebe)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit a6d5473018d13a2cf2f733715196711fafea2b28)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 0de8b0640c500533a316a74c7a0bb4eca3ee9126)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 7ae3325fff114d794d2dc50fc0996df0f685c654)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit dc8553d174838c18eda611ba94119a903fd84c42)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_input.c
+++ net/mptcp/mptcp_input.c
@@ -984,6 +984,7 @@ static int mptcp_queue_skb(struct sock *sk)
 			tp->copied_seq = TCP_SKB_CB(tmp1)->end_seq;
 			mptcp_prepare_skb(tmp1, sk);
 			__skb_unlink(tmp1, &sk->sk_receive_queue);
+			sk_forced_mem_schedule(meta_sk, tmp1->truesize);
 			/* MUST be done here, because fragstolen may be true later.
 			 * Then, kfree_skb_partial will not account the memory.
 			 */
@@ -1015,6 +1016,7 @@ static int mptcp_queue_skb(struct sock *sk)
 			tp->copied_seq = TCP_SKB_CB(tmp1)->end_seq;
 			mptcp_prepare_skb(tmp1, sk);
 			__skb_unlink(tmp1, &sk->sk_receive_queue);
+			sk_forced_mem_schedule(meta_sk, tmp1->truesize);
 			/* MUST be done here, because fragstolen may be true.
 			 * Then, kfree_skb_partial will not account the memory.
 			 */
