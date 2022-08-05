df7dd2c14f044de8c453fabb525bc8e50a677d2c
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-08-05 16:57:14 +0200

    mptcp: un-expose skb_clone_fraglist()
    skb_clone_fraglist() had been exposed for MPTCP a while ago in
    commit f5ad780c48e2 ("Fix: Create full new skb for the subflow").
    
    But later, the function using it from MPTCP side has been removed in
    commit a4b4ed06cc45 ("mptcp: remove mptcp_pskb_copy").
    
    We can then reduce the difference with upstream by reverting the
    modifications around skb_clone_fraglist().
    
    Fixes: a4b4ed06cc45 ("mptcp: remove mptcp_pskb_copy")
    Acked-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit a670e50ecfad76dedb0922e5b81741619011c3b4)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -582,7 +582,7 @@ static inline void skb_drop_fraglist(struct sk_buff *skb)
 	skb_drop_list(&skb_shinfo(skb)->frag_list);
 }
 
-void skb_clone_fraglist(struct sk_buff *skb)
+static void skb_clone_fraglist(struct sk_buff *skb)
 {
 	struct sk_buff *list;
 
