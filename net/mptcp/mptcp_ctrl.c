74a258e4608805771382f60ce5fc6fbce0307699
Christoph Paasch <cpaasch@apple.com>
2022-08-16 20:11:43 -0700

    mptcp: Don't sock_put too early if we still access the meta
    In the error code-paths the meta is supposed to get free'd. Problem is:
    we do a bh_unlock_sock() after the last sock_put().
    
    (that last sock-put happens in the error code-path when going into
    inet_csk_destroy_sock() coming from inet_child_forget())
    
    The reason this is the last sock_put() is because we sock_put already in
    __mptcp_check_req_master. Thus, this is error-prone.
    
    Instread of having __mptcp_check_req_master do the last sock_put, let's
    let the callers do it. That avoid a bh_unlock_sock() on a free'd socket.
    
    Fixes: Zero-day bug
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 7b0343da208fb836df729b3e2cce40094d55271c)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 99ec1d76854930c67aae8229314f8f247830469e)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit d616ea5bc7e6f0faf1f7d0c9b7613804aac02d39)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2052,9 +2052,6 @@ static int __mptcp_check_req_master(struct sock *child,
 	 */
 	mptcp_reqsk_remove_tk(req);
 
-	/* Hold when creating the meta-sk in tcp_vX_syn_recv_sock. */
-	sock_put(meta_sk);
-
 	return 0;
 }
 
@@ -2130,9 +2127,7 @@ int mptcp_check_req_master(struct sock *sk, struct sock *child,
 		reqsk_queue_removed(&inet_csk(sk)->icsk_accept_queue, req);
 		if (!inet_csk_reqsk_queue_add(sk, req, meta_sk)) {
 			bh_unlock_sock(meta_sk);
-			/* No sock_put() of the meta needed. The reference has
-			 * already been dropped in __mptcp_check_req_master().
-			 */
+			sock_put(meta_sk);
 			sock_put(child);
 			return -1;
 		}
@@ -2142,14 +2137,14 @@ int mptcp_check_req_master(struct sock *sk, struct sock *child,
 		tcp_sk(meta_sk)->tsoffset = tsoff;
 		if (!inet_csk_reqsk_queue_add(sk, req, meta_sk)) {
 			bh_unlock_sock(meta_sk);
-			/* No sock_put() of the meta needed. The reference has
-			 * already been dropped in __mptcp_check_req_master().
-			 */
+			sock_put(meta_sk);
 			sock_put(child);
 			reqsk_put(req);
 			return -1;
 		}
 	}
+
+	sock_put(meta_sk);
 
 	return 0;
 }
