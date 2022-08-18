7d7555fb1c30cc29d64dfe689b84c04f4799b2e0
Tim Froidcoeur <tim.froidcoeur@tessares.net>
2022-08-18 17:02:13 +0200

    mptcp: correct check user owned socket
    correct the check for user owned socket in TCP handling of routing
    redirect.
    this issue could lead to a race condition and a kernel crash, see
    45caeaa5ac0b ("dccp/tcp: fix routing redirect race")
    
    Fixes: e56ce5051a71 ("Merge tag 'v4.11' into mptcp_trunk")
    Reported-by: Christoph Paasch <cpaasch@apple.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>
    Acked-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 041e84d3550e7477e388ac861806d5690c961437)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -437,7 +437,7 @@ static int tcp_v6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 	np = tcp_inet6_sk(sk);
 
 	if (type == NDISC_REDIRECT) {
-		if (!sock_owned_by_user(sk)) {
+		if (!sock_owned_by_user(meta_sk)) {
 			struct dst_entry *dst = __sk_dst_check(sk, np->dst_cookie);
 
 			if (dst)
