384bc073fe4fc6cbd0d7a20d627e3293ca37292c
Tim Froidcoeur <tim.froidcoeur@tessares.net>
2022-08-18 17:02:12 +0200

    mptcp: correct user owned check during FIN_ACK1
    correctly check if a (meta) socket is user owned for mptcp subflows
    during tcp handling in TCP_FIN_WAIT1.
    This issue could cause the loss of a FIN and require a subsequent
    retransmission from the peer.
    
    Fixes: Zero-day Bug
    Reported-by: Christoph Paasch <cpaasch@apple.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>
    Acked-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 2beaf5840d3aa7b49b1d7aa5038093177dcae08e)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 1d6a567a0ece2c2f9b38b5e20294a7daff454204)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 071adf410fbe1438137445896c1f71ca35f740b7)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 44095eee7ba47067b1292bcaa41d4efb6d6acea3)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6286,6 +6286,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 		break;
 
 	case TCP_FIN_WAIT1: {
+		struct sock *meta_sk = mptcp(tcp_sk(sk)) ? mptcp_meta_sk(sk) : sk;
 		struct dst_entry *dst;
 		int tmo;
 
@@ -6327,7 +6328,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 		if (tmo > TCP_TIMEWAIT_LEN) {
 			inet_csk_reset_keepalive_timer(sk, tmo - TCP_TIMEWAIT_LEN);
 		} else if (th->fin || mptcp_is_data_fin(skb) ||
-			   sock_owned_by_user(sk)) {
+			   sock_owned_by_user(meta_sk)) {
 			/* Bad case. We could lose such FIN otherwise.
 			 * It is not a big problem, but it looks confusing
 			 * and not so rare event. We still can lose it now,
