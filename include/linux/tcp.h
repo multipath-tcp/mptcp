multipath-tcp/mptcp

commit 0418112ef2daa79549158bafdcec7dfcb17f5e38

mptcp: Cleanup
Remove unused variable and use "tp"

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -240,7 +240,6 @@ struct mptcp_pm_ops {
 	struct module	*owner;
 };
 
-#define MPTCP_SCHED_NAME_MAX 16
 struct mptcp_sched_ops {
 	struct list_head list;
 

net/mptcp/mptcp_output.c
@@ -983,7 +983,7 @@ void mptcp_syn_options(const struct sock *sk, struct tcp_out_options *opts,
 	opts->options |= OPTION_MPTCP;
 	if (is_master_tp(tp)) {
 		opts->mptcp_options |= OPTION_MP_CAPABLE | OPTION_TYPE_SYN;
-		opts->mptcp_ver = tcp_sk(sk)->mptcp_ver;
+		opts->mptcp_ver = tp->mptcp_ver;
 		*remaining -= MPTCP_SUB_LEN_CAPABLE_SYN_ALIGN;
 		opts->mp_capable.sender_key = tp->mptcp_loc_key;
 		opts->dss_csum = !!sysctl_mptcp_checksum;

commit 289570865f4e1bff49407424a541497d3766f1c9

mptcp: fix compilation error without MPTCP
Fixing this error:

  In file included from net/core/sock.c:138:
  net/mptcp.h:258:38: error: 'MPTCP_SCHED_NAME_MAX' undeclared here (not in a function); did you mean 'MPTCP_PM_NAME_MAX'?
    258 |         char                    name[MPTCP_SCHED_NAME_MAX];
        |                                      ^~~~~~~~~~~~~~~~~~~~
        |                                      MPTCP_PM_NAME_MAX
  make[3]: *** [scripts/Makefile.build:262: net/core/sock.o] Error 1

Instead of duplicating the define, it is probably better to define it
once even if MPTCP is not set.

Fixes: 0418112ef2da ("mptcp: Cleanup")
Reviewed-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/linux/tcp.h
@@ -480,9 +480,9 @@ struct tcp_sock {
 		record_master_info:1,
 		tcp_disconnect:1;
 	struct mptcp_tcp_sock *mptcp;
-#ifdef CONFIG_MPTCP
 #define MPTCP_SCHED_NAME_MAX 16
 #define MPTCP_PM_NAME_MAX 16
+#ifdef CONFIG_MPTCP
 	struct hlist_nulls_node tk_table;
 	u32		mptcp_loc_token;
 	u64		mptcp_loc_key;

include/net/mptcp.h
@@ -215,7 +215,6 @@ struct mptcp_tw {
 	   in_list:1;
 };
 
-#define MPTCP_PM_NAME_MAX 16
 struct mptcp_pm_ops {
 	struct list_head list;
 