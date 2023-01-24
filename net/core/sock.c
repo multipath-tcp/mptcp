multipath-tcp/mptcp

commit d10b6f71b4f8f0400e8bd621f2e8aa6b879d7932

mptcp: Support for some more socket-options
Support IPV6_TCLASS as well as complete the support for IP_TOS.
Also, add missing bits for SO_MARK.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/core/sock.c
@@ -1073,6 +1073,19 @@ set_rcvbuf:
 		} else if (val != sk->sk_mark) {
 			sk->sk_mark = val;
 			sk_dst_reset(sk);
+
+			if (is_meta_sk(sk)) {
+				struct mptcp_tcp_sock *mptcp;
+
+				mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp) {
+					struct sock *sk_it = mptcp_to_sock(mptcp);
+
+					if (val != sk_it->sk_mark) {
+						sk_it->sk_mark = val;
+						sk_dst_reset(sk_it);
+					}
+				}
+			}
 		}
 		break;
 

net/core/filter.c
@@ -73,6 +73,7 @@
 #include <net/lwtunnel.h>
 #include <net/ipv6_stubs.h>
 #include <net/bpf_sk_storage.h>
+#include <net/mptcp.h>
 
 /**
  *	sk_filter_trim_cap - run a packet through a socket filter
@@ -4282,6 +4283,19 @@ BPF_CALL_5(bpf_setsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 			if (sk->sk_mark != val) {
 				sk->sk_mark = val;
 				sk_dst_reset(sk);
+
+				if (is_meta_sk(sk)) {
+					struct mptcp_tcp_sock *mptcp;
+
+					mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp) {
+						struct sock *sk_it = mptcp_to_sock(mptcp);
+
+						if (val != sk_it->sk_mark) {
+							sk_it->sk_mark = val;
+							sk_dst_reset(sk_it);
+						}
+					}
+				}
 			}
 			break;
 		default:
@@ -4304,6 +4318,14 @@ BPF_CALL_5(bpf_setsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 				if (val == -1)
 					val = 0;
 				inet->tos = val;
+
+				/* Update TOS on mptcp subflow */
+				if (is_meta_sk(sk)) {
+					struct mptcp_tcp_sock *mptcp;
+
+					mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp)
+						inet_sk(mptcp_to_sock(mptcp))->tos = val;
+				}
 			}
 			break;
 		default:
@@ -4326,6 +4348,17 @@ BPF_CALL_5(bpf_setsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 				if (val == -1)
 					val = 0;
 				np->tclass = val;
+
+				if (is_meta_sk(sk)) {
+					struct mptcp_tcp_sock *mptcp;
+
+					mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp) {
+						struct sock *sk_it = mptcp_to_sock(mptcp);
+
+						if (sk_it->sk_family == AF_INET6)
+							inet6_sk(sk_it)->tclass = val;
+					}
+				}
 			}
 			break;
 		default:

net/ipv6/ipv6_sockglue.c
@@ -345,6 +345,17 @@ static int do_ipv6_setsockopt(struct sock *sk, int level, int optname,
 		if (val == -1)
 			val = 0;
 		np->tclass = val;
+
+		if (is_meta_sk(sk)) {
+			struct mptcp_tcp_sock *mptcp;
+
+			mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp) {
+				struct sock *sk_it = mptcp_to_sock(mptcp);
+
+				if (sk_it->sk_family == AF_INET6)
+					inet6_sk(sk_it)->tclass = val;
+			}
+		}
 		retv = 0;
 		break;
 

net/mptcp/mptcp_ctrl.c
@@ -1097,6 +1097,10 @@ static void mptcp_sub_inherit_sockopts(const struct sock *meta_sk, struct sock *
 		sk_dst_reset(sub_sk);
 	}
 
+	/* IPV6_TCLASS */
+	if (sub_sk->sk_family == AF_INET6 && meta_sk->sk_family == AF_INET6)
+		inet6_sk(sub_sk)->tclass = inet6_sk(meta_sk)->tclass;
+
 	/* Inherit SO_REUSEADDR */
 	sub_sk->sk_reuse = meta_sk->sk_reuse;
 

commit d57a5d7f898fd8da061452a1212b7e8d9a2522f2

mptcp: fix compilation error without MPTCP
Fixing these errors:

  net/core/sock.c: In function 'sock_setsockopt':
  net/core/sock.c:1072:29: error: implicit declaration of function 'is_meta_sk'; did you mean 'inet6_sk'? [-Werror=implicit-function-declaration]
   1072 |                         if (is_meta_sk(sk)) {
        |                             ^~~~~~~~~~
        |                             inet6_sk
  net/core/sock.c:1075:33: error: implicit declaration of function 'mptcp_for_each_sub' [-Werror=implicit-function-declaration]
   1075 |                                 mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp) {
        |                                 ^~~~~~~~~~~~~~~~~~
  net/core/sock.c:1075:76: error: expected ';' before '{' token
   1075 |                                 mptcp_for_each_sub(tcp_sk(sk)->mpcb, mptcp) {
        |                                                                            ^~
        |                                                                            ;
  cc1: some warnings being treated as errors
  make[3]: *** [scripts/Makefile.build:262: net/core/sock.o] Error 1

No need to restrict the include of mptcp.h (and inet_common.h), it
doesn't conflict with anything. If CONFIG_MPTCP is not set, structure
and stubs will be available to be able to compile code while still doing
nothing more as expected.

Fixes: d10b6f71b4f8 ("mptcp: Support for some more socket-options")
Reviewed-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/core/sock.c
@@ -135,10 +135,8 @@
 
 #include <trace/events/sock.h>
 
-#ifdef CONFIG_MPTCP
 #include <net/mptcp.h>
 #include <net/inet_common.h>
-#endif
 
 #include <net/tcp.h>
 #include <net/busy_poll.h>