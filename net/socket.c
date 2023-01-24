multipath-tcp/mptcp

commit 8785d1102d5f494f64567855f205c525a6a26935

mptcp: Allow using IPPROTO_MPTCP
This allows the use of MPTCP-upstream's API using IPPROTO_MPTCP to
enable MPTCP.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/socket.c
@@ -91,6 +91,7 @@
 #include <asm/unistd.h>
 
 #include <net/compat.h>
+#include <net/mptcp.h>
 #include <net/wext.h>
 #include <net/cls_cgroup.h>
 
@@ -1350,6 +1351,7 @@ int __sock_create(struct net *net, int family, int type, int protocol,
 	int err;
 	struct socket *sock;
 	const struct net_proto_family *pf;
+	int old_protocol = protocol;
 
 	/*
 	 *      Check protocol is in range
@@ -1369,6 +1371,9 @@ int __sock_create(struct net *net, int family, int type, int protocol,
 			     current->comm);
 		family = PF_PACKET;
 	}
+
+	if (old_protocol == IPPROTO_MPTCP)
+		protocol = IPPROTO_TCP;
 
 	err = security_socket_create(family, type, protocol, kern);
 	if (err)
@@ -1418,6 +1423,10 @@ int __sock_create(struct net *net, int family, int type, int protocol,
 	err = pf->create(net, sock, protocol, kern);
 	if (err < 0)
 		goto out_module_put;
+
+	if (sysctl_mptcp_enabled && old_protocol == IPPROTO_MPTCP &&
+	    type == SOCK_STREAM && (family == AF_INET || family == AF_INET6))
+		mptcp_enable_sock(sock->sk);
 
 	/*
 	 * Now to bump the refcnt of the [loadable] module that owns this

include/uapi/linux/in.h
@@ -76,6 +76,8 @@ enum {
 #define IPPROTO_MPLS		IPPROTO_MPLS
   IPPROTO_RAW = 255,		/* Raw IP packets			*/
 #define IPPROTO_RAW		IPPROTO_RAW
+  IPPROTO_MPTCP = 262,		/* Multipath TCP connection 		*/
+#define IPPROTO_MPTCP		IPPROTO_MPTCP
   IPPROTO_MAX
 };
 #endif

commit ebbb755ebb21ccf31e2d74af12cc7d4c9bd0f517

mptcp: fix compilation error without MPTCP
Fixing these errors:

  net/socket.c: In function '__sock_create':
  net/socket.c:1416:13: error: 'sysctl_mptcp_enabled' undeclared (first use in this function)
   1416 |         if (sysctl_mptcp_enabled && old_protocol == IPPROTO_MPTCP &&
        |             ^~~~~~~~~~~~~~~~~~~~
  net/socket.c:1416:13: note: each undeclared identifier is reported only once for each function it appears in
  net/socket.c:1418:17: error: implicit declaration of function 'mptcp_enable_sock'; did you mean 'mptcp_del_sock'? [-Werror=implicit-function-declaration]
   1418 |                 mptcp_enable_sock(sock->sk);
        |                 ^~~~~~~~~~~~~~~~~
        |                 mptcp_del_sock
  cc1: some warnings being treated as errors
  make[2]: *** [scripts/Makefile.build:262: net/socket.o] Error 1

sysctl_mptcp_enabled is an external variable, only defined if
CONFIG_MPTCP is set. We could create new functions doing nothing but the
two undeclared identifiers are only used here. It looks then OK to add
a new 'ifdef' here, clearly showing nothing is done when MPTCP is not
enabled.

Fixes: 8785d1102d5f ("mptcp: Allow using IPPROTO_MPTCP")
Reviewed-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/socket.c
@@ -1424,9 +1424,11 @@ int __sock_create(struct net *net, int family, int type, int protocol,
 	if (err < 0)
 		goto out_module_put;
 
+#ifdef CONFIG_MPTCP
 	if (sysctl_mptcp_enabled && old_protocol == IPPROTO_MPTCP &&
 	    type == SOCK_STREAM && (family == AF_INET || family == AF_INET6))
 		mptcp_enable_sock(sock->sk);
+#endif
 
 	/*
 	 * Now to bump the refcnt of the [loadable] module that owns this