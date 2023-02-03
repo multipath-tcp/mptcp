multipath-tcp/mptcp

commit a55f953cd71bb3b9c0e4d0c2466c596d65f2b539

mptcp: Schedule TCP_TSQ_DEFERRED on subflow
It is the subflow that will encounter such kind of blockage, not the
meta-socket. Thus, we need to make sure that the flag is being set on
the subflow.

This was broken when merge v4.18.

Reported-by: Anh Vu Vu <vuanh.vu@ikt.uni-hannover.de>
Fixes: dd64d1c40b48 ("Merge tag 'v4.18' into mptcp_trunk")
Tested-by: Anh Vu Vu <vuanh.vu@ikt.uni-hannover.de>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp_output.c
@@ -825,8 +825,8 @@ static void tcp_tsq_handler(struct sock *sk)
 		if (mptcp(tp))
 			tcp_tsq_write(meta_sk);
 	} else {
-		if (!test_and_set_bit(TCP_TSQ_DEFERRED, &meta_sk->sk_tsq_flags))
-			sock_hold(meta_sk);
+		if (!test_and_set_bit(TCP_TSQ_DEFERRED, &sk->sk_tsq_flags))
+			sock_hold(sk);
 
 		if ((mptcp(tp)) && (sk->sk_state != TCP_CLOSE))
 			mptcp_tsq_flags(sk);

commit f65016752478baf8ded83a260bf22a3ccc7123a6

mptcp: Fix TSQ issue with mptcp
In the tasklet that handles TSQ, if the meta_sk is user owned, it will
try to get a ref on the sk, because it will be used later in the release
callback instead of directly here.  This makes the assumption that the
socket refcount is not zero here (a call to sk_hold is valid).  With the
MPTCP stack this assumption can be false. sk_hold triggers a WARN
because the refcount is already at zero (although the socket has not
actually been freed because sk->sk_wmem_alloc has explicitly been left
at a value of 1 by tcp_wfree). When this happens, both the subflow and
the meta are in TCP_CLOSE state and the subflow has been removed from
the meta list of subflows.

It looks like there was already an attempt to avoid this situation
because the line after is already looking at sk_state, but maybe we
are missing part of the story here as the modification has not been
detailed in commit dd64d1c40b48 ("Merge tag 'v4.18' into mptcp_trunk")

Stack trace observed:

  ------------[ cut here ]------------
  refcount_t: addition on 0; use-after-free.
  WARNING: CPU: 0 PID: 616 at lib/refcount.c:25 refcount_warn_saturate (lib/refcount.c:25 (discriminator 3))
  Kernel panic - not syncing: panic_on_warn set ...
  CPU: 0 PID: 616 Comm: mp_worker_0 Not tainted 5.4.227-mptcp+ #2
  Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS 0.5.1 01/01/2011
  Call Trace:
   <IRQ>
  dump_stack (lib/dump_stack.c:120)
  panic (kernel/panic.c:239)
  ? refcount_warn_saturate (lib/refcount.c:25 (discriminator 3))
  __warn.cold.13 (kernel/cpu.c:1247)
  ? refcount_warn_saturate (lib/refcount.c:25 (discriminator 3))
  report_bug (lib/bug.c:194)
  ? refcount_warn_saturate (lib/refcount.c:25 (discriminator 3))
  do_error_trap (arch/x86/kernel/traps.c:179 arch/x86/kernel/traps.c:174 arch/x86/kernel/traps.c:272)
  do_invalid_op (arch/x86/kernel/traps.c:291)
  ? refcount_warn_saturate (lib/refcount.c:25 (discriminator 3))
  invalid_op (arch/x86/entry/entry_64.S:1032)
  ? refcount_warn_saturate (lib/refcount.c:25 (discriminator 3))
  ? tcp_tsq_handler (include/linux/refcount.h:191 include/linux/refcount.h:228 include/net/sock.h:656 net/ipv4/tcp_output.c:836)
  ? tcp_tasklet_func (net/ipv4/tcp_output.c:872)
  ? tasklet_action_common.isra.16 (arch/x86/include/asm/bitops.h:75 include/asm-generic/bitops-instrumented.h:57 include/linux/interrupt.h:623 kernel/softirq.c:524)
  ? __do_softirq (include/linux/compiler.h:268 arch/x86/include/asm/atomic.h:31 include/asm-generic/atomic-instrumented.h:27 include/linux/jump_label.h:254 include/linux/jump_label.h:264 include/trace/events/irq.h:142 kernel/softirq.c:293)
  ? do_softirq_own_stack (arch/x86/entry/entry_64.S:1087)
   </IRQ>
  ? do_softirq.part.15 (kernel/softirq.c:337)
  ? __local_bh_enable_ip (kernel/softirq.c:329 kernel/softirq.c:189)
  ? sock_setsockopt (net/core/sock.c:754)
  ? __fget (fs/file.c:775)
  ? __sys_setsockopt (net/socket.c:2077)
  ? __x64_sys_setsockopt (net/socket.c:2098 net/socket.c:2095 net/socket.c:2095)
  ? do_syscall_64 (arch/x86/entry/common.c:290)
  ? entry_SYSCALL_64_after_hwframe (arch/x86/entry/entry_64.S:188)

Only visible with a kernel 5.4, not with 4.19.

The status is then checked for both conditions in the modified
if-statement.

Fixes: a55f953cd71b ("mptcp: Schedule TCP_TSQ_DEFERRED on subflow")

net/ipv4/tcp_output.c
@@ -825,11 +825,11 @@ static void tcp_tsq_handler(struct sock *sk)
 
 		if (mptcp(tp))
 			tcp_tsq_write(meta_sk);
-	} else {
+	} else if (sk->sk_state != TCP_CLOSE) {
 		if (!test_and_set_bit(TCP_TSQ_DEFERRED, &sk->sk_tsq_flags))
 			sock_hold(sk);
 
-		if ((mptcp(tp)) && (sk->sk_state != TCP_CLOSE))
+		if (mptcp(tp))
 			mptcp_tsq_flags(sk);
 	}
 