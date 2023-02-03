f13bc2ba7093fbb80b5302f680b0909f8b596d18
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-02-03 11:36:39 +0100

    mptcp: Stable Release v0.94.9
    Benjamin Hesmans <benjamin.hesmans@tessares.net> (2):
          [a09a768daabf] mptcp: remember backup flag sent in the join SYN+ACK
          [44df01d3a990] mptcp: copy DSS and flags on TCP fragment
    
    Christoph Paasch <cpaasch@apple.com> (19):
          [7d9611b73f45] mptcp: Avoid one more mptcp_sock_def_error_report
          [140297d03bf3] mptcp: Don't allow SOCK_ZEROCOPY
          [7ea6f5f566cf] mptcp: Handle fallback while meta is closed
          [d4562440992f] mptcp: Properly handle failures in mptcp_create_master_sk
          [1c9734bb1416] mptcp: Avoid tcp_fin warning
          [3603f76d10e8] mptcp: Don't warn when creating 32 subflows
          [a55f21da09a9] mptcp: Prevent mptcp_sock_def_error_report
          [5f21f89e836d] mptcp: Never disable MPTCP's static branch
          [e8b198b5d8b3] mptcp: Set tp->mptcp to NULL when free'ing
          [aea1af352e9f] mptcp: Do not race forced-closure with packet reception
          [c35e7c2b6705] mptcp: Avoid ever-increasing count of orphan sockets
          [e885b9938490] mptcp: Clear highest_sack when emptying rtx-queue
          [aa0aa06c5436] mptcp: First check for TIME_WAIT_SOCK in mptcp_v4/6_do_rcv
          [fbb5123e8b97] mptcp: Don't unset inet_num
          [74a258e46088] mptcp: Don't sock_put too early if we still access the meta
          [092cd78786df] mptcp: Fix input-path when mapping is covering several skbs but ends on a partial skb
          [edb51a48d650] mptcp: Only announce ADD_ADDR when fully established in MPTCPv1
          [1c6edd5edaa4] mptcp: Fix mptcp_check_lock warning
          [49eb5c159856] mptcp: Fix unlocking-check on wrong socket
    
    Gregory Detal <gregory.detal@tessares.net> (1):
          [4bb3c24c41bb] mptcp: ensure meta-level retrans counters are updated
    
    Matthieu Baerts <matthieu.baerts@tessares.net> (19):
          [08c571d0bb76] Revert "mptcp: Handle fallback while meta is closed"
          [a3b05029de9d] Merge tag 'v4.14.276' into mptcp_v0.94
          [2ed0817ac26e] Merge tag 'v4.14.279' into mptcp_v0.94
          [1c20abf8876d] mptcp: Build-Fix
          [978c884f96c1] Merge tag 'v4.14.282' into mptcp_v0.94
          [ff2e628cef6f] mptcp: add missing default for netlink PM
          [6ca26eca167a] Merge tag 'v4.14.285' into mptcp_v0.94
          [684e7388a3f5] mptcp: properly increase retrans-segs MIB
          [ad5d20ab5de5] Merge tag 'v4.14.290' into mptcp_v0.94
          [cac7f8fb1bfa] mptcp: un-expose skb_clone_fraglist()
          [37fdf52fa25d] mptcp: Fix a data-race around sysctl_tcp_retries1
          [d25af0e66cee] mptcp: Fix a data-race around sysctl_tcp_thin_linear_timeouts
          [3bad6f0e333d] mptcp: remove copy_skb_header()
          [81df2c06559c] Merge tag 'v4.14.291' into mptcp_v0.94
          [285eef396dcb] Merge tag 'v4.14.295' into mptcp_v0.94
          [51dfbfd28d3d] Merge tag 'v4.14.298' into mptcp_v0.94
          [f425af6af2ed] Merge tag 'v4.14.302' into mptcp_v0.94
          [73ff6b6989ee] mptcp: set icsk_af_ops to mptcp_v6 one if mptcp
          [20e3d8e68a81] Merge tag 'v4.14.304' into mptcp_v0.94
    
    Tim Froidcoeur <tim.froidcoeur@tessares.net> (7):
          [7ae3325fff11] mptcp: fix tcp-mem accounting
          [2c0da2bda8df] mptcp: fix accounting on rmem
          [872a566eb45c] mptcp: missing memory schedule on FIN
          [00d2cf8c5add] kernel: mptcp lock debug instrumentation
          [44095eee7ba4] mptcp: correct user owned check during FIN_ACK1
          [17343e8ba898] mptcp: correct check user owned socket
          [b6c7594d8b1c] mptcp: fix master unlock race in mptcp_disconnect
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -2996,7 +2996,7 @@ void __init mptcp_init(void)
 	if (mptcp_register_scheduler(&mptcp_sched_default))
 		goto register_sched_failed;
 
-	pr_info("MPTCP: Stable release v0.94.8");
+	pr_info("MPTCP: Stable release v0.94.9");
 
 	mptcp_init_failed = false;
 
