multipath-tcp/mptcp

commit 1f8a9bf7e17bf6b8c1df5cfb625f42c353aef4ff

mptcp: Handle fallback while meta is closed
There is yet another packet flow that can lead to a
mptcp_sock_def_error_report warning. It can happen that the meta-socket
times out and subflows are still active and then a subflow is triggered
to fallback:

+0      `sysctl -w net.ipv4.tcp_retries1=3`
+0      `sysctl -w net.ipv4.tcp_retries2=15`
+0      `sysctl -w net.ipv4.tcp_orphan_retries=2`
+0      `sysctl -w net.mptcp.mptcp_enabled=1`
+0      socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
+0      setsockopt(3, SOL_TCP, TCP_NODELAY, [1], 4) = 0
+0      setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
+0      bind(3, {sa_family = AF_INET, sin_port = htons(13000), sin_addr = inet_addr("192.168.0.1")}, ...) = 0
+0      listen(3, 1) = 0

+0      < S 0:0(0) win 32792 <mss 1460,sackOK,nop,nop,nop,wscale 7,mp_capable key_a> sock(3)
+0      > S. 0:0(0) ack 1 <mss 1460,nop,nop,sackOK,nop,wscale 7,mp_capable key_b> sock(3)
+0.1    < . 1:1(0) ack 1 win 257 <mp_capable key_a key_b> sock(3)
+0      accept(3, ..., ...) = 4

+0      < P. 1:11(10) ack 1 win 257 <dss dack4 dsn4> sock(4)
+0      > . 1:1(0) ack 11 <dss dack4=11> sock(4)

+0      read(4, ..., 100) = 10

+0      write(4, ..., 100) = 100
+0      > P. 1:101(100) ack 11 <dss dack4=11 dsn4> sock(4)
+0      write(4, ..., 100) = 100
+0      > P. 101:201(100) ack 11 <dss dack4=11 dsn4> sock(4)

+0.1~+0.3       > P. 101:201(100) ack 11 <dss dack4=11 dsn4> sock(4)
+0.1~+0.35      > P. 1:101(100) ack 11 <dss dack4=11 dsn4> sock(4)
+0.6~+0.8       > P. 1:101(100) ack 11 <dss dack4=11 dsn4> sock(4)
+0      close(4) = 0
+0.6~+0.8       > F. 201:201(0) ack 11 <dss dack4=11> sock(4)

/* Meta timedout above, so this packet causes a transparent fallback
 * while meta is already closed. This will trigger a RST
 */
+0.1    < . 11:11(0) ack 201 win 257 sock(4)
+0      > R. 202:202(0) ack 11 sock(4)

So, we need to make sure that subflows are force-closed when the meta is
already closed. Because, otherwise we end up timing out and get the

include/net/mptcp.h
@@ -1303,6 +1303,12 @@ static inline void mptcp_sub_close_passive(struct sock *sk)
 static inline void mptcp_fallback_close(struct mptcp_cb *mpcb,
 					struct sock *except)
 {
+	/* It can happen that the meta is already closed. In that case, don't
+	 * keep the subflow alive - close everything!
+	 */
+	if (mpcb->meta_sk->sk_state == TCP_CLOSE)
+		except = NULL;
+
 	mptcp_sub_force_close_all(mpcb, except);
 
 	if (mpcb->pm_ops->close_session)

net/mptcp/mptcp_input.c
@@ -1642,11 +1642,11 @@ bool mptcp_handle_ack_in_infinite(struct sock *sk, const struct sk_buff *skb,
 	if (!(flag & MPTCP_FLAG_DATA_ACKED))
 		return false;
 
-	pr_debug("%s %#x will fallback - pi %d, src %pI4:%u dst %pI4:%u rcv_nxt %u from %pS\n",
+	pr_debug("%s %#x will fallback - pi %d, src %pI4:%u dst %pI4:%u rcv_nxt %u\n",
 		 __func__, mpcb->mptcp_loc_token, tp->mptcp->path_index,
 		 &inet_sk(sk)->inet_saddr, ntohs(inet_sk(sk)->inet_sport),
 		 &inet_sk(sk)->inet_daddr, ntohs(inet_sk(sk)->inet_dport),
-		 tp->rcv_nxt, __builtin_return_address(0));
+		 tp->rcv_nxt);
 	if (!is_master_tp(tp)) {
 		MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_FBACKSUB);
 		return true;

commit 54a839412d432dd47486033b1ba9f2f43232a102

mptcp: fallback_close: fix NULL pointer deref
We witnessed repetitive crashes. Stack trace analysis pointed to a ...

  BUG: unable to handle kernel NULL pointer dereference at 00000000000007f0

... in the line: mpcb->pm_ops->close_session(mptcp_meta_sk(except))

The problem is that the 'except' variable can be set to NULL before, to
adopt the appropriated behaviour in mptcp_sub_force_close_all().

If 'except' is NULL, trying to get the meta (offset 0x7f0) causes the
bug.

If the meta is already closed, it means mpcb->pm_ops->close_session()
has already been called from mptcp_close(). Hence no need to call it
twice.

Fixes: 1f8a9bf7e17b ("mptcp: Handle fallback while meta is closed")
Change-Id: Ic3537e36100e6ddfccc58d02bad3a99e02bcff8d
Signed-off-by: Timothee Boucher-Lambert <timothee.boucher-lambert@tessares.net>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

include/net/mptcp.h
@@ -1316,7 +1316,7 @@ static inline bool mptcp_fallback_close(struct mptcp_cb *mpcb,
 
 	mptcp_sub_force_close_all(mpcb, except);
 
-	if (mpcb->pm_ops->close_session)
+	if (mpcb->pm_ops->close_session && except)
 		mpcb->pm_ops->close_session(mptcp_meta_sk(except));
 
 	return !except;