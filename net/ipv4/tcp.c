aosp-mirror/kernel_common

commit 5a9dc14df2ef3771d62e41a32c4a72417d845c07

tcp: factor out __tcp_close() helper
[ Upstream commit 77c3c95637526f1e4330cc9a4b2065f668c2c4fe ]

unlocked version of protocol level close, will be used by
MPTCP to allow decouple orphaning and subflow level close.

Signed-off-by: Paolo Abeni <pabeni@redhat.com>
Signed-off-by: Jakub Kicinski <kuba@kernel.org>
Signed-off-by: Sasha Levin <sashal@kernel.org>

net/ipv4/tcp.c
@@ -2351,13 +2351,12 @@ bool tcp_check_oom(struct sock *sk, int shift)
 	return too_many_orphans || out_of_socket_memory;
 }
 
-void tcp_close(struct sock *sk, long timeout)
+void __tcp_close(struct sock *sk, long timeout)
 {
 	struct sk_buff *skb;
 	int data_was_unread = 0;
 	int state;
 
-	lock_sock(sk);
 	sk->sk_shutdown = SHUTDOWN_MASK;
 
 	if (sk->sk_state == TCP_LISTEN) {
@@ -2521,6 +2520,12 @@ adjudge_to_death:
 out:
 	bh_unlock_sock(sk);
 	local_bh_enable();
+}
+
+void tcp_close(struct sock *sk, long timeout)
+{
+	lock_sock(sk);
+	__tcp_close(sk, timeout);
 	release_sock(sk);
 	sock_put(sk);
 }

include/net/tcp.h
@@ -391,6 +391,7 @@ void tcp_update_metrics(struct sock *sk);
 void tcp_init_metrics(struct sock *sk);
 void tcp_metrics_init(void);
 bool tcp_peer_is_proven(struct request_sock *req, struct dst_entry *dst);
+void __tcp_close(struct sock *sk, long timeout);
 void tcp_close(struct sock *sk, long timeout);
 void tcp_init_sock(struct sock *sk);
 void tcp_init_transfer(struct sock *sk, int bpf_op);

multipath-tcp/mptcp

commit a5d12bd950f574957b1cd36e953d5ec9687d71ed

mptcp: do not call mptcp_close() under lock
It is not supposed to be called like that: it will take the appropriated
logs when needed.

A recent refactoring on TCP side changed that.

Fixes: 5a9dc14df2ef ("tcp: factor out __tcp_close() helper")
Fixes: 5640e066b99e ("Merge tag 'v5.4.301' into mptcp_v0.96")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp.c
@@ -2453,17 +2453,6 @@ void __tcp_close(struct sock *sk, long timeout)
 	struct sk_buff *skb;
 	int state;
 
-	if (is_meta_sk(sk)) {
-		/* TODO: Currently forcing timeout to 0 because
-		 * sk_stream_wait_close will complain during lockdep because
-		 * of the mpcb_mutex (circular lock dependency through
-		 * inet_csk_listen_stop()).
-		 * We should find a way to get rid of the mpcb_mutex.
-		 */
-		mptcp_close(sk, 0);
-		return;
-	}
-
 	WRITE_ONCE(sk->sk_shutdown, SHUTDOWN_MASK);
 
 	if (sk->sk_state == TCP_LISTEN) {
@@ -2633,6 +2622,17 @@ out:
 
 void tcp_close(struct sock *sk, long timeout)
 {
+	if (is_meta_sk(sk)) {
+		/* TODO: Currently forcing timeout to 0 because
+		 * sk_stream_wait_close will complain during lockdep because
+		 * of the mpcb_mutex (circular lock dependency through
+		 * inet_csk_listen_stop()).
+		 * We should find a way to get rid of the mpcb_mutex.
+		 */
+		mptcp_close(sk, 0);
+		return;
+	}
+
 	lock_sock(sk);
 	__tcp_close(sk, timeout);
 	release_sock(sk);