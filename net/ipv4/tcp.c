multipath-tcp/mptcp

commit b0b64e79bff79aaeefba51d90d6f24b90d458d65

mptcp: Maintain a "Current Established" counter for MPTCP
Useful for monitoring, the same way as there is for TCP.

Signed-off-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp.c
@@ -2318,8 +2318,11 @@ void tcp_set_state(struct sock *sk, int state)
 
 	switch (state) {
 	case TCP_ESTABLISHED:
-		if (oldstate != TCP_ESTABLISHED)
+		if (oldstate != TCP_ESTABLISHED) {
 			TCP_INC_STATS(sock_net(sk), TCP_MIB_CURRESTAB);
+			if (is_meta_sk(sk))
+				MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+		}
 		break;
 
 	case TCP_CLOSE:
@@ -2332,8 +2335,11 @@ void tcp_set_state(struct sock *sk, int state)
 			inet_put_port(sk);
 		/* fall through */
 	default:
-		if (oldstate == TCP_ESTABLISHED)
+		if (oldstate == TCP_ESTABLISHED) {
 			TCP_DEC_STATS(sock_net(sk), TCP_MIB_CURRESTAB);
+			if (is_meta_sk(sk))
+				MPTCP_DEC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+		}
 	}
 
 	/* Change state AFTER socket is unhashed to avoid closed

include/net/mptcp.h
@@ -728,6 +728,7 @@ static inline struct sock *mptcp_to_sock(const struct mptcp_tcp_sock *mptcp)
 	mptcp_for_each_bit_set(~b, i)
 
 #define MPTCP_INC_STATS(net, field)	SNMP_INC_STATS((net)->mptcp.mptcp_statistics, field)
+#define MPTCP_DEC_STATS(net, field)	SNMP_DEC_STATS((net)->mptcp.mptcp_statistics, field)
 #define MPTCP_INC_STATS_BH(net, field)	__SNMP_INC_STATS((net)->mptcp.mptcp_statistics, field)
 
 enum
@@ -774,6 +775,7 @@ enum
 	MPTCP_MIB_REMADDRRX,		/* Received a REMOVE_ADDR */
 	MPTCP_MIB_REMADDRTX,		/* Sent a REMOVE_ADDR */
 	MPTCP_MIB_JOINALTERNATEPORT,	/* Established a subflow on a different destination port-number */
+	MPTCP_MIB_CURRESTAB,		/* Current established MPTCP connections */
 	__MPTCP_MIB_MAX
 };
 

net/mptcp/mptcp_ctrl.c
@@ -3038,6 +3038,7 @@ static const struct snmp_mib mptcp_snmp_list[] = {
 	SNMP_MIB_ITEM("RemAddrRx", MPTCP_MIB_REMADDRRX),
 	SNMP_MIB_ITEM("RemAddrTx", MPTCP_MIB_REMADDRTX),
 	SNMP_MIB_ITEM("MPJoinAlternatePort", MPTCP_MIB_JOINALTERNATEPORT),
+	SNMP_MIB_ITEM("MPCurrEstab", MPTCP_MIB_CURRESTAB),
 	SNMP_MIB_SENTINEL
 };
 

commit 4092ed640476f141b8e3fb8abc90ffe2240e23e9

mptcp: currestab: also support TCP_SYN_RECV
The "Current Established" counter for MPTCP introduced in commit
b0b64e79bff7 ("mptcp: Maintain a "Current Established" counter for
MPTCP") tied to the TCP one. Recently, the TCP counter is also
incremented for CLOSE-WAIT socketes.

It is then needed to align with TCP.

Fixes: b2ca635b82f2 ("tcp: count CLOSE-WAIT sockets for TCP_MIB_CURRESTAB").
Fixes: 5640e066b99e ("Merge tag 'v5.4.301' into mptcp_v0.96")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ipv4/tcp.c
@@ -2342,8 +2342,11 @@ void tcp_set_state(struct sock *sk, int state)
 		}
 		break;
 	case TCP_CLOSE_WAIT:
-		if (oldstate == TCP_SYN_RECV)
+		if (oldstate == TCP_SYN_RECV) {
 			TCP_INC_STATS(sock_net(sk), TCP_MIB_CURRESTAB);
+			if (is_meta_sk(sk))
+				MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+		}
 		break;
 
 	case TCP_CLOSE: