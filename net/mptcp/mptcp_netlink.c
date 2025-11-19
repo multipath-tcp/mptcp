multipath-tcp/mptcp

commit 5eb489952a2040fd577b208104e8f16096221875

mptcp: netlink: only force ACK when possible
Similar to commit 5a42b1bddbe4 ("mptcp: Only force ACK upon new_session
for MPTCPv0"), in MPTCPv1, we need to wait to become "fully established"
before we can signal additional addresses. So, let's avoid that call if
there is no need to do so.

Reviewed-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>
Acked-by: Christoph Paasch <cpaasch@apple.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_netlink.c
@@ -686,11 +686,15 @@ mptcp_nl_genl_announce(struct sk_buff *skb, struct genl_info *info)
 
 	mpcb->addr_signal = 1;
 
-	rcu_read_lock_bh();
-	subsk = mptcp_select_ack_sock(meta_sk);
-	if (subsk)
-		tcp_send_ack(subsk);
-	rcu_read_unlock_bh();
+	/* Only version 0 can send ADD_ADDR right at the beginning */
+	if (mpcb->mptcp_ver < MPTCP_VERSION_1 ||
+	    tcp_sk(meta_sk)->mptcp->fully_established) {
+		rcu_read_lock_bh();
+		subsk = mptcp_select_ack_sock(meta_sk);
+		if (subsk)
+			tcp_send_ack(subsk);
+		rcu_read_unlock_bh();
+	}
 
 exit:
 	release_sock(meta_sk);

commit 1f8f0325344b88978d01b5887b2816262f6ada6b

mptcp: netlink: fix kernel NULL pointer dereference
When using a userspace PM to announce addresses (e.g. on the server
side), and when MPTCPv1 is used, it is only possible to send the
ADD_ADDR once in 'fully established' mode (HMAC required).

The check in the userspace PM was looking at the meta_tp->mptcp, which
is not set before being in fully_established mode. Instead, checking the
same info via the master_sk to avoid a NULL pointer dereference.

Fixes: 5eb489952a20 ("mptcp: netlink: only force ACK when possible")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/mptcp_netlink.c
@@ -682,7 +682,7 @@ mptcp_nl_genl_announce(struct sk_buff *skb, struct genl_info *info)
 
 	/* Only version 0 can send ADD_ADDR right at the beginning */
 	if (mpcb->mptcp_ver < MPTCP_VERSION_1 ||
-	    tcp_sk(meta_sk)->mptcp->fully_established) {
+	    (mpcb->master_sk && tcp_sk(mpcb->master_sk)->mptcp->fully_established)) {
 		rcu_read_lock_bh();
 		subsk = mptcp_select_ack_sock(meta_sk);
 		if (subsk)
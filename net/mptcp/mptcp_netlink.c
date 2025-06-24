1f8f0325344b88978d01b5887b2816262f6ada6b
Matthieu Baerts <matthieu.baerts@tessares.net>
2025-06-24 18:36:39 +0200

    mptcp: netlink: fix kernel NULL pointer dereference
    When using a userspace PM to announce addresses (e.g. on the server
    side), and when MPTCPv1 is used, it is only possible to send the
    ADD_ADDR once in 'fully established' mode (HMAC required).
    
    The check in the userspace PM was looking at the meta_tp->mptcp, which
    is not set before being in fully_established mode. Instead, checking the
    same info via the master_sk to avoid a NULL pointer dereference.
    
    Fixes: 5eb489952a20 ("mptcp: netlink: only force ACK when possible")
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/mptcp_netlink.c
+++ net/mptcp/mptcp_netlink.c
@@ -682,7 +682,7 @@ mptcp_nl_genl_announce(struct sk_buff *skb, struct genl_info *info)
 
 	/* Only version 0 can send ADD_ADDR right at the beginning */
 	if (mpcb->mptcp_ver < MPTCP_VERSION_1 ||
-	    tcp_sk(meta_sk)->mptcp->fully_established) {
+	    (mpcb->master_sk && tcp_sk(mpcb->master_sk)->mptcp->fully_established)) {
 		rcu_read_lock_bh();
 		subsk = mptcp_select_ack_sock(meta_sk);
 		if (subsk)
